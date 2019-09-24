63a8d3565ad82e7489558a1c416f93533ff0dd9b
Pivovarov19 <55756274+Pivovarov19@users.noreply.github.com>
2019-09-24 19:28:03 +0300

    iteen.by lesson 3

---
+++ Main.java
@@ -0,0 +1,17 @@
+package by.it.gorevoy.jd01_01;
+
+import java.util.Scanner;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of day of the week :");
+        String Day = scanner.next();
+
+
+
+
+
+    }
+}
