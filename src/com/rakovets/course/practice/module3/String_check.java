c7e7c422074a4a77f1a59821ad6ae03b9251d83e
belyaninoleg <pobelan@gmail.com>
2019-09-29 16:27:44 +0300

    Task12 - done

---
+++ src/com/rakovets/course/practice/module3/String_check.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.practice.module3;
+
+import java.util.Scanner;
+
+public class String_check {
+    public static void main(String[] args) {}
+
+    Scanner scanner = new Scanner(System.in);
+
+}
--- src/com/rakovets/course/practice/module3/Task12.java
+++ src/com/rakovets/course/practice/module3/Task12.java
@@ -25,5 +25,89 @@ public class Task12 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+
+        int numberYear = year % 12;
+        String chineseZodiac = "";
+        String zodiacSign = "";
+                switch (numberYear) {
+                     case 0:
+                       chineseZodiac = "Monkey";
+                       break;
+                   case 1:
+                       chineseZodiac = "Rooster";
+                       break;
+                   case 2:
+                       chineseZodiac = "Dog";
+                       break;
+                   case 3:
+                       chineseZodiac = "Pig";
+                       break;
+                   case 4:
+                       chineseZodiac = "Rat";
+                       break;
+                   case 5:
+                       chineseZodiac = "Ox";
+                       break;
+                   case 6:
+                       chineseZodiac = "Tiger";
+                       break;
+                   case 7:
+                       chineseZodiac = "Rabbit";
+                       break;
+                   case 8:
+                       chineseZodiac = "Dragon";
+                       break;
+                   case 9:
+                       chineseZodiac = "Snake";
+                       break;
+                   case 10:
+                       chineseZodiac = "Horse";
+                       break;
+                   case 11:
+                       chineseZodiac = "Ram";
+                       break;
+                }
+        switch (month) {
+            case 1:
+                zodiacSign = "Sea-Goat";
+                break;
+            case 2:
+                zodiacSign = "Water-Bearer";
+                break;
+            case 3:
+                zodiacSign = "Fishes";
+                break;
+            case 4:
+                zodiacSign = "Ram";
+                break;
+            case 5:
+                zodiacSign = "Bull";
+                break;
+            case 6:
+                zodiacSign = "Twins";
+                break;
+            case 7:
+                zodiacSign = "Crab";
+                break;
+            case 8:
+                zodiacSign = "Lion";
+                break;
+            case 9:
+                zodiacSign = "Maiden";
+                break;
+            case 10:
+                zodiacSign = "Scales";
+                break;
+            case 11:
+                zodiacSign = "Scorpion";
+                break;
+            case 12:
+                zodiacSign = "Archer";
+                break;
+            default:
+                System.out.println("Number of Month is incorrect!!! Try again!");
+                break;
+        }
+             System.out.println("Zodiac Sign: " + zodiacSign + ". Chinese Zodiac: " + chineseZodiac);
     }
 }
