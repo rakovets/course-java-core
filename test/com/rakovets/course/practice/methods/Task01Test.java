3edbfe8d3028c82c86afd58bc8273420133e7876
Dmitry Rakovets <dmitryrakovets@gmail.com>
2019-10-02 16:37:38 +0300

    test: add tests for theme 'Methods'

---
+++ test/com/rakovets/course/practice/methods/Task01Test.java
@@ -0,0 +1,28 @@
+package com.rakovets.course.practice.methods;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Task01Test {
+    static Stream<Arguments> testProvider() {
+        return Stream.of(
+                Arguments.of(15, 5, 1, true),
+                Arguments.of(15, 5, 5, true),
+                Arguments.of(15, 5, 6, false),
+                Arguments.of(15, 5, 13, false)
+        );
+    }
+
+    @ParameterizedTest(name = "Size team: {0}. Top size: {1}. Position player bu damage: {2}")
+    @MethodSource("testProvider")
+    @DisplayName("Position player")
+    void test(int sizeTeam, int sizeTop, int positionPlayerByDamage, boolean expected) {
+        assertEquals(Task01.isTopPlayer(sizeTeam, sizeTop, positionPlayerByDamage), expected);
+    }
+}
