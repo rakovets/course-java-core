bfb1483b5b1d977b6f94cf428e891bad846e4dab
Dmitry Rakovets <dmitryrakovets@gmail.com>
2019-10-03 08:19:20 +0300

    task: fix Task01 from 'Methods'

--- src/com/rakovets/course/practice/methods/Task01.java
+++ src/com/rakovets/course/practice/methods/Task01.java
@@ -8,29 +8,27 @@ import com.rakovets.course.util.StandardInputTask;
  * от урона нанесенного в бою. Определить вошел ли игрок в ТОП игроков своей команды.
  *
  * @author Dmitry Rakovets
- * @version 1.0
+ * @version 1.1
  */
 public class Task01 extends StandardInputTask {
 	public static void main(String[] args) {
 		// Ввод данных осуществляется в Standard Input, для проверки различных вариантов входных параметров
-		int sizeTeam = INPUT_SCANNER.nextInt();
 		int sizeTop = INPUT_SCANNER.nextInt();
 		int positionPlayerByDamage = INPUT_SCANNER.nextInt();
 
-        isTopPlayer(sizeTeam, sizeTop, positionPlayerByDamage);
-    }
+		isTopPlayer(sizeTop, positionPlayerByDamage);
+	}
 
-    /**
-     * Определяет, вошел ли игрок в ТОП игроков своей команды.
-     * @param sizeTeam позиция игрока по убыванию урона в своей команде
-     * @param sizeTop позиция игрока по убыванию урона в своей команде
-     * @param positionPlayerByDamage позиция игрока по убыванию урона в своей команде
-     * @return true когда вошел, false если не вошел
-     */
-	static boolean isTopPlayer(int sizeTeam, int sizeTop, int positionPlayerByDamage) {
+	/**
+	 * Определяет, вошел ли игрок в ТОП игроков своей команды.
+	 * @param sizeTop позиция игрока по убыванию урона в своей команде
+	 * @param positionPlayerByDamage позиция игрока по убыванию урона в своей команде
+	 * @return true когда вошел, false если не вошел
+	 */
+	static boolean isTopPlayer(int sizeTop, int positionPlayerByDamage) {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используя параметры метода
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-        return false;
+		return false;
 	}
 }
