e12182addee96a1b822a7244b3a1f5f93a0c1d1e
belyaninoleg <pobelan@gmail.com>
2019-10-05 21:58:51 +0300

    task03 - done

--- src/com/rakovets/course/practice/module4/Task03.java
+++ src/com/rakovets/course/practice/module4/Task03.java
@@ -26,5 +26,9 @@ public class Task03 {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		for (int i = 1; i <= depositTerm; i++) {
+			depositAmount = ((depositAmount * annualDepositPercent / 100) + depositAmount);
+		}
+		System.out.print(Math.round(depositAmount * 100) / 100.0);
 	}
 }
