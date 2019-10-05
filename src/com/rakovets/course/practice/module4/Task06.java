5e0afcc45f90900c6ab9eef681d90a6ec57d6b66
belyaninoleg <pobelan@gmail.com>
2019-10-05 23:48:18 +0300

    task06 - done

--- src/com/rakovets/course/practice/module4/Task06.java
+++ src/com/rakovets/course/practice/module4/Task06.java
@@ -18,10 +18,30 @@ public class Task06 {
 		Scanner scanner = new Scanner(System.in);
 
 		// Код необходимый для тестирования, не изменять
-		long amount = (args.length!=1 ? scanner.nextLong():Long.parseLong(args[0]));
+		long amount = (args.length != 1 ? scanner.nextLong() : Long.parseLong(args[0]));
 
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		String answer = "";
+		long amount2;
+
+		if (amount >= 0) {
+			for (int i = 1; amount >= 1000 && i < 5; i++) {
+				amount2 = amount % 1000;
+				amount = (amount - amount2) / 1000;
+				answer = (" " + amount2 + answer);
+			}
+			System.out.print(amount + answer);
+		}
+		else {
+			amount = 0 - amount;
+			for (int i = 1; amount >= 1000 && i < 5; i++) {
+				amount2 = amount % 1000;
+				amount = (amount - amount2) / 1000;
+				answer = (" " + amount2 + answer);
+			}
+			System.out.print("-" + amount + answer);
+			}
 	}
 }
