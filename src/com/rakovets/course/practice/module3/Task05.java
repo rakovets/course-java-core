1287768766749b11bb2bc41ed2e7f65337a83b6c
George Lashko <george.lashko@gmail.com>
2019-10-07 16:53:18 +0300

    Solution: Module3: Task05

--- src/com/rakovets/course/practice/module3/Task05.java
+++ src/com/rakovets/course/practice/module3/Task05.java
@@ -23,5 +23,42 @@ public class Task05 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        if(monthNumber==1){
+            System.out.println("JANUARY");
+        }
+        if(monthNumber==2){
+            System.out.println("FEBRUARY");
+        }
+        if(monthNumber==3){
+            System.out.println("MARCH");
+        }
+        if(monthNumber==4){
+            System.out.println("APRIL");
+        }
+        if(monthNumber==5){
+            System.out.println("MAY");
+        }
+        if(monthNumber==6){
+            System.out.println("JUNE");
+        }
+        if(monthNumber==7){
+            System.out.println("JULY");
+        }
+        if(monthNumber==8){
+            System.out.println("AUGUST");
+        }
+        if(monthNumber==9){
+            System.out.println("SEPTEMBER");
+        }
+        if(monthNumber==10){
+            System.out.println("OCTOBER");
+        }
+        if(monthNumber==11){
+            System.out.println("NOVEMBER");
+        }
+        if(monthNumber==12){
+            System.out.println("DECEMBER");
+        }
+
     }
 }
