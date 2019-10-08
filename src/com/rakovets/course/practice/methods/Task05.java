b919fe7c445f6eb68202f3b77451f6b9cf485d41
Mark <marksvirsk@gmail.com>
2019-10-08 23:11:18 +0300

    Solution: Methods: Task01 - Taks05

--- src/com/rakovets/course/practice/methods/Task05.java
+++ src/com/rakovets/course/practice/methods/Task05.java
@@ -11,19 +11,47 @@ import com.rakovets.course.util.StandardInputTask;
  * @version 1.0
  */
 public class Task05 extends StandardInputTask {
-	public static void main(String[] args) {
-		// Ввод данных осуществляется в Standard Input, для проверки различных вариантов входных параметров
-		byte monthNumber = INPUT_SCANNER.nextByte();
+    public static void main(String[] args) {
+        // Ввод данных осуществляется в Standard Input, для проверки различных вариантов входных параметров
+        byte monthNumber = INPUT_SCANNER.nextByte();
 
-		getNameMonth(monthNumber);
-	}
+        getNameMonth(monthNumber);
+    }
 
-	/**
-	 * Возвращает название месяца, в зависимости от его порядкового номера
-	 * @param monthNumber - номер месяца
-	 * @return месяц на английском языке (UPPER CASE)
-	 */
-	static String getNameMonth(byte monthNumber) {
-		return null;
-	}
+    /**
+     * Возвращает название месяца, в зависимости от его порядкового номера
+     *
+     * @param monthNumber - номер месяца
+     * @return месяц на английском языке (UPPER CASE)
+     */
+    static String getNameMonth(byte monthNumber) {
+        if (monthNumber == 1) {
+            return "JANUARY";
+        } else if (monthNumber == 2) {
+            return "FEBRUARY";
+        } else if (monthNumber == 3) {
+            return "MARCH";
+        } else if (monthNumber == 4) {
+            return "APRIL";
+        } else if (monthNumber == 5) {
+            return "MAY";
+        } else if (monthNumber == 6) {
+            return "JUNE";
+        } else if (monthNumber == 7) {
+            return "JULY";
+        } else if (monthNumber == 8) {
+            return "AUGUST";
+        } else if (monthNumber == 9) {
+            return "SEPTEMBER";
+        } else if (monthNumber == 10) {
+            return "OCTOBER";
+        } else if (monthNumber == 11) {
+            return "NOVEMBER";
+        } else if (monthNumber == 12) {
+            return "DECEMBER";
+        } else {
+            return null;
+        }
+    }
+
 }
