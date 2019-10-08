55f3bd5f4cf2206dfb76f48ce6bc4c3d226e173c
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-10-08 18:06:45 +0300

    task: add Task04 in Module 4

--- src/com/rakovets/course/practice/module4/Task04.java
+++ src/com/rakovets/course/practice/module4/Task04.java
@@ -27,5 +27,11 @@ public class Task04 {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		double result = healthPoints;
+		for (int i = 0; i < hoursAfterRespawn; i++) {
+			result += result * regenerationPercentFromCurrentHealth / 100;
+		}
+
+		System.out.println((int) result);
 	}
 }
