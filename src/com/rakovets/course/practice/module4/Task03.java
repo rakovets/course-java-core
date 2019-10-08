45ebc917eefacf18b20e9dc90a9764eed13a6872
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-10-08 18:06:34 +0300

    task: add Task03 in Module 4

--- src/com/rakovets/course/practice/module4/Task03.java
+++ src/com/rakovets/course/practice/module4/Task03.java
@@ -26,5 +26,12 @@ public class Task03 {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
+		double profit = 0;
+		for (int i = 0; i < depositTerm; i++) {
+			profit += depositAmount * annualDepositPercent / 100;
+			depositAmount += depositAmount * annualDepositPercent / 100;
+		}
+
+		System.out.println(Math.round(depositAmount * 100.0) / 100.0);
 	}
 }
