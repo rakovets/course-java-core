3fbf990c131a9009cc16b8d34990a9b5a8c01dd9
belyaninoleg <pobelan@gmail.com>
2019-10-09 21:16:44 +0300

    module4/Task09 - done

--- src/com/rakovets/course/practice/module4/Task09.java
+++ src/com/rakovets/course/practice/module4/Task09.java
@@ -30,6 +30,20 @@ public class Task09 {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-//		I don't understand condition'
+		double coast;
+		double coastAll;
+		int numberItems;
+		double differentialSellSize;
+		coast = startPriceAllItems / startNumberItems; // Цена за 1 штуку
+		for(int i = 0; i < (sizeTotalPrice - 1); i++) {
+			numberItems = startNumberItems + differentialNumberItems * i;
+			differentialSellSize = differentialSell * i;
+			coastAll = numberItems * coast * (100.0 - differentialSellSize)  / 100;
+			System.out.print(numberItems + " - " + coastAll + " with sell " + differentialSellSize + "%\n");
+		}
+		numberItems = startNumberItems + differentialNumberItems * (sizeTotalPrice - 1);
+		differentialSellSize = differentialSell * (sizeTotalPrice - 1);
+		double coastAllLast = numberItems * coast * (100.0 - differentialSellSize) / 100.0;
+		System.out.print(numberItems + " - " + coastAllLast + " with sell " + differentialSellSize + "%");
 	}
 }
