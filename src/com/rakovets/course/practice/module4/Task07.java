68246acd7b5177570266a402e128d6251769f468
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-10-13 01:24:21 +0300

    task: add Task07 in Module 4

--- src/com/rakovets/course/practice/module4/Task07.java
+++ src/com/rakovets/course/practice/module4/Task07.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 /**
  * Разработать программу для фитнес браслета:
  * Программа позволяет расчитать пробег, который необходимо совершить спортсмену для подготовки к марафону. Известно:
- * @param startDistance дистанция которую пробегает спортсмен до начала тренировки
+ *
+ * @param startDistance  дистанция которую пробегает спортсмен до начала тренировки
  * @param finishDistance дистанция которую желает пробежать спортсмен после окончания тренировок
  * @param dailyProgress ежедневный прогресс % по одношению к предыдущему забегу
  * @return пробег (точность 10 метром)
@@ -14,17 +15,27 @@ import java.util.Scanner;
  * @version 1.0
  */
 public class Task07 {
-	public static void main(String[] args) {
-		// Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
+        Scanner scanner = new Scanner(System.in);
 
-		// Код необходимый для тестирования, не изменять
-		int startDistance = (args.length!=3) ? scanner.nextInt():Integer.parseInt(args[0]);
-		int finishDistance = (args.length!=3) ? scanner.nextInt():Integer.parseInt(args[1]);
-		double dailyProgress = (args.length!=3) ? scanner.nextDouble():Double.parseDouble(args[2]);
+        // Код необходимый для тестирования, не изменять
+        int startDistance = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[0]);
+        int finishDistance = (args.length != 3) ? scanner.nextInt() : Integer.parseInt(args[1]);
+        double dailyProgress = (args.length != 3) ? scanner.nextDouble() : Double.parseDouble(args[2]);
 
-		//TODO
-		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
-		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-	}
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        double distanceAfterProgress = startDistance;
+        double distance = 0;
+        while (distanceAfterProgress < finishDistance) {
+        	if (startDistance == 0 || dailyProgress == 0) {
+				break;
+			}
+            distance += distanceAfterProgress + distanceAfterProgress * dailyProgress / 100;
+            distanceAfterProgress += distanceAfterProgress * dailyProgress / 100;
+        }
+		System.out.println(distance - startDistance);
+    }
 }
