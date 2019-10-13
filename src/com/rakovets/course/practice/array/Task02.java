9cf56f14371c2c1074451012bd9c6d886df675e2
Mark <marksvirsk@gmail.com>
2019-10-13 20:11:29 +0300

    solution: array: Task01 - Task03

--- src/com/rakovets/course/practice/array/Task02.java
+++ src/com/rakovets/course/practice/array/Task02.java
@@ -12,61 +12,98 @@ import java.util.Arrays;
  * @version 1.0
  */
 public class Task02 extends StandardInputTask {
-	public static void main(String[] args) {
-		// Ввод данных осуществляется в Standard Input, для проверки различных вариантов входных параметров
-		int countDisciplines = INPUT_SCANNER.nextInt();
-		int countSemesters = INPUT_SCANNER.nextInt();
-		int[][] marks = nextArray(countDisciplines, countSemesters);
+    public static void main(String[] args) {
+        // Ввод данных осуществляется в Standard Input, для проверки различных вариантов входных параметров
+        int countDisciplines = INPUT_SCANNER.nextInt();
+        int countSemesters = INPUT_SCANNER.nextInt();
+        int[][] marks = nextArray(countDisciplines, countSemesters);
 
-		// Вызов методов
-		getAverageMark(marks);
-		getMinMark(marks);
-		getMaxMark(marks);
-	}
+        // Вызов методов
+        getAverageMark(marks);
+        getMinMark(marks);
+        getMaxMark(marks);
+    }
 
-	/**
-	 * Возвращает средне арифметическую отметку за весь период обучения с округлением до 2 знаков.
-	 * @param marks отметки
-	 * @return средняя арифметическая отметка
-	 */
-	static double getAverageMark(int[][] marks) {
-		//TODO
-		// Код, решающий задачу пишем ниже, при этом используя параметры метода
-		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return 0.0;
-	}
+    /**
+     * Возвращает средне арифметическую отметку за весь период обучения с округлением до 2 знаков.
+     *
+     * @param marks отметки
+     * @return средняя арифметическая отметка
+     */
+    static double getAverageMark(int[][] marks) {
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используя параметры метода
+        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        double averageMark = 0;
 
-	/**
-	 * Возвращает минимальную отметку за весь период обучения.
-	 * @param marks отметки
-	 * @return минимальная отметка
-	 */
-	static int getMinMark(int[][] marks) {
-		//TODO
-		// Код, решающий задачу пишем ниже, при этом используя параметры метода
-		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return 0;
-	}
+        for (int i = 0; i < marks.length; i++) {
+            for (int j = 0; j < marks[i].length; j++) {
+                averageMark += marks[i][j];
+            }
+        }
+        averageMark /= marks.length * 3;
+        averageMark = Math.round(averageMark * 100) / 100.0;
+        return averageMark;
+    }
 
-	/**
-	 * Возвращает максимальну отметку за весь период обучения.
-	 * @param marks отметки
-	 * @return максимальная отметка
-	 */
-	static int getMaxMark(int[][] marks) {
-		//TODO
-		// Код, решающий задачу пишем ниже, при этом используя параметры метода
-		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return 0;
-	}
 
-	private static int[][] nextArray(int countDisciplines, int countSemesters) {
-		int[][] marks = new int[countDisciplines][countSemesters];
-		for (int i = 0; i < countDisciplines; i++) {
-			for (int j = 0; j < countSemesters; j++) {
-				marks[i][j] = INPUT_SCANNER.nextInt();
-			}
-		}
-		return marks;
-	}
+    /**
+     * Возвращает минимальную отметку за весь период обучения.
+     *
+     * @param marks отметки
+     * @return минимальная отметка
+     */
+    static int getMinMark(int[][] marks) {
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используя параметры метода
+        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        int min = marks[0][0];
+        for (int i = 0; i < marks.length; i++) {
+            for (int k = 0; k < marks[i].length; k++) {
+                if (marks[i][k] < min) {
+                    min = marks[i][k];
+                }
+            }
+        }
+        return min;
+    }
+
+    /**
+     * Возвращает максимальну отметку за весь период обучения.
+     *
+     * @param marks отметки
+     * @return максимальная отметка
+     */
+    static int getMaxMark(int[][] marks) {
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используя параметры метода
+        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        int max = marks[0][0];
+        int[] num = new int[marks.length];
+        for (int i = 0; i < marks.length; i++) {
+            for (int j = 0; j < marks[i].length; j++) {
+                if (marks[i][j] > max) {
+                    max = marks[i][j];
+                }
+            }
+            num[i] = max;
+        }
+        int max1 = num[0];
+        for (int mark : num) {
+            if (mark > max1) {
+                max1 = mark;
+            }
+        }
+        return max;
+    }
+
+    private static int[][] nextArray(int countDisciplines, int countSemesters) {
+        int[][] marks = new int[countDisciplines][countSemesters];
+        for (int i = 0; i < countDisciplines; i++) {
+            for (int j = 0; j < countSemesters; j++) {
+                marks[i][j] = INPUT_SCANNER.nextInt();
+            }
+        }
+        return marks;
+    }
 }
--- src/com/rakovets/course/practice/array/Task03.java
+++ src/com/rakovets/course/practice/array/Task03.java
@@ -12,61 +12,93 @@ import java.util.Arrays;
  * @version 1.0
  */
 public class Task03 extends StandardInputTask {
-	public static void main(String[] args) {
-		// Ввод данных осуществляется в Standard Input, для проверки различных вариантов входных параметров
-		int countDisciplines = INPUT_SCANNER.nextInt();
-		int countSemesters = INPUT_SCANNER.nextInt();
-		int[][] marks = nextArray(countDisciplines, countSemesters);
+    public static void main(String[] args) {
+        // Ввод данных осуществляется в Standard Input, для проверки различных вариантов входных параметров
+        int countDisciplines = INPUT_SCANNER.nextInt();
+        int countSemesters = INPUT_SCANNER.nextInt();
+        int[][] marks = nextArray(countDisciplines, countSemesters);
 
-		// Вызов методов
-		getAverageMark(marks);
-		getMinMark(marks);
-		getMaxMark(marks);
-	}
+        // Вызов методов
+        getAverageMark(marks);
+        getMinMark(marks);
+        getMaxMark(marks);
+    }
 
-	/**
-	 * Возвращает средне арифметическую отметку по предметам за весь период обучения с округлением до 2 знаков.
-	 * @param marks отметки
-	 * @return средняя арифметическая отметка
-	 */
-	static double[] getAverageMark(int[][] marks) {
-		//TODO
-		// Код, решающий задачу пишем ниже, при этом используя параметры метода
-		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return null;
-	}
+    /**
+     * Возвращает средне арифметическую отметку по предметам за весь период обучения с округлением до 2 знаков.
+     *
+     * @param marks отметки
+     * @return средняя арифметическая отметка
+     */
+    static double[] getAverageMark(int[][] marks) {
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используя параметры метода
+        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        double[] averageMarkForSubjects = new double[marks.length];
+        for (int i = 0; i < marks.length; i++) {
+            double averegeMarks = 0;
+            for (int j = 0; j < marks[i].length; j++) {
+                averegeMarks += marks[i][j];
+            }
+            averegeMarks = Math.round(averegeMarks / marks[i].length * 100) / 100.0;
+            averageMarkForSubjects[i] = averegeMarks;
+        }
+        return averageMarkForSubjects;
+    }
 
-	/**
-	 * Возвращает минимальную отметку по предметам за весь период обучения.
-	 * @param marks отметки
-	 * @return минимальная отметка
-	 */
-	static int[] getMinMark(int[][] marks) {
-		//TODO
-		// Код, решающий задачу пишем ниже, при этом используя параметры метода
-		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return null;
-	}
 
-	/**
-	 * Возвращает максимальну отметку по предметам за весь период обучения.
-	 * @param marks отметки
-	 * @return максимальная отметка
-	 */
-	static int[] getMaxMark(int[][] marks) {
-		//TODO
-		// Код, решающий задачу пишем ниже, при этом используя параметры метода
-		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return null;
-	}
+    /**
+     * Возвращает минимальную отметку по предметам за весь период обучения.
+     *
+     * @param marks отметки
+     * @return минимальная отметка
+     */
+    static int[] getMinMark(int[][] marks) {
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используя параметры метода
+        int[] minMarkForSubjects = new int[marks.length];
+        for (int i = 0; i < marks.length; i++) {
+            int MinMark = marks[i][0];
+            for (int j = 0; j < marks[i].length; j++) {
+                if (marks[i][j] < MinMark) {
+                    MinMark = marks[i][j];
+                }
+            }
+            minMarkForSubjects[i] = MinMark;
+        }
+        return minMarkForSubjects;
+    }
 
-	private static int[][] nextArray(int countDisciplines, int countSemesters) {
-		int[][] marks = new int[countDisciplines][countSemesters];
-		for (int i = 0; i < countDisciplines; i++) {
-			for (int j = 0; j < countSemesters; j++) {
-				marks[i][j] = INPUT_SCANNER.nextInt();
-			}
-		}
-		return marks;
-	}
+    /**
+     * Возвращает максимальну отметку по предметам за весь период обучения.
+     *
+     * @param marks отметки
+     * @return максимальная отметка
+     */
+    static int[] getMaxMark(int[][] marks) {
+        //TODO
+        // Код, решающий задачу пишем ниже, при этом используя параметры метода
+        // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        int[] maxMarkForSubjects = new int[marks.length];
+        for (int i = 0; i < marks.length; i++) {
+            int MaxMark = marks[i][0];
+            for (int j = 0; j < marks[i].length; j++) {
+                if (marks[i][j] > MaxMark) {
+                    MaxMark = marks[i][j];
+                }
+            }
+            maxMarkForSubjects[i] = MaxMark;
+        }
+        return maxMarkForSubjects;
+    }
+
+    private static int[][] nextArray(int countDisciplines, int countSemesters) {
+        int[][] marks = new int[countDisciplines][countSemesters];
+        for (int i = 0; i < countDisciplines; i++) {
+            for (int j = 0; j < countSemesters; j++) {
+                marks[i][j] = INPUT_SCANNER.nextInt();
+            }
+        }
+        return marks;
+    }
 }
