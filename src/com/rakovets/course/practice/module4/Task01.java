ab16bcc443d205c490babb6a6ce4cbe2aaa96b1f
Litvinchuk0104 <55504744+Litvinchuk0104@users.noreply.github.com>
2019-10-13 20:44:06 +0300

    itstep.by/j1019/7.10.2019/module4/Done tasks 1-6

---
+++ src/com/rakovets/course/practice/module4/Task01.java
@@ -0,0 +1,34 @@
+package com.rakovets.course.practice.module4;
+
+import java.util.Scanner;
+
+/**
+ * Разработать программу для табличного процессора:
+ * Программа генерирует номера строк при создании таблицы. Известно:
+ * @param rows количество строк в таблице
+ * @return номера строк, где каждый номер на новой строке
+ *
+ * @author Dmitry Rakovets
+ * @version 1.0
+ */
+public class Task01 {
+	public static void main(String[] args) {
+		// Ввод данных осуществляется в Console, для проверки различных вариантов входных параметров
+		Scanner scanner = new Scanner(System.in);
+
+		// Код необходимый для тестирования, не изменять
+		int rows = (args.length != 1) ? scanner.nextInt() : Integer.parseInt(args[0]);
+
+		//TODO
+		// Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
+		// Для проверки решения необходимо запустить @Test для данного class (в директории tes
+
+		for (int i = 1; i <= rows; i++) {
+			System.out.print(i);
+			if (i < rows) {
+				System.out.print("\n");
+
+			}
+		}
+	}
+}
