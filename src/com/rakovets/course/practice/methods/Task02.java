2641d8bf68067bb0b5d378d60f5689e21f50f81d
George Lashko <george.lashko@gmail.com>
2019-10-14 14:37:14 +0300

    Solution: Methods: Task02

--- src/com/rakovets/course/practice/methods/Task02.java
+++ src/com/rakovets/course/practice/methods/Task02.java
@@ -26,6 +26,20 @@ public class Task02 extends StandardInputTask {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
-        return null;
+        String greeting;
+        if (hour >= 0 && hour <= 5) {
+            greeting = "Good night";
+        } else if (hour >= 6 && hour <= 11) {
+            greeting = "Good morning";
+        } else if (hour >= 12 && hour <= 17) {
+            greeting = "Good day";
+        } else if (hour >= 18 && hour <= 23) {
+            greeting = "Good evening";
+        } else {
+            greeting = null;
+        }
+
+        return greeting;
+
     }
 }
