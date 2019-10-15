957844a9372fef0080f722486da933a7b569ad63
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-10-15 18:01:26 +0300

    task: done Task05 in methods

--- src/com/rakovets/course/practice/methods/Task05.java
+++ src/com/rakovets/course/practice/methods/Task05.java
@@ -5,25 +5,65 @@ import com.rakovets.course.util.StandardInputTask;
 /**
  * Разработать программу для игрового движка:
  * Игрок при регистрации вводит свою дату рождения и она хранится в формате dd/MM/yyyy. В профиле игрока месяц
- * еобходимо выводить не в числовом формате, а в текстовом. Определить название месяца в зависимости от номера.
+ * необходимо выводить не в числовом формате, а в текстовом. Определить название месяца в зависимости от номера.
  *
  * @author Dmitry Rakovets
  * @version 1.0
  */
 public class Task05 extends StandardInputTask {
-	public static void main(String[] args) {
-		// Ввод данных осуществляется в Standard Input, для проверки различных вариантов входных параметров
-		byte monthNumber = INPUT_SCANNER.nextByte();
+    public static void main(String[] args) {
+        // Ввод данных осуществляется в Standard Input, для проверки различных вариантов входных параметров
+        byte monthNumber = INPUT_SCANNER.nextByte();
 
-		getNameMonth(monthNumber);
-	}
+        getNameMonth(monthNumber);
+    }
 
-	/**
-	 * Возвращает название месяца, в зависимости от его порядкового номера
-	 * @param monthNumber - номер месяца
-	 * @return месяц на английском языке (UPPER CASE)
-	 */
-	static String getNameMonth(byte monthNumber) {
-		return null;
-	}
+    /**
+     * Возвращает название месяца, в зависимости от его порядкового номера
+     *
+     * @param monthNumber - номер месяца
+     * @return месяц на английском языке (UPPER CASE)
+     */
+    static String getNameMonth(byte monthNumber) {
+        String monthName = null;
+        switch (monthNumber) {
+            case 1:
+                monthName = "JANUARY";
+                break;
+            case 2:
+                monthName = "FEBRUARY";
+                break;
+            case 3:
+                monthName = "MARCH";
+                break;
+            case 4:
+                monthName = "APRIL";
+                break;
+            case 5:
+                monthName = "MAY";
+                break;
+            case 6:
+                monthName = "JUNE";
+                break;
+            case 7:
+                monthName = "JULY";
+                break;
+            case 8:
+                monthName = "AUGUST";
+                break;
+            case 9:
+                monthName = "SEPTEMBER";
+                break;
+            case 10:
+                monthName = "OCTOBER";
+                break;
+            case 11:
+                monthName = "NOVEMBER";
+                break;
+            case 12:
+                monthName = "DECEMBER";
+                break;
+        }
+        return monthName;
+    }
 }
