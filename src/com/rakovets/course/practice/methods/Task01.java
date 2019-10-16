60ebbe65a0838fd1fc03479b50bcda6ab161ade7
belyaninoleg <pobelan@gmail.com>
2019-10-16 13:31:18 +0300

    methods / Task01 - done

--- src/com/rakovets/course/practice/methods/Task01.java
+++ src/com/rakovets/course/practice/methods/Task01.java
@@ -29,6 +29,7 @@ public class Task01 extends StandardInputTask {
 		//TODO
 		// Код, решающий задачу пишем ниже, при этом используя параметры метода
 		// Для проверки решения необходимо запустить @Test для данного class (в директории test)
-		return false;
+		boolean result = (positionPlayerByDamage <= sizeTop);
+		return result;
 	}
 }
