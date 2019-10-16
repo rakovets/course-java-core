3eed09df715552904f9d86d2db641f8b35232cf3
belyaninoleg <pobelan@gmail.com>
2019-10-16 14:03:23 +0300

    methods / Task05 - done

--- src/com/rakovets/course/practice/methods/Task05.java
+++ src/com/rakovets/course/practice/methods/Task05.java
@@ -24,6 +24,48 @@ public class Task05 extends StandardInputTask {
 	 * @return месяц на английском языке (UPPER CASE)
 	 */
 	static String getNameMonth(byte monthNumber) {
-		return null;
+		String month = "";
+		switch (monthNumber) {
+			case 1:
+				month = "JANUARY";
+				break;
+			case 2:
+				month = "FEBRUARY";
+				break;
+			case 3:
+				month = "MARCH";
+				break;
+			case 4:
+				month = "APRIL";
+				break;
+			case 5:
+				month = "MAY";
+				break;
+			case 6:
+				month = "JUNE";
+				break;
+			case 7:
+				month = "JULY";
+				break;
+			case 8:
+				month = "AUGUST";
+				break;
+			case 9:
+				month = "SEPTEMBER";
+				break;
+			case 10:
+				month = "OCTOBER";
+				break;
+			case 11:
+				month = "NOVEMBER";
+				break;
+			case 12:
+				month = "DECEMBER";
+				break;
+			default:
+				month = null;
+				break;
+		}
+		return month;
 	}
 }
