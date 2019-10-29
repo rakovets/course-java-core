2b109c853a68b4bca1b010100e2d5b5e80a76a64
Andrew Pivovarov <pib2703@gmail.com>
2019-10-29 18:56:48 +0300

    done: Task04

--- src/com/rakovets/course/practice/methods/Task04.java
+++ src/com/rakovets/course/practice/methods/Task04.java
@@ -23,6 +23,30 @@ public class Task04 extends StandardInputTask {
 	 * @return день недели на английском языке (UPPER CASE)
 	 */
 	static String getNameWeekday(byte weekdayNumber) {
-		return null;
+		String day = null;
+		switch(weekdayNumber) {
+			case 1:
+				day = "MONDAY";
+				break;
+			case 2:
+				day = "TUESDAY";
+				break;
+			case 3:
+				day = "WEDNESDAY";
+				break;
+			case 4:
+				day = "THURSDAY";
+				break;
+			case 5:
+				day = "FRIDAY";
+				break;
+			case 6:
+				day = "SATURDAY";
+				break;
+			case 7:
+				day = "SUNDAY";
+				break;
+		}
+		return day;
 	}
 }
