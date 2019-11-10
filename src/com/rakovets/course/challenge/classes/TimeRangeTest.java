86ad7be6b45680dfe3c24800f9210cd2b38b8a31
dzenisshaban <Shabantrade@mail.ru>
2019-11-10 18:13:40 +0300

    iteen.by/j1019/classes/03.11.2019/Done Task01,03,04, Task02 dont all

--- src/com/rakovets/course/challenge/classes/TimeRangeTest.java
+++ src/com/rakovets/course/challenge/classes/TimeRangeTest.java
@@ -1,4 +1,13 @@
 package com.rakovets.course.challenge.classes;
 
 public class TimeRangeTest {
+    public static void main(String[] args) {
+        TimeRange FirstTime = new TimeRange(7856);
+        System.out.println("Hour:" + FirstTime.getHour() + " Min:" + FirstTime.getMin() + " Sec:" + FirstTime.getSec());
+
+        TimeRange SecondTime = new TimeRange(5, 26, 9);
+        System.out.println("Hour:" + SecondTime.getHour() + " Min:" + SecondTime.getMin() + " Sec:" + SecondTime.getSec());
+
+        System.out.println(SecondTime.convertTimeRangeToSeconds());
+    }
 }
