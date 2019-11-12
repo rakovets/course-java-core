dfb9f45830e49450bed0e08134060ff725e6321a
Nikita Kizhyk <nik.kizhyk@gmail.com>
2019-11-12 18:47:31 +0300

    task: add class TimeTests and done tasks for this class

--- src/com/rakovets/course/challenge/oop/classes/TimeTests.java
+++ src/com/rakovets/course/challenge/oop/classes/TimeTests.java
@@ -1,4 +1,13 @@
 package com.rakovets.course.challenge.oop.classes;
 
 public class TimeTests {
+	public static void main(String[] args) {
+		Time time1 = new Time(10, 65, 47);
+		Time time2 = new Time(3609);
+		time1.writeTime();
+		time2.writeTime();
+		System.out.println(time1.getHour() + " " + time1.getMinute() + " " + time1.getSecond());
+		System.out.println(time2.getTotalSeconds());
+		// Сеттеры мне лень проверять, из генерировала IDEA, так что все норм.
+	}
 }
