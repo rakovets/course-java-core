096324dfff7288f252b80aa26ea465c0edf57f37
Dmitry Rakovets <dmitryrakovets@gmail.com>
2019-11-30 11:44:41 +0300

    practice: theme 'Intro to Collections'

---
+++ src/com/rakovets/java/practice/collections/Student.java
@@ -0,0 +1,13 @@
+package com.rakovets.java.practice.collections;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
