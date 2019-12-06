1c79323db1fd97aced1ba8fe8a755abf18faa119
Nikitatrip228 <nikitaatesh@icloud.com>
2019-12-06 22:11:53 +0300

    version 02

--- src/com/rakovets/java/task/collections/list/Garage.java
+++ src/com/rakovets/java/task/collections/list/Garage.java
@@ -10,11 +10,27 @@ public class Garage {
 
     public void park(Car car) {
         if (map.containsKey(car)) {
-            int cuurentCount = map.get(car);
-            map.put(car, cuurentCount + 1);
+            int currentCount = map.get(car);
+            map.put(car, currentCount + 1);
         } else {
             map.put(car, 1);
         }
     }
 
+    public void leave(Car car) {
+        if (map.containsKey(car)) {
+            int currentCount = map.get(car);
+            if (currentCount > 1) {
+                map.put(car, currentCount - 1);
+            } else {
+                map.remove(car);
+            }
+        }
+    }
+
+
+//    leave()
+//    getCount(String )
+//    getCount(brand)
+//    getCount(brand, model)
 }
--- src/com/rakovets/java/task/collections/list/Main.java
+++ src/com/rakovets/java/task/collections/list/Main.java
@@ -2,10 +2,19 @@ package com.rakovets.java.task.collections.list;
 
 public class Main {
     public static void main(String[] args) {
+        Car car1 = new Bmw("Bmw", "M3", "Brown", 2017, 84337, "2 liters");
+        Car car2 = new Audi("Audi", "A4", "Black", 2018, 84447, "10 liters");
+        Car car3 = new Bmw("Bmw", "M3", "Brown", 2016, 84337, "2 liters");
+
+
         Garage garage = new Garage();
-        garage.park(new Bmw("Bmw", "M3", "Brown", 2017, 84337, "2 liters"));
-        garage.park(new Audi("Audi", "A4", "Black", 2018, 84447, "10 liters"));
-        garage.park(new Bmw("Bmw", "M3", "Brown", 2017, 84337, "2 liters"));
-        garage.park(new Bmw("Bmw", "M3", "Brown", 2016, 84337, "2 liters"));
+        garage.park(car1);
+        garage.park(car2);
+        garage.park(car1);
+        garage.park(car3);
+
+
+        garage.leave(car1);
+        garage.leave(car1);
     }
 }
