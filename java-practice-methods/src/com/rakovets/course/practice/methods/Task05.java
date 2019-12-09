3f9bab039648fb3dc33d597ae0830506d6f3b73d
Yeroshevich Valeriy <pybin2001@gmail.com>
2019-12-09 13:02:10 +0300

    Done 1-2;4-5

--- java-practice-methods/src/com/rakovets/course/practice/methods/Task05.java
+++ java-practice-methods/src/com/rakovets/course/practice/methods/Task05.java
@@ -24,15 +24,47 @@ public class Task05 extends StandardInputTask {
 	 * @return месяц на английском языке (UPPER CASE)
 	 */
 	static String getNameMonth(byte monthNumber) {
-		String mounth = null;
-		if (monthNumber = 1) {
-			mounth = "JANUARY";
-		} else if (monthNumber = 2) {
-			mounth = "FEBRUARY";
+		String month = null;
+		switch(monthNumber) {
+			case 1:
+				month = "JANUARY";
+				break;
+			case 2:
+				month = "FEBRUARY";
+				break;
+			case 3:
+				month = "MARCH";
+				break;
+			case 4:
+				month = "APRIL";
+				break;
+			case 5:
+				month = "MAY";
+				break;
+			case 6:
+				month = "JUNE";
+				break;
+			case 7:
+				month = "JULY";
+				break;
+			case 8:
+				month = "AUGUST";
+				break;
+			case 9:
+				month = "SEPTEMBER";
+				break;
+			case 10:
+				month = "OCTOBER";
+				break;
+			case 11:
+				month = "NOVEMBER";
+				break;
+			case 12:
+				month = "DECEMBER";
+				break;
 		}
+		return month;
 
 
-
-		return mounth;
 	}
 }
