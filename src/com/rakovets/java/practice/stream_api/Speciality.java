c166cd8196234a7342f28f9657c4283a5d47ed75
Dmitry Rakovets <dmitryrakovets@gmail.com>
2019-12-21 20:31:01 +0300

    practice: theme 'Stream API'

---
+++ src/com/rakovets/java/practice/stream_api/Speciality.java
@@ -0,0 +1,12 @@
+package com.rakovets.java.practice.stream_api;
+
+public enum Speciality {
+    Biology,
+    ComputerScience,
+    Economics,
+    Finance,
+    History,
+    Philosophy,
+    Physics,
+    Psychology
+}
