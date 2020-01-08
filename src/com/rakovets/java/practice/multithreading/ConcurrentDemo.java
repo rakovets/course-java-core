78f51bcac32ca3ca38f1e2a4e6a9ef12baef8a79
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-01-08 20:55:04 +0300

    practice: add theme 'multithreading'

---
+++ src/com/rakovets/java/practice/multithreading/ConcurrentDemo.java
@@ -0,0 +1,11 @@
+package com.rakovets.java.practice.multithreading;
+
+public class ConcurrentDemo {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+
+
+        long finish = System.currentTimeMillis();
+    }
+}
