275c3f8697243a1b5f7a728268e62cb5d6b62076
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-10-03 14:23:53 +0300

    feat: add solution for Task01.

--- src/main/java/com/rakovets/course/javabasics/practice/methods/Task01.java
+++ src/main/java/com/rakovets/course/javabasics/practice/methods/Task01.java
@@ -30,6 +30,6 @@ public class Task01 extends StandardInputTask {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
-        return true;
+        return false;
     }
 }
