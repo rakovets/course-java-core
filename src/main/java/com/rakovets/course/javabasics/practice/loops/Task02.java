59647b5cf3b77128b852718e54412b9b0e8850f9
dimazm@live.com <Pvfxbycrbq1981>
2020-10-09 17:44:19 +0300

    Test02 Done

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task02.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task02.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
  * Разработать программу для табличного процессора:
  * Программа генерирует номера строк при создании таблицы. Известно:
  *
- * @param// rows            количество строк в таблице
- * @param// headerRowEnable имеет ли таблица строку-заголовок 'true'/'false', если имеет, то для первой строки не нужно
- *                        генерировать номер
  * @author Dmitry Rakovets
  * @version 1.0
+ * @param// rows            количество строк в таблице
+ * @param// headerRowEnable имеет ли таблица строку-заголовок 'true'/'false', если имеет, то для первой строки не нужно
+ * генерировать номер
  * @return номера строк, где каждый номер на новой строке
  */
 public class Task02 {
@@ -26,16 +26,21 @@ public class Task02 {
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
         if (headerRowEnable) {
-           // System.out.print("\n");
-            for (int i = 1; i < rows; i++) {
-                   System.out.print((i) + "\n");
+            // System.out.print("\n");
+            for (int i = 1; i <= rows; i++) {
+                if (i == rows - 1) {
+                    System.out.print(i);
+                    break;
+                } else {
+                    System.out.println(i);
+                }
             }
         } else {
             for (int i = 1; i <= rows; i++) {
-                if ( i >=rows){
-                System.out.print(i);
+                if (i >= rows) {
+                    System.out.print(i);
                 } else {
-                    System.out.println(i);// заменил на лн
+                    System.out.println(i);
                 }
             }
         }
