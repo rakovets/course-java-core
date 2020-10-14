99e6ca4b0bd38878cdbbfc9da3fb49e1fdb3515f
dimazm@live.com <Pvfxbycrbq1981>
2020-10-14 23:35:14 +0300

    Task04 Almost Done

--- src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Atm.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Atm.java
@@ -6,7 +6,6 @@ public class Atm {
     private int numberBanknotes20;
 
 
-
     Atm(int numOf100, int numof50, int numOf20) {
         this.numberBanknotes100 = numOf100;
         this.numberBanknotes50 = numof50;
@@ -30,30 +29,49 @@ public class Atm {
     }
 
     public boolean isPossibleIssue(int amount) {
-        int fullCount = (numberBanknotes100*100 + numberBanknotes50*50 + numberBanknotes20*20);
-         if ((amount <= fullCount) && (amount%100 == 0 || amount%50 == 0 || amount%20 == 0 )) {
-          return true;
+        int fullCount = (numberBanknotes100 * 100 + numberBanknotes50 * 50 + numberBanknotes20 * 20);
+        if ((amount <= fullCount) && (amount % 100 == 0 || amount % 50 == 0 || amount % 20 != 0)) {
+            return true;
         } else
-        return false;
+            return false;
     }
 
     public int getOptionsCombinationBanknotes(int amount) {
-        int cash100 = amount/100;
-        int cash50 = amount/50;
-        int cash20 = amount/20;
-        int fullCount = (numberBanknotes100*100 + numberBanknotes50*50 + numberBanknotes20*20);
-        if (amount <= 100){
-
+        int cash100 = amount / 100;
+        int cash50 = amount / 50;
+        int cash20 = amount / 20;
+        int options = 0;
+        int fullCount = (numberBanknotes100 * 100 + numberBanknotes50 * 50 + numberBanknotes20 * 20);
+        if (amount % 100 == 0) {
+            options = 1;
+        } else if (amount % 50 == 0 && amount % 100 != 0) {
+            options = 2;
+        } else {
+            options = 3;
         }
-        int one = 1;
-        return one;
+        return options;
     }
 
 
-    public boolean getCash(int option) {
-        if (option == 1);
-            return true;
+    public void getCash(int option, int amount) {
+        int cash100 = amount / 100;
+        int cash50 = amount / 50;
+        int cash20 = amount / 20;
+        if (option == 1) {
+            System.out.println("Take 100 x " + cash100);
+        } else if (option == 2) {
+            int hunder = amount % 100;
+            int fifth = 50 / hunder;
+            System.out.println("Take 100x" + amount/100 + " 50x" + fifth);
+        }
+        if (option == 3) {
+            int hunder = amount % 100;
+            int fifth = amount%50;
+            System.out.println("Take 100 x " + amount/100 + " 50x" + hunder/50 + " 20x" + fifth/20);
+        }
 
+
+        //    return true;
 
 
     }
--- src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/AtmTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/AtmTest.java
@@ -10,18 +10,20 @@ public class AtmTest {
         System.out.println(ColorConsants.ANSI_GREEN + "...Please inter amount of cash... \n" + ColorConsants.ANSI_RESET);
         Scanner in = new Scanner(System.in);
         int amountCash = in.nextInt();
+        Scanner optIn = new Scanner(System.in);
+
         if (cash.isPossibleIssue(amountCash)) {
             System.out.println(ColorConsants.ANSI_GREEN + "...Please choose options... \n" + ColorConsants.ANSI_RESET + cash.getOptionsCombinationBanknotes(amountCash));
             cash.getOptionsCombinationBanknotes(amountCash);
+            int options = in.nextInt();
+            if(cash.getOptionsCombinationBanknotes(amountCash)!=0){
+                cash.getCash(options, amountCash);
+            }
         } else
             System.out.println(ColorConsants.ANSI_RED + "...Sorry, ATM hasn't this cash... \n" + ColorConsants.ANSI_RESET + cash.getOptionsCombinationBanknotes(amountCash));
 
-        Scanner optIn = new Scanner(System.in);
-        int options = in.nextInt();
-        if(cash.getCash(options)){
-            System.out.println(ColorConsants.ANSI_GREEN + "...Please take a money \n" + ColorConsants.ANSI_RESET );
 
-        }
+
 
 
     }
