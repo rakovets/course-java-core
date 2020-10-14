8aedc84b2f34c0c742ced1e69d8b4a620e16fb99
dimazm@live.com <Pvfxbycrbq1981>
2020-10-15 00:43:11 +0300

    Task04 extend and fix Options

--- src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Atm.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Atm.java
@@ -30,7 +30,7 @@ public class Atm {
 
     public boolean isPossibleIssue(int amount) {
         int fullCount = (numberBanknotes100 * 100 + numberBanknotes50 * 50 + numberBanknotes20 * 20);
-        if ((amount <= fullCount) && (amount % 100 == 0 || amount % 50 == 0 || amount/20 < 0)) {
+        if ((amount <= fullCount) && (amount % 100 == 0 || amount % 50 == 0 || amount / 20 > 0)) {
             return true;
         } else
             return false;
@@ -38,26 +38,31 @@ public class Atm {
 
     public void getOptionsCombinationBanknotes(int amount) {
 
-        int fullCount = (numberBanknotes100 * 100 + numberBanknotes50 * 50 + numberBanknotes20 * 20);
+        // int fullCount = (numberBanknotes100 * 100 + numberBanknotes50 * 50 + numberBanknotes20 * 20);
         if (amount % 100 == 0) {
-            System.out.println("Available option 1: 100 x " + amount / 100);
+            System.out.println("Available option 1: 100x" + amount / 100);
 
         } else if (amount % 50 == 0 && amount % 100 != 0) {
             int hunder = amount % 100;
-            System.out.println("Available option 2: 100 x" + amount / 100 + " 50x" + hunder / 50);
+            System.out.println("Available option 2: 100x" + amount / 100 + " 50x" + hunder / 50);
 
-        } else {
+        } else if ( amount > 100 && (amount % 50) % 20 == 0) {
             int hunder = amount % 100;
             int fifth = amount % 50;
-            System.out.println("Available option 3: 100 x" + amount / 100 + " 50x" + hunder / 50 + " 20x" + fifth / 20);
+            System.out.println("Available option 3: 100x" + amount / 100 + " 50x" + hunder / 50 + " 20x" + fifth / 20);
+        }else if ( amount > 100 && (amount % 100) % 20 == 0) {
+            int hunder = amount % 100;
+            System.out.println("Available option 4: 100x" + amount / 100 +  " 20x" + hunder / 20);
+        } else if (amount < 100 && (amount % 50) % 20 == 0) {
+            int twenty = amount % 50;
+            System.out.println("Available option 5: 50x" + amount / 50 + " 20x" + twenty / 20);
         }
     }
-
 
     public void getCash(int option, int amount) {
         int cash100 = amount / 100;
         if (option == 1) {
-            System.out.println("Take 100 x " + cash100);
+            System.out.println("Take 100x " + cash100);
         } else if (option == 2) {
             int hunder = amount % 100;
 
@@ -66,7 +71,13 @@ public class Atm {
         if (option == 3) {
             int hunder = amount % 100;
             int fifth = amount % 50;
-            System.out.println("Take 100 x " + amount / 100 + " 50x" + hunder / 50 + " 20x" + fifth / 20);
+            System.out.println("Take your money: 100x" + amount / 100 + " 50x" + hunder / 50 + " 20x" + fifth / 20);
+        }if (option == 4) {
+            int hunder = amount % 100;
+            System.out.println("Take your money: 100x" + amount / 100 +  " 20x" + hunder / 20);
+        }if (option == 5) {
+            int twenty = amount % 50;
+            System.out.println("Take your money: 50x" + amount / 50 + " 20x" + twenty / 20);
         }
 
 
