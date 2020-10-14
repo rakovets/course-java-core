a78051f00d552d97b972d55ecc3f7711b9c4bd65
dimazm@live.com <Pvfxbycrbq1981>
2020-10-14 21:51:51 +0300

    Task04 Added

--- src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Atm.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Atm.java
@@ -6,11 +6,13 @@ public class Atm {
     private int numberBanknotes20;
 
 
+
     Atm(int numOf100, int numof50, int numOf20) {
         this.numberBanknotes100 = numOf100;
         this.numberBanknotes50 = numof50;
         this.numberBanknotes20 = numOf20;
     }
+
 
     public void addBanknotes100(int number) {
         this.numberBanknotes100 = number;
@@ -28,11 +30,21 @@ public class Atm {
     }
 
     public boolean isPossibleIssue(int amount) {
-        //if (amount <= (this.numberBanknotes100 + this.numberBanknotes50 + this.numberBanknotes20 ))
-        return true;
+        int fullCount = (numberBanknotes100*100 + numberBanknotes50*50 + numberBanknotes20*20);
+         if ((amount <= fullCount) && (amount%100 == 0 || amount%50 == 0 || amount%20 == 0 )) {
+          return true;
+        } else
+        return false;
     }
 
     public int getOptionsCombinationBanknotes(int amount) {
+        int cash100 = amount/100;
+        int cash50 = amount/50;
+        int cash20 = amount/20;
+        int fullCount = (numberBanknotes100*100 + numberBanknotes50*50 + numberBanknotes20*20);
+        if (amount <= 100){
+
+        }
         int one = 1;
         return one;
     }
--- src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/AtmTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/AtmTest.java
@@ -6,14 +6,16 @@ import java.util.Scanner;
 
 public class AtmTest {
     public static void main(String[] args) {
-        Atm cash = new Atm(100, 20, 200);
+        Atm cash = new Atm(1, 2, 5);
         System.out.println(ColorConsants.ANSI_GREEN + "...Please inter amount of cash... \n" + ColorConsants.ANSI_RESET);
         Scanner in = new Scanner(System.in);
         int amountCash = in.nextInt();
         if (cash.isPossibleIssue(amountCash)) {
+            System.out.println(ColorConsants.ANSI_GREEN + "...Please choose options... \n" + ColorConsants.ANSI_RESET + cash.getOptionsCombinationBanknotes(amountCash));
             cash.getOptionsCombinationBanknotes(amountCash);
-        }
-        System.out.println(ColorConsants.ANSI_GREEN + "...Please choose options... \n" + ColorConsants.ANSI_RESET + cash.getOptionsCombinationBanknotes(amountCash));
+        } else
+            System.out.println(ColorConsants.ANSI_RED + "...Sorry, ATM hasn't this cash... \n" + ColorConsants.ANSI_RESET + cash.getOptionsCombinationBanknotes(amountCash));
+
         Scanner optIn = new Scanner(System.in);
         int options = in.nextInt();
         if(cash.getCash(options)){
