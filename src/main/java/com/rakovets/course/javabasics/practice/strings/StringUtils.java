4b133a50e2949b0d689e96123114e22cb9470203
dimazm@live.com <Pvfxbycrbq1981>
2020-10-15 21:06:20 +0300

    Added StringUtils

--- src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtils.java
+++ src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtils.java
@@ -1,4 +1,10 @@
 package com.rakovets.course.javabasics.practice.strings;
 
+
 public class StringUtils {
+    String getUpperCase(String string) {
+        return string.toUpperCase();
+    }
+
+
 }
---
+++ src/test/java/com/rakovets/course/javabasics/practice/strings/StringUtilsTest.java
@@ -0,0 +1,12 @@
+package com.rakovets.course.javabasics.practice.strings;
+
+import org.junit.jupiter.api.Test;
+
+public class StringUtilsTest {
+
+   @Test
+    void getUpperCaseTest{
+
+
+    }
+}
