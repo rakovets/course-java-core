4a1436318d3fc3b0479ddf6c75ddfdf40ed40cae
dimazm@live.com <Pvfxbycrbq1981>
2020-10-15 21:17:56 +0300

    Added StringUtils

--- src/test/java/com/rakovets/course/javabasics/practice/strings/StringUtilsTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/strings/StringUtilsTest.java
@@ -1,11 +1,18 @@
 package com.rakovets.course.javabasics.practice.strings;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class StringUtilsTest {
 
    @Test
-    void getUpperCaseTest{
+    void getUpperCaseTest(){
+       StringUtils stringUtTest = new StringUtils();
+        Assertions.assertEquals("QWERTY", stringUtTest.getUpperCase("qwerty"));
+        Assertions.assertEquals("QWERT Y", stringUtTest.getUpperCase("qwert y"));
+//        Assertions.assertEquals("QWERTY", stringUtTest.getUpperCase("qwerty"));
+//        Assertions.assertEquals("QWERTY", stringUtTest.getUpperCase("qwerty"));
+//        Assertions.assertEquals("QWERTY", stringUtTest.getUpperCase("qwerty"));
 
 
     }
