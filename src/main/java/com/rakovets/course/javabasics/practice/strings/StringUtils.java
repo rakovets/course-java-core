34f5685ad3b9459729dea5ec573caa5194758b50
dimazm@live.com <Pvfxbycrbq1981>
2020-10-16 23:23:11 +0300

    Task06 Done

--- src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtils.java
+++ src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtils.java
@@ -14,13 +14,29 @@ public class StringUtils {
 
     }
 
-    public boolean getStringEqual(String str1, String str2){
+    public boolean getStringEqual(String str1, String str2) {
         return str1.equals(str2);
     }
 
-    public String getStringUpperAndTrim(String str1){
-        return str1.trim().toUpperCase();
-
+    public String getStringUpperAndTrim(String str) {
+        return str.trim().toUpperCase();
     }
+
+    public String getStringSubstring(String str) {
+        if (str.length() < 10) {
+            return "<10";
+        }
+        String str2 = str.substring(10);
+        if (str2.length() > 23) {
+            String str3 = str2.substring(0, 23);
+            return str3;
+        }
+        return str2;
+    }
+
+    public String getStringReplace(String str) {
+        return str.replace(":(", ":)");
+    }
+
 
 }
--- src/test/java/com/rakovets/course/javabasics/practice/strings/StringUtilTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/strings/StringUtilTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 public class StringUtilTest {
 
 
-
     @Test
     void getConcatStringsTest() {
         StringUtils stringUtTest = new StringUtils();
@@ -32,14 +31,31 @@ public class StringUtilTest {
         Assertions.assertEquals(7, stringUtTest.getStringIndex("Coffe is the best", "S"));
         Assertions.assertEquals(-1, stringUtTest.getStringIndex("LIvE in BelArus is ...", "Z"));
     }
+
     @Test
     void getStringUpperAndTrimTest() {
         StringUtils stringUtTest = new StringUtils();
         Assertions.assertEquals("HELLO", stringUtTest.getStringUpperAndTrim(" Hello "));
-        Assertions.assertEquals("BELARUS", stringUtTest.getStringUpperAndTrim("BelarUs "));
+        Assertions.assertEquals("BELARUS", stringUtTest.getStringUpperAndTrim("BelarUs     "));
         Assertions.assertEquals("ЦОЙ ЖИВ", stringUtTest.getStringUpperAndTrim("Цой жив "));
         Assertions.assertEquals("BRUTTO", stringUtTest.getStringUpperAndTrim("   brUtto  "));
 
     }
 
+    @Test
+    void getStringSubstringTest() {
+        StringUtils stringUtTest = new StringUtils();
+        Assertions.assertEquals("ия нашей страны", stringUtTest.getStringSubstring("Приватизация нашей страны"));
+        Assertions.assertEquals("12345678901234567890123", stringUtTest.getStringSubstring("1234567890123456789012345678901234567890"));
+        Assertions.assertEquals("1234567890", stringUtTest.getStringSubstring("12345678901234567890"));
+        Assertions.assertEquals("<10", stringUtTest.getStringSubstring("123456789"));
+    }
+
+    @Test
+    void getStringReplace() {
+        StringUtils stringUtTest = new StringUtils();
+        Assertions.assertEquals("Hello :)", stringUtTest.getStringReplace("Hello :("));
+        Assertions.assertEquals("Hello :)(", stringUtTest.getStringReplace("Hello :(("));
+        Assertions.assertEquals(":): Hello :)", stringUtTest.getStringReplace(":(: Hello :("));
+    }
 }
