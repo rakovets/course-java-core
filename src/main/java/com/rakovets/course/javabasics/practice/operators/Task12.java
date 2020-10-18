e77caab35432cc4b2bd0975853ef9f0bb515bfee
Dmitry Sedun <sedun.dima@ya.ru>
2020-10-18 17:33:50 +0300

    feat: add solution for Task01.

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task12.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task12.java
@@ -11,7 +11,7 @@ public class Task12 {
     public static void main(String[] args) {
         //FIXME
         // Переменные, которые можно изменять для проверки различных вариантов входных параметров
-        int playingTimeInSeconds = 156;
+        int playingTimeInSeconds = 100000;
 
         // Код необходимый для тестирования, не изменять
         playingTimeInSeconds = (args.length == 1) ? Integer.parseInt(args[0]) : playingTimeInSeconds;
@@ -19,5 +19,10 @@ public class Task12 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        int days = playingTimeInSeconds / (60 * 60 * 24);
+        int hours = (playingTimeInSeconds - days * (60 * 60 * 24)) / 3600;
+        int minutes = ((playingTimeInSeconds - (hours * 3600 + days * (60 * 60 * 24))) / 60);
+        int seconds = playingTimeInSeconds - (days * (60 * 60 * 24) + hours * 3600 + minutes * 60);
+        System.out.println(days + " " + hours + ":" + minutes + ":" + seconds);
     }
 }
