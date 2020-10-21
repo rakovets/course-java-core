7c5a548ae93360fb5cc4e50c02ebe9fe49461aaa
dimazm@live.com <Pvfxbycrbq1981>
2020-10-21 23:16:34 +0300

    Battle Game done

--- src/main/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/battleground/ProgramBattle.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/battleground/ProgramBattle.java
@@ -7,35 +7,22 @@ public class ProgramBattle {
 
     public static void main(String[] args) {
         Warrior ivan = new Warrior("Shrek");
-        ivan.setHeath(150);
-        Zoomby zoomby = new Zoomby(150);
-        System.out.println(ANSI_GREEN + "Zoombe has " + zoomby.getHealth() + "-----Hero has " + ivan.getHealth() + ANSI_RESET);
-        for (int i = 0; zoomby.getHealth() > 0; i++) {
+        ivan.setHeath(100);
+        Zoomby zoomby = new Zoomby(250);
+        System.out.println(ANSI_GREEN + "Zoombe has " + zoomby.getHealth() + ANSI_RESET + "-----" + ANSI_RED + "Hero has " + ivan.getHealth() + ANSI_RESET);
+
+        for (int i = 0; zoomby.isAlive(); i++) {
+            System.out.println("----------------");
             ivan.attackEnemy(zoomby);
             System.out.println(ANSI_GREEN + "Zoombe has " + zoomby.getHealth() + ANSI_RESET);
-            if (zoomby.getHealth() > 0) {
+            if (zoomby.isAlive()) {
                 zoomby.attackHero(ivan);
                 System.out.println(ANSI_RED + "Hero has " + ivan.getHealth() + ANSI_RESET);
-                if (ivan.getHealth() > 0) {
-                    System.out.println("Next step");
-                } else
+                if (!ivan.isAlive()) {
+                    System.out.println(ANSI_RED + "Hero is dead with " + ivan.getHealth() + ANSI_RESET);
                     break;
-                System.out.println(ANSI_RED + "Hero is dead with " + ivan.getHealth() + ANSI_RESET);
-
-
+                }
             } else System.out.println(ANSI_RED + "Enemy is dead with " + zoomby.getHealth() + ANSI_RESET);
         }
-
-
-        // enemy.attackHero(ivan);
-
-
-        // System.out.print(ANSI_GREEN + ivan.attackEnemy(zoomby)+ ANSI_RESET);
-        //     Mag magic = new Mag("Gendolf");
-        //     magic.attackEnemy();
-        //    Archer robin = new Archer("Robin Good");
-        //    robin.attackEnemy();
-        //     Enemy unit1 = new Enemy(100);
-
     }
 }
