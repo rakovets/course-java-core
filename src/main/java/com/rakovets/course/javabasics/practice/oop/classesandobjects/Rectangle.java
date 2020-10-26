e5fb3119d4b8e9428a23586a8e0b3af9c71488d4
Dmitry Sedun <sedun.dima@ya.ru>
2020-10-26 12:59:53 +0300

    add feature for Task StringUtilTest final

---
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Rectangle.java
@@ -0,0 +1,37 @@
+package com.rakovets.course.javabasics.practice.oop.classesandobjects;
+
+public class Rectangle {
+    public int firstPoint;
+    public int secondPoint;
+
+
+    public Rectangle (int firstPoint, int secondPoint) {
+        this.firstPoint = firstPoint;
+        this.secondPoint = secondPoint;
+    }
+
+    public int getFirstPoint() {
+         return firstPoint;
+    }
+
+    public  void setFirstPoint(int i) {
+      this.secondPoint = secondPoint;
+    }
+
+    public int getSecondPoint() {
+        return secondPoint;
+    }
+
+    public void setSecondPoint(int i) {
+        this.secondPoint = secondPoint;
+    }
+
+//    public int getPerimeter() { // два метода
+//      return ...;
+//    }
+//
+//    public int getArea() {
+//
+//    }
+
+}
