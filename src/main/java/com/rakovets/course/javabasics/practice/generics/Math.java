1025feda74e0c94760f2461a35e214e44a5f16f6
dimazm@live.com <Pvfxbycrbq1981>
2020-10-26 12:07:41 +0300

    Spec2

--- src/main/java/com/rakovets/course/javabasics/practice/generics/Math.java
+++ src/main/java/com/rakovets/course/javabasics/practice/generics/Math.java
@@ -9,41 +9,38 @@ public class Math<T extends Number> {
 
     public T getMaxOf(T a, T b, T c) {
 
-               if (a.intValue() > b.intValue() && b.intValue() > c.intValue()) {
+        if (a.doubleValue() > b.doubleValue() && b.doubleValue() > c.doubleValue()) {
             return a;
-        } else if (a.intValue() > c.intValue() && c.intValue() > b.intValue()) {
+        } else if (a.doubleValue() > c.doubleValue() && c.doubleValue() > b.doubleValue()) {
             return a;
-        } else if (b.intValue() > c.intValue() && c.intValue() > a.intValue()) {
+        } else if (b.doubleValue() > c.doubleValue() && c.doubleValue() > a.doubleValue()) {
             return b;
-        } else if (b.intValue() > a.intValue() && a.intValue() > c.intValue()) {
+        } else if (b.doubleValue() > a.doubleValue() && a.doubleValue() > c.doubleValue()) {
             return b;
-        } else if (c.intValue() > a.intValue() && a.intValue() > b.intValue()) {
+        } else if (c.doubleValue() > a.doubleValue() && a.doubleValue() > b.doubleValue()) {
             return c;
-        } else if (c.intValue() > b.intValue() && b.intValue() > a.intValue()) {
+        } else if (c.doubleValue() > b.doubleValue() && b.doubleValue() > a.doubleValue()) {
             return c;
         }
         return null;
 
     }
 
-    // public T getMinOf(T a, T b, T c, T d, T e) {
-    //     double[] array = new double[5];
-    //     array[0] = a.intValue();
-    //     array[1] = b.intValue();
-    //     array[2] = c.intValue();
-    //     array[3] = d.intValue();
-    //     array[4] = e.intValue();
-    //     T min = array[0];
-//
-    //     for ( int i =0 ; i<=5; i++){
-    //         if(array[i]<array[i+1]){
-//
-    //         }
-    //     }
-
-
-
-
+    public double getMinOf(T a, T b, T c, T d, T e) {
+        double[] array = new double[5];
+        array[0] = a.doubleValue();
+        array[1] = b.doubleValue();
+        array[2] = c.doubleValue();
+        array[3] = d.doubleValue();
+        array[4] = e.doubleValue();
+        double min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+            }
+        } return min;
+    }
 
 
 }
+
--- src/main/java/com/rakovets/course/javabasics/practice/generics/Program.java
+++ src/main/java/com/rakovets/course/javabasics/practice/generics/Program.java
@@ -14,6 +14,9 @@ public class Program {
         double max = math.getMaxOf(600.0, 1000.6,970.7);
         System.out.println(max);
 
+        Math<Double> math1  = new Math();
+        double min = math1.getMinOf(1.5,12.0,22.0,33.0,14.0);
+        System.out.println(min);
 
     }
 
