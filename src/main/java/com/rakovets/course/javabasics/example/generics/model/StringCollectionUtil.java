fee82b6a2289e7e43f8f7e79b6e92e225746fea2
RomaHoncharuk <orgtexby1@gmail.com>
2020-10-28 16:07:42 +0300

    1

--- src/main/java/com/rakovets/course/javabasics/example/generics/model/StringCollectionUtil.java
+++ src/main/java/com/rakovets/course/javabasics/example/generics/model/StringCollectionUtil.java
@@ -3,7 +3,7 @@ package com.rakovets.course.javabasics.example.generics.model;
 import java.util.ArrayList;
 
 public class StringCollectionUtil {
-    public ArrayList<String> resetWordsByLength(ArrayList<String> list, int length) {
+    public static ArrayList<String> resetWordsByLength(ArrayList<String> list, int length) {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).length() == length) {
                 list.set(i, "*");
@@ -12,7 +12,7 @@ public class StringCollectionUtil {
         return list;
     }
 
-    public ArrayList<String> removeWordsByLength(ArrayList<String> list, int length) {
+    public static ArrayList<String> removeWordsByLength(ArrayList<String> list, int length) {
         for(int i = 0; i< list.size(); i++) {
             if (list.get(i).length() == length) {
                 list.remove(i);
