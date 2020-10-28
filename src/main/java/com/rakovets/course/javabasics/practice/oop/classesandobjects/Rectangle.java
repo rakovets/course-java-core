6a227e35165acdc370dd11525258ec62bd9309ad
Dmitry Sedun <sedun.dima@ya.ru>
2020-10-28 10:53:56 +0300

    add feature for Task Rectangle

--- src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Rectangle.java
+++ src/main/java/com/rakovets/course/javabasics/practice/oop/classesandobjects/Rectangle.java
@@ -1,36 +1,38 @@
 package com.rakovets.course.javabasics.practice.oop.classesandobjects;
 
 public class Rectangle {
-    public int firstPoint;
-    public int secondPoint;
+    private Point firstPoint;
+    private Point secondPoint;
 
 
-    public Rectangle (int firstPoint, int secondPoint) {
+    public Rectangle (Point firstPoint, Point secondPoint) {
         this.firstPoint = firstPoint;
         this.secondPoint = secondPoint;
     }
 
-    public int getFirstPoint() {
+    public Point getFirstPoint() {
          return firstPoint;
     }
 
-    public  void setFirstPoint(int i) {
-      this.secondPoint = secondPoint;
+    public  void setFirstPoint(Point firstPoint) {
+      this.firstPoint = firstPoint;
     }
 
-    public int getSecondPoint() {
+    public Point getSecondPoint() {
         return secondPoint;
     }
 
-    public void setSecondPoint(int i) {
+    public void setSecondPoint(Point secondPoint) {
         this.secondPoint = secondPoint;
     }
 
-////    public int getPerimeter() { // два метода
-//        Point firstpoint = new Point(firstPoint.getX(), secondPoint.getY());
-//        double length = Math.sqrt
-////      return ...;
-////    }
+    public int getPerimeter() { // два метода
+        Point firstpoint = new Point(firstPoint.getX(), secondPoint.getY());
+        double length = Math.sqrt((secondPoint.getY() - firstPoint.getY() * (secondPoint.getY() - firstPoint.getY()) + (secondPoint.getX() - firstPoint.getX()) *(secondPoint.getX() - firstPoint.getX()));
+        double width = Math.sqrt((firstpoint.getY() - secondPoint.getY()) * (firstpoint.getY() - secondPoint.getY()) + firstpoint.getX() - secondPoint.getX()) * (firstpoint.getX() - secondPoint.getX());
+        double perimeter = length * width / 2;
+      return (int) perimeter;
+    }
 //
 //    public int getArea() {
 //
