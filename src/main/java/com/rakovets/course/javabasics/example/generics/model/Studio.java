9719c3d6208a6e03e07237bcea03100e24c045c2
RomaHoncharuk <orgtexby1@gmail.com>
2020-10-28 21:43:06 +0300

    1.2

--- src/main/java/com/rakovets/course/javabasics/example/generics/model/Studio.java
+++ src/main/java/com/rakovets/course/javabasics/example/generics/model/Studio.java
@@ -1,6 +1,7 @@
 package com.rakovets.course.javabasics.example.generics.model;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Studio {
     private ArrayList<Actor> actors;
@@ -10,8 +11,14 @@ public class Studio {
     }
 
     public void fire() {
-
+        actors.sort(fee);
+        actors.remove(0);
     }
 
+    public static Comparator<? super Actor> fee = new Comparator<Actor>() {
+        public int compare(Actor actor1, Actor actor2) {
+            return (int)(actor2.getFee() - actor1.getFee());
+        }
+    };
 
 }
