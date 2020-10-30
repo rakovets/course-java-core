9a341c430b6ee02c2b5bba6f54a2fa6f8b2cfa23
RomaHoncharuk <orgtexby1@gmail.com>
2020-10-30 22:26:05 +0300

    1

--- src/test/java/com/rakovets/course/javabasics/practice/jcf/collection/StringCollectionUtilTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/jcf/collection/StringCollectionUtilTest.java
@@ -1,36 +1,3 @@
 package com.rakovets.course.javabasics.practice.jcf.collection;
 
-import com.rakovets.course.javabasics.example.generics.model.StringCollectionUtil;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
-public class StringCollectionUtilTest {
-
-    @Test
-
-    void replacement() {
-        String[] array = new String[]{"this", "is", "lots", "of", "fun", "for", "every", "Java", "programmer"};
-        String[] test = new String[]{"this", "*", "lots", "*", "fun", "for", "every", "Java", "programmer"};
-        ArrayList<String> list = new ArrayList<>();
-        for (String string : array) {
-            list.add(string);
-        }
-        StringCollectionUtil.resetWordsByLength(list, 2);
-        Assertions.assertArrayEquals(test, list.toArray());
-    }
-
-    @Test
-    void remove() {
-        String[] array = new String[]{"this", "is", "lots", "of", "fun", "for", "every", "Java", "programmer"};
-        String[] test = new String[]{"this", "lots", "fun", "for", "every", "Java", "programmer"};
-        ArrayList<String> list = new ArrayList<>();
-        for (String string : array) {
-            list.add(string);
-        }
-        StringCollectionUtil.removeWordsByLength(list, 2);
-        Assertions.assertArrayEquals(test, list.toArray());
-    }
-
-}
