0d872090cc03d94429fb94bcaac7273ed2706f66
RomaHoncharuk <orgtexby1@gmail.com>
2020-11-02 15:02:19 +0300

    1

--- src/main/java/com/rakovets/course/javabasics/example/generics/model/StringCollectionUtil.java
+++ src/main/java/com/rakovets/course/javabasics/example/generics/model/StringCollectionUtil.java
@@ -1,4 +1,24 @@
 package com.rakovets.course.javabasics.example.generics.model;
+import java.util.ArrayList;
 
+public class StringCollectionUtil {
+    public static ArrayList<String> resetWordsByLength(ArrayList<String> list, int length) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).length() == length) {
+                list.set(i, "*");
+            }
+        }
+        return list;
+    }
+
+    public static ArrayList<String> removeWordsByLength(ArrayList<String> list, int length) {
+        for(int i = 0; i< list.size(); i++) {
+            if (list.get(i).length() == length) {
+                list.remove(i);
+            }
+        }
+        return list;
+    }
+}
 
 
