d08baf36819dc9faa6f5d646594a0186c823cafd
RomaHoncharuk <orgtexby1@gmail.com>
2020-11-04 22:13:02 +0300

    1

--- src/main/java/com/rakovets/course/javabasics/example/generics/model/StringCollectionUtil.java
+++ src/main/java/com/rakovets/course/javabasics/example/generics/model/StringCollectionUtil.java
@@ -1,24 +1,43 @@
 package com.rakovets.course.javabasics.example.generics.model;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class StringCollectionUtil {
-    public static ArrayList<String> resetWordsByLength(ArrayList<String> list, int length) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).length() == length) {
-                list.set(i, "*");
+    public static Collection<String> resetWordsByLength(Collection<String> collection, int length) {
+        Collection<String> collectionNewReset = new LinkedList<>();
+        for (String string : collection) {
+            if (string.length() != length) {
+                collectionNewReset.add(string);
+            } else {
+                collectionNewReset.add("*");
             }
         }
-        return list;
+        return collectionNewReset;
     }
 
-    public static ArrayList<String> removeWordsByLength(ArrayList<String> list, int length) {
-        for(int i = 0; i< list.size(); i++) {
-            if (list.get(i).length() == length) {
-                list.remove(i);
+    public static Collection<String> removeWordsByLength(Collection<String> collection, int length) {
+        Collection<String> collectionNewRemove = new LinkedList<>();
+        for (String string : collection) {
+            if (string.length() != length) {
+                collectionNewRemove.add(string);
+            } else {
+                collectionNewRemove.add("");
             }
         }
-        return list;
+        return collectionNewRemove;
     }
+
+//    public static Collection<String> removeWordsByLength(Collection<String> collection, int length) {
+//        Collection<String> collectionNewRemove = new LinkedList<>();
+//        Iterator<String> iterator = collection.iterator();
+//        if (iterator.hasNext()) {
+//            if (iterator.next().length() == length) {
+//                iterator.remove();
+//            }
+//        } return collectionNewRemove;
+//    }
 }
 
 
--- src/test/java/com/rakovets/course/javabasics/practice/jcf/collection/StringCollectionUtilTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/jcf/collection/StringCollectionUtilTest.java
@@ -5,32 +5,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class StringCollectionUtilTest {
+    private Collection<String> collection;
+    private Collection<String> collectionNewResetter;
+    private Collection<String> collectionNewRemooving;
 
     @Test
-
     void replacement() {
+
         String[] array = new String[]{"this", "is", "lots", "of", "fun", "for", "every", "Java", "programmer"};
         String[] test = new String[]{"this", "*", "lots", "*", "fun", "for", "every", "Java", "programmer"};
-        ArrayList<String> list = new ArrayList<>();
-        for (String string : array) {
-            list.add(string);
-        }
-        StringCollectionUtil.resetWordsByLength(list, 2);
-        Assertions.assertArrayEquals(test, list.toArray());
+        collection = new ArrayList<>(Arrays.asList(array));
+        collectionNewResetter = new ArrayList<>(Arrays.asList(test));
+        Assertions.assertEquals(collectionNewResetter, StringCollectionUtil.resetWordsByLength(collection, 2));
     }
 
     @Test
     void remove() {
         String[] array = new String[]{"this", "is", "lots", "of", "fun", "for", "every", "Java", "programmer"};
-        String[] test = new String[]{"this", "lots", "fun", "for", "every", "Java", "programmer"};
-        ArrayList<String> list = new ArrayList<>();
-        for (String string : array) {
-            list.add(string);
-        }
-        StringCollectionUtil.removeWordsByLength(list, 2);
-        Assertions.assertArrayEquals(test, list.toArray());
+        String[] test = new String[]{"this","", "lots","", "fun", "for", "every", "Java", "programmer"};
+        collection = new ArrayList<>(Arrays.asList(array));
+        collectionNewRemooving = new ArrayList<>(Arrays.asList(test));
+        Assertions.assertEquals(collectionNewRemooving, StringCollectionUtil.removeWordsByLength(collection, 2));
+
+
+
     }
 
 }
