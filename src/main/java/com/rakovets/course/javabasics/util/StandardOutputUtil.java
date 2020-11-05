520c09fc8c3da6b4257a7227cd31b9a5e4748ea4
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-11-05 20:51:24 +0300

    feat: update util for standard output

--- src/main/java/com/rakovets/course/javabasics/util/StandardOutputUtil.java
+++ src/main/java/com/rakovets/course/javabasics/util/StandardOutputUtil.java
@@ -1,11 +1,23 @@
 package com.rakovets.course.javabasics.util;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public abstract class StandardOutputUtil {
     public static void println(String string,  AnsiColorCode... codes) {
-        String color = Arrays.stream(codes).map(String::valueOf).collect(Collectors.joining());
-        System.out.printf("%s%s%s\n", color, string, AnsiColorCode.RESET);
+        System.out.printf("%s%s%s\n", getAnsiColor(codes), string, AnsiColorCode.RESET);
+    }
+
+    public static void printlnWithTime(String string,  AnsiColorCode... codes) {
+        System.out.printf("%s%s - %s%s\n", getAnsiColor(codes), LocalDateTime.now(), string, AnsiColorCode.RESET);
+    }
+
+    public static void printlnWithTimeAndThread(String string,  AnsiColorCode... codes) {
+        System.out.printf("%s%s - %s - %s%s\n", getAnsiColor(codes), LocalDateTime.now(), Thread.currentThread().getName(), string, AnsiColorCode.RESET);
+    }
+
+    private static String getAnsiColor(AnsiColorCode... codes) {
+        return Arrays.stream(codes).map(String::valueOf).collect(Collectors.joining());
     }
 }
