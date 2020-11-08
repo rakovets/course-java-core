bce5fd8ad68aecc03759ab7dafc1be9b7aba5e79
dimazm@live.com <Pvfxbycrbq1981>
2020-11-08 15:47:16 +0300

    Task 2 Done

--- src/main/java/com/rakovets/course/javabasics/practice/exceptionhandling/ProgramExeption.java
+++
@@ -1,9 +0,0 @@
-package com.rakovets.course.javabasics.practice.exceptionhandling;
-
-public class ProgramExeption {
-
-    public static void main(String[] args) {
-        ExeptionsLesson
-
-    }
-}
