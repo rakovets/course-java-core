8aaac88b63367fdae0bb7acf8e8cb6c3b0a64659
RomaHoncharuk <orgtexby1@gmail.com>
2020-11-08 16:52:22 +0300

    1

--- src/test/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtilTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtilTest.java
@@ -4,15 +4,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileAnalyzeUtilTest {
 
     @Test
     void Task1() {
+        List<String> list = Arrays.asList("Honcharuk", "Roman", "Viktorovich");
         String sep = File.separator;
         String pathFile = "src" + sep + "test" + sep + "io" + sep + "String";
-//        String pathFile = "C:\\home\\git\\course-java-basics\\src\\test\\io\\String";
-        Assertions.assertEquals("123", FileAnalyzeUtil.getStrings(pathFile));
+        Assertions.assertEquals(list, FileAnalyzeUtil.getStrings(pathFile));
     }
-
 }
+
+
