cd314372f89a96b09d1a89d844378128cd9c6ca5
RomaHoncharuk <orgtexby1@gmail.com>
2020-11-08 16:12:07 +0300

    1

--- src/test/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtilTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/javaio/FileAnalyzeUtilTest.java
@@ -1,9 +1,18 @@
 package com.rakovets.course.javabasics.practice.javaio;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
 
 public class FileAnalyzeUtilTest {
 
     @Test
+    void Task1() {
+//        String sep = File.separator;
+//        String pathFile = "src" + sep + "test" + sep + "io" + sep + "String.txt";
+        String pathFile = "C/home/git/course-java-basics/scr/test/io/String.txt";
+        Assertions.assertEquals("123", FileAnalyzeUtil.getStrings(pathFile));
+    }
 
 }
