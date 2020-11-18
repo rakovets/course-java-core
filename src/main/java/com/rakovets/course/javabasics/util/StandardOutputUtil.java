9c808f547161e02dec334256dd37b2357ba3b7b7
Dmitry Rakovets <dmitryrakovets@gmail.com>
2020-11-18 21:54:03 +0300

    refactor: apply .editconfig for all

--- src/main/java/com/rakovets/course/javabasics/util/StandardOutputUtil.java
+++ src/main/java/com/rakovets/course/javabasics/util/StandardOutputUtil.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public abstract class StandardOutputUtil {
-    public static void println(Object string,  AnsiColorCode... codes) {
+    public static void println(Object string, AnsiColorCode... codes) {
         System.out.printf("%s%s%s\n", getAnsiColor(codes), string, AnsiColorCode.RESET);
     }
 
-    public static void printlnWithTime(Object string,  AnsiColorCode... codes) {
+    public static void printlnWithTime(Object string, AnsiColorCode... codes) {
         System.out.printf("%s%s - %s%s\n", getAnsiColor(codes), LocalDateTime.now(), string, AnsiColorCode.RESET);
     }
 
-    public static void printlnWithTimeAndThread(Object string,  AnsiColorCode... codes) {
+    public static void printlnWithTimeAndThread(Object string, AnsiColorCode... codes) {
         System.out.printf("%s%s - %s - %s%s\n", getAnsiColor(codes), LocalDateTime.now(), Thread.currentThread().getName(), string, AnsiColorCode.RESET);
     }
 
