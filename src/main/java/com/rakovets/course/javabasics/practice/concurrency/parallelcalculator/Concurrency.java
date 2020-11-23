e22b560bc06445d18db1bdbc82aa3c10363a113d
dimazm@live.com <Pvfxbycrbq1981>
2020-11-23 23:06:22 +0300

    Threads Calc

---
+++ src/main/java/com/rakovets/course/javabasics/practice/concurrency/parallelcalculator/Concurrency.java
@@ -0,0 +1,49 @@
+package com.rakovets.course.javabasics.practice.concurrency.parallelcalculator;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Concurrency {
+
+ //   public HashMap<Arrays, Integer > getMaxElementFromArray(LinkedList<Arrays> arraysCollection) {
+
+
+  //  } return;
+
+    public static int getLenghtForArray(int i) {
+
+        int lenght = i;
+        lenght += ((int) (Math.random() * 139986) - 12);
+        if (lenght > 1000000) {
+            lenght = 999999 / 3 + 130 - i;
+        }
+        return lenght;
+    }
+
+    public static int getElementForArray(int i) {
+
+        int element = i;
+        element = ((int) (Math.random() * 34) - i);
+        if (element > 300) {
+            element = 300 / 7 + 130 + i;
+        }
+        return element;
+    }
+
+    public static LinkedList<int[]> getArraysList(){
+
+        LinkedList<int[]> arrayList = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            int lenght = Concurrency.getLenghtForArray(i);
+            int[] tempArray = new int[lenght];
+            System.out.println("Lenght = " + lenght);
+            for (int j = 0; j < tempArray.length; j++) {
+                int element = Concurrency.getElementForArray(i);
+                tempArray[i] = element;
+            }
+            arrayList.add(tempArray);
+            tempArray = null;
+        }
+        return arrayList;
+    }
+}
---
+++ src/test/java/com/rakovets/course/javabasics/practice/concurrency/CalculatorDemo.java
@@ -0,0 +1,31 @@
+package com.rakovets.course.javabasics.practice.concurrency;
+
+import com.rakovets.course.javabasics.practice.concurrency.parallelcalculator.Concurrency;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class CalculatorDemo {
+    public static void main(String[] args) {
+
+        LinkedList<int[]> arrayList = Concurrency.getArraysList();
+        System.out.println(arrayList);
+
+      // for (int i = 0; i < 10; i++) {
+      //     int lenght = Concurrency.getLenghtForArray(i);
+      //     int[] tempArray = new int[lenght];
+      //     System.out.println("Lenght = " + lenght);
+      //     for (int j = 0; j < tempArray.length; j++) {
+      //         int element = Concurrency.getElementForArray(i);
+      //         tempArray[i] = element;
+      //     }
+      //     arrayList.add(tempArray);
+      //     System.out.println(arrayList);
+      //     tempArray = null;
+      // }
+
+
+
+
+    }
+}
