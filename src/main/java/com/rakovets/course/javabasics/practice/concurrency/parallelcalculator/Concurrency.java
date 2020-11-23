4e81e8cf005e1a20cb694b862264e787ba59ca8b
dimazm@live.com <Pvfxbycrbq1981>
2020-11-24 00:58:37 +0300

    Threads task 1 Done

--- src/main/java/com/rakovets/course/javabasics/practice/concurrency/parallelcalculator/Concurrency.java
+++ src/main/java/com/rakovets/course/javabasics/practice/concurrency/parallelcalculator/Concurrency.java
@@ -12,8 +12,8 @@ public class Concurrency {
         for (int[] array : arraysCollection) {
             int max = array[0];
             for (int i = 0; i < array.length - 1; i++) {
-                if (max < array[i + 1]) {
-                    max = array[i + 1];
+                if (max < array[i]) {
+                    max = array[i];
                 }
             }
             resultList.put(array, max);
@@ -21,22 +21,23 @@ public class Concurrency {
         return resultList;
     }
 
-    public static int getLenghtForArray(int i) {
 
-        int lenght = i;
-        lenght += ((int) (Math.random() * 36798) - 2);
+    public static int getLengthForArray(int i) {
+
+        int lenght;
+        lenght = ((int) (Math.random() * 363878));
         if (lenght > 1000000) {
-            lenght = 999999 / 3 + 130 - i;
+            lenght = 999999 - 84573 / (i+1);
         }
         return lenght;
     }
 
     public static int getElementForArray(int i) {
 
-        int element = i;
-        element = ((int) (Math.random() * 34));
+        int element;
+        element = ((int) (Math.random() * (141 + 1)) - 17) / (i + 1);
         if (element > 300) {
-            element = 300 / 7 + 130 + i;
+            element = 300 / 2 + 16 / i;
         }
         return element;
     }
@@ -45,11 +46,11 @@ public class Concurrency {
 
         LinkedList<int[]> arrayList = new LinkedList<>();
         for (int i = 0; i < 10; i++) {
-            int lenght = Concurrency.getLenghtForArray(i);
+            int lenght = Concurrency.getLengthForArray(i);
             int[] tempArray = new int[lenght];
-            for (int j = 0; j < tempArray.length; j++) {
+            for (int j = 0; j < lenght; j++) {
                 int element = Concurrency.getElementForArray(i);
-                tempArray[i] = element;
+                tempArray[j] = element;
             }
             arrayList.add(tempArray);
             tempArray = null;
