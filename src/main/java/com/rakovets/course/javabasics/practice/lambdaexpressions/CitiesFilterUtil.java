902378b0cdbc107c25cfca3f8d53f4999fa29c33
GreyMatterImpulsion <smaroleg@gmail.com>
2020-11-28 11:08:48 +0300

    Lambda Expression 1 is Done, 2 not

--- src/main/java/com/rakovets/course/javabasics/practice/lambdaexpressions/CitiesFilterUtil.java
+++ src/main/java/com/rakovets/course/javabasics/practice/lambdaexpressions/CitiesFilterUtil.java
@@ -5,16 +5,25 @@ import java.util.ArrayList;
 public class CitiesFilterUtil {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<>(){{
-            add("Tokyo");
-            add("Minsk");
+            add("Belgrade");
+            add("Minsk  ");
             add("Moscow");
             add("Kiev");
+            add("London");
+            add("New York");
+            add("Sydney");
+            add("St.Petersburg");
+            add("Dresden");
+            add("Hamburg");
+            add("Berlin");
+            add("Potsdam");
+            add("Essen");
+            add("Leipzig");
+            add("Stuttgart");
+            add("Konigsberg");
+            add("Minsk");
         }};
         list.stream()
                 .forEach(System.out::println);
          }
-
-
-
-
 }
