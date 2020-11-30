813090976ca820bce984075974d09184ec5eedfb
Julia Portnova <portnova.julia@gmail.com>
2020-11-30 11:57:57 +0300

    feat: Math's statistics, City Helper done

--- src/main/java/com/rakovets/course/javabasics/practice/lambdaexpressions/CityHelper.java
+++ src/main/java/com/rakovets/course/javabasics/practice/lambdaexpressions/CityHelper.java
@@ -1,4 +1,27 @@
 package com.rakovets.course.javabasics.practice.lambdaexpressions;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CityHelper {
+    public static List<String> getUniqueCities(List<String> list) {
+        return list.stream().
+                distinct().collect(Collectors.toList());
+    }
+    public static List<String> getCitiesWithLongName(List<String> list) {
+        return list.stream().
+                filter(city -> city.length() > 6).
+                collect(Collectors.toList());
+    }
+    public static List<String> getCitiesNamesStartedWithLetter (List<String> list, char letter) {
+        return list.stream().
+                distinct().
+                filter(city -> city.charAt(0) == letter).
+                collect(Collectors.toList());
+    }
+    public static long getCountCityInList(List<String> list, String city) {
+        return list.stream().
+                filter(cityInStream -> cityInStream.equals(city)).
+                count();
+    }
 }
---
+++ src/test/java/com/rakovets/course/javabasics/practice/lambdaexpressions/CityHelperTest.java
@@ -0,0 +1,38 @@
+package com.rakovets.course.javabasics.practice.lambdaexpressions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class CityHelperTest {
+    private static List<String> cities;
+    private static List<String> uniqueCities;
+    private static List<String> citiesWithLongName;
+    private static List<String> citiesNamesStartedWithM;
+    @BeforeAll
+    static void init() {
+        cities = List.of("London", "Minsk", "Berlin", "Moscow", "Oslo", "Boston", "Minsk", "London", "Moscow");
+        uniqueCities = List.of("London", "Minsk", "Berlin", "Moscow", "Oslo", "Boston");
+        citiesWithLongName = List.of();
+        citiesNamesStartedWithM = List.of("Minsk", "Moscow");
+
+    }
+    @Test
+    void getUniqueCitiesTest() {
+        Assertions.assertEquals(uniqueCities, CityHelper.getUniqueCities(cities));
+    }
+    @Test
+    void getCitiesWithLongNameTest() {
+        Assertions.assertEquals(citiesWithLongName, CityHelper.getCitiesWithLongName(cities));
+    }
+    @Test
+    void getCitiesNamesStartedWithLetterTest() {
+        Assertions.assertEquals(citiesNamesStartedWithM, CityHelper.getCitiesNamesStartedWithLetter(cities, 'M'));
+    }
+    @Test
+    void getCountCityInListTest() {
+        Assertions.assertEquals(2, CityHelper.getCountCityInList(cities, "Minsk"));
+    }
+}
