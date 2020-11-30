37e53c2ceb2c00907c717fce3c6ce0420162de62
dimazm@live.com <Pvfxbycrbq1981>
2020-11-30 22:47:46 +0300

    data Task1

--- src/main/java/com/rakovets/course/javabasics/practice/dateandtype/DateWrapper.java
+++ src/main/java/com/rakovets/course/javabasics/practice/dateandtype/DateWrapper.java
@@ -1,6 +1,10 @@
 package com.rakovets.course.javabasics.practice.dateandtype;
 
+import javax.swing.text.DateFormatter;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class DateWrapper {
 
@@ -13,4 +17,20 @@ public class DateWrapper {
 
         return current.plusMonths(month);
     }
+
+    public static String getDateUsePattern(LocalDate current, String format){
+
+        DateTimeFormatter form = DateTimeFormatter.ofPattern(format);
+          return current.format(form);
+    }
+
+    public static LocalDateTime getDateUsePatternString(String date, DateTimeFormatter formatter){
+
+        return LocalDateTime.parse(date, formatter);
+    }
+
+    public static Period getPeriodBetweenMonth(LocalDate d1, LocalDate d2){
+        return Period.between(d1,d2);
+    }
+
 }
--- src/test/java/com/rakovets/course/javabasics/practice/dateandtype/DateDemo.java
+++ src/test/java/com/rakovets/course/javabasics/practice/dateandtype/DateDemo.java
@@ -1,17 +1,35 @@
 package com.rakovets.course.javabasics.practice.dateandtype;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Date;
 
 public class DateDemo {
     public static void main(String[] args) {
 
         LocalDate currentDate = DateWrapper.getLocaleDate(1981,12,05);
+        LocalDate currentDate2 = DateWrapper.getLocaleDate(1983,10,07);
 
-        System.out.println("Current Date" + currentDate + "\n");
+        System.out.println("Current Date  " + currentDate + "\n");
         int n = 3;
-        System.out.println("Current Date + " + n + DateWrapper.getLocaleDatePlusN(currentDate, n));
+        System.out.println("Current Date + " + n + " " + DateWrapper.getLocaleDatePlusN(currentDate, n));
+
+        String format = "dd / M / yyyy";
+        String result = DateWrapper.getDateUsePattern(currentDate,format);
+        System.out.println(result);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM, yyyy HH:mm");
+        String date = "3 September, 2018 12:10";
+        LocalDateTime resultDate = DateWrapper.getDateUsePatternString(date, formatter);
+        System.out.println("last " + resultDate.toString());
+
+        System.out.println("Delta " + DateWrapper.getPeriodBetweenMonth(currentDate,currentDate2));
+
+
+
 
     }
 }
