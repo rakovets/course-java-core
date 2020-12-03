b6899586b38ead6471fcaa1ad94ac9bc96544b6f
Julia Portnova <portnova.julia@gmail.com>
2020-12-03 17:56:37 +0300

    feat: task1 done

--- src/main/java/com/rakovets/course/javabasics/practice/dateandtype/DateWrapper.java
+++ src/main/java/com/rakovets/course/javabasics/practice/dateandtype/DateWrapper.java
@@ -7,23 +7,46 @@ import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 
 public class DateWrapper {
     public LocalDate getDate(int year, int month, int day) {
         return LocalDate.of(year, month, day);
     }
+
     public LocalDate getNextDate(LocalDate localDate, int numberOfMonth) {
         return localDate.plusMonths(numberOfMonth);
     }
+
     public String getDateFormat(LocalDate localDate, String formatter) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(formatter);
         return localDate.format(dateTimeFormatter);
     }
+
     public LocalDate getLocalDate(String date, String formatter) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(formatter);
-            return LocalDate.parse(date, dateTimeFormatter);
+        return LocalDate.parse(date, dateTimeFormatter);
     }
+
     public long getDaysBetweenDates(LocalDate firstDate, LocalDate secondDate) {
         return ChronoUnit.DAYS.between(firstDate, secondDate);
+    }
+
+    public LocalDate changeDate(LocalDate localDate, int days) {
+        TemporalAdjuster temporalAdjuster = temporal -> temporal.plus(Period.ofDays(days));
+        return localDate.with(temporalAdjuster);
+    }
+
+    public LocalDate changeDateToNearest1stJanuary(LocalDate localDate) {
+        LocalDate firstDayOfTheYear = localDate.with(TemporalAdjusters.firstDayOfYear());
+        LocalDate firstDayOfTheNextYear = localDate.with(TemporalAdjusters.firstDayOfNextYear());
+        TemporalAdjuster temporalAdjuster = temporal -> {
+            if (getDaysBetweenDates(localDate, firstDayOfTheNextYear) <
+                    (getDaysBetweenDates(firstDayOfTheYear, localDate))) {
+                temporal = firstDayOfTheNextYear;
+            } else {
+                temporal = firstDayOfTheYear;
+
     }
 }
