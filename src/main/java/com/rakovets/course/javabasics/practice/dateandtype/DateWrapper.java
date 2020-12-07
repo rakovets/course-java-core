ca948e82125eff5d1dce4851cb1c9dec808c2b38
Polina Ushakova <polushakova@mail.ru>
2020-12-07 21:26:07 +0300

    feat: add solutions.

---
+++ src/main/java/com/rakovets/course/javabasics/practice/dateandtype/DateWrapper.java
@@ -0,0 +1,53 @@
+package com.rakovets.course.javabasics.practice.dateandtype;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateWrapper {
+    public LocalDate getLocalDate(int year, int month, int day) {
+        return LocalDate.of(year, month, day);
+    }
+
+    public LocalDate getDateAfterPeriod(LocalDate localDate, int month) {
+        return localDate.plusMonths(month);
+    }
+
+    public Date getDateAfterPeriod(Calendar calendar, int month) {
+        calendar.add(Calendar.MONTH, month);
+        return calendar.getTime();
+    }
+
+    public String getDateByPattern(LocalDate localDate, String pattern) {
+        DateTimeFormatter dataFormat = DateTimeFormatter.ofPattern(pattern);
+        return localDate.format(dataFormat);
+    }
+
+    public String getDateByPattern(Date date, String pattern) {
+        DateFormat dataFormat = new SimpleDateFormat(pattern);
+        return dataFormat.format(date);
+    }
+
+    public String getDateByPattern(Calendar calendar, String pattern) {
+        DateFormat dataFormat = new SimpleDateFormat(pattern);
+        return dataFormat.format(calendar.getTime());
+    }
+
+    public LocalDate getDate(String date, String pattern) {
+        DateTimeFormatter dataFormat = DateTimeFormatter.ofPattern(pattern);
+        return LocalDate.parse(date, dataFormat);
+    }
+
+    public long getDifferenceBetweenLocalDate(LocalDate localDate1, LocalDate localDate2) {
+        return ChronoUnit.DAYS.between(localDate1, localDate2);
+    }
+
+    public static LocalDate getDateAfterNDays(LocalDate localDate, int n) {
+        return localDate.with(temporal -> temporal.plus(Period.ofDays(n)));
+    }
+}
---
+++ src/test/java/com/rakovets/course/javabasics/practice/dateandtype/DateWrapperTest.java
@@ -0,0 +1,67 @@
+package com.rakovets.course.javabasics.practice.dateandtype;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DateWrapperTest {
+    private DateWrapper dateWrapper;
+    private LocalDate firstDate;
+    private LocalDate secondDate;
+    private Date firstDateInCalendar;
+    private Calendar calendar;
+    private String dateFormat;
+    private Date firstDateByPattern;
+    private LocalDate secondDateByPattern;
+    private LocalDate thirdDate;
+
+    @BeforeEach
+    void init() {
+        dateWrapper = new DateWrapper();
+        dateFormat = "dd-MM-yyyy";
+        firstDate = LocalDate.of(1000, 10, 10);
+        secondDate = LocalDate.of(1000, 12, 10);
+        thirdDate = LocalDate.of(1000,12,23);
+        firstDateInCalendar = new GregorianCalendar(1000, 12, 10).getTime();
+        firstDateByPattern = new GregorianCalendar(1000,10,10).getTime();
+        secondDateByPattern = LocalDate.of(1000,10,10);
+        calendar = new GregorianCalendar(1000, 10,10);
+    }
+
+    @Test
+    void getLocalDateTest() {
+        assertEquals(firstDate, dateWrapper.getLocalDate(1000, 10, 10));
+    }
+
+    @Test
+    void getDateAfterPeriodTest() {
+        assertEquals("1000-10-10", dateWrapper.getDateAfterPeriod(secondDate, -2).toString());
+        assertEquals(firstDateInCalendar, dateWrapper.getDateAfterPeriod(calendar, 2));
+    }
+
+    @Test
+     void getDateByPatternTest() {
+        assertEquals("10-11-1000", dateWrapper.getDateByPattern(firstDateByPattern, dateFormat));
+        assertEquals("10-10-1000", dateWrapper.getDateByPattern(secondDateByPattern, dateFormat));
+        assertEquals("10-11-1000", dateWrapper.getDateByPattern(calendar,dateFormat));
+    }
+
+    @Test
+    void  getDateTest() {
+        assertEquals(firstDate, dateWrapper.getDate("10-10-1000", dateFormat));
+    }
+
+    @Test
+    void getDifferenceBetweenLocalDateTest() {
+        assertEquals(13, dateWrapper.getDifferenceBetweenLocalDate(secondDate, thirdDate));
+    }
+
+    @Test
+    void getDateAfterNDaysTest() {
+        assertEquals("1000-10-24", dateWrapper.getDateAfterNDays(firstDate, 14).toString());
+    }
+
+}
