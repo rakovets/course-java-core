971d897fde5aa30bdd9fa8b045d28ee9073c6fad
Dmitry Sedun <sedun.dima@ya.ru>
2020-12-14 09:00:06 +0300

    add feature for Task operators

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task01.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task01.java
@@ -22,5 +22,7 @@ public class Task01 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        int result = cargo / carryingCapacity;
+        System.out.println(result);
     }
 }
