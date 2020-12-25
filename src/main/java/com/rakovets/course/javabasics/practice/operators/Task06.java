ea5f2286f2fbefb5943fb0d6450dad0439d27931
OxanaPeregud <ox.peregud@gmail.com>
2020-12-25 17:32:26 +0300

    solution: Task06 for theme 'Operators'

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task06.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task06.java
@@ -11,7 +11,7 @@ public class Task06 {
     public static void main(String[] args) {
         //FIXME
         // Переменные, которые можно изменять для проверки различных вариантов входных параметров
-        int playingTimeInSeconds = 156;
+        int playingTimeInSeconds = 212;
 
         // Код необходимый для тестирования, не изменять
         playingTimeInSeconds = (args.length == 1) ? Integer.parseInt(args[0]) : playingTimeInSeconds;
@@ -19,5 +19,13 @@ public class Task06 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+
+        int nDays = playingTimeInSeconds / 86400;
+        playingTimeInSeconds = playingTimeInSeconds - (nDays * 86400);
+        int nHours = playingTimeInSeconds / 3600;
+        playingTimeInSeconds = playingTimeInSeconds - (nHours * 3600);
+        int nMinutes = playingTimeInSeconds / 60;
+        playingTimeInSeconds = playingTimeInSeconds - (nMinutes * 60);
+        System.out.print(nDays + " " + nHours + ":" + nMinutes + ":" + playingTimeInSeconds);
     }
 }
