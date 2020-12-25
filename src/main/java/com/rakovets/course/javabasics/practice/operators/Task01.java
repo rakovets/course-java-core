abef644b2db4c6f41cbab837d079d5522cb71126
Eugenoil <e.dunin@bk.ru>
2020-12-26 01:09:47 +0300

    solution: task 1 for theme 'Operators'

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task01.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task01.java
@@ -12,8 +12,8 @@ public class Task01 {
     public static void main(String[] args) {
         //FIXME
         // Переменные, которые можно изменять для проверки различных вариантов входных параметров
-        int cargo = 1;
-        int carryingCapacity = 2;
+        int cargo = 120;
+        int carryingCapacity = 24;
 
         // Код необходимый для тестирования, не изменять
         cargo = (args.length == 0) ? cargo : Integer.parseInt(args[0]);
@@ -22,5 +22,7 @@ public class Task01 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        int cargovalue = cargo/carryingCapacity;
+        System.out.print(cargovalue);
     }
 }
