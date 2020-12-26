b501cfaa699c38cc61e5ac62128e848f366889c5
Igrok023451 <hommeworkit@gmail.com>
2020-12-26 14:03:08 +0300

    fix: tasks for theme 'Intro'

---
+++ src/main/java/com/rakovets/course/javabasics/practice/intro/Task2.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.javabasics.practice.intro;
+
+public class Task2 {
+	public static void main(String[] args) {
+		System.out.println("Java: version 11.");
+		System.out.print("JVM: HotSpot.");
+	}
+}
