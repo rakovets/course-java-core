6e7716e80d0aab401bb4eab698e910b0de5eb64c
Igrok023451 <hommeworkit@gmail.com>
2020-12-27 14:33:13 +0300

    solution: task 5 for theme 'Operators'

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task05.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task05.java
@@ -14,8 +14,8 @@ public class Task05 {
     public static void main(String[] args) {
         //FIXME
         // Переменные, которые можно изменять для проверки различных вариантов входных параметров
-        int height = 186;
-        int weight = 86;
+        int height = 195;
+        int weight = 74;
 
         // Код необходимый для тестирования, не изменять
         height = (args.length == 2) ? Integer.parseInt(args[0]) : height;
@@ -24,5 +24,6 @@ public class Task05 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        System.out.print(weight - (height - 110));
     }
 }
