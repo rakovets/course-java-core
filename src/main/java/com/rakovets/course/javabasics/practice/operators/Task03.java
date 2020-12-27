904a3df4167408a4fd043dece9b6c69c54eb6dd3
Eugenoil <e.dunin@bk.ru>
2020-12-28 00:48:11 +0300

    solution: task 3 for theme 'Operators'

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task03.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task03.java
@@ -14,6 +14,7 @@ public class Task03 {
         // Переменные, которые можно изменять для проверки различных вариантов входных параметров
         int deposit = 1500;
         int annualPercentage = 3;
+        final int YEARS = 5;
 
         // Код необходимый для тестирования, не изменять
         deposit = (args.length == 0) ? deposit : Integer.parseInt(args[0]);
@@ -22,7 +23,7 @@ public class Task03 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
-        float finalsum = deposit + (deposit * annualPercentage/100)*5;
+        float finalsum = deposit + (deposit * annualPercentage/100)*YEARS;
         System.out.print(finalsum);
     }
 }
