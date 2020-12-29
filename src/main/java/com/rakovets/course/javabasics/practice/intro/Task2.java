670ab6a24e1220d80490c413e274f49476e3302d
Germanand <70859000+Germanand@users.noreply.github.com>
2020-12-29 20:58:13 +0300

    solution: tasks 1-2 for theme 'Intro'

--- src/main/java/com/rakovets/course/javabasics/practice/intro/Task2.java
+++ src/main/java/com/rakovets/course/javabasics/practice/intro/Task2.java
@@ -2,7 +2,7 @@ package com.rakovets.course.javabasics.practice.intro;
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println( "Java : version 11." );
-        System.out.println( "JVM: HotSpot." );
+        System.out.println("Java: version 11.");
+        System.out.println("JVM: HotSpot.");
     }
 }
