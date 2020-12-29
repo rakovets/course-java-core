bf58a8a341ef81bd207d1e536af447b9fec79cdf
igor <gob1in@Mail.ru>
2020-12-29 21:03:04 +0300

    solution: done task 1 for theme Conditional statement

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task01.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task01.java
@@ -21,7 +21,7 @@ public class Task01 extends StandardInputTask {
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
         int sizeTop = 15;
-        int positionPlayerByDamage = 5;
+        int positionPlayerByDamage = 13;
 
         boolean topPlayer = isTopPlayer(sizeTop, positionPlayerByDamage);
         System.out.printf("Result: %b", topPlayer);
@@ -40,6 +40,6 @@ public class Task01 extends StandardInputTask {
     static boolean isTopPlayer(int sizeTop, int positionPlayerByDamage) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return false;
+        return sizeTop >= positionPlayerByDamage;
     }
 }
