f00c8a4b74a6e63ef98554f73df30ef14742a4e3
sergey.grinkevich <serg.grinkevich@gmail.com>
2020-12-29 14:52:49 +0300

    solution: Task 1 for theme 'Operators'

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task01.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task01.java
@@ -12,8 +12,8 @@ public class Task01 {
     public static void main(String[] args) {
         //FIXME
         // Переменные, которые можно изменять для проверки различных вариантов входных параметров
-        int cargo = 1;
-        int carryingCapacity = 2;
+        int cargo = 125;
+        int carryingCapacity = 23;
 
         // Код необходимый для тестирования, не изменять
         cargo = (args.length == 0) ? cargo : Integer.parseInt(args[0]);
