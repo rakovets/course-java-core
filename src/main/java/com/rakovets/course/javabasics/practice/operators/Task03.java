11a33ed5a1598a67eb0e8b5c5030e3da0f4f048e
Андрей <andrei.veg@gmail.com>
2020-12-31 11:31:29 +0300

    solutions: Tasks 1-6. theme "operators". Exept 3,6

--- src/main/java/com/rakovets/course/javabasics/practice/operators/Task03.java
+++ src/main/java/com/rakovets/course/javabasics/practice/operators/Task03.java
@@ -12,8 +12,8 @@ public class Task03 {
     public static void main(String[] args) {
         //FIXME
         // Переменные, которые можно изменять для проверки различных вариантов входных параметров
-        int deposit = 1500;
-        int annualPercentage = 3;
+        int deposit = 100;
+        int annualPercentage = 1;
 
         // Код необходимый для тестирования, не изменять
         deposit = (args.length == 0) ? deposit : Integer.parseInt(args[0]);
@@ -22,5 +22,6 @@ public class Task03 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используяся переменные объявленные выше (их можно изменять)
         // Для проверки решения необходимо запустить @Test для данного class (в директории test)
+        System.out.print(deposit+(deposit*annualPercentage));
     }
 }
