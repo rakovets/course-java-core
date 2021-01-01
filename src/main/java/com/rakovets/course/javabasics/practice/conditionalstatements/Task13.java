31c7531785745a6682976d6c0af3f922c9ef5997
Alex Rudoi <rudenw01@gmail.com>
2021-01-01 23:37:36 +0300

    solution: task13 for theme 'conditional-statements'

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task13.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task13.java
@@ -15,9 +15,9 @@ public class Task13 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int numberFloors = 9;
-        int numberApartmentsPerFloor = 4;
-        int apartmentNumber = 68;
+        int numberFloors = 20;
+        int numberApartmentsPerFloor = 30;
+        int apartmentNumber = 31;
 
         int floorNumber = getFloorNumber(numberFloors, numberApartmentsPerFloor, apartmentNumber);
         System.out.printf("Result: %s", floorNumber);
@@ -34,6 +34,15 @@ public class Task13 {
     static int getFloorNumber(int numberFloors, int numberApartmentsPerFloor, int apartmentNumber) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int a = numberFloors * numberApartmentsPerFloor;
+        System.out.println("numberApartmentsPerEntrance " + a);
+
+        if (apartmentNumber == a) return numberFloors;
+        if (apartmentNumber < a)  return apartmentNumber / numberApartmentsPerFloor + 1;
+
+        int b = apartmentNumber / a;
+        int c = apartmentNumber - b * a;
+        return c / numberApartmentsPerFloor + 1;
+
     }
 }
