9d54ec7352525df9c2771a7a38fdb450270b3864
Igrok023451 <76533814+Igrok023451@users.noreply.github.com>
2021-01-01 21:06:38 +0300

    Done Task5

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task05.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task05.java
@@ -2,6 +2,8 @@ package com.rakovets.course.javabasics.practice.conditionalstatements;
 
 import com.rakovets.course.javabasics.util.StandardInputTask;
 
+import java.time.Month;
+
 /**
  * Разработать программу для игрового движка.
  * <p>
@@ -20,7 +22,7 @@ public class Task05 extends StandardInputTask {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        byte monthNumber = 1;
+        byte monthNumber = 9;
 
         String monthName = getMonthName(monthNumber);
         System.out.printf("Result: %s", monthName);
@@ -34,7 +36,34 @@ public class Task05 extends StandardInputTask {
      */
     static String getMonthName(byte monthNumber) {
         //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        // Код, решающий задачу пишем ниже, при этом используя параметры метод
+        switch (monthNumber) {
+            case 1:
+                return Month.JANUARY.toString();
+            case 2:
+                return Month.FEBRUARY.toString();
+            case 3:
+                return Month.MARCH.toString();
+            case 4:
+                return Month.APRIL.toString();
+            case 5:
+                return Month.MAY.toString();
+            case 6:
+                return Month.JUNE.toString();
+            case 7:
+                return Month.JULY.toString();
+            case 8:
+                return Month.AUGUST.toString();
+            case 9:
+                return Month.SEPTEMBER.toString();
+            case 10:
+                return Month.OCTOBER.toString();
+            case 11:
+                return Month.NOVEMBER.toString();
+            case 12:
+                return Month.DECEMBER.toString();
+            default:
+                return null;
+        }
     }
 }
