08e42b06b8a1b7c51c99de3d242b206b33fb5958
Alex Rudoi <rudenw01@gmail.com>
2021-01-01 23:36:04 +0300

    solution: task11 for theme 'conditional-statements'

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
@@ -25,7 +25,7 @@ public class Task11 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        char symbol = 'а';
+        char symbol = 'V';
 
         String monthName = getSymbolType(symbol);
         System.out.printf("Result: %s", monthName);
@@ -33,13 +33,18 @@ public class Task11 {
 
     /**
      * Определяет к какому типу относится данный символ.
-     *
+     *     return "latin";
      * @param symbol символ
      * @return <code>latin</code>/<code>cyrillic</code>/<code>digit</code>/<code>undefined</code>
      */
     static String getSymbolType(char symbol) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        if ((symbol >= 65 & symbol <= 90) | (symbol >= 97 & symbol <= 122))  return "latin";
+        if (symbol >= 48 & symbol <= 57) return "digit";
+        else if (symbol >= 1024 & symbol <= 1327) return "cyrillic";
+        else if (symbol >= 65520 & symbol <= 65535) return "special character";
+        else return "undefined";
+
     }
 }
