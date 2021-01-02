713ff74ecc58b281a194f3db016ac217efda8cfb
Semargl92 <gavriljuk.9292@gmail.com>
2021-01-02 23:00:10 +0300

    solution: task 11 for theme 'conditional statements'

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
@@ -25,7 +25,7 @@ public class Task11 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        char symbol = 'а';
+        char symbol = 'N';
 
         String monthName = getSymbolType(symbol);
         System.out.printf("Result: %s", monthName);
@@ -40,6 +40,17 @@ public class Task11 {
     static String getSymbolType(char symbol) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+
+        if (symbol>='a'&& symbol<='z' || symbol>='A'&& symbol<='Z') {
+            return "latin";
+        }
+        if (symbol>='а'&& symbol<='я' || symbol>='А'&& symbol<='Я') {
+            return "cyrillic";
+        }
+        if (symbol>='0'&& symbol<='9') {
+            return "digit";
+        } else {
+            return "undefined";
+        }
     }
 }
