989cd9bb48d55f2d5ccf4e31744c9e669276b594
sergey.grinkevich <serg.grinkevich@gmail.com>
2021-01-03 01:58:38 +0300

    solution: Task13 for theme Conditional statements

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task13.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task13.java
@@ -17,7 +17,7 @@ public class Task13 {
         // аргументов. Типы данных изменять нельзя
         int numberFloors = 9;
         int numberApartmentsPerFloor = 4;
-        int apartmentNumber = 68;
+        int apartmentNumber = 169;
 
         int floorNumber = getFloorNumber(numberFloors, numberApartmentsPerFloor, apartmentNumber);
         System.out.printf("Result: %s", floorNumber);
@@ -34,6 +34,8 @@ public class Task13 {
     static int getFloorNumber(int numberFloors, int numberApartmentsPerFloor, int apartmentNumber) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int totalApartments = numberFloors*numberApartmentsPerFloor;
+        return  (apartmentNumber -1) % totalApartments/numberApartmentsPerFloor+1;
+
     }
 }
