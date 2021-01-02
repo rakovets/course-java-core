747ce374a09ce017733ff4c366781f1b8df48a95
Eugenoil <e.dunin@bk.ru>
2021-01-02 23:55:27 +0300

    solution: task 5 for theme 'Conditional statements'

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task05.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task05.java
@@ -22,7 +22,7 @@ public class Task05 extends StandardInputTask {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        byte monthNumber = 5;
+        byte monthNumber = 6;
 
 
         String monthName = getMonthName(monthNumber);
