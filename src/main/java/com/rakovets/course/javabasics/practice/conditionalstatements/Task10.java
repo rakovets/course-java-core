753f9da9a005ea6b2889b3e873b8bf7e380b97e7
sergey.grinkevich <serg.grinkevich@gmail.com>
2021-01-02 22:21:45 +0300

    solution: Task10 for theme Conditional statements

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task10.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task10.java
@@ -15,9 +15,9 @@ public class Task10 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int firstFuelReserves = 100;
-        int secondFuelReserves = 50;
-        int thirdFuelReserves = 70;
+        int firstFuelReserves = 5;
+        int secondFuelReserves = 6;
+        int thirdFuelReserves = 3;
 
         int numberStation = getNumberStation(firstFuelReserves, secondFuelReserves, thirdFuelReserves);
         System.out.printf("Result: %s", numberStation);
@@ -37,6 +37,12 @@ public class Task10 {
     static int getNumberStation(int firstFuelReserves, int secondFuelReserves, int thirdFuelReserves) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        if (firstFuelReserves <= secondFuelReserves && firstFuelReserves <= thirdFuelReserves) {
+            return 1;
+        } else if (secondFuelReserves <= thirdFuelReserves){
+            return 2;
+        } else {
+            return 3;
+        }
     }
 }
