eda19379357d416e377381d4469a7f9eb4317db9
Владислав Тушинский <xpomsteam1997@gmail.com>
2021-01-04 15:32:56 +0300

    fix: task 1-2 for theme 'intro'

--- src/main/java/com/rakovets/course/javabasics/practice/intro/Task2.java
+++ src/main/java/com/rakovets/course/javabasics/practice/intro/Task2.java
@@ -1,6 +1,6 @@
 package com.rakovets.course.javabasics.practice.intro;
 
-public class Task1 {
+public class Task2 {
     public static void main(String[] args) {
         System.out.println("Java: version 11.\nJVM: HotSpot.");
     }
