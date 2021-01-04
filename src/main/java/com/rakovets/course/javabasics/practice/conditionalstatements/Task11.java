751dcec39fd0927a5a660c2d3115bcf7aad124ae
Владислав Тушинский <xpomsteam1997@gmail.com>
2021-01-04 21:05:27 +0300

    solution: task11 for theme "conditional-statements"

--- src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
+++ src/main/java/com/rakovets/course/javabasics/practice/conditionalstatements/Task11.java
@@ -40,6 +40,14 @@ public class Task11 {
     static String getSymbolType(char symbol) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        if (symbol >= 48 && symbol <= 57) {
+            return "digit";
+        } else if (symbol >= 65 && symbol <= 90 || symbol >= 97 && symbol <= 122) {
+            return "latin";
+        } else if (symbol >= 1040 && symbol <= 1103) {
+            return "cyrillic";
+        } else {
+            return "undefined";
+        }
     }
 }
