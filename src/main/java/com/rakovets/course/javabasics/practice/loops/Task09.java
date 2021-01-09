ac21b2dc4dcf57347044f6360acdf49e6bb3ac67
OxanaPeregud <ox.peregud@gmail.com>
2021-01-09 14:21:15 +0300

    solution: Done Task09 for theme 'loops'

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task09.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task09.java
@@ -44,11 +44,31 @@ class Task09 {
      * @param differentialSell        скидка в процентах на всю покупку за приобретение дополнительных
      *                                <code>differentialNumberItems</code> товара
      * @return список цен, где формат вывода одной записи в списке цен:
-     * '${numberItems} - ${cost} with sell ${totalSell}%'
+     * '${numberItems} - ${cost} with sell ${totalSell}%'. Для ${cost} использовать точность до 2 знаков после
+     * вещественного разделителя:
+     * <code>BigDecimal.valueOf(currentPriceAllItemsWithSell).setScale(2, RoundingMode.HALF_UP).doubleValue()</code>
      */
     static String generateTotalPriceList(int startNumberItems, double startPriceAllItems, int differentialNumberItems, double differentialSell, int sizeTotalPrice) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+
+        StringBuilder totalPriceList = new StringBuilder();
+        double totalSell = 0;
+        double cost = 0;
+        double itemPrise = startPriceAllItems / startNumberItems;
+        for (int i = 1; i <= sizeTotalPrice; i++) {
+            if (i == 1) {
+                totalPriceList.toString();
+            } else
+            startNumberItems = startNumberItems + differentialNumberItems;
+            totalSell = differentialSell * (i - 1);
+            cost = startNumberItems * itemPrise - (startNumberItems * itemPrise * totalSell / 100);
+            cost = Math.round(100.0 * cost) / 100.0;
+            totalPriceList.append(startNumberItems).append(" - ").append(cost).append(" with sell ").append(totalSell).append("%").append("\n");
+        }
+        return totalPriceList.toString().replaceAll("[\n\r]$", "");
     }
 }
+
+
+
