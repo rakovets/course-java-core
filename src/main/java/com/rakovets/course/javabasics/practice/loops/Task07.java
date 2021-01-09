d8fd2c1b5da7ef52dcfa8cc5006c5def80d9d3cf
Dmitry Rakovets <dmitryrakovets@gmail.com>
2021-01-09 13:10:34 +0300

    fix: tasks and tests for theme 'Looping statements'

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task07.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task07.java
@@ -1,5 +1,8 @@
 package com.rakovets.course.javabasics.practice.loops;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Разработать программу для фитнес браслета.
  *
@@ -17,23 +20,25 @@ class Task07 {
         // аргументов. Типы данных изменять нельзя
         int startDistance = 1000;
         int finishDistance = 40000;
-        int dailyProgress = 5;
+        double dailyProgress = 5;
 
         double totalDistance = calculateTotalDistance(startDistance, finishDistance, dailyProgress);
         System.out.printf("Result: %f", totalDistance);
     }
 
     /**
-     * Рассчитывает пробег, который совершит спортсмен при подготовке к марафону. Известно:
+     * Рассчитывает пробег (т.е дистанцию, которую пробежал спортсмен за все тренировки), который совершит спортсмен
+     * при подготовке к марафону.
      *
-     * @param startDistance  дистанция которую пробегает спортсмен до начала тренировки
-     * @param finishDistance дистанция которую желает пробежать спортсмен после окончания тренировок
-     * @param dailyProgress  ежедневный прогресс в процентах по отношению к предыдущему забегу
-     * @return пробег (точность 10 метром)
+     * @param startDistance             дистанция которую пробегает спортсмен до начала тренировки
+     * @param finishDistance            дистанция которую желает пробежать спортсмен после окончания тренировок
+     * @param dailyProgressAsPercentage ежедневный прогресс в процентах по отношению к предыдущему забегу
+     * @return пробег (с точностью до 2 знаков после десятичного разделителя)
      */
-    static double calculateTotalDistance(int startDistance, int finishDistance, int dailyProgress) {
+    static double calculateTotalDistance(int startDistance, int finishDistance, double dailyProgressAsPercentage) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0.0;
+        double totalDistance = 0;
+        return BigDecimal.valueOf(totalDistance).setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 }
