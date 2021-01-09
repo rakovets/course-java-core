62183424039a180c7a352e7b290c7b16455ed1b5
tanya-java <termalickaya@gmail.com>
2021-01-09 17:34:22 +0300

    solution: task 1 for theme "Loops"

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task01.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task01.java
@@ -31,6 +31,10 @@ class Task01 {
     static String generateNumbersColumn(int numberRows) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
-    }
+        StringBuilder allRows = new StringBuilder();
+        for (int i = 1; i < numberRows; i++) {
+            allRows.append(i).append("\n");
+        }
+        return allRows.toString() + numberRows;
+            }
 }
