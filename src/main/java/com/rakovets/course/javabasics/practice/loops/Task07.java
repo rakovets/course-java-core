8cd95214bc4b1997df18117f733b852cb236cc9b
OxanaPeregud <ox.peregud@gmail.com>
2021-01-10 15:07:29 +0300

    solution: Fixed Task7 for theme 'loops'

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task07.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task07.java
@@ -1,5 +1,8 @@
 package com.rakovets.course.javabasics.practice.loops;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Разработать программу для фитнес браслета.
  *
@@ -15,23 +18,24 @@ class Task07 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int startDistance = 13;
-        int finishDistance = 20;
-        int dailyProgress = 5;
+        int startDistance = 1000;
+        int finishDistance = 40000;
+        double dailyProgress = 5;
 
         double totalDistance = calculateTotalDistance(startDistance, finishDistance, dailyProgress);
         System.out.printf("Result: %f", totalDistance);
     }
 
     /**
-     * Рассчитывает пробег, который совершит спортсмен при подготовке к марафону. Известно:
+     * Рассчитывает пробег (т.е дистанцию, которую пробежал спортсмен за все тренировки), который совершит спортсмен
+     * при подготовке к марафону.
      *
-     * @param startDistance  дистанция которую пробегает спортсмен до начала тренировки
-     * @param finishDistance дистанция которую желает пробежать спортсмен после окончания тренировок
-     * @param dailyProgress  ежедневный прогресс в процентах по отношению к предыдущему забегу
-     * @return пробег (точность 10 метром)
+     * @param startDistance             дистанция которую пробегает спортсмен до начала тренировки
+     * @param finishDistance            дистанция которую желает пробежать спортсмен после окончания тренировок
+     * @param dailyProgressAsPercentage ежедневный прогресс в процентах по отношению к предыдущему забегу
+     * @return пробег (с точностью до 2 знаков после десятичного разделителя)
      */
-    static double calculateTotalDistance(int startDistance, int finishDistance, int dailyProgress) {
+    static double calculateTotalDistance(int startDistance, int finishDistance, double dailyProgressAsPercentage) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
 
@@ -41,8 +45,8 @@ class Task07 {
             totalDistance = 0;
         } else {
             while (dailyDistance < finishDistance) {
+                dailyDistance = dailyDistance + dailyDistance * dailyProgressAsPercentage / 100;
                 totalDistance += dailyDistance;
-                dailyDistance += dailyProgress * dailyDistance / 100;
                 totalDistance = Math.round(100.0 * totalDistance) / 100.0;
             }
         }
