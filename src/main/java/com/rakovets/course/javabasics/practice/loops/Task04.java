f646bcba7008922fb2a4978bc12f0832f0f8948d
FilippovZ <6479556@gmail.com>
2021-01-10 16:04:17 +0300

    solution: Task04 for theme loops

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task04.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task04.java
@@ -36,6 +36,11 @@ class Task04 {
     static int calculateHealthPointsByTime(int healthPoints, double regenerationPercentFromCurrentHealth, int hoursAfterRespawn) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        double healthPointsAfterStart = healthPoints;
+        for (int i = 1; i <= hoursAfterRespawn; i++) {
+            healthPointsAfterStart += healthPointsAfterStart * regenerationPercentFromCurrentHealth / 100;
+        }
+        int result = (int)healthPointsAfterStart;
+        return result;
     }
 }
