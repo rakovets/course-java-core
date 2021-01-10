5e34b78ec834f75c0f3db0b2e1b2a88496e4615b
Igrok023451 <76533814+Igrok023451@users.noreply.github.com>
2021-01-10 21:19:33 +0300

    Done Task6

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task06.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task06.java
@@ -20,7 +20,7 @@ class Task06 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        long amount = 1500;
+        long amount = -834563456234L;
 
         String amountWithAccountingFormat = convertToAccountingFormat(amount);
         System.out.printf("Result: %s", amountWithAccountingFormat);
@@ -36,6 +36,35 @@ class Task06 {
     static String convertToAccountingFormat(long amount) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String number = String.valueOf(amount);
+        String numberReverse = "";
+        int i0 = 0;
+        if (amount < 0 ){
+            i0 = 1;
+        }
+
+        for (int i = number.length() - 1; i >= i0; i--) {
+            numberReverse += number.charAt(i);
+        }
+
+        String returnStrReverse = "";
+        int count = 0;
+        for (int i = 0; i < numberReverse.length(); i++) {
+            count++;
+            returnStrReverse += numberReverse.charAt(i);
+            if (count % 3 == 0 && i < numberReverse.length() - 1) {
+                count = 0;
+                returnStrReverse += " ";
+            }
+        }
+        String returnStr = "";
+        if (amount < 0 ) {
+            returnStr += "-";
+        }
+        for (int i = returnStrReverse.length() - 1; i >= 0; i--) {
+            returnStr += returnStrReverse.charAt(i);
+        }
+
+        return returnStr;
     }
 }
