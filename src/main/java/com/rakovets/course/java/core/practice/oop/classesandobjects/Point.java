dcab23e2e8b39da0085ebb76763e6cee2c2cf594
OxanaPeregud <ox.peregud@gmail.com>
2021-01-17 17:49:02 +0300

    solution: Fixed Task Point for theme 'oop-classes-and-objects'

--- src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/Point.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/Point.java
@@ -4,21 +4,21 @@ public class Point {
     private int x;
     private int y;
 
-    Point(int x, int y) {
-    this.x = x;
-    this.y = y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
     public int getX() {
-    return this.x;
+        return x;
     }
 
     public void setX(int x) {
-    this.x = x;
+        this.x = x;
     }
 
     public int getY() {
-        return this.y;
+        return y;
     }
 
     public void setY(int y) {
