246d0e0cfccd23b5323eed84fd482495f296c3fa
FilippovZ <6479556@gmail.com>
2021-01-18 15:42:54 +0300

    solution: test for task03 for theme oop-classes-and-objects

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classesandobjects/RectangleTest.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.java.core.practice.oop.classesandobjects;
+
+public class RectangleTest {
+    public static void main(String[] args) {
+        Point firstPoint = new Point(-3.0, -3.2);
+        Point secondPoint = new Point(1.0, -1.0);
+        Rectangle myRectangle = new Rectangle(firstPoint, secondPoint);
+        System.out.printf("Perimeter of a rectangle is: %f\nArea of a rectangle is: %f",myRectangle.getPerimeter(), myRectangle.getArea());
+    }
+}
