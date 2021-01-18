87dc7e7271ba2403a6f424207590dfd4df66b60d
FilippovZ <6479556@gmail.com>
2021-01-18 15:31:26 +0300

    solution: test for task01 for theme oop-classes-and-objects

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classesandobjects/TimeTest.java
@@ -0,0 +1,9 @@
+package com.rakovets.course.java.core.practice.oop.classesandobjects;
+
+public class TimeTest {
+    public static void main(String[] args) {
+        Time firstTimeValue = new Time(23, 14,15);
+        Time secondTimeValue = new Time(185);
+        System.out.printf("TotalSeconds:\n%d\n%d", firstTimeValue.getTotalSeconds(), secondTimeValue.getTotalSeconds());
+    }
+}
