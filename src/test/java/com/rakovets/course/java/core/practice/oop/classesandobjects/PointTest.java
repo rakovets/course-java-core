88eed796f78e79d8c99d1078f67b851b12095682
FilippovZ <6479556@gmail.com>
2021-01-18 15:36:40 +0300

    solution: test for task02 for theme oop-classes-and-objects

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classesandobjects/PointTest.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.java.core.practice.oop.classesandobjects;
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point firstPoint = new Point(5.0, 5.0);
+        Point secondPoint = new Point(11.0, 5.1);
+        System.out.print(firstPoint.distance(secondPoint));
+    }
+}
+
