ff62a6c8dfcce04f8d70a76722f821697a469a6e
Semargl92 <gavriljuk.9292@gmail.com>
2021-01-19 15:01:18 +0300

    solution: task 1 for theme 'oop. classes and objects'

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/TimeInitialization.java
@@ -0,0 +1,17 @@
+package com.rakovets.course.java.core.practice.oop.classesandobjects;
+
+public class TimeInitialization {
+    public static void main(String[] args) {
+        Time lesson = new Time(123153);
+        System.out.println(lesson.getHours() + ":" + lesson.getMinutes() + ":" + lesson.getSeconds());
+
+        Time playtime = new Time(23, 55, 48);
+        System.out.println(playtime.getHours() + ":" + playtime.getMinutes() + ":" + playtime.getSeconds());
+        playtime.setMinutes(345);
+        playtime.setSeconds(10);
+        playtime.setHours(6);
+        System.out.println(playtime.getHours() + ":" + playtime.getMinutes() + ":" + playtime.getSeconds());
+        System.out.println(playtime.getTotalSeconds());
+
+    }
+}
