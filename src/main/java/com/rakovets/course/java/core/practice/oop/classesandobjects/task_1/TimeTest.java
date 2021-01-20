922d4d2ae31860c3a4ca8db486967aeadbef7e13
Eugene <e.dunin97@gmail.com>
2021-01-20 18:58:15 +0300

    solution: task 1 for theme 'OOP.Classes and Objects'

--- src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/task_1/TimeTest.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/task_1/TimeTest.java
@@ -4,7 +4,11 @@ import com.rakovets.course.java.core.practice.oop.classesandobjects.task_1.model
 
 public class TimeTest {
     public static void main(String[] args) {
-        Time seconds = new Time (80500);
-        seconds.getHHMMSS();
+        Time seconds = new Time (15025);
+        seconds.getHours();
+        seconds.getMinutes();
+        seconds.getSeconds();
+        Time hours = new Time (12,5,8);
+        hours.getTotalseconds();
     }
 }
--- src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/task_1/model/Time.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/task_1/model/Time.java
@@ -1,53 +1,67 @@
 package com.rakovets.course.java.core.practice.oop.classesandobjects.task_1.model;
 
 public class Time {
-    public int hours;
-    public int minutes;
-    public int seconds;
+    private int hours;
+    private int minutes;
+    private int seconds;
 
     public Time(int hours, int minutes, int seconds) {
-        this.hours = hours;
-        this.minutes = minutes;
-        this.seconds = seconds;
+        if (hours >= 0 && hours < 24) {
+            this.hours = hours;
+        } else {
+            System.out.print("\nInvalid value of Hours\n\r");
+        }
+        if (minutes >= 0 && minutes < 60) {
+            this.minutes = minutes;
+        } else {
+            System.out.print("\nInvalid value of Minutes\n\r");
+        }
+        if (seconds >= 0 && seconds < 60) {
+            this.seconds = seconds;
+        } else {
+            System.out.print("\nInvalid value of Seconds\n\r");
+        }
     }
-
     public Time(int totalseconds) {
         this.seconds = totalseconds;
     }
 
-    public int getHours() {
+    public void getHours() {
         this.hours = this.seconds / 3600;
-        return this.hours;
+        System.out.printf("\nHours - %d", this.hours);
     }
-
     public void setHours(int hours) {
-        this.hours = hours;
+        if (hours >= 0 && hours < 24) {
+            this.hours = hours;
+        } else {
+            System.out.print("Invalid value");
+        }
     }
-
-    public int getMinutes() {
+    public void getMinutes() {
         this.minutes = (this.seconds % 3600) / 60;
-        return this.minutes;
+        System.out.printf("\nMinutes - %d", this.minutes);
     }
-
     public void setMinutes(int minutes) {
-        this.minutes = minutes;
+        if (minutes >= 0 && minutes < 60) {
+            this.minutes = minutes;
+        } else {
+            System.out.print("Invalid value");
+        }
     }
-
-    public int getSeconds() {
+    public void getSeconds() {
         this.seconds = this.seconds - ((this.minutes * 60) + (this.hours * 3600));
-        return this.seconds;
+        System.out.printf("\nSeconds - %d", this.seconds);
     }
-
     public void setSeconds(int seconds) {
-        this.seconds = seconds;
+        if (seconds >= 0 && seconds < 60) {
+            this.seconds = seconds;
+        } else {
+            System.out.print("Invalid value");
+        }
     }
-
     public void getTotalseconds() {
-        this.seconds += (this.hours * 3600) + (this.minutes * 60);
-        System.out.printf("Total seconds: %d", this.seconds);
-    }
-    public  void getHHMMSS() {
-        System.out.printf("%d:%d:%d", getHours(), getMinutes(), getSeconds());
+        this.seconds = (this.hours * 3600) + (this.minutes * 60);
+        System.out.printf("\nTotal seconds: %d", this.seconds);
     }
 }
 
