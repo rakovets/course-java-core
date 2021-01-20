d14d0df4835f2c34fcc3bbad6a8c89de38ef49cd
sergey.grinkevich <serg.grinkevich@gmail.com>
2021-01-20 19:01:41 +0300

    solution: Task03 for theme oop.classesandobjects

---
+++ src/main/java/com/rakovets/course/java/core/example/oop/classesandobjects/Rectangle.java
@@ -0,0 +1,35 @@
+package com.rakovets.course.java.core.example.oop.classesandobjects;
+
+public class Rectangle {
+    private Point firstPoint;
+    private Point secondPoint;
+
+    public Rectangle(Point firstPoint, Point secondPoint) {
+        this.firstPoint = firstPoint;
+        this.secondPoint = secondPoint;
+    }
+
+    public Point getFirstPoint() {
+        return firstPoint;
+    }
+
+    public void setFirstPoint(Point firstPoint) {
+        this.firstPoint = firstPoint;
+    }
+
+    public Point getSecondPoint() {
+        return secondPoint;
+    }
+
+    public void setSecondPoint(Point secondPoint) {
+        this.secondPoint = secondPoint;
+    }
+
+    public double getPerimeter() {
+        return ((Math.abs(firstPoint.getX() - secondPoint.getX()) + Math.abs(firstPoint.getY() - secondPoint.getY())) * 2);
+    }
+
+    public  double getArea() {
+        return  Math.abs((firstPoint.getX() - secondPoint.getX()) * Math.abs(firstPoint.getY() - secondPoint.getY()));
+    }
+}
