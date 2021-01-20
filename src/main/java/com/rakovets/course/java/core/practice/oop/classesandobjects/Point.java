daa93bc56e824a128cae818eab91f9d7051d201f
tanya-java <termalickaya@gmail.com>
2021-01-20 21:36:11 +0300

    solution: task 2 for theme "OOP classes and objects"

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/Point.java
@@ -0,0 +1,37 @@
+package com.rakovets.course.java.core.practice.oop.classesandobjects;
+import java.awt.geom.Point2D;
+
+public class Point {
+    private double x;
+    public double y;
+
+    Point() {
+    }
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double distance (Point pointIn2D) {
+        return Point2D.distance(x, y, pointIn2D.x, pointIn2D.y);
+
+    }
+
+}
