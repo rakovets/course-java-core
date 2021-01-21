b033c11698e032464091bd9ede6ff64500b03bc5
FilippovZ <6479556@gmail.com>
2021-01-21 14:01:17 +0300

    solution: Test01 for theme string

---
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -0,0 +1,27 @@
+package com.rakovets.course.java.core.practice.string;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilTest {
+    static Stream<Arguments> concatProviderArguments() {
+        return Stream.of(
+                Arguments.of("HelloJava", "Hello", "Java"),
+                Arguments.of("Java", "", "Java"),
+                Arguments.of("Hello", "Hello", "")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("concatProviderArguments")
+    void getOneStringOfTwoTest(String expectedString, String str1, String str2) {
+        String actualString = StringUtil.getOneStringOfTwo(str1, str2);
+        Assertions.assertEquals(expectedString, actualString);
+    }
+}
