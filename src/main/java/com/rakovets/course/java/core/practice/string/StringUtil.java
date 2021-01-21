85b8f502304c471f57f210756838f60fc39d05f0
FilippovZ <6479556@gmail.com>
2021-01-22 00:51:12 +0300

    completed 4/19 tasks

--- src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
@@ -1,7 +1,21 @@
 package com.rakovets.course.java.core.practice.string;
 
+import java.util.Locale;
+
 public class StringUtil {
     static String getOneStringOfTwo(String str1, String str2) {
         return str1.concat(str2);
+    }
+
+    static int getIndexOfChar(char c, String str) {
+        return str.indexOf(c);
+    }
+
+    static boolean isEqualsTwoStrings(String str1, String str2) {
+        return str1.equals(str2);
+    }
+
+    static String getTrimAndLowerCase(String str){
+        return str.trim().toLowerCase();
     }
 }
