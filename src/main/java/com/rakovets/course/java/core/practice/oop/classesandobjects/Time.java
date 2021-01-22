c22bcfa21b687485716e4ea49a7779124ee45aa1
OxanaPeregud <ox.peregud@gmail.com>
2021-01-22 15:08:10 +0300

    solution: Fixed Task Time for theme 'oop-classes-and-objects'

--- src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/Time.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/Time.java
@@ -6,7 +6,9 @@ public class Time {
     private int seconds;
 
     public Time(int totalSeconds) {
-        this.seconds = totalSeconds;
+        this.hours = totalSeconds / 3600;
+        this.minutes = totalSeconds % 3600 / 60;
+        this.seconds = totalSeconds % 60;
     }
 
     public Time(int hours, int minutes, int seconds) {
