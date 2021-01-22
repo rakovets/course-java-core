ed700cdde541c0a7a049cdd517e4fcd1dbfe7f5d
OxanaPeregud <ox.peregud@gmail.com>
2021-01-22 15:06:50 +0300

    solution: Fixed AtmTest for theme 'oop-classes-and-objects'

--- src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/AtmTest.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/AtmTest.java
@@ -3,19 +3,12 @@ package com.rakovets.course.java.core.practice.oop.classesandobjects;
 public class AtmTest {
     public static void main(String[] args) {
 
-        Atm newAtm = new Atm(10, 20, 9, 1000);
-
-        if (newAtm.isPossibleIssue(1000))
-            System.out.println("true");
-        else
-            System.out.println("false");
-
-        if (newAtm.isPossibleIssue(1000))
-            newAtm.getOptionsCombinationBanknotes(1000);
-        else
-            System.out.println("The amount you selected is not available. Enter another amount");
-
-        if (newAtm.isPossibleIssue(1000))
-            newAtm.getCash(5);
+        Atm testAtm = new Atm(10,20,100);
+        testAtm.addBanknotes100(5);
+        testAtm.addBanknotes50(5);
+        testAtm.addBanknotes20(5);
+        testAtm.isPossibleIssue(100);
+        testAtm.getOptionsCombinationBanknotes(1000);
+        testAtm.getCash(3);
     }
 }
