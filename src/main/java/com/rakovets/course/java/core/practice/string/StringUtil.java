d29932070954ddae6e36abed5365299811341a67
Semargl92 <gavriljuk.9292@gmail.com>
2021-01-23 22:26:35 +0300

    solution: tasks 6-14 for theme 'string'

--- src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
@@ -1,5 +1,10 @@
 package com.rakovets.course.java.core.practice.string;
 
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtil {
 
     // task 1. Метод возвращает строку, которая является результатом склеивания двух строк.
@@ -12,7 +17,7 @@ public class StringUtil {
         }
 
     // task 2. Метод возвращает индекс символа x (x – символ вводимый пользователем) в строке.
-    // Если такого символа нет, то возвращает значение -1. В случае не инициализированной строки или символа возвращает .
+    // Если такого символа нет, то возвращает значение -1. В случае не инициализированной строки или символа возвращает -1.
         static public int getIndexOf (String example, char x) {
             if (example != null && x != 0) {
                 return example.indexOf(x);
@@ -52,6 +57,127 @@ public class StringUtil {
         return null;
     }
 
+    // task 6. Метод заменяет в строке все сочетания символов ":(" на ":)".
+    // В случае строки равной null возвращает null.
+    static public String replaceSadEmoji (String sad) {
+        if (sad != null) {
+            return sad.replace(":(", ":)");
+            }
+        return null;
+    }
 
+    // task 7. Метод возвращает булинное значение true, если параметр строки начинает и заканчивается с параметра слова.
+    // В случае строки или слова равных null возвращает false. Метод учитывает регистр параметров.
+    static public boolean doesStringStartsAndEndsWithWord (String example, String word) {
+        if (example != null && word != null) {
+            return (example.startsWith(word) && example.endsWith(word));
+        }
+        return false;
+    }
+
+    // task 8. Метод возвращает количество гласных букв в заданной строке (для английского языка).
+    // В случае строки равной null возвращает -1.
+    static public int getNumberOfVowels (String example) {
+        if (example != null) {
+            String exampleInProgress = example.toLowerCase(Locale.ROOT);
+            char[] exampleInProgressArray = exampleInProgress.toCharArray();
+            int numberOfVowels = 0;
+
+            for (char c : exampleInProgressArray) {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y') {
+                    numberOfVowels += 1;
+                }
+            }
+            return numberOfVowels;
+        }
+        return -1;
+    }
+
+    // task 9. Метод возвращает количество всех точек, запятых и восклицательных знаков в строке.
+    // В случае строки равной null возвращает -1.
+    static public int getNumberOfPunctuationMarks (String example) {
+        if (example != null) {
+            char[] exampleArray = example.toCharArray();
+            int numberOfPunctuationMarks = 0;
+
+            for (char c : exampleArray) {
+                if (c == '.' || c == ',' || c == '!') {
+                    numberOfPunctuationMarks += 1;
+                }
+            }
+            return numberOfPunctuationMarks;
+        }
+        return -1;
+    }
+
+    //task 10. Метод возвращает булинное значение того, является ли введённая строка палиндромом.
+    // В случае строки равной null возвращает false.
+    static public boolean isPalindrome (String example) {
+        if (example != null) {
+
+            Pattern punctuationDeleting = Pattern.compile("\\s*(\\s|,|\\?|!|:|;|-|\\.)\\s*");
+            Matcher matcher = punctuationDeleting.matcher(example);
+            String examplePure = matcher.replaceAll("");
+
+            StringBuilder palindromeCheckingBuilder = new StringBuilder(examplePure);
+            palindromeCheckingBuilder = palindromeCheckingBuilder.reverse();
+            String palindromeChecking = palindromeCheckingBuilder.toString();
+
+            return examplePure.equalsIgnoreCase(palindromeChecking);
+        }
+        return false;
+    }
+
+   // task 11. Метод разбивает строку на равные части по n символов возвращает массив из этих частей.
+   // В случае строки равной null возвращает null.
+    static public String[] splitByIndex (String example, int n) {
+        if (example != null && n > 0) {
+
+            StringBuilder exampleWithDivider = new StringBuilder(example);
+
+                for (int x = n; x < exampleWithDivider.length(); ) {
+                    exampleWithDivider.insert(x, "%@#");
+                    x += n + 3;
+            }
+            return exampleWithDivider.toString().split("%@#");
+        }
+        return null;
+    }
+
+    //task 12. Метод возвращает количество слов в тексте.
+    // В случае строки равной null возвращает -1.
+    static public int getNumberOfWords (String example) {
+        if (example != null) {
+            Pattern punctuationDeleting = Pattern.compile("[,.!?*\\-:+]");
+            Matcher matcher = punctuationDeleting.matcher(example);
+            String examplePure = matcher.replaceAll("");
+
+            String[] exampleArray = examplePure.trim().split("\\s+");
+            return exampleArray.length;
+        }
+        return -1;
+    }
+
+    // task 13. Метод возвращает инициалы в формате Ф.И.О при заданной строке с именем, фамилией и отчеством.
+    // В случае строки равной null возвращает null.
+    static public String getInitials (String example) {
+        if (example != null) {
+            String[] exampleArray = example.trim().toUpperCase().split("\\s+");
+            char name = exampleArray[0].charAt(0);
+            char surname = exampleArray[1].charAt(0);
+            char patronymic = exampleArray[2].charAt(0);
+            return surname + "." + name + "." + patronymic;
+        }
+        return null;
+    }
+
+    // task 14. Метод возвращает строку, которая содержит все имеющиеся в тексте цифры.
+    // В случае строки равной null возвращает null.
+    static public String getOnlyDigits (String example) {
+        if (example != null) {
+            return example.replaceAll("\\D", "");
+        }
+        return null;
+    }
 
 }
--- src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 import java.util.stream.Stream;
 
 public class StringUtilTest {
@@ -129,4 +131,215 @@ public class StringUtilTest {
         Assertions.assertEquals(expectedString, actualString);
     }
 
+   // task 6. Метод заменяет в строке все сочетания символов ":(" на ":)".
+    // В случае строки равной null возвращает null.
+    static Stream<Arguments> provideArgumentsForReplaceSadEmoji() {
+        return Stream.of(
+                Arguments.of(":)", ":("),
+                Arguments.of(null, null),
+                Arguments.of("Happy day :)", "Happy day :("),
+                Arguments.of("(::::)(((", "(::::(((("),
+                Arguments.of(";( :-( v:)v : (", ";( :-( v:(v : (")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForReplaceSadEmoji")
+    void replaceSadEmoji(String expectedString, String str1) {
+        // GIVEN
+
+        // WHEN
+        String actualString = StringUtil.replaceSadEmoji(str1);
+
+        // THAT
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    // task 7. Метод возвращает булинное значение true, если параметр строки начинает и заканчивается с параметра слова.
+    // В случае строки или слова равным null возвращает false. Метод учитывает регистр параметров.
+    static Stream<Arguments> provideArgumentsForDoesStringStartsAndEndsWithWord() {
+        return Stream.of(
+                Arguments.of(true, "Hello", "Hello"),
+                Arguments.of(true, "King is dead. Hail the King", "King"),
+                Arguments.of(false, " King is dead. Hail the King ", " King"),
+                Arguments.of(false, null, "King"),
+                Arguments.of(false, "King", null),
+                Arguments.of(false, "King", "king")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForDoesStringStartsAndEndsWithWord")
+    void doesStringStartsAndEndsWithWord(boolean expectedBoolean, String str1, String str2) {
+        // GIVEN
+
+        // WHEN
+        boolean actualBoolean = StringUtil.doesStringStartsAndEndsWithWord(str1, str2);
+
+        // THAT
+        Assertions.assertEquals(expectedBoolean, actualBoolean);
+    }
+
+    // task 8. Метод возвращает количество гласных букв в заданной строке (для английского языка).
+    // В случае строки равной null возвращает -1.
+    static Stream<Arguments> provideArgumentsForGetNumberOfVowels() {
+        return Stream.of(
+                Arguments.of(2, "Hello"),
+                Arguments.of(15, "King Arthur was a great warrior defending England."),
+                Arguments.of(0, "С русским языком не работаем"),
+                Arguments.of(-1, null)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForGetNumberOfVowels")
+    void getNumberOfVowels(int expectedInt, String str1) {
+        // GIVEN
+
+        // WHEN
+        int actualInt = StringUtil.getNumberOfVowels(str1);
+
+        // THAT
+        Assertions.assertEquals(expectedInt, actualInt);
+    }
+
+    // task 9. Метод возвращает количество всех точек, запятых и восклицательных знаков в строке.
+    // В случае строки равной null возвращает -1.
+    static Stream<Arguments> provideArgumentsForGetNumberOfPunctuationMarks() {
+        return Stream.of(
+                Arguments.of(0, "Hello"),
+                Arguments.of(7, "Wait!!! I suggest we keep it between ourselves, shall we? I beg you... "),
+                Arguments.of(2, "Для кириллицы тоже ок, круто же!"),
+                Arguments.of(-1, null)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForGetNumberOfPunctuationMarks")
+    void getNumberOfPunctuationMarks(int expectedInt, String str1) {
+        // GIVEN
+
+        // WHEN
+        int actualInt = StringUtil.getNumberOfPunctuationMarks(str1);
+
+        // THAT
+        Assertions.assertEquals(expectedInt, actualInt);
+    }
+
+    //task 10. Метод возвращает булинное значение того, является ли введённая строка палиндромом.
+    // В случае строки равной null возвращает false.
+    static Stream<Arguments> provideArgumentsForIsPalindrome() {
+        return Stream.of(
+                Arguments.of(true, "Rotator"),
+                Arguments.of(true, "No lemon - no melon"),
+                Arguments.of(false, "King"),
+                Arguments.of(false, null),
+                Arguments.of(true, "Eva, can I see bees in a cave?")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForIsPalindrome")
+    void isPalindrome(boolean expectedBoolean, String str1) {
+        // GIVEN
+
+        // WHEN
+        boolean actualBoolean = StringUtil.isPalindrome(str1);
+
+        // THAT
+        Assertions.assertEquals(expectedBoolean, actualBoolean);
+    }
+
+    // task 11. Метод разбивает строку на равные части по n символов возвращает массив из этих частей
+    // В случае строки равной null возвращает null.
+    static Stream<Arguments> provideArgumentsForSplitByIndex() {
+        return Stream.of(
+                Arguments.of(new String []{"123","456", "789", "0"}, "1234567890", 3),
+                Arguments.of(new String []{"abcde","fghij", "klmno", "pqrst", "uvwxy","z"}, "abcdefghijklmnopqrstuvwxyz", 5),
+                Arguments.of(new String []{"H","e", "l", "l", "o"}, "Hello", 1),
+                Arguments.of(new String []{"Several"}, "Several", 7),
+                Arguments.of(null, null, 1),
+                Arguments.of(null, "Hello", -1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForSplitByIndex")
+    void splitByIndex(String[] expectedString, String str1, int n) {
+        // GIVEN
+
+        // WHEN
+        String[] actualString = StringUtil.splitByIndex(str1, n);
+
+        // THAT
+        assertArrayEquals(expectedString, actualString);
+    }
+
+    //task 12. Метод возвращает количество слов в тексте.
+    // В случае строки равной null возвращает -1.
+    static Stream<Arguments> provideArgumentsForGetNumberOfWords() {
+        return Stream.of(
+                Arguments.of(1, "  Hello "),
+                Arguments.of(13, "Wait!!! I suggest we keep it between ourselves, shall we? I beg you... "),
+                Arguments.of(2, "  Sam, * - : +  look! "),
+                Arguments.of(-1, null)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForGetNumberOfWords")
+    void getNumberOfWords(int expectedInt, String str1) {
+        // GIVEN
+
+        // WHEN
+        int actualInt = StringUtil.getNumberOfWords(str1);
+
+        // THAT
+        Assertions.assertEquals(expectedInt, actualInt);
+    }
+
+    // task 13. Метод возвращает инициалы в формате Ф.И.О при заданной строке с именем, фамилией и отчеством.
+    // В случае строки равной null возвращает null.
+    static Stream<Arguments> provideArgumentsForGetInitials() {
+        return Stream.of(
+                Arguments.of("G.A.S", "Anastasia Gawriljuk Sergeevna"),
+                Arguments.of(null, null),
+                Arguments.of("G.S.V", "sergei grinkevich vitaljevich")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForGetInitials")
+    void getInitials(String expectedString, String str1) {
+        // GIVEN
+
+        // WHEN
+        String actualString = StringUtil.getInitials(str1);
+
+        // THAT
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    // task 14. Метод возвращает строку, которая содержит все имеющиеся в тексте цифры.
+    // В случае строки равной null возвращает null.
+    static Stream<Arguments> provideArgumentsForGetOnlyDigits() {
+        return Stream.of(
+                Arguments.of("", "Sasuke"),
+                Arguments.of(null, null),
+                Arguments.of("375298974562", "tel: +37529 8974562; "),
+                Arguments.of("301236", "ЕKkjd  3e0n}! @12#e3 els?6")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForGetOnlyDigits")
+    void getOnlyDigits(String expectedString, String str1) {
+        // GIVEN
+
+        // WHEN
+        String actualString = StringUtil.getOnlyDigits(str1);
+
+        // THAT
+        Assertions.assertEquals(expectedString, actualString);
+    }
 }
