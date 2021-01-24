8635efc93aa1a87b94d805f7aa82809caa2789ec
FilippovZ <6479556@gmail.com>
2021-01-24 12:06:28 +0300

    intermediate storage

--- src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
@@ -2,26 +2,88 @@ package com.rakovets.course.java.core.practice.string;
 
 import java.util.Locale;
 
-public class StringUtil {
+public class StringUtil {//01
     static String getOneStringOfTwo(String str1, String str2) {
         return str1.concat(str2);
     }
 
-    static int getIndexOfChar(char c, String str) {
+    static int getIndexOfChar(char c, String str) {//02
         return str.indexOf(c);
     }
 
-    static boolean isEqualsTwoStrings(String str1, String str2) {
+    static boolean isEqualsTwoStrings(String str1, String str2) {//03
         return str1.equals(str2);
     }
 
-    static String getTrimAndLowerCase(String str){
-        return str.trim().toLowerCase();
+    static String getTrimAndLowerCase(String str) {//04
+        return str.trim().toUpperCase();
     }
 
-    static String getCharsOfString(String str, int start) {
-        char[] dst = new char[10 - start];
-        str.getChars(start, 10, dst, 0);
-        return dst.toString();
+    static String getPartOfString(String str, int start) {//05
+        if (str.length() - start >= 23) {
+            return str.substring(start, start + 23);
+        }
+            return str.substring(start);
+
+    }
+
+    static String replaceSmiles(String str) {//06
+        return str.replace(":(", ":)");
+    }
+
+    static  boolean startAndEndWord(String str1, String word) {//07
+        if (str1.startsWith(word) && str1.endsWith(word)) {
+            return true;
+        }
+        return false;
+    }
+
+    static int getVowels(String str) {//08
+        int count = 0;
+        str = str.toLowerCase();
+        char[] strInChars=new char[str.length()];
+        str.getChars(0, str.length(), strInChars, 0);
+        for (char c : strInChars) {
+            if (c == 'а' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static int getSumOfSymbols(String str) {//09
+        char[] symbolsOfString = new char[str.length()];
+        str.getChars(0, symbolsOfString.length, symbolsOfString, 0);
+        int sumOfSymbols = 0;
+        for (int i = 0; i <= (symbolsOfString.length - 1); i++) {
+            if (symbolsOfString[i] == '.' || symbolsOfString[i] == ',' || symbolsOfString[i] == '!') {
+                sumOfSymbols++;
+            }
+        }
+        return sumOfSymbols;
+    }
+
+    static boolean isPalindrome(String str) {//10
+        str = str.replaceAll("\\W", "").toLowerCase();
+        char[] palindrome = new char[str.length()];
+        str.getChars(0, palindrome.length, palindrome, 0);
+        for (int i = 0; i <= (palindrome.length - 1) / 2; i++) {
+            if (palindrome[i] != palindrome[palindrome.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static String[] getPartsOfString(String str, int parts) {
+        int startIndex = 0;
+        double quantityParts = Math.ceil(str.length() * 1.0 / parts);
+        String[] partsOfString = new String[(int)quantityParts];
+        for (int i = 1; i < quantityParts; i++) {
+            partsOfString[i - 1] = str.substring(startIndex, startIndex + parts);
+            startIndex += parts;
+        }
+        partsOfString[(int)quantityParts - 1] = str.substring(startIndex);
+        return partsOfString;
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilTest {
-    static Stream<Arguments> concatProviderArguments() {
+    static Stream<Arguments> concatProviderArguments() {//task01
         return Stream.of(
                 Arguments.of("HelloJava", "Hello", "Java"),
                 Arguments.of("Java", "", "Java"),
@@ -25,7 +25,7 @@ class StringUtilTest {
         Assertions.assertEquals(expectedString, actualString);
     }
 
-    static Stream<Arguments> findIndexProviderArguments() {
+    static Stream<Arguments> findIndexProviderArguments() {//Task02
         return Stream.of(
                 Arguments.of(7, "v", "Hello everybody!"),
                 Arguments.of(-1, "z", "Good job!"),
@@ -40,7 +40,7 @@ class StringUtilTest {
         Assertions.assertEquals(expectedIndex, actualIndex);
     }
 
-    static Stream<Arguments> equalsProviderArguments() {
+    static Stream<Arguments> equalsProviderArguments() {//task03
         return Stream.of(
                 Arguments.of(true, "How do you do?", "How do you do?"),
                 Arguments.of(false, " How do you do?", "How do you do?"),
@@ -51,17 +51,17 @@ class StringUtilTest {
 
     @ParameterizedTest
     @MethodSource("equalsProviderArguments")
-    void getIndexOfChar(boolean expectedValue, String str1, String str2) {
+    void isEqualsTwoStrings(boolean expectedValue, String str1, String str2) {
         boolean actualValue = StringUtil.isEqualsTwoStrings(str1, str2);
         Assertions.assertEquals(expectedValue, actualValue);
     }
 
-    static Stream<Arguments> trimProviderArguments() {
+    static Stream<Arguments> trimProviderArguments() {//task04
         return Stream.of(
-                Arguments.of("how do you do?", "How Do you do?"),
-                Arguments.of("how do you do?", "  How do yoU do?   "),
-                Arguments.of("how   do you do?", "How   do yoU do? "),
-                Arguments.of("how   do you do?", "   How   do yoU do?")
+                Arguments.of("HOW DO YOU DO?", "How Do you do?"),
+                Arguments.of("HOW DO YOU DO?", "  How do yoU do?   "),
+                Arguments.of("HOW   DO YOU DO?", "How   do yoU do? "),
+                Arguments.of("HOW   DO YOU DO?", "   How   do yoU do?")
         );
     }
 
@@ -72,17 +72,111 @@ class StringUtilTest {
         Assertions.assertEquals(expectedString, actualString);
     }
 
-    static Stream<Arguments> getCharsProviderArguments() {
+    static Stream<Arguments> getSubProviderArguments() {//task05
         return Stream.of(
-                Arguments.of(" you do?", "how do you do?", 5),
+                Arguments.of("o you do?", "how do you do?", 5),
                 Arguments.of("would be nice to get 10", "It would be nice to get 10 points for homework", 3)
         );
     }
 
     @ParameterizedTest
-    @MethodSource("getCharsProviderArguments")
-    void getCharsOfString(String expectedString, String str, int start) {
-        String actualString = StringUtil.getCharsOfString(str, start);
+    @MethodSource("getSubProviderArguments")
+    void getPartOfString(String expectedString, String str, int start) {
+        String actualString = StringUtil.getPartOfString(str, start);
         Assertions.assertEquals(expectedString, actualString);
+    }
+
+    static Stream<Arguments> ReplaceProviderArguments() {//task06
+        return Stream.of(
+                Arguments.of("Hop hei :) halalej :)", "Hop hei :( halalej :)"),
+                Arguments.of(":):):)", ":):(:("),
+                Arguments.of(" ", " ")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("ReplaceProviderArguments")
+    void replaceSmiles(String expectedString, String str) {
+        String actualString = StringUtil.replaceSmiles(str);
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    static Stream<Arguments> ProvideArgumentsForStartAndEndWordMethod() {//task07
+        return Stream.of(
+                Arguments.of(true, "hello It would be nice to get 10 points for homework hello", "hello"),
+                Arguments.of(false, "hello It would be nice to get 10 points for homework", "hello"),
+                Arguments.of(false, " It would be nice to get 10 points for homework hello", "hello"),
+                Arguments.of(false, "It would be nice to get 10 points for homework", "hello"),
+                Arguments.of(false, "Hello It would be nice to get 10 points for homework hello", "hello")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("ProvideArgumentsForStartAndEndWordMethod")
+    void startAndEndWord(boolean expected, String str1, String word) {
+        boolean actual = StringUtil.startAndEndWord(str1, word);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> ProvideArgumentsForGetVowelsMethod() {//task08
+        return Stream.of(
+                Arguments.of(14, "it would be nice to get 10 points for homework"),
+                Arguments.of(0, "ddd 55 fff"),
+                Arguments.of(0, " "),
+                Arguments.of(14, "It woUld be nice to get 10 points for homEwork")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("ProvideArgumentsForGetVowelsMethod")
+    void getVowels(int expected, String str) {
+        int actual = StringUtil.getVowels(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> ProvideArgumentsForgetSumOfSymbolsMethod() {//task09
+        return Stream.of(
+                Arguments.of(9, "A man!, .a p!l,an.. a c,anal-Pa!nama"),
+                Arguments.of(1, "A man, a plan a canal-Panama"),
+                Arguments.of(0, "A man a plan a canal-Panama")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("ProvideArgumentsForgetSumOfSymbolsMethod")
+    void isPalindrome(int expected, String str) {
+        int actual = StringUtil.getSumOfSymbols(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> ProvideArgumentsForPalindromeMethod() {//task10
+        return Stream.of(
+                Arguments.of(true, "A man, a plan a canal-Panama"),
+                Arguments.of(true, "A man, a plan a canal-Panama  "),
+                Arguments.of(false, "A man, aa plan a canal-Panama")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("ProvideArgumentsForPalindromeMethod")
+    void isPalindrome(boolean expected, String str) {
+        boolean actual = StringUtil.isPalindrome(str);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //task11
+    static Stream<Arguments> ProvideArgumentsForGetPartsOfStringMethod() {
+        return Stream.of(
+                Arguments.of(new String[] {"How", " ar", "e y", "ou?"}, "How are you?", 3),
+                Arguments.of(new String[] {"12", "34", "5"}, "12345", 2),
+                Arguments.of(new String[] {"t", "o", "d", "a", "y"}, "today", 1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("ProvideArgumentsForGetPartsOfStringMethod")
+    void getPartsOfString(String[] expected, String str, int parts) {
+        String[] actual = StringUtil.getPartsOfString(str, parts);
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
