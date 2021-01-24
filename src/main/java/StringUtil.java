d19164b15b726633f0ae15eeddce9c6a3601957d
tanya-java <termalickaya@gmail.com>
2021-01-24 20:51:41 +0300

    solution: tests task 1-10,12 for theme "Strings"

--- src/main/java/StringUtil.java
+++ src/main/java/StringUtil.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class StringUtil {
     //Task 1
     public static String main(String[] args) {
@@ -7,7 +9,7 @@ public class StringUtil {
     }
 
     //Task 2
-   public static int indexOf(String str, char x) {
+    public static int indexOf(String str, char x) {
         return str.indexOf(x);
     }
 
@@ -17,27 +19,76 @@ public class StringUtil {
     }
 
     //Task 4
-   public static String trimAndToUpperCase(String str) {
+    public static String trimAndToUpperCase(String str) {
         return str.trim().toUpperCase();
-      }
+    }
 
-   //Task 5
-   public static String substring (String str) {
+    //Task 5
+    public static String substring(String str) {
         if (str.length() >= 33) {
             return str.substring(10, 33);
         } else {
             return str.substring(10);
         }
 
-   }
-   //Task 6
-   public static String replace(String str) {
-        return str.replace(":(",":)");
-   }
+    }
 
-   //Task 7
+    //Task 6
+    public static String replace(String str) {
+        return str.replace(":(", ":)");
+    }
 
+    //Task 7
+    public static boolean startsWithAndEndsWith(String str, String word) {
+        return str.startsWith(word) && str.endsWith(word);
+    }
+
+    //Task 8
+    public static int vowels(String str) {
+        int vowels = 0;
+        str = str.toLowerCase();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' ||
+                    str.charAt(i) == 'o' || str.charAt(i) == 'u' || str.charAt(i) == 'y')
+                vowels++;
+        }
+        return vowels;
+    }
+
+    //Task 9
+    public static int punctuation(String str) {
+        int punctuation = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ',' || str.charAt(i) == '.' || str.charAt(i) == '!')
+                punctuation++;
+        }
+        return punctuation;
+    }
+
+    //Task 10
+    public static boolean palindrome(String text) {
+        String clean = text.replaceAll("\\s+", "").toLowerCase();
+        int length = clean.length();
+        int forward = 0;
+        int backward = length - 1;
+        while (backward > forward) {
+            char forwardChar = clean.charAt(forward++);
+            char backwardChar = clean.charAt(backward--);
+            if (forwardChar != backwardChar)
+                return false;
+        }
+        return true;
+    }
+
+   //Task 12
+    public static int numbersOfWords(String str) {
+        String[] word=str.split("\\s+");
+        return word.length;
+    }
 
 }
+
+
+
 
 
