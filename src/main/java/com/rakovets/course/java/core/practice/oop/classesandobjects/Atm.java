341beb0e989c9be7f1a8d082fac63b4deea2b758
FilippovZ <6479556@gmail.com>
2021-01-25 18:24:08 +0300

    fix bugs

--- src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/Atm.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/Atm.java
@@ -4,19 +4,11 @@ public class Atm {
     private int numberBanknotes100;
     private int numberBanknotes50;
     private int numberBanknotes20;
-    private int amount;
-
-    Atm() {
-    }
 
     Atm(int numberBanknotes100, int numberBanknotes50, int numberBanknotes20) {
         this.numberBanknotes100 = numberBanknotes100;
         this.numberBanknotes50 = numberBanknotes50;
         this.numberBanknotes20 = numberBanknotes20;
-    }
-
-    Atm(int amount) {
-        this.amount = amount;
     }
 
     public void addBanknotes100(int number) {
@@ -31,61 +23,50 @@ public class Atm {
         numberBanknotes20 += number;
     }
 
-    public void isPossibleIssue(int amount) {
+    public boolean isPossibleIssue(int amount) {
         int count = 0;
         for (int i = 0; i <= numberBanknotes100 && i * 100 <= amount && count < 1; i++) {
             for (int j = 0; j <= numberBanknotes50 && j * 50 <= amount  && count < 1; j++) {
                 for (int k = 0; k <= numberBanknotes20 && k * 20 <= amount && count < 1; k++) {
                     if (100 * i + 50 * j + 20 * k == amount) {
                         count++;
-                        System.out.println("You can get this amount");
+                        return true;
                     }
                 }
             }
         }
-        if (count == 0) {
-            System.out.println("No available options. Choose another amount");
-        }
+        return false;
     }
 
-    public void getOptionsCombinationBanknotes(int amount) {
+    public String getOptionsCombinationBanknotes(int amount) {
         int count = 0;
-        this.amount = amount;
+        String options = "";
         System.out.println("Available options:");
         for (int i = 0; i <= numberBanknotes100 && i * 100 <= amount; i++) {
             for (int j = 0; j <= numberBanknotes50 && j * 50 <= amount; j++) {
                 for (int k = 0; k <= numberBanknotes20 && k * 20 <= amount; k++) {
                     if (i * 100 + j * 50 + k * 20 == amount) {
                         count++;
-                        System.out.printf("%d) 100$ - %d\n50$ - %d\n20$ - %d\n\n",count, i, j, k);
+                        options += count + ") 100$ - " + i + "\n50$ - " + j + "\n20$ - " + k + "\n\n";
                     }
                 }
             }
         }
-        if (count == 0) {
-            System.out.println("No available options. Choose another amount");
-        }
+        return options;
     }
 
-    public void getCash(int option) {
-        int count = 0;
-        for (int i = 0; i <= numberBanknotes100 && i * 100 <= amount && count <= option; i++) {
-            for (int j = 0; j <= numberBanknotes50 && j * 50 <= amount && count <= option; j++) {
-                for (int k = 0; k <= numberBanknotes20 && k * 20 <= amount && count <= option; k++) {
-                    if (i * 100 + j * 50 + k * 20 == amount) {
-                        count++;
-                        if (option == count) {
-                            numberBanknotes100 -= i;
-                            numberBanknotes50 -= j;
-                            numberBanknotes20 -= k;
-                            System.out.printf("Take you money:\n100$ - %d\n50$ - %d\n20$ - %d\n", i, j, k);
-                        }
+    public void getCash (String option) {
+        for (int a = 0; a <= this.numberBanknotes100; a++) {
+            for (int b = 0; b <= this.numberBanknotes50; b++) {
+                for (int c = 0; c <= this.numberBanknotes20; c++) {
+                    String checkedOption = a + "*100$, " + b + "*50$, " + c + "*20$";
+                    if (checkedOption.equals(option)) {
+                        this.numberBanknotes100 -= a;
+                        this.numberBanknotes50 -= b;
+                        this.numberBanknotes20 -= c;
                     }
                 }
             }
-        }
-        if (option > count) {
-            System.out.println("Error option. Try again");
         }
     }
 }
