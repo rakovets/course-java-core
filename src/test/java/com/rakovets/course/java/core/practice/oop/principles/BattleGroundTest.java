3af0b79ebe00724b7269b60cf2b199102d852d18
Igrok023451 <76533814+Igrok023451@users.noreply.github.com>
2021-01-27 10:13:34 +0300

    Done 'Battle Ground' Test

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop/principles/BattleGroundTest.java
@@ -0,0 +1,57 @@
+package com.rakovets.course.java.core.practice.oop.principles;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BattleGroundTest {
+
+    static Stream<Arguments> getPlay() {
+        return Stream.of(
+                Arguments.of(new int[]{100, 40}, new Mag("Mag"), new Ghost(100)),
+                Arguments.of(new int[]{55, 70}, new Warrior("Warrior"), new Zombie(100)),
+                Arguments.of(new int[]{85, 10}, new Archer("Archer"), new Skeleton(100))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getPlay")
+    void startBattleGroundTest(int[] expected, Hero hero, Enemy enemy) {
+        for (int i = 0; i < 3; i++) {
+            hero.attackEnemy(enemy);
+            System.out.printf("Здоровье врага: %d\n", enemy.getHealth());
+            enemy.attackHero(hero);
+            System.out.printf("Здоровье героя: %d\n", hero.getHealthHero());
+        }
+        Assertions.assertArrayEquals(expected, new int[]{hero.getHealthHero(), enemy.getHealth()});
+    }
+
+    static Stream<Arguments> getPlayed() {
+        return Stream.of(
+                Arguments.of(new int[]{100, 0}, new Mag("Mag"), new Ghost(100)),
+                Arguments.of(new int[]{0, 30}, new Warrior("Warrior"), new Zombie(100)),
+                Arguments.of(new int[]{85, 0}, new Archer("Archer"), new Skeleton(100))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getPlayed")
+    void startBattleGroundTest2(int[] expected, Hero hero, Enemy enemy) {
+        while (enemy.isAlive() && hero.isAlive()) {
+            if (hero.isAlive()) {
+                hero.attackEnemy(enemy);
+                System.out.printf("Здоровье врага: %d\n", enemy.getHealth());
+            }
+            if (enemy.isAlive()) {
+                enemy.attackHero(hero);
+                System.out.printf("Здоровье героя: %d\n", hero.getHealthHero());
+            }
+        }
+
+        Assertions.assertArrayEquals(expected, new int[]{hero.getHealthHero(), enemy.getHealth()});
+
+    }
+}
