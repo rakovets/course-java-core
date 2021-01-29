9dad0078e3aa87d8c69143d55775a7ab1fb8cff9
FilippovZ <6479556@gmail.com>
2021-01-29 18:53:18 +0300

    fix bugs

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/Sphynx.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/Sphynx.java
@@ -1,6 +1,6 @@
 package com.rakovets.course.java.core.practice.oop.principles;
 
-public class Sphynx extends Cat{
+public class Sphynx extends Cat {
     public Sphynx(String name) {
         super(name);
     }
