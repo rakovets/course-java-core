0a2c42cc533ff28fdfe6ba7b13960edffd82b011
FilippovZ <6479556@gmail.com>
2021-01-29 18:52:13 +0300

    fix bugs

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/Goblin.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/Goblin.java
@@ -1,11 +1,12 @@
 package com.rakovets.course.java.core.practice.oop.principles;
 
-public class Goblin extends Enemy{
+public class Goblin extends Enemy {
+    private final int DAMAGE_OF_POISON = 2;
     public Goblin(int health) {
         super(health);
     }
 
     public void givePoison(Hero hero) {
-        hero.takeDamage(hero.getHealth() / 2);
+        hero.takeDamage(hero.getHealth() / DAMAGE_OF_POISON);
     }
 }
