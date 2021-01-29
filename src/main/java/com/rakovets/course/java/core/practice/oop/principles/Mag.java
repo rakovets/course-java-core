834962bfb05dad8ba6479f1eecbacca3d8bed0ce
FilippovZ <6479556@gmail.com>
2021-01-29 18:52:34 +0300

    fix bugs

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/Mag.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/Mag.java
@@ -2,6 +2,10 @@ package com.rakovets.course.java.core.practice.oop.principles;
 
 public class Mag extends Hero {
     private int health;
+    private final int MIN_HEALTH_FOR_RECOVERY = 20;
+    private final int MAX_RECOVERY = 10;
+    private final int MAX_HEALTH_FOR_RECOVERY = 50;
+    private final int MIN_RECOVERY = 5;
     public Mag(String name, int health) {
         super(name, health);
         this.health = health;
@@ -14,10 +18,10 @@ public class Mag extends Hero {
 
     @Override
     public void superPower() {
-        if (getHealth() < 20) {
-            setHealth(health + 10);
-        } else if (getHealth() >=20 || getHealth() < 50) {
-            setHealth(health + 5);
+        if (getHealth() < MIN_HEALTH_FOR_RECOVERY) {
+            setHealth(health + MAX_RECOVERY);
+        } else if (getHealth() >=MIN_HEALTH_FOR_RECOVERY || getHealth() < MAX_HEALTH_FOR_RECOVERY) {
+            setHealth(health + MIN_RECOVERY);
         }
     }
 }
