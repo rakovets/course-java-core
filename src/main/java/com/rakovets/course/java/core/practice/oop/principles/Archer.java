b3d94f229045b021cceb05ea447e56acc8845134
FilippovZ <6479556@gmail.com>
2021-01-29 18:50:00 +0300

    fix bugs

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/Archer.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/Archer.java
@@ -1,7 +1,11 @@
 package com.rakovets.course.java.core.practice.oop.principles;
 
-public class Archer extends Hero{
+public class Archer extends Hero {
     private int health;
+    private final int MIN_HEALTH_FOR_RECOVERY = 20;
+    private final int MAX_RECOVERY = 11;
+    private final int MAX_HEALTH_FOR_RECOVERY = 50;
+    private final int MIN_RECOVERY = 6;
     public Archer(String name, int health) {
         super(name, health);
         this.health = health;
@@ -14,10 +18,10 @@ public class Archer extends Hero{
 
     @Override
     public void superPower() {
-        if (getHealth() < 20) {
-            setHealth(health + 11);
-        } else if (getHealth() >=20 || getHealth() < 50) {
-            setHealth(health + 6);
+        if (getHealth() < MIN_HEALTH_FOR_RECOVERY) {
+            setHealth(health + MAX_RECOVERY);
+        } else if (getHealth() >= MIN_HEALTH_FOR_RECOVERY || getHealth() < MAX_HEALTH_FOR_RECOVERY) {
+            setHealth(health + MIN_RECOVERY);
         }
     }
 }
