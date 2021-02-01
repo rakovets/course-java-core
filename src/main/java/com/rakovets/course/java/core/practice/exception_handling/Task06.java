52b8a0db453bc8c1071b5bafd62caed9811fc218
OxanaPeregud <ox.peregud@gmail.com>
2021-02-01 15:08:17 +0300

    solution: Done Task06 for theme 'exception-handling'

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task06.java
@@ -0,0 +1,27 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+import java.io.FileNotFoundException;
+
+public class Task06 {
+    public static void main(String[] args) {
+
+        try {
+            randomExceptions();
+        } catch (FileNotFoundException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("File doesn't exist");
+        } catch (NullPointerException e) {
+            System.out.println("String is null");
+        }
+    }
+
+    public static void randomExceptions() throws FileNotFoundException, ArrayIndexOutOfBoundsException, NullPointerException {
+        switch((int) (Math.random() * 3)) {
+            case 1:
+                throw new FileNotFoundException();
+            case 2:
+                throw new ArrayIndexOutOfBoundsException();
+            default:
+                throw new NullPointerException();
+        }
+    }
+}
