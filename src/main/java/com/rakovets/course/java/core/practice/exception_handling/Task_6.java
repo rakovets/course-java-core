53b2145d9ebd7ec8c31898a6a53be99e3f8708bd
Aliaksei <phrismin@users.noreply.github.com>
2021-02-01 18:08:05 +0300

    solution: task_6 for theme "exception-handling"

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task_6.java
@@ -0,0 +1,29 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+import java.lang.instrument.IllegalClassFormatException;
+import java.util.Random;
+
+public class Task_6 {
+    public static void main(String[] args) {
+        try {
+            someMethod();
+        } catch (InterruptedException | NumberFormatException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalClassFormatException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static String someMethod() throws InterruptedException, NumberFormatException, IllegalClassFormatException {
+        Random random = new Random();
+        int i = random.nextInt(3);
+        if (i == 1) {
+            throw new InterruptedException("Interrupted format exception");
+        } else if (i == 2) {
+            throw new NumberFormatException("Check format number");
+        } else {
+            throw new IllegalClassFormatException("Check format");
+        }
+    }
+
+}
