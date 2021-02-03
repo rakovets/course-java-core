fcfbdaaffb77a1b44e581c2345aa2c955aba0208
FilippovZ <6479556@gmail.com>
2021-02-03 21:37:35 +0300

    fix bugs

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task06.java
@@ -1,14 +1,16 @@
 package com.rakovets.course.java.core.practice.exception_handling;
 
+import java.io.FileNotFoundException;
+
 public class Task06 {
-    public static void getRandomException() throws Exception {
+    public static void getRandomException() throws FileNotFoundException, ArrayIndexOutOfBoundsException, NullPointerException {
         double i = Math.random();
         if (i > 0 && i <= 0.33) {
-            throw new Exception("Exception 1");
+            throw new FileNotFoundException();
         } else if (i > 0.33 && i <= 0.66) {
-            throw new Exception("Exception 2");
+            throw new ArrayIndexOutOfBoundsException();
         } else {
-            throw new Exception("Exception 3");
+            throw new NullPointerException();
         }
     }
 }
