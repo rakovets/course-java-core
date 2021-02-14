9c593a3a2c6fce893dca671fb7c81c64938eee7e
FilippovZ <6479556@gmail.com>
2021-02-14 22:47:59 +0300

    quick save

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
@@ -1,15 +1,28 @@
 package com.rakovets.course.java.core.practice.lambda_expressions;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task01 {
+    public static void main(String[] args) {
+        String[] str = new String[] {"-i", "in.txt", "--limit", "40", "-d", "1", "-o", "out.txt"};
+        System.out.println(Arrays.toString(getArray(getMap(str))));
+    }
+
     public static Map<String, String> getMap(String[] keyValue) {
         keyValue[0] = keyValue[0].substring(1);
         String[] str = String.join(" ", keyValue).split(" -");
         return Stream.of(str)
                 .map(e -> e.split(" "))
                 .collect(Collectors.toMap(e -> "-" + e[0], e -> e[1]));
+    }
+
+    public static String[] getArray(Map<String, String> mapOfString) {
+        return mapOfString.entrySet().stream()
+                .map(e -> e.getKey() + " : " + e.getValue())
+                .toArray(String[]::new);
     }
 }
