b955958f4807ddd4afb401ab3f296cb8d372ef4a
OxanaPeregud <ox.peregud@gmail.com>
2021-02-14 22:52:10 +0300

    solution: Done demo for tasks 'Enum Types' for theme 'enum-types'

---
+++ src/test/java/com/rakovets/course/java/core/practice/enum_types/EnumTypes/EnumTypesDemo.java
@@ -0,0 +1,15 @@
+package com.rakovets.course.java.core.practice.enum_types.EnumTypes;
+
+class EnumTypesDemo {
+    public static void main(String[] args) {
+
+        EnumTypes enumTypes = new EnumTypes();
+        System.out.print("My favourite season is ");
+        enumTypes.getFavoriteSeason();
+        System.out.println("Description: ");
+        enumTypes.favorite();
+        System.out.println(enumTypes.getDescription(Season.SUMMER));
+        System.out.println("All seasons:");
+        enumTypes.display();
+    }
+}
