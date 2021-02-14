37e949631acababb763d56f2b2faa1cafa9a915c
OxanaPeregud <ox.peregud@gmail.com>
2021-02-14 12:26:37 +0300

    solution: Done demo for Task2 for theme 'lambda-expressions'

---
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/Task2Demo.java
@@ -0,0 +1,11 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+class Task2Demo {
+    public static void main(String[] args) {
+
+        Task2 task2 = new Task2();
+
+        task2.tableOneColumn();
+        task2.tableFiveColumn();
+    }
+}
