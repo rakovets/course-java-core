7ba4975b20b744c0acf360ba678670671315535a
Semargl92 <gavriljuk.9292@gmail.com>
2021-02-14 22:43:31 +0300

    solution: task 3 for project 'date wrapper' of theme 'date and time'

--- src/main/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapper.java
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapper.java
@@ -1,26 +1,34 @@
 package com.rakovets.course.java.core.practice.date_and_time.date_wrapper;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
-public class DateWrapper {
+public abstract class DateWrapper {
     public static LocalDate getLocalDate(int year, int month, int days) {
         return LocalDate.of(year, month, days);
     }
 
-    public static LocalDate getLocalDateAfterMonth(LocalDate now, int month) {
+    public static LocalDate getDateAfterMonth(LocalDate now, int month) {
         return now.plusMonths(month);
     }
 
-    public static String formatLocalDate(LocalDate now, DateTimeFormatter formatter) {
+    public static String formatDateToString(LocalDate now, DateTimeFormatter formatter) {
         return now.format(formatter);
     }
 
-    public static LocalDate parseStringToLocalDate(String string, DateTimeFormatter formatter) {
+    public static LocalDate parseStringToDate(String string, DateTimeFormatter formatter) {
         return LocalDate.parse(string, formatter);
     }
 
@@ -28,13 +36,73 @@ public class DateWrapper {
         return Math.abs(DAYS.between(first, second));
     }
 
+    // TASK 2
     public static LocalDate getDateAfterDays (LocalDate date, int days) {
-        TemporalAdjuster afterDays = (temporal) -> temporal.plus(days, DAYS);
+        TemporalAdjuster afterDays = temporal -> temporal.plus(days, DAYS);
         return (LocalDate) afterDays.adjustInto(date);
     }
 
-    public static TemporalAdjuster closestNewYear = (temporal) ->
-            Math.abs(DAYS.between(temporal, temporal.with(TemporalAdjusters.firstDayOfYear()))) >= 183L
+    public static TemporalAdjuster closestNewYear = temporal ->
+            (temporal.get(ChronoField.DAY_OF_YEAR) >= 183)
                     ? temporal.with(TemporalAdjusters.firstDayOfNextYear())
                     : temporal.with(TemporalAdjusters.firstDayOfYear());
+
+    // TASK 3
+    public static Date getDate(int year, int month, int days) throws ParseException {
+        String string = days + "." + month + "." + year;
+        return new SimpleDateFormat( "dd.MM.yyyy" ).parse(string);
+    }
+
+    public static Date getDateAfterMonth(Date date, int month) {
+        int monthDate = date.getMonth();
+        int yearDate = date.getYear();
+        int monthNumber = monthDate + month;
+
+        while (monthNumber > 12) {
+            monthNumber -= 12;
+            yearDate++;
+        }
+
+        date.setMonth(monthNumber);
+        date.setYear(yearDate);
+        return date;
+    }
+
+    public static String formatDateToString(Date date,  SimpleDateFormat formatter) {
+        return formatter.format(date);
+    }
+
+    public static Date parseStringToDateForDate(String string, SimpleDateFormat formatter) throws ParseException {
+        return formatter.parse(string);
+    }
+
+    public static long getDaysBetweenDates(Date first, Date second) {
+        long difference = Math.abs(first.getTime() - second.getTime());
+        return TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);
+    }
+
+    public static Calendar getCalendar(int year, int month, int days) {
+        month = month - 1;
+        return new GregorianCalendar(year, month , days);
+    }
+
+    public static Calendar getDateAfterMonth(Calendar calendar, int month) {
+        calendar.add(Calendar.MONTH, month);
+        return calendar;
+    }
+
+    public static String formatDateToString(Calendar calendar,  SimpleDateFormat formatter) {
+        return formatter.format(calendar.getTime());
+    }
+
+    public static Calendar parseStringToDate(String string, SimpleDateFormat formatter) throws ParseException {
+        Calendar calendar = Calendar.getInstance(Locale.US);
+        calendar.setTime(formatter.parse(string));
+        return calendar;
+    }
+
+    public static long getDaysBetweenDates(Calendar first, Calendar second) {
+        long difference = Math.abs(first.getTime().getTime() - second.getTime().getTime());
+        return TimeUnit.DAYS.convert(difference, TimeUnit.MILLISECONDS);
+    }
 }
--- src/test/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapperTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapperTest.java
@@ -6,8 +6,11 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.stream.Stream;
 
@@ -38,13 +41,13 @@ public class DateWrapperTest {
         // GIVEN
 
         // WHEN
-        String actualString = DateWrapper.getLocalDateAfterMonth(test, month).toString();
+        String actualString = DateWrapper.getDateAfterMonth(test, month).toString();
 
         // THEN
         Assertions.assertEquals(expectedString, actualString);
     }
 
-    static Stream<Arguments> provideArgumentsForFormatLocalDate() {
+    static Stream<Arguments> provideArgumentsForFormatDateToString() {
         return Stream.of(
                 Arguments.of("23-Feb-2020", "dd-MMM-yyyy"),
                 Arguments.of("February, 23, 2020", "MMMM, dd, yyyy"),
@@ -53,20 +56,20 @@ public class DateWrapperTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideArgumentsForFormatLocalDate")
-    void formatLocalDate(String expectedString, String pattern) {
+    @MethodSource("provideArgumentsForFormatDateToString")
+    void formatDateToString(String expectedString, String pattern) {
         // GIVEN
         LocalDate test = LocalDate.of(2020, 2, 23);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern, Locale.US);
 
         // WHEN
-       String actualString = DateWrapper.formatLocalDate(test, formatter);
+       String actualString = DateWrapper.formatDateToString(test, formatter);
 
         // THEN
         Assertions.assertEquals(expectedString, actualString);
     }
 
-    static Stream<Arguments> provideArgumentsForParseStringToLocalDate() {
+    static Stream<Arguments> provideArgumentsForParseStringToDate() {
         return Stream.of(
                 Arguments.of("23-Feb-2020", "dd-MMM-yyyy"),
                 Arguments.of("February, 23, 2020", "MMMM, dd, yyyy"),
@@ -75,13 +78,13 @@ public class DateWrapperTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideArgumentsForParseStringToLocalDate")
-    void parseStringToLocalDate(String string, String pattern) {
+    @MethodSource("provideArgumentsForParseStringToDate")
+    void parseStringToDate(String string, String pattern) {
         // GIVEN
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern, Locale.US);
 
         // WHEN
-        LocalDate actualDate = DateWrapper.parseStringToLocalDate(string, formatter);
+        LocalDate actualDate = DateWrapper.parseStringToDate(string, formatter);
 
         // THEN
         Assertions.assertEquals(LocalDate.of(2020, 2, 23), actualDate);
@@ -147,5 +150,218 @@ public class DateWrapperTest {
 
         // THEN
         Assertions.assertEquals(expectedDate, actualDate);
+    }
+
+    @Test
+    void getDate() {
+        // GIVEN
+        Date actualDate = new Date();
+
+        // WHEN
+        try {
+            actualDate = DateWrapper.getDate(2021, 2, 14);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        // THEN
+        Assertions.assertEquals("Sun Feb 14 00:00:00 MSK 2021", actualDate.toString());
+    }
+
+    static Stream<Arguments> provideArgumentsForGetDateAfterMonth() {
+        return Stream.of(
+                Arguments.of("Wed Jul 14 00:00:00 MSK 2021", 5),
+                Arguments.of("Sat May 14 00:00:00 MSK 2022", 15),
+                Arguments.of("Tue Feb 14 00:00:00 MSK 2023", 24)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForGetDateAfterMonth")
+    void getDateAfterMonth(String expectedString, int month) {
+        // GIVEN
+        Date test = new Date();
+        try {
+            test = DateWrapper.getDate(2021, 2, 14);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        // WHEN
+        String actualString = DateWrapper.getDateAfterMonth(test, month).toString();
+
+        // THEN
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    static Stream<Arguments> provideArgumentsForFormatDateToStringForDate() {
+        return Stream.of(
+                Arguments.of("23-Feb-2020", "dd-MMM-yyyy"),
+                Arguments.of("February, 23, 2020", "MMMM, dd, yyyy"),
+                Arguments.of("23.02.2020", "dd.MM.yyyy")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForFormatDateToStringForDate")
+    void formatDateToStringForDate(String expectedString, String pattern) {
+        // GIVEN
+        Date test = new Date();
+        try {
+            test = DateWrapper.getDate(2020, 2, 23);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern, Locale.US);
+
+        // WHEN
+        String actualString = DateWrapper.formatDateToString(test, formatter);
+
+        // THEN
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    static Stream<Arguments> provideArgumentsForParseStringToDateForDate() {
+        return Stream.of(
+                Arguments.of("23-Feb-2020", "dd-MMM-yyyy"),
+                Arguments.of("February, 23, 2020", "MMMM, dd, yyyy"),
+                Arguments.of("23.02.2020", "dd.MM.yyyy")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForParseStringToDateForDate")
+    void parseStringToDateForDate(String string, String pattern) {
+        // GIVEN
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern, Locale.US);
+        Date actualDate = new Date();
+
+        // WHEN
+        try {
+        actualDate = DateWrapper.parseStringToDateForDate(string, formatter);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        // THEN
+        Assertions.assertEquals("Sun Feb 23 00:00:00 MSK 2020", actualDate.toString());
+    }
+
+    @Test
+    void getDaysBetweenDates() {
+        // GIVEN
+        Date test1 = new Date();
+        try {
+            test1 = DateWrapper.getDate(2020, 2, 14);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        Date test2 = new Date();
+        try {
+            test2 = DateWrapper.getDate(2021, 2, 23);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        // WHEN
+        Long actualLong = DateWrapper.getDaysBetweenDates(test1, test2);
+
+        // THEN
+        Assertions.assertEquals(375L, actualLong);
+    }
+
+    @Test
+    void getCalendar() {
+        // GIVEN
+
+        // WHEN
+        Calendar actualCalendar = DateWrapper.getCalendar(2021, 2, 14);
+
+        // THEN
+        Assertions.assertEquals("Sun Feb 14 00:00:00 MSK 2021", actualCalendar.getTime().toString());
+    }
+
+    static Stream<Arguments> provideArgumentsForGetDateAfterMonthForCalendar() {
+        return Stream.of(
+                Arguments.of("Wed Jul 14 00:00:00 MSK 2021", 5),
+                Arguments.of("Sat May 14 00:00:00 MSK 2022", 15),
+                Arguments.of("Tue Feb 14 00:00:00 MSK 2023", 24)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForGetDateAfterMonthForCalendar")
+    void getDateAfterMonthForCalendar(String expectedString, int month) {
+        // GIVEN
+        Calendar test = DateWrapper.getCalendar(2021, 2, 14);
+
+        // WHEN
+        String actualString = DateWrapper.getDateAfterMonth(test, month).getTime().toString();
+
+        // THEN
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+
+    static Stream<Arguments> provideArgumentsForFormatDateToStringForCalendar() {
+        return Stream.of(
+                Arguments.of("23-Feb-2020", "dd-MMM-yyyy"),
+                Arguments.of("February, 23, 2020", "MMMM, dd, yyyy"),
+                Arguments.of("23.02.2020", "dd.MM.yyyy")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForFormatDateToStringForCalendar")
+    void formatDateToStringForCalendar(String expectedString, String pattern) {
+        // GIVEN
+        Calendar test = DateWrapper.getCalendar(2020, 2, 23);
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern, Locale.US);
+
+        // WHEN
+        String actualString = DateWrapper.formatDateToString(test, formatter);
+
+        // THEN
+        Assertions.assertEquals(expectedString, actualString);
+    }
+
+    static Stream<Arguments> provideArgumentsForParseStringToDateForCalendar() {
+        return Stream.of(
+                Arguments.of("23-Feb-2020", "dd-MMM-yyyy"),
+                Arguments.of("February, 23, 2020", "MMMM, dd, yyyy"),
+                Arguments.of("23.02.2020", "dd.MM.yyyy")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForParseStringToDateForCalendar")
+    void parseStringToDateForCalendar(String string, String pattern) {
+        // GIVEN
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern, Locale.US);
+        Calendar actualCalendar = Calendar.getInstance(Locale.US);
+
+        // WHEN
+        try {
+            actualCalendar = DateWrapper.parseStringToDate(string, formatter);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        // THEN
+        Assertions.assertEquals("Sun Feb 23 00:00:00 MSK 2020", actualCalendar.getTime().toString());
+    }
+
+    @Test
+    void getDaysBetweenDatesForCalendar() {
+        // GIVEN
+        Calendar test1 = DateWrapper.getCalendar(2020, 2, 14);
+        Calendar test2 = DateWrapper.getCalendar(2021, 2, 23);
+
+        // WHEN
+        Long actualLong = DateWrapper.getDaysBetweenDates(test1, test2);
+
+        // THEN
+        Assertions.assertEquals(375L, actualLong);
     }
 }
