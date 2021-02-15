c40c957b77432d7a738b4b9f75a6c688315142f3
Eugene <e.dunin97@gmail.com>
2021-02-15 19:35:26 +0300

    solution: all tasks for theme "Date-and-time"

--- src/main/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapper.java
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapper.java
@@ -1,14 +1,18 @@
 package com.rakovets.course.java.core.practice.date_and_time.date_wrapper;
 
-import javax.swing.text.DateFormatter;
-import java.text.SimpleDateFormat;
-import java.time.DateTimeException;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.time.LocalDate;
-import java.time.Period;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DateWrapper {
-    public static LocalDate getLocalDate(int year, int month, int day) {
+    public static LocalDate getDate(int year, int month, int day) {
         return LocalDate.of(year, month, day);
     }
 
@@ -21,12 +25,122 @@ public class DateWrapper {
     }
 
     public static LocalDate getLocalDateFromString(String date, DateTimeFormatter formatter) {
-        return LocalDate.parse(date,formatter);
+        return LocalDate.parse(date, formatter);
     }
 
-    public static Period getPeriodBetweenLocalDate(LocalDate localDate1, LocalDate localDate2) {
-        return Period.between(localDate1,localDate2);
+    public static long getDaysBetween(LocalDate localDate1, LocalDate localDate2) {
+        return ChronoUnit.DAYS.between(localDate1, localDate2);
+    }
+
+    public static LocalDate adjustDay(LocalDate localDate, int days) {
+        TemporalAdjuster dayAdjuster = TemporalAdjusters.ofDateAdjuster(date -> (date).plus(days, ChronoUnit.DAYS));
+        return localDate.with(dayAdjuster);
+    }
+
+    public static LocalDate getFirstJanuary(LocalDate localDate) {
+        TemporalAdjuster firstJanuaryAdjusterOfThisYear = TemporalAdjusters.firstDayOfYear();
+        TemporalAdjuster firstJanuaryAdjusterOfNextYear = TemporalAdjusters.firstDayOfNextYear();
+        if (localDate.getMonthValue() > 7) {
+            return localDate.with(firstJanuaryAdjusterOfNextYear);
+        }
+        if (localDate.getMonthValue() == 7) {
+            if (localDate.getDayOfMonth() >= 2) {
+                return localDate.with(firstJanuaryAdjusterOfNextYear);
+            }
+        }
+        return localDate.with(firstJanuaryAdjusterOfThisYear);
+    }
+
+    public static String getDate(Date date, DateFormat formatter) {
+        return formatter.format(date);
+    }
+
+    public static String getDatePlusMonth(Date date, int month, DateFormat formatter) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.MONTH, month);
+        date = calendar.getTime();
+        return formatter.format(date);
+    }
+
+    public static String getStringDate(Date date, DateFormat formatter) {
+        return formatter.format(date);
+    }
+
+    public static Date getDateFromString(String date, DateFormat formatter) throws ParseException {
+        return formatter.parse(date);
+    }
+
+    public static long getDaysBetween(Date date1, Date date2) {
+        LocalDate localDate1 = LocalDate.ofInstant(date1.toInstant(), ZoneId.systemDefault());
+        LocalDate localDate2 = LocalDate.ofInstant(date2.toInstant(), ZoneId.systemDefault());
+        return ChronoUnit.DAYS.between(localDate1, localDate2);
+    }
+
+    public static String adjustDay(Date date, int days, DateFormat formatter) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DATE, days);
+        date = calendar.getTime();
+        return formatter.format(date);
+    }
+
+    public static String getFirstJanuary(Date date, DateFormat formatter) {
+        LocalDate localDate = new java.sql.Date(date.getTime()).toLocalDate();
+        TemporalAdjuster firstJanuaryAdjusterOfThisYear = TemporalAdjusters.firstDayOfYear();
+        TemporalAdjuster firstJanuaryAdjusterOfNextYear = TemporalAdjusters.firstDayOfNextYear();
+        LocalDate localDateThisYear = localDate.with(firstJanuaryAdjusterOfThisYear);
+        LocalDate localDateNextYear = localDate.with(firstJanuaryAdjusterOfNextYear);
+        if (localDate.getMonthValue() > 7) {
+            return formatter.format(java.sql.Date.valueOf(localDateNextYear));
+        }
+        if (localDate.getMonthValue() == 7) {
+            if (localDate.getDayOfMonth() >= 2) {
+                return formatter.format(java.sql.Date.valueOf(localDateNextYear));
+            }
+        }
+        return formatter.format(java.sql.Date.valueOf(localDateThisYear));
+    }
+
+    public static String getDate(Calendar calendar, DateFormat formatter) {
+        return formatter.format(calendar.getTime());
+    }
+
+    public static String getDatePlusMonth(Calendar calendar, int month, DateFormat formatter) {
+        calendar.add(Calendar.MONTH, month);
+        return formatter.format(calendar.getTime());
+    }
+
+    public static String getStringDate(Calendar calendar, DateFormat formatter) {
+        Date date = calendar.getTime();
+        return formatter.format(date);
     }
 
 
+    public static long getDaysBetween(Calendar calendar1, Calendar calendar2) {
+        LocalDate localDate1 = LocalDate.ofInstant(calendar1.toInstant(), ZoneId.systemDefault());
+        LocalDate localDate2 = LocalDate.ofInstant(calendar2.toInstant(), ZoneId.systemDefault());
+        return ChronoUnit.DAYS.between(localDate1, localDate2);
+    }
+
+    public static String adjustDay(Calendar calendar, int days, DateFormat formatter) {
+        calendar.add(Calendar.DATE, days);
+        return formatter.format(calendar.getTime());
+    }
+
+    public static String getFirstJanuary(Calendar calendar, DateFormat formatter) {
+        Date date = calendar.getTime();
+        LocalDate localDate = new java.sql.Date(date.getTime()).toLocalDate();
+        TemporalAdjuster firstJanuaryAdjusterOfThisYear = TemporalAdjusters.firstDayOfYear();
+        TemporalAdjuster firstJanuaryAdjusterOfNextYear = TemporalAdjusters.firstDayOfNextYear();
+        if (localDate.getMonthValue() > 7) {
+            return formatter.format(java.sql.Date.valueOf(localDate.with(firstJanuaryAdjusterOfNextYear)));
+        }
+        if (localDate.getMonthValue() == 7) {
+            if (localDate.getDayOfMonth() > 1) {
+                return formatter.format(java.sql.Date.valueOf(localDate.with(firstJanuaryAdjusterOfNextYear)));
+            }
+        }
+        return formatter.format(java.sql.Date.valueOf(localDate.with(firstJanuaryAdjusterOfThisYear)));
+    }
 }
---
+++ src/test/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapperTest.java
@@ -0,0 +1,268 @@
+package com.rakovets.course.java.core.practice.date_and_time.date_wrapper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.stream.Stream;
+
+class DateWrapperTest {
+
+    @Test
+    void getLocalDate() {
+        //Given
+        LocalDate expectedResult = LocalDate.of(2020, 10, 20);
+        //When
+        LocalDate actualResult = DateWrapper.getDate(2020, 10, 20);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void getLocalDatePlusMonth() {
+        //Given
+        LocalDate expectedResult = LocalDate.of(2020, 12, 20);
+        LocalDate localDate = LocalDate.of(2020, 10, 20);
+        //When
+        LocalDate actualResult = DateWrapper.getLocalDatePlusMonth(localDate, 2);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void getStringDate() {
+        //Given
+        String expectedResult = "20-10-2020";
+        LocalDate localDate = LocalDate.of(2020, 10, 20);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        //When
+        String actualResult = DateWrapper.getStringDate(localDate, dateFormatter);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void getLocalDateFromString() {
+        //Given
+        LocalDate expectedResult = LocalDate.of(2020, 10, 20);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        //When
+        LocalDate actualResult = DateWrapper.getLocalDateFromString("20-10-2020", dateFormatter);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void getDaysBetween() {
+        //Given
+        LocalDate localDate1 = LocalDate.of(2020, 10, 20);
+        LocalDate localDate2 = LocalDate.of(2020, 12, 10);
+        int expectedResult = 51;
+        //When
+        long actualResult = DateWrapper.getDaysBetween(localDate1, localDate2);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void adjustDay() {
+        //Given
+        LocalDate localDate = LocalDate.of(2020, 10, 20);
+        LocalDate expectedResult = LocalDate.of(2020, 10, 25);
+        //When
+        LocalDate actualResult = DateWrapper.adjustDay(localDate, 5);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    static Stream<Arguments> getFirstJanuaryProviderArguments() {
+        return Stream.of(
+                Arguments.of(LocalDate.of(2021, 1, 1), LocalDate.of(2020, 7, 2)),
+                Arguments.of(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 7, 1))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getFirstJanuaryProviderArguments")
+    void getFirstJanuary(LocalDate expectedResult, LocalDate localDate) {
+        // WHEN
+        LocalDate actualResult = DateWrapper.getFirstJanuary(localDate);
+        // THAT
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Test
+    void getDateForDate() {
+        //Given
+        String expectedResult = "01-11-2021";
+        Date date = new Date(1635770792000L);
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        //When
+        String actualResult = DateWrapper.getDate(date, dateFormat);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testGetLocalDatePlusMonth() {
+        //Given
+        String expectedResult = "01-01-2022";
+        Date date = new Date(1635770792000L);
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        //When
+        String actualResult = DateWrapper.getDatePlusMonth(date, 2, dateFormat);
+        //ThanalDate
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testGetStringDate() {
+        //Given
+        String expectedResult = "01-11-2021";
+        Date date = new Date(1635770792000L);
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        //When
+        String actualResult = DateWrapper.getStringDate(date, dateFormat);
+        //ThanalDate
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testGetDateFromString() throws ParseException {
+        //Given
+        Date date = new Date(1625086800000L);
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        //When
+        Date actualResult = DateWrapper.getDateFromString("01-07-2021", dateFormat);
+        //Than
+        Assertions.assertEquals(date, actualResult);
+    }
+
+    @Test
+    void testGetDaysBetween() {
+        //Given
+        Date date1 = new Date(1635770792000L);
+        Date date2 = new Date(1640436392000L);
+        long expectedResult = 54;
+        //When
+        long actualResult = DateWrapper.getDaysBetween(date1, date2);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testAdjustDay() {
+        //Given
+        String expectedResult = "07-11-2021";
+        Date date = new Date(1635770792000L);
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        //When
+        String actualResult = DateWrapper.adjustDay(date, 6, dateFormat);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    static Stream<Arguments> getFirstJanuaryForDateProviderArguments() {
+        return Stream.of(
+                Arguments.of("01-01-2022", new Date(1635770792000L), new SimpleDateFormat("dd-MM-yyyy")),
+                Arguments.of("01-01-2021", new Date(1625143592000L), new SimpleDateFormat("dd-MM-yyyy"))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getFirstJanuaryForDateProviderArguments")
+    void getFirstJanuaryForDate(String expectedResult, Date date, DateFormat dateFormat) {
+        // WHEN
+        String actualResult = DateWrapper.getFirstJanuary(date, dateFormat);
+        // THAT
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Test
+    void testGetDateForCalendar() {
+        //Given
+        String expectedResult = "20-12-2020";
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar calendar = new GregorianCalendar(2020, Calendar.DECEMBER, 20);
+        //When
+        String actualResult = DateWrapper.getDate(calendar, dateFormat);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testGetDatePlusMonth() {
+        //Given
+        String expectedResult = "20-12-2020";
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar calendar = new GregorianCalendar(2020, Calendar.OCTOBER, 20);
+        //When
+        String actualResult = DateWrapper.getDatePlusMonth(calendar, 2, dateFormat);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testGetStringDate1() {
+        //Given
+        String expectedResult = "20-12-2020";
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar calendar = new GregorianCalendar(2020, Calendar.DECEMBER, 20);
+        //When
+        String actualResult = DateWrapper.getStringDate(calendar, dateFormat);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testGetDaysBetween1() {
+        //Given
+        Calendar calendar1 = new GregorianCalendar(2020, Calendar.NOVEMBER, 1);
+        Calendar calendar2 = new GregorianCalendar(2020, Calendar.DECEMBER, 21);
+        long expectedResult = 50;
+        //When
+        long actualResult = DateWrapper.getDaysBetween(calendar1, calendar2);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testAdjustDay1() {
+        //Given
+        String expectedResult = "07-11-2021";
+        Calendar calendar = new GregorianCalendar(2021, Calendar.NOVEMBER, 1);
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        //When
+        String actualResult = DateWrapper.adjustDay(calendar, 6, dateFormat);
+        //Than
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+        static Stream<Arguments> getFirstJanuaryForCalendarProviderArguments() {
+            return Stream.of(
+                    Arguments.of("01-01-2021", new GregorianCalendar(2020, Calendar.JULY,2), new SimpleDateFormat("dd-MM-yyyy")),
+                    Arguments.of("01-01-2020", new GregorianCalendar(2020, Calendar.JULY,1), new SimpleDateFormat("dd-MM-yyyy"))
+            );
+        }
+
+        @ParameterizedTest
+        @MethodSource("getFirstJanuaryForCalendarProviderArguments")
+        void getFirstJanuaryForCalendar(String expectedResult, Calendar calendar, DateFormat dateFormat) {
+            // WHEN
+            String actualResult = DateWrapper.getFirstJanuary(calendar, dateFormat);
+            // THAT
+            Assertions.assertEquals(expectedResult, actualResult);
+        }
+    }
