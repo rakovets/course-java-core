db5d64d856c39048d0fcf14636e0f8208df3b119
Владислав Тушинский <xpomsteam1997@gmail.com>
2021-02-15 22:34:29 +0300

    fix: Project 'DateWrapper' for theme 'Date and Time'

--- src/main/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapper.java
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapper.java
@@ -10,21 +10,27 @@ import java.time.temporal.TemporalAdjusters;
 import java.util.Calendar;
 
 public class DateWrapper {
+
     public static LocalDate getLocalDate(int year, int mount, int day) {
         return LocalDate.of(year, mount, day);
     }
+
     public static LocalDate getLocalDateAfterPlusMount(LocalDate dateTime, int mount) {
         return dateTime.plusMonths(mount);
     }
+
     public static String getStringDateTime(LocalDate localDate, DateTimeFormatter formatter) {
         return localDate.format(formatter);
     }
+
     public static LocalDate getLocalDateFromString(String date, DateTimeFormatter formatter) {
         return LocalDate.parse(date, formatter);
     }
+
     public static long getDaysBetween(LocalDate localDate1, LocalDate localDate2) {
         return ChronoUnit.DAYS.between(localDate1, localDate2);
     }
+
     public static TemporalAdjuster getAddDays(int days) {
         return temporal -> temporal.plus(Period.ofDays(days));
     }
