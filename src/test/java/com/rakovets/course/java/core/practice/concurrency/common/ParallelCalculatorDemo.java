0c145fdc9a4f439ac321573795389177ce83590f
FilippovZ <6479556@gmail.com>
2021-03-02 10:44:03 +0300

    quicksave

--- src/test/java/com/rakovets/course/java/core/practice/concurrency/common/ParallelCalculatorDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/concurrency/common/ParallelCalculatorDemo.java
@@ -8,14 +8,17 @@ import java.util.List;
 import java.util.Random;
 
 public class ParallelCalculatorDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         List<int[]> list = new ArrayList<>();
         for (int x = 0; x < 10; x++) {
             list.add(new Random().ints(new Random().nextInt(1000000), 1, 301).toArray());
         }
         ParallelCalculator.calculateWithThreads(list, 1, AnsiColorCode.FG_RED_BOLD_BRIGHT);
+        Thread.sleep(1000);
         ParallelCalculator.calculateWithThreads(list, 2, AnsiColorCode.FG_GREEN_BOLD_BRIGHT);
+        Thread.sleep(1000);
         ParallelCalculator.calculateWithThreads(list, 5, AnsiColorCode.FG_CYAN_BOLD_BRIGHT);
+        Thread.sleep(1000);
         ParallelCalculator.calculateWithThreads(list, 10, AnsiColorCode.FG_YELLOW_BOLD_BRIGHT);
     }
 }
