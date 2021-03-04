4c70f0f845b1d31189383cf583cdd6a4766e72bc
FilippovZ <6479556@gmail.com>
2021-03-05 02:31:02 +0300

    quicksave

--- src/test/java/com/rakovets/course/java/core/practice/concurrency/thread_synchronization/SkyNetDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/concurrency/thread_synchronization/SkyNetDemo.java
@@ -1,11 +1,8 @@
 package com.rakovets.course.java.core.practice.concurrency.thread_synchronization;
 
 import com.rakovets.course.java.core.practice.concurrency.thread_synchronization.skyNet.*;
-
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class SkyNetDemo {
     public static void main(String[] args) {
@@ -14,11 +11,8 @@ public class SkyNetDemo {
         Factory factory = new Factory(storage);
         World world = new World(storage);
         Wednesday wednesday = new Wednesday(storage);
-
         world.start();
         wednesday.start();
         factory.start();
-
-
     }
 }
