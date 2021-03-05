99ec024b500680398603a74197a43274d277442d
OxanaPeregud <ox.peregud@gmail.com>
2021-03-05 09:14:06 +0300

    solution: Fixed project 'Store'

--- src/main/java/com/rakovets/course/java/core/practice/concurrency/thread_synchronization/store/Store.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/thread_synchronization/store/Store.java
@@ -1,22 +1,16 @@
 package com.rakovets.course.java.core.practice.concurrency.thread_synchronization.store;
 
-import com.rakovets.course.java.core.util.AnsiColorCode;
-import com.rakovets.course.java.core.util.StandardOutputUtil;
+import com.rakovets.course.java.core.practice.concurrency.thread_synchronization.store.view.StoreView;
 
 import java.util.Random;
 
 public class Store {
-    Cashiers cashiers = new Cashiers();
+    private final Cashiers cashiers = new Cashiers();
+    public int cashier = new Random().nextInt(cashiers.getSize());
 
     public void consume(CustomerThread customerThread) {
-        int cashier = new Random().nextInt(cashiers.getSize());
         synchronized (cashiers.get(cashier)) {
-            StandardOutputUtil.println(cashiers.get(cashier) + " served " +
-                    customerThread.getCustomer(), AnsiColorCode.FG_RED_BOLD);
-            for(Product product : customerThread.getShoppingList()) {
-                StandardOutputUtil.println(cashiers.get(cashier) + " checked out " +
-                        product, AnsiColorCode.FG_BLACK_BOLD_BRIGHT);
-            }
+            StoreView.cashiersServed(customerThread);
         }
     }
 }
