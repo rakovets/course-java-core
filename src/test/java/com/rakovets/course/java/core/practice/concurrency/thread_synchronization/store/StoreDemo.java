2a9df0350121f269887f731cba3ad37064afe856
OxanaPeregud <ox.peregud@gmail.com>
2021-03-05 09:14:25 +0300

    solution: Fixed demo for project 'Store'

--- src/test/java/com/rakovets/course/java/core/practice/concurrency/thread_synchronization/store/StoreDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/concurrency/thread_synchronization/store/StoreDemo.java
@@ -3,12 +3,12 @@ package com.rakovets.course.java.core.practice.concurrency.thread_synchronizatio
 public class StoreDemo {
     public static void main(String[] args) {
 
-        int numberOfBuyers = 10;
+        int numberOfCustomers = 10;
 
-        CustomerThread[] customerThread = new CustomerThread[numberOfBuyers];
+        CustomerThread[] customerThread = new CustomerThread[numberOfCustomers];
         Store store = new Store();
 
-        for (int i = 0; i < numberOfBuyers; i++) {
+        for (int i = 0; i < numberOfCustomers; i++) {
             customerThread[i] = new CustomerThread(store);
             new Thread(customerThread[i]).start();
         }
