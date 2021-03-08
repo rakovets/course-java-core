38b1202883ec0c803a48498ceb55a0192c7856ac
Semargl92 <gavriljuk.9292@gmail.com>
2021-03-09 01:25:42 +0300

    fix: project 'improved parallel calculator' for theme 'concurrent utilities' (threads safety)

--- src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/improved_parallel_calculator/ImprovedParallelCalculator.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/improved_parallel_calculator/ImprovedParallelCalculator.java
@@ -3,6 +3,7 @@ package com.rakovets.course.java.core.practice.concurrent.utilities.improved_par
 import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executor;
+import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class ImprovedParallelCalculator {
     public static Map<int[], Integer> calculate(List<int[]> data) {
@@ -19,20 +20,26 @@ public abstract class ImprovedParallelCalculator {
     public static Map<int[], Integer> calculateInThreads(List<int[]> data, int numberOfThreads) {
         Map<int[], Integer> result = new HashMap<>();
         CopyOnWriteArrayList<int[]> stackForProcessing = new CopyOnWriteArrayList<>(data);
-        long start = System.currentTimeMillis();
+        ReentrantLock lock = new ReentrantLock();
         String marker = String.format("Thread of %d threads", numberOfThreads);
+        long start = System.currentTimeMillis();
 
         Runnable calculatorUnit = () -> {
                 while (!stackForProcessing.isEmpty()) {
-                    int[] array = stackForProcessing.remove(0);
-                    OptionalInt sum = Arrays.stream(array).reduce(Integer::sum);
-                    if (sum.isPresent()) {
-                        result.put(array, sum.getAsInt());
+                    lock.lock();
+                    if (!stackForProcessing.isEmpty()) {
+                        int[] array = stackForProcessing.remove(0);
+                        lock.unlock();
+                        OptionalInt sum = Arrays.stream(array).reduce(Integer::sum);
+                        if (sum.isPresent()) {
+                            result.put(array, sum.getAsInt());
+                        }
+                    } else {
+                        lock.unlock();
                     }
                 }
             System.out.println(marker + " - " + (System.currentTimeMillis() - start));
         };
-
         Executor executor = (runnable) -> new Thread(runnable).start();
 
         for (int x = 1; x <= numberOfThreads; x++) {
