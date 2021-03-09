f59e67f62349cd15eac65e1101991ba9a3601aae
Semargl92 <gavriljuk.9292@gmail.com>
2021-03-09 12:14:16 +0300

    fix: project 'improved parallel calculator' for theme 'concurrent utilities' (variable name)

--- src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/improved_parallel_calculator/ImprovedParallelCalculator.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/improved_parallel_calculator/ImprovedParallelCalculator.java
@@ -19,16 +19,16 @@ public abstract class ImprovedParallelCalculator {
 
     public static Map<int[], Integer> calculateInThreads(List<int[]> data, int numberOfThreads) {
         Map<int[], Integer> result = new HashMap<>();
-        CopyOnWriteArrayList<int[]> stackForProcessing = new CopyOnWriteArrayList<>(data);
+        CopyOnWriteArrayList<int[]> listForProcessing = new CopyOnWriteArrayList<>(data);
         ReentrantLock lock = new ReentrantLock();
         String marker = String.format("Thread of %d threads", numberOfThreads);
         long start = System.currentTimeMillis();
 
         Runnable calculatorUnit = () -> {
-                while (!stackForProcessing.isEmpty()) {
+                while (!listForProcessing.isEmpty()) {
                     lock.lock();
-                    if (!stackForProcessing.isEmpty()) {
-                        int[] array = stackForProcessing.remove(0);
+                    if (!listForProcessing.isEmpty()) {
+                        int[] array = listForProcessing.remove(0);
                         lock.unlock();
                         OptionalInt sum = Arrays.stream(array).reduce(Integer::sum);
                         if (sum.isPresent()) {
