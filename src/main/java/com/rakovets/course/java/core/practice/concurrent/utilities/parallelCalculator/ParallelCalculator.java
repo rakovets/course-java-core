06b60846d1336c6f818afc06ad8212d859c8ac3c
FilippovZ <6479556@gmail.com>
2021-03-09 15:12:22 +0300

    quicksave

--- src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/parallelCalculator/ParallelCalculator.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/parallelCalculator/ParallelCalculator.java
@@ -3,6 +3,7 @@ package com.rakovets.course.java.core.practice.concurrent.utilities.parallelCalc
 import com.rakovets.course.java.core.util.AnsiColorCode;
 
 import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class ParallelCalculator {
     public static Map<int[], Integer> calculate(List<int[]> arraysOfInt) {
@@ -20,13 +21,20 @@ public abstract class ParallelCalculator {
         Map<int[], Integer> map = new HashMap<>();
         List<int[]> list = new ArrayList<>(arraysOfInt);
         long startTime = System.currentTimeMillis();
+        ReentrantLock locker = new ReentrantLock();
         Runnable thread = () -> {
             while (!list.isEmpty()) {
+                locker.lock();
+                if (!list.isEmpty()) {
                     int[] array = list.remove(0);
+                    locker.unlock();
                     OptionalInt sum = Arrays.stream(array).reduce(Integer::sum);
                     if (sum.isPresent()) {
                         map.put(array, sum.getAsInt());
                     }
+                } else {
+                    locker.unlock();
+                }
             }
                     System.out.printf(codes + "%d Threads: %d ms\n" + codes, numberOfThreads, System.currentTimeMillis() - startTime);
         };
