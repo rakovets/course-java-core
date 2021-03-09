31e07ec71b328890e14aefe0f1db026119b4e69e
Semargl92 <gavriljuk.9292@gmail.com>
2021-03-09 12:18:17 +0300

    fix: project 'reflection utilities' for theme 'reflection' (unnecessary field deleting)

--- src/main/java/com/rakovets/course/java/core/practice/reflection/reflection_utilities/ReflectionUtilException.java
+++ src/main/java/com/rakovets/course/java/core/practice/reflection/reflection_utilities/ReflectionUtilException.java
@@ -1,9 +1,7 @@
 package com.rakovets.course.java.core.practice.reflection.reflection_utilities;
 
 public class ReflectionUtilException extends Exception {
-    private final String message;
-
     public ReflectionUtilException(String message) {
-        this.message = message;
+        super(message);
     }
 }
