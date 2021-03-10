0eb5ba331e4c760783f34bbc9e9e786b0d26c7c8
FilippovZ <6479556@gmail.com>
2021-03-10 16:42:34 +0300

    quicksave

--- src/test/java/com/rakovets/course/java/core/practice/concurrent/utilities/MasterAndWorkerDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/concurrent/utilities/MasterAndWorkerDemo.java
@@ -5,12 +5,18 @@ import com.rakovets.course.java.core.practice.concurrent.utilities.masterAndWork
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MasterAndWorkerDemo {
     public static void main(String[] args) {
         List<Integer> list = new LinkedList<>();
-        ThreadWorker threadWorker = new ThreadWorker(list);
+        ReentrantLock locker = new ReentrantLock();
+        ThreadWorker threadWorker = new ThreadWorker(list, locker);
         threadWorker.start();
+        ThreadWorker threadWorker2 = new ThreadWorker(list, locker);
+        threadWorker2.start();
+        ThreadWorker threadWorker3 = new ThreadWorker(list, locker);
+        threadWorker3.start();
         ThreadMaster threadMaster = new ThreadMaster(list);
         threadMaster.start();
     }
