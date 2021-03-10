f9a97c8c06a271348bb8df621c89dd0871501409
FilippovZ <6479556@gmail.com>
2021-03-10 17:06:39 +0300

    quicksave

--- src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/masterAndWorker/ThreadWorker.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/masterAndWorker/ThreadWorker.java
@@ -19,8 +19,6 @@ public class ThreadWorker extends Thread {
 
     @Override
     public void run() {
-        int counter = 0;
-
         int timeSleep = 0;
         while (isActive) {
             try (FileWriter writer = new FileWriter("src/test/java/resources/practice/concurrent/utilities/masterWorker/worker.txt", true)) {
@@ -31,19 +29,16 @@ public class ThreadWorker extends Thread {
                     writer.write(timestamp + Thread.currentThread().getName() + " - ...\n");
                     locker.unlock();
                 } else if (listOfNumbers.size() > 0 && listOfNumbers.get(0) != -1) {
-
                     timeSleep = listOfNumbers.get(listOfNumbers.size() - 1);
                     listOfNumbers.remove(0);
                     Thread.sleep(timeSleep * 1000);
                     locker.lock();
                     Timestamp timestamp = new Timestamp(System.currentTimeMillis());
                     writer.write(timestamp + Thread.currentThread().getName() + " - I slept " + timeSleep + " seconds\n");
-
                     locker.unlock();
                 } else if (listOfNumbers.get(0) == - 1) {
                     isActive = false;
                 }
-
             } catch (InterruptedException | IOException e) {
                 e.printStackTrace();
             }
