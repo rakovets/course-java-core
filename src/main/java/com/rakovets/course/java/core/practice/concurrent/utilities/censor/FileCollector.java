6293b80ec5287cf87c005bb75eb29bad76270c34
Semargl92 <gavriljuk.9292@gmail.com>
2021-03-11 14:25:31 +0300

    fix: project 'censor' for theme 'concurrency thread synchronization' (add multithreaded process of checking files )

--- src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/censor/FileCollector.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrent/utilities/censor/FileCollector.java
@@ -1,9 +1,10 @@
 package com.rakovets.course.java.core.practice.concurrent.utilities.censor;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
 
 public class FileCollector implements Callable<List<File>> {
     private final String directoryForSearch;
@@ -19,29 +20,57 @@ public class FileCollector implements Callable<List<File>> {
     public List<File> call() {
         File directory = new File(directoryForSearch);
         List<File> chosenFiles = new ArrayList<>();
+        ReentrantLock lock = new ReentrantLock();
 
         if (directory.isDirectory()) {
-            File[] directoryFiles = directory.listFiles();
+            List<File> directoryFiles = Arrays.stream(Objects.requireNonNull(directory.listFiles())).collect(Collectors.toList());
 
-            if (directoryFiles != null) {
-                for (File file : directoryFiles) {
-                    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                        String string;
-                        while ((string = reader.readLine()) != null) {
-                            if (string.contains(searchWord)) {
-                                chosenFiles.add(file);
-                                break;
+                Callable<Boolean> fileCollectorUnit = () -> {
+                    while (!directoryFiles.isEmpty()) {
+                        lock.lock();
+                        if (!directoryFiles.isEmpty()) {
+                            File fileForCheck = directoryFiles.remove(0);
+                            lock.unlock();
+
+                            try (BufferedReader reader = new BufferedReader(new FileReader(fileForCheck))) {
+                                String string;
+                                while ((string = reader.readLine()) != null) {
+                                    if (string.contains(searchWord)) {
+                                        chosenFiles.add(fileForCheck);
+                                        break;
+                                    }
+                                }
+                            } catch (Exception ex) {
+                                try {
+                                    throw new CensorException("Collector: Some files in this directory contains no text");
+                                } catch (CensorException exception) {
+                                    System.out.println(exception.getMessage());
+                                }
                             }
-                        }
-                    } catch (Exception ex) {
-                        try {
-                            throw new CensorException("Collector: Some files in this directory contains no text");
-                        } catch (CensorException exception) {
-                            System.out.println(exception.getMessage());
+                        } else {
+                            lock.unlock();
                         }
                     }
-                }
+                    return true;
+                };
+
+            int numberOfThreads = Runtime.getRuntime().availableProcessors();
+            ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
+            Collection<Callable<Boolean>> tasks = new ArrayList<>();
+            for (int x = 1; x <= numberOfThreads; x++) {
+                tasks.add(fileCollectorUnit);
             }
+
+            try {
+                List<Future<Boolean>> doneTasks = executor.invokeAll(tasks);
+                for (Future<Boolean> check : doneTasks) {
+                    check.get();
+                }
+            } catch (InterruptedException | ExecutionException ex) {
+                ex.printStackTrace();
+            }
+
+            executor.shutdown();
             System.out.println("Files were found - " + chosenFiles.size());
         } else {
             try {
