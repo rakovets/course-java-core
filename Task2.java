3bde7b696211fd5391a318f74b4b9e68adca5253
kiriller1990 <80848391+kiriller1990@users.noreply.github.com>
2021-03-18 22:10:13 +0200

    Update Task2.java
    fixed

--- Task2.java
+++ Task2.java
@@ -1,6 +1,6 @@
 public class Task2 {
-    public static void main (String[] args) {
-      System.out.println ("Java: version 11.");
-      System.out.println ("JVM: HotSpot");
+    public static void main(String[] args) {
+      System.out.println("Java: version 11.");
+      System.out.println("JVM: HotSpot.");
     }
 }
