3032c0e03c30e0cc9fc4a9e94007528693038ee0
DealerSSil <evgeni_ermakov@mail.ru>
2021-03-20 14:00:00 +0300

    solution: task 4 for theme 'Operators'

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task04.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task04.java
@@ -33,8 +33,7 @@ class Task04 {
      * @param milliseconds время (в миллисекундах) между срабатыванием датчика света и звука
      */
     static float getDistance(float milliseconds) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0.0f;
+
+        return (milliseconds / 1000) * (SOUND_SPEED * 1000 / 3600);
     }
 }
