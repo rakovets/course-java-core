455e2fd9f2e79fe82678a989f2131c0b76c72670
Aleksei <worcers@gmail.com>
2021-03-20 23:00:32 +0300

    feat: task solution Task6 for theme "operators"

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task06.java
@@ -15,7 +15,7 @@ class Task06 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int playingTimeInSeconds = 4567;
+        int playingTimeInSeconds = 860000;
 
         String playingTime = getPlayingTime(playingTimeInSeconds);
         System.out.printf("Result: %s", playingTime);
@@ -28,8 +28,11 @@ class Task06 {
      * @return время в формате 'D H:m:s', где D - дни, H - часы, m - минуты, s - секунды
      */
     static String getPlayingTime(int playingTimeInSeconds) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        int days = playingTimeInSeconds / 86400;
+        int hours = (playingTimeInSeconds % 86400) / 3600;
+        int minutes = (playingTimeInSeconds / 60) % 60;
+        int seconds = playingTimeInSeconds % 60;
+
+        return (days + " " + hours + ":" + minutes + ":" + seconds);
     }
 }
