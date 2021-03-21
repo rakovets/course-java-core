68078830a58a0196dc1dbde096f75872af916add
Alex-210778 <Lukjanvich@gmail.com>
2021-03-22 01:24:49 +0300

    solution: tasks 3 for theme 'Operators'

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task03.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task03.java
@@ -37,6 +37,7 @@ class Task03 {
     static float getTotalDepositAmount(int depositAmount, int depositYears, int depositAnnualPercentage) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0.0f;
+
+        return (float) (depositAmount + ((float) (depositAmount * depositAnnualPercentage) / 100) * depositYears);
     }
 }
