f49a41150c5270f4603a9a59f233f6d66a1460fa
Alex-210778 <Lukjanvich@gmail.com>
2021-03-24 01:09:19 +0300

    fix: task 4 for theme 'Operators'

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task04.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task04.java
@@ -35,6 +35,6 @@ class Task04 {
     static float getDistance(float milliseconds) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return (SOUND_SPEED * milliseconds / 3600);
+        return SOUND_SPEED * milliseconds / 3600;
     }
 }
