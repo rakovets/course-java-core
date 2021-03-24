31658a53133051a24bbb7265b988801364e7404d
Aleksei <worcers@gmail.com>
2021-03-24 21:35:20 +0300

    feat: task solution Task11 for theme "decision-making-statement"

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task11.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task11.java
@@ -25,7 +25,7 @@ class Task11 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        char symbol = 'а';
+        char symbol = '&';
 
         String monthName = getSymbolType(symbol);
         System.out.printf("Result: %s", monthName);
@@ -38,8 +38,13 @@ class Task11 {
      * @return <code>latin</code>/<code>cyrillic</code>/<code>digit</code>/<code>undefined</code>
      */
     static String getSymbolType(char symbol) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        if (symbol >= 'A' && symbol <= 'Z' || symbol >= 'a' && symbol <= 'z') {
+            return "latin";
+        } else if (symbol >= 'А' && symbol <= 'Я' || symbol > 'a' && symbol < 'я') {
+            return "cyrillic";
+        } else if (symbol >= '0' && symbol <= '9') {
+            return "digit";
+        }
+        return "undefined";
     }
 }
