44c6499ece2b4b90636a6cacc9aef71f5b4600e0
DealerSSil <evgeni_ermakov@mail.ru>
2021-03-24 20:11:24 +0300

    solution: task 5 for theme 'Decision-making statement'

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task05.java
@@ -1,13 +1,5 @@
 package com.rakovets.course.java.core.practice.decision_making_statement;
 
-/**
- * Разработать программу для игрового движка.
- * <p>
- * Игрок при регистрации вводит свою дату рождения и она хранится в формате 'dd/MM/yyyy'. В профиле игрока необходимо
- * месяц выводить не в числовом формате, а в текстовом. Определить название месяца в зависимости от номера.
- *
- * @author Dmitry Rakovets
- */
 class Task05 {
     /**
      * The entry point of the task
@@ -31,8 +23,47 @@ class Task05 {
      * @return месяц на английском языке (UPPER CASE)
      */
     static String getMonthName(byte monthNumber) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String dayOfMounth = null;
+
+        switch (monthNumber) {
+            case 1:
+                dayOfMounth = "JANUARY";
+                break;
+            case 2:
+                dayOfMounth = "FEBRUARY";
+                break;
+            case 3:
+                dayOfMounth = "MARCH";
+                break;
+            case 4:
+                dayOfMounth = "APRIL";
+                break;
+            case 5:
+                dayOfMounth = "MAY";
+                break;
+            case 6:
+                dayOfMounth = "JUNE";
+                break;
+            case 7:
+                dayOfMounth = "JULY";
+                break;
+            case 8:
+                dayOfMounth = "AUGUST";
+                break;
+            case 9:
+                dayOfMounth = "SEPTEMBER";
+                break;
+            case 10:
+                dayOfMounth = "OCTOBER";
+                break;
+            case 11:
+                dayOfMounth = "NOVEMBER";
+                break;
+            case 12:
+                dayOfMounth = "DECEMBER";
+                break;
+        }
+
+        return dayOfMounth;
     }
 }
