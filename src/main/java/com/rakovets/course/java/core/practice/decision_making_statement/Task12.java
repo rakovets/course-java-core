faba4c11ccf6df3e32639c3106b3e5f89322ecb8
Alex-210778 <Lukjanvich@gmail.com>
2021-03-28 01:04:56 +0300

    solusion: task 12 for theme 'decision_making_statement'

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task12.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task12.java
@@ -28,8 +28,8 @@ class Task12 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int day = 12;
-        int month = 12;
+        int day = 25;
+        int month = 5;
         int year = 1989;
 
         String zodiacSign = getZodiacSign(day, month, year);
@@ -45,8 +45,59 @@ class Task12 {
      * @return знак зодиака
      */
     static String getZodiacSign(int day, int month, int year) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String zodiac = "";
+        double birthday = (double) month + (double) day / 100;
+        if (day <= 0 || day > 31) {
+            zodiac = "Wrong day";
+        } else {
+            if (month <= 0 || month > 12) {
+                zodiac = "Wrong month";
+            } else {
+                if (year < 0) {
+                    zodiac = "Wrong year";
+                } else {
+                    if (birthday >= 3.21 && birthday <= 4.20) {
+                        zodiac = "Ram";
+                    }
+                    if (birthday >= 4.21 && birthday <= 5.20) {
+                        zodiac = "Bull";
+                    }
+                    if (birthday >= 5.21 && birthday <= 6.21) {
+                        zodiac = "Twins";
+                    }
+                    if (birthday >= 6.22 && birthday <= 7.22) {
+                        zodiac = "Crab";
+                    }
+                    if (birthday >= 7.23 && birthday <= 8.22) {
+                        zodiac = "Lion";
+                    }
+                    if (birthday >= 8.23 && birthday <= 9.21) {
+                        zodiac = "Maiden";
+                    }
+                    if (birthday >= 9.22 && birthday <= 10.22) {
+                        zodiac = "Scales";
+                    }
+                    if (birthday >= 10.23 && birthday <= 11.22) {
+                        zodiac = "Scorpion";
+                    }
+                    if (birthday >= 11.23 && birthday <= 12.21) {
+                        zodiac = "Archer";
+                    }
+                    if (birthday >= 12.22 && birthday <= 12.31) {
+                        zodiac = "Goat";
+                    }
+                    if (birthday >= 1.01 && birthday <= 1.20) {
+                        zodiac = "Goat";
+                    }
+                    if (birthday >= 1.21 && birthday <= 2.19) {
+                        zodiac = "Water-bearer";
+                    }
+                    if (birthday >= 2.20 && birthday <= 3.20) {
+                        zodiac = "Fish";
+                    }
+                }
+            }
+        }
+        return zodiac;
     }
 }
