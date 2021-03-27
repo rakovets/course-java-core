488f411a4ac43e8a7cf3bf6a3bc80ef7a1c35029
DealerSSil <evgeni_ermakov@mail.ru>
2021-03-27 11:04:55 +0300

    Task 3/10

--- src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task03.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task03.java
@@ -1,10 +1,14 @@
 package com.rakovets.course.java.core.practice.looping_statement;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Разработать программу для банка.
  *
  * @author Dmitry Rakovets
  */
+
 class Task03 {
     /**
      * The entry point of the task
@@ -15,8 +19,8 @@ class Task03 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        double depositAmount = 1500.0;
-        double annualDepositPercent = 7.0;
+        double depositAmount = 10000.0;
+        double annualDepositPercent = 18.0;
         int depositTerm = 5;
 
         double totalDepositAmount = getTotalDepositAmount(depositAmount, annualDepositPercent, depositTerm);
@@ -32,8 +36,11 @@ class Task03 {
      * @return прибыль (с точностью до 2 знаков после десятичного разделителя)
      */
     static double getTotalDepositAmount(double depositAmount, double annualDepositPercent, int depositTerm) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0.0;
+
+        for (int i = 0; i < depositTerm; i++) {
+            depositAmount += (depositAmount * annualDepositPercent) / 100;
+        }
+
+        return BigDecimal.valueOf(depositAmount).setScale(2, RoundingMode.HALF_UP).doubleValue() * 1.00f;
     }
 }
