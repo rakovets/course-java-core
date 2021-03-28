7629e80612dc74b12a13b744a8e4c3e3620b8346
Aleksei <worcers@gmail.com>
2021-03-28 12:28:16 +0300

    feat: solution Task5 for theme "looping-statements"

--- src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task05.java
@@ -1,5 +1,7 @@
 package com.rakovets.course.java.core.practice.looping_statement;
 
+import org.w3c.dom.ls.LSOutput;
+
 /**
  * Разработать программу для игрового движка:
  * Спрогнозировать через какое время party(team) игроков убьет RaidBoss и получит вознаграждение.
@@ -16,9 +18,9 @@ class Task05 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int healthPoints = 1000;
-        double regenerationPercentFromCurrentHealth = 10;
-        int averageDamagePerHour = 200;
+        int healthPoints = 10000;
+        double regenerationPercentFromCurrentHealth = 5.0;
+        int averageDamagePerHour = 740;
 
         double raidTime = calculateRaidTime(healthPoints, regenerationPercentFromCurrentHealth, averageDamagePerHour);
         System.out.printf("Result: %f", raidTime);
@@ -33,8 +35,20 @@ class Task05 {
      * @return время для убийства RaidBoss (когда party не справляется за 24 часа, то вывести -1)
      */
     static int calculateRaidTime(int healthPoints, double regenerationPercentPerHour, int averageDamagePerHour) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int gameOverHours = 24;
+        int bossKillTime = 0;
+
+        while (healthPoints > 0){
+            healthPoints += healthPoints * 0.01 * regenerationPercentPerHour;
+            healthPoints -= averageDamagePerHour;
+            bossKillTime ++;
+            if (bossKillTime > 24) {
+                break;
+            }
+        }
+        if (bossKillTime > 24) {
+            return -1;
+        }
+        return bossKillTime;
     }
 }
