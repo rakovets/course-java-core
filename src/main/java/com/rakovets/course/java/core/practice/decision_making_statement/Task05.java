b720c409db60611cd46b6e1c4e02d74de287ef9c
kiriller1990 <kiriller290890@gmail.com>
2021-03-29 00:41:41 +0300

    solution: task 5 for theme 'decision_making_statement'

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task05.java
@@ -31,8 +31,32 @@ class Task05 {
      * @return месяц на английском языке (UPPER CASE)
      */
     static String getMonthName(byte monthNumber) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        if (monthNumber == 1) {
+            return "JANUARY";
+        } else if (monthNumber == 2) {
+            return "FEBRUARY";
+        } else if (monthNumber == 3) {
+            return "MARCH";
+        } else if (monthNumber == 4) {
+            return "APRIL";
+        } else if (monthNumber == 5) {
+            return "MAY";
+        } else if (monthNumber == 6) {
+            return "JUNE";
+        } else if (monthNumber == 7) {
+            return "JULY";
+        } else if (monthNumber == 8) {
+            return "AUGUST";
+        } else if (monthNumber == 9) {
+            return "SEPTEMBER";
+        } else if (monthNumber == 10) {
+            return "OCTOBER";
+        } else if (monthNumber == 11) {
+            return "NOVEMBER";
+        } else if (monthNumber == 12) {
+            return "DECEMBER";
+        } else {
+            return null;
+        }
     }
 }
