36a41914a6d67566356e9f17814ac31bbb63d4bf
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-03-28 22:20:15 +0300

    solution: Task10 for theme `decision_making_statement`

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task10.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task10.java
@@ -35,8 +35,12 @@ class Task10 {
      * @return номер заправки.
      */
     static int getNumberStation(int firstFuelReserves, int secondFuelReserves, int thirdFuelReserves) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        if (firstFuelReserves <= secondFuelReserves && firstFuelReserves <= thirdFuelReserves) {
+            return 1;
+        } else if (secondFuelReserves <= firstFuelReserves && secondFuelReserves <= thirdFuelReserves) {
+            return 2;
+        } else {
+            return 3;
+        }
     }
 }
