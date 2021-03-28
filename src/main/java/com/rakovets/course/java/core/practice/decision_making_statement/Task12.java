02c3025d0234bf350ce14b3851f77bdcd4c2bb37
kiriller1990 <kiriller290890@gmail.com>
2021-03-29 00:43:34 +0300

    solution: task 12 for theme 'decision_making_statement'

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task12.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task12.java
@@ -45,8 +45,31 @@ class Task12 {
      * @return знак зодиака
      */
     static String getZodiacSign(int day, int month, int year) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        if (month == 12 && day >= 22 || month == 1 && day <= 20) {
+            return "Goat";
+        } else if (month == 1 && day >= 21 || month == 2 && day <= 19) {
+            return "Water-bearer";
+        } else if (month == 2 && day >= 20 || month == 3 && day <= 20) {
+            return "Fish";
+        } else if (month == 3 && day >= 21 || month == 4 && day <= 20) {
+            return "Ram";
+        } else if (month == 4 && day >= 21 || month == 5 && day <= 20) {
+            return "Bull";
+        } else if (month == 5 && day >= 21 || month == 6 && day <= 21) {
+            return "Twins";
+        } else if (month == 6 && day >= 22 || month == 7 && day <= 22) {
+            return "Crab";
+        } else if (month == 7 && day >= 23 || month == 8 && day <= 22) {
+            return "Lion";
+        } else if (month == 8 && day >= 23 || month == 9 && day <= 21) {
+            return "Maiden";
+        } else if (month == 9 && day >= 22 || month == 10 && day <= 22) {
+            return "Scales";
+        } else if (month == 10 && day >= 23 || month == 11 && day <= 22) {
+            return "Scorpion";
+        } else if (month == 11 && day >= 23 || month == 12 && day <= 21) {
+            return "Archer";
+        }
+            return null;
     }
 }
