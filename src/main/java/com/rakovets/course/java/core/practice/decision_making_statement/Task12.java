d68cf9a9f5e331e1f51e9136ab37e0cd51e43275
andrewvolkov2021 <sangarskoy@gmail.com>
2021-03-28 03:32:37 +0300

    solution: task12 for theme "Decision-making-statement"

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task12.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task12.java
@@ -28,9 +28,9 @@ class Task12 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int day = 12;
-        int month = 12;
-        int year = 1989;
+        int day = 20;
+        int month = 1;
+        int year = 2001;
 
         String zodiacSign = getZodiacSign(day, month, year);
         System.out.printf("Result: %s", zodiacSign);
@@ -45,8 +45,44 @@ class Task12 {
      * @return знак зодиака
      */
     static String getZodiacSign(int day, int month, int year) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        int numberDayInFebruary;
+
+        if (year%4 == 0) {
+            if (year%400 == 0) {
+                numberDayInFebruary = 28;
+            } else {
+                numberDayInFebruary = 29 ;
+            }
+        } else {
+            numberDayInFebruary = 28;
+        }
+
+        if ((month == 3 && day >= 21 && day <= 31) || (month == 4 && day > 0 && day <= 20)) {
+            return "Ram";
+        } else if ((month == 4 && day >= 21 && day <= 30) || (month == 5 && day > 0 && day <= 20)) {
+            return "Bull";
+        } else  if ((month == 5 && day >= 21 && day <= 31) || (month == 6 && day > 0 && day <= 21)) {
+            return "Twins";
+        } else if ((month == 6 && day >= 22 && day <= 30) || (month == 7 && day > 0 && day <= 22)) {
+            return "Crab";
+        } else if ((month == 7 && day >= 23 && day <= 31) || (month == 8 && day > 0 && day <= 22)) {
+            return "Lion";
+        } else if ((month == 8 && day >= 23 && day <= 31) || (month == 9 && day > 0 && day <= 21)) {
+            return "Maiden";
+        } else if ((month == 9 && day >= 22 && day <= 30) || (month == 10 && day > 0 && day <= 22)) {
+            return "Scales";
+        } else if ((month == 10 && day >= 23 && day <= 31) || (month == 11 && day > 0 && day <= 22)) {
+            return "Scorpion";
+        } else if ((month == 11 && day >= 23 && day <= 30) || (month == 12 && day > 0 && day <= 21)) {
+            return "Archer";
+        } else if ((month == 12 && day >= 23 && day <= 31) || (month == 1 && day > 0 && day <= 20)) {
+            return "Goat";
+        } else if ((month == 1 && day >= 21 && day <= 30) || (month == 2 && day > 0 && day <= 19)) {
+            return "Water-bearer";
+        } else if ((month == 2 && day >= 20 && day <= numberDayInFebruary) || (month == 3 && day >0 && day <= 20)) {
+            return "Fish";
+        } else {
+            return null;
+        }
     }
 }
