e71403f040804fb44c8a49bd63a6dc6213313d90
Alex-210778 <Lukjanvich@gmail.com>
2021-03-29 21:25:51 +0300

    solution: task 4 for theme 'looping_statement'

--- src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task04.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task04.java
@@ -17,8 +17,8 @@ class Task04 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int healthPoints = 1000;
-        double regenerationPercentFromCurrentHealth = 100;
+        int healthPoints = 100;
+        double regenerationPercentFromCurrentHealth = 18.0;
         int hoursAfterRespawn = 10;
 
         int featureHealthPoint = calculateHealthPointsByTime(healthPoints, regenerationPercentFromCurrentHealth, hoursAfterRespawn);
@@ -34,8 +34,11 @@ class Task04 {
      * @return количество HP
      */
     static int calculateHealthPointsByTime(int healthPoints, double regenerationPercentFromCurrentHealth, int hoursAfterRespawn) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        double AmountHp = healthPoints;
+        for (int i = 1; i <= hoursAfterRespawn; i++) {
+            AmountHp = AmountHp + AmountHp * regenerationPercentFromCurrentHealth / 100;
+        }
+         int totalAmountHp = (int) AmountHp;
+        return totalAmountHp;
     }
 }
