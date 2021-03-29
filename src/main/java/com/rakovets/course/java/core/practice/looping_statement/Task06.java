bedfe3c6560ba0b47466b4919c055b6d083b0c8c
andrewvolkov2021 <sangarskoy@gmail.com>
2021-03-29 23:41:38 +0300

    solution: task6 for theme "Looping-statements"

--- src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task06.java
@@ -20,7 +20,7 @@ class Task06 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        long amount = -92346234;
+        long amount = 121212;
 
         String amountWithAccountingFormat = convertToAccountingFormat(amount);
         System.out.printf("Result: %s", amountWithAccountingFormat);
