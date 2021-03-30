8c6785446b544b41970a34176ef15cd61471f069
davydovbv <bogedavydov@gmail.com>
2021-03-30 21:48:17 +0300

    fix: tasks for theme "looping statements"

--- src/main/java/com/rakovets/course/java/core/practice/array/Task02.java
+++ src/main/java/com/rakovets/course/java/core/practice/array/Task02.java
@@ -1,5 +1,8 @@
 package com.rakovets.course.java.core.practice.array;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Разработать программу для электронного дневника, которая работает с отметками по всем предметам.
  *
@@ -31,9 +34,15 @@ class Task02 {
      * @return средняя арифметическая отметка
      */
     static double getAverageMark(int[][] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0.0;
+        double summ = 0.0;
+        int quantityMarks = 0;
+        for (int[] mark : marks) {
+            for (int i : mark) {
+                summ += i;
+            }
+            quantityMarks += mark.length;
+        }
+        return BigDecimal.valueOf(summ / quantityMarks).setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 
     /**
@@ -43,9 +52,15 @@ class Task02 {
      * @return минимальная отметка
      */
     static int getMinMark(int[][] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+      int minMark = marks [0][0];
+        for (int[] mark : marks) {
+            for (int i : mark) {
+                if (minMark > i) {
+                    minMark = i;
+                }
+            }
+        }
+        return minMark;
     }
 
     /**
@@ -55,8 +70,14 @@ class Task02 {
      * @return максимальная отметка
      */
     static int getMaxMark(int[][] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int maxMark = marks [0][0];
+        for (int[] mark : marks) {
+            for (int i : mark) {
+                if (maxMark < i) {
+                    maxMark = i;
+                }
+            }
+        }
+        return maxMark;
     }
 }
