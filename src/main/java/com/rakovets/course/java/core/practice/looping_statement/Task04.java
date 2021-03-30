b8aa5fcc0a3acaa456218ac0b64728512d1a3890
tatsianapuchkouskaya <tanyshkina@gmail.com>
2021-03-31 00:44:30 +0300

    solution: task 4 for theme 'Looping-statements'

--- src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task04.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task04.java
@@ -34,8 +34,10 @@ class Task04 {
      * @return количество HP
      */
     static int calculateHealthPointsByTime(int healthPoints, double regenerationPercentFromCurrentHealth, int hoursAfterRespawn) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        double healthPointsByTime = healthPoints;
+        for (int i = 1; i <= hoursAfterRespawn; i++) {
+            healthPointsByTime += healthPointsByTime * (regenerationPercentFromCurrentHealth * 0.01);
+        }
+        return (int) healthPointsByTime;
     }
 }
