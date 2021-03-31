6a9876eff9bd3935e186ed386435fa99aceb4115
Alex-210778 <Lukjanvich@gmail.com>
2021-04-01 01:58:38 +0300

    fix: task 14 for theme 'decision_making_statement'

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task14.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statement/Task14.java
@@ -48,52 +48,43 @@ class Task14 {
     static String getZodiacYearName(int day, int month, int year) {
         String zodiac = "";
         int zodiacYear = year % 12;
-        if (day <= 0 || day > 31) {
-            zodiac = "Wrong day";
-        } else {
-            if (month <= 0 || month > 12) {
-                zodiac = "Wrong month";
-            }
-            else {
-                switch (zodiacYear) {
-                    case 0:
-                        zodiac = "Monkey";
-                        break;
-                    case 1:
-                        zodiac = "Rooster";
-                        break;
-                    case 2:
-                        zodiac = "Dog";
-                        break;
-                    case 3:
-                        zodiac = "Pig";
-                        break;
-                    case 4:
-                        zodiac = "Rat";
-                        break;
-                    case 5:
-                        zodiac = "Ox";
-                        break;
-                    case 6:
-                        zodiac = "Tiger";
-                        break;
-                    case 7:
-                        zodiac = "Rabbit";
-                        break;
-                    case 8:
-                        zodiac = "Dragon";
-                        break;
-                    case 9:
-                        zodiac = "Snake";
-                        break;
-                    case 10:
-                        zodiac = "Horse";
-                        break;
-                    case 11:
-                        zodiac = "Ram";
-                        break;
-                }
-            }
+        switch (zodiacYear) {
+            case 0:
+                zodiac = "Monkey";
+                break;
+            case 1:
+                zodiac = "Rooster";
+                break;
+            case 2:
+                zodiac = "Dog";
+                break;
+            case 3:
+                zodiac = "Pig";
+                break;
+            case 4:
+                zodiac = "Rat";
+                break;
+            case 5:
+                zodiac = "Ox";
+                break;
+            case 6:
+                zodiac = "Tiger";
+                break;
+            case 7:
+                zodiac = "Rabbit";
+                break;
+            case 8:
+                zodiac = "Dragon";
+                break;
+            case 9:
+                zodiac = "Snake";
+                break;
+            case 10:
+                zodiac = "Horse";
+                break;
+            case 11:
+                zodiac = "Ram";
+                break;
         }
         return zodiac;
     }
