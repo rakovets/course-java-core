4f25f1e26ee33d09edfedaebd5814ecd901ab9e0
Paul Beliakovich <psyhocat@gmail.com>
2021-04-01 21:22:25 +0300

    commit for Time

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Time.java
@@ -0,0 +1,44 @@
+package com.rakovets.course.java.core.practice.oop.classes_and_objects;
+
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+
+    public Time(int totalSeconds) {
+        hours *= 60 * 60;
+        minutes *= 60;
+        seconds += minutes + hours;
+    }
+    public Time(int hours, int minutes, int seconds){
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+}
