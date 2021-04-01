03a5bba4c3bfff4b279bfa2762400d0d9dcffb88
Artem Shapel <shapel.artem@mail.ru>
2021-04-01 16:45:57 +0300

    solution: task 1 for theme 'looping-statements'

--- src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statement/Task01.java
@@ -29,8 +29,11 @@ class Task01 {
      * @return текст, который содержит столбец с номерами строк, где каждый номер на новой строке
      */
     static String generateNumbersColumn(int numberRows) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String result = "";
+
+        for (int i = 1; i < numberRows; i++) {
+            result += i + "\n";
+        }
+        return result + numberRows;
     }
 }
