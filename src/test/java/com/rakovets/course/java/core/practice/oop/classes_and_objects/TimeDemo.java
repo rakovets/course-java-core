29caf676cca0cef4f94ce73612fcae8989ed307b
Paul Beliakovich <psyhocat@gmail.com>
2021-04-01 21:22:55 +0300

    commit for TimeDemo

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/TimeDemo.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.oop.classes_and_objects;
+
+public class TimeDemo {
+    public static void main(String[] args) {
+        Time time = new Time(20,20,30);
+        System.out.printf("%d,%d,%d");
+        }
+    }
