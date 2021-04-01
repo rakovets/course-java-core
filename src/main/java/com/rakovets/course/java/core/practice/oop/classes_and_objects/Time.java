df5a638bfd4c9aa3a31b5585c9a39f114b17b440
igor <gob1in@Mail.ru>
2021-04-01 21:23:58 +0300

    class work

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Time.java
@@ -0,0 +1,26 @@
+package com.rakovets.course.java.core.practice.oop.classes_and_objects;
+
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours ;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int GetHours() {
+        return hours;
+    }
+
+    public int GetMinutes() {
+        return seconds;
+    }
+
+    public int GetSeconds(){
+        return seconds;
+    }
+}
---
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/TimeDemo.java
@@ -0,0 +1,11 @@
+package com.rakovets.course.java.core.practice.oop.classes_and_objects;
+
+public class TimeDemo {
+    public class TimeDemo {
+        public static void main(String[] args) {
+            Time time = new Time (1,2,3) {
+                System.out.printf ("%d:%d:%d", time.);
+            }
+        }
+    }
+}
