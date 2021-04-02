dd70d3603feb9ca5ee27e4a69820af9d5754a56d
alex <adyuba@icloud.com>
2021-04-02 12:50:29 +0300

    solution: Task02 (class) for theme 'oop-classes-and-objects'

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Point.java
@@ -0,0 +1,33 @@
+package com.rakovets.course.java.core.practice.oop.classes_and_objects;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getDistance(Point point) {
+        double distance;
+        distance = Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2));
+        return distance;
+    }
+}
