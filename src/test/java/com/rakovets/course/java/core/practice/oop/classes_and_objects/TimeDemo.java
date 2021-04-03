eaef7f7701d24c242f87d85dbff368cf612b26da
Aleksei <worcers@gmail.com>
2021-04-04 01:46:07 +0300

    feat: task TimeDemo for theme "oop-classes-and-objects"

--- src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/TimeDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/TimeDemo.java
@@ -3,6 +3,12 @@ package com.rakovets.course.java.core.practice.oop.classes_and_objects;
 public class TimeDemo {
     public static void main(String[] args) {
         Time time = new Time (30, 20, 50);
-        System.out.printf("%d %d %d");
+        System.out.printf("%d:%d:%d\n", time.getHours(), time.getMinutes(), time.getSeconds());
+        System.out.printf("%d\n", time.getTotalSeconds());
+        time.setHours(12);
+        time.setMinutes(26);
+        time.setSeconds(54);
+        System.out.printf("%d:%d:%d\n", time.getHours(), time.getMinutes(), time.getSeconds());
+        System.out.printf("%d\n", time.getTotalSeconds());
     }
 }
