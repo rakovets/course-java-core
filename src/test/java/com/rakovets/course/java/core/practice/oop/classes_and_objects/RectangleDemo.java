eef082ae9745a2296e27244cd6cc569e08e453cf
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-04 12:12:59 +0300

    OOP Rectangle

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/RectangleDemo.java
@@ -0,0 +1,9 @@
+package com.rakovets.course.java.core.practice.oop.classes_and_objects;
+
+public class RectangleDemo {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(new Point(2, 2), new Point(4, 4));
+        System.out.println(rectangle.getArea());
+        System.out.println(rectangle.getPerimeter());
+    }
+}
