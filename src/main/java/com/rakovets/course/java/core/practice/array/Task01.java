fc8bd0772d11ea9952ef9428dea5be360ccb21d0
Alex-210778 <Lukjanvich@gmail.com>
2021-04-04 17:59:15 +0300

    solution: task 1 for theme 'array'

--- src/main/java/com/rakovets/course/java/core/practice/array/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/array/Task01.java
@@ -1,5 +1,8 @@
 package com.rakovets.course.java.core.practice.array;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Разработать программу для электронного дневника, которая работает с отметками только по одному предмету.
  *
@@ -32,9 +35,12 @@ class Task01 {
      * @return средняя арифметическая отметка
      */
     static double getAverageMark(int[] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0.0;
+        double sum = 0;
+        for (int i = 0; i < marks.length; i++) {
+            sum += marks[i];
+        }
+        sum /= marks.length;
+        return BigDecimal.valueOf(sum).setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 
     /**
@@ -44,9 +50,13 @@ class Task01 {
      * @return минимальная отметка
      */
     static int getMinMark(int[] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int minMark = marks[0];
+        for (int i = 0; i < marks.length; i++) {
+            if (marks[i] < minMark) {
+                minMark = marks[i];
+            }
+        }
+        return minMark;
     }
 
     /**
@@ -56,8 +66,12 @@ class Task01 {
      * @return максимальная отметка
      */
     static int getMaxMark(int[] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int maxMark = marks[0];
+        for (int i = 0; i < marks.length; i++) {
+            if (marks[i] > maxMark) {
+                maxMark = marks[i];
+            }
+        }
+        return maxMark;
     }
 }
