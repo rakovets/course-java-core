a6d0d38b430c85242fbafd1d1397f566ccc1087d
Aleksei <worcers@gmail.com>
2021-04-04 16:00:24 +0300

    update: task RectangleDemo for theme "oop-classes-and-objects"

--- src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/RectangleDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/RectangleDemo.java
@@ -3,13 +3,13 @@ package com.rakovets.course.java.core.practice.oop.classes_and_objects;
 public class RectangleDemo {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(10, 5);
-        System.out.printf("Start value:\nFirst point = %f\nSecond point = %f\n", rectangle.getFirstPoint(),
+        System.out.printf("Start value:\nFirst point = %s\nSecond point = %s\n", rectangle.getFirstPoint(),
                 rectangle.getSecondPoint());
-        System.out.printf("Perimeter = %f\nArea = %f\n", rectangle.getPerimeter(), rectangle.getArea());
+        System.out.printf("Perimeter = %s\nArea = %s\n", rectangle.getPerimeter(), rectangle.getArea());
         rectangle.setFirstPoint(18.34);
         rectangle.setSecondPoint(34.25);
-        System.out.printf("New value:\nFirst point = %f\nSecond point = %f\n", rectangle.getFirstPoint(),
+        System.out.printf("New value:\nFirst point = %s\nSecond point = %s\n", rectangle.getFirstPoint(),
                 rectangle.getSecondPoint());
-        System.out.printf("Perimeter = %f\nArea = %f", rectangle.getPerimeter(), rectangle.getArea());
+        System.out.printf("Perimeter = %s\nArea = %s", rectangle.getPerimeter(), rectangle.getArea());
     }
 }
