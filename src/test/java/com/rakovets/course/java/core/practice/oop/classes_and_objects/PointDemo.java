ba1495d98006781b5812a49dc5d9b85ecd1072c6
Aleksei <worcers@gmail.com>
2021-04-04 16:00:11 +0300

    update: task PointDemo for theme "oop-classes-and-objects"

--- src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/PointDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/PointDemo.java
@@ -4,15 +4,15 @@ public class PointDemo {
     public static void main(String[] args) {
         Point point1 = new Point(10, 5);
         Point point2 = new Point(12, 30);
-        System.out.printf("Start value:\nX1 = %f\nY1 = %f\n", point1.getX(), point1.getY());
-        System.out.printf("X2 = %f\nY2 = %f\n", point2.getX(), point2.getY());
+        System.out.printf("Start value:\nX1 = %s\nY1 = %s\n", point1.getX(), point1.getY());
+        System.out.printf("X2 = %s\nY2 = %s\n", point2.getX(), point2.getY());
         System.out.printf("distance = %f\n", point1.getDistance(point2));
         point1.setX(23.24);
         point1.setY(41.12);
         point2.setX(83.34);
         point2.setY(78.98);
-        System.out.printf("New value:\nX1 = %f\nY1 = %f\n", point1.getX(), point1.getY());
-        System.out.printf("X2 = %f\nY2 = %f\n", point2.getX(), point2.getY());
+        System.out.printf("New value:\nX1 = %s\nY1 = %s\n", point1.getX(), point1.getY());
+        System.out.printf("X2 = %s\nY2 = %s\n", point2.getX(), point2.getY());
         System.out.printf("distance = %f\n", point1.getDistance(point2));
     }
 }
