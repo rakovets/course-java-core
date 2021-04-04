2ec7359395b1447a4b60d5dbfd9057981c105538
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-04 12:13:18 +0300

    OOP Point

--- src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/PointDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/PointDemo.java
@@ -2,7 +2,7 @@ package com.rakovets.course.java.core.practice.oop.classes_and_objects;
 
 public class PointDemo {
     public static void main(String[] args){
-        Point pointOne = new Point(1,1);
-        System.out.println(pointOne.getDistance(new Point(3,3)));
+        Point pointOne = new Point(2,2);
+        System.out.println(pointOne.getDistance(new Point(2,7)));
     }
 }
