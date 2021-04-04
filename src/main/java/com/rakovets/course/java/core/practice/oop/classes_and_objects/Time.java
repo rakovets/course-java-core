2af1806aeaa79d3f53e71c5fd5841ba35a125df5
Alex-210778 <Lukjanvich@gmail.com>
2021-04-05 00:08:01 +0300

    solution: task Time for theme 'oop_classes_and_objects'

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Time.java
@@ -0,0 +1,41 @@
+package com.rakovets.course.java.core.practice.oop.classes_and_objects;
+
+public class Time {
+    private int hours;
+    private int minutes;
+    private int seconds;
+
+    public Time (int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+    public Time (int totalSeconds) {
+        final int SECONDS_IN_MINUTE = 60;
+        final int SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60;
+        hours = totalSeconds / SECONDS_IN_HOUR;
+        minutes =  (totalSeconds - hours * SECONDS_IN_HOUR) / SECONDS_IN_MINUTE;
+        seconds = totalSeconds % SECONDS_IN_MINUTE;
+    }
+    public int getHours() {
+        return hours;
+    }
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+    public int getMinutes() {
+        return minutes;
+    }
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+    public int getSeconds() {
+        return seconds;
+    }
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+    public int getTotalSeconds() {
+        return hours * 3600 + minutes * 60 + seconds;
+    }
+}
