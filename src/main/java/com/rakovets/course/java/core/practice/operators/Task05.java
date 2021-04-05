8d6bcf7f841d0e32d8f1659ba31fed25cdc23803
davydovbv <53862456+davydovbv@users.noreply.github.com>
2021-04-05 10:18:56 +0300

    Update Task05.java

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task05.java
@@ -34,9 +34,8 @@ class Task05 {
      * @param weight - вес
      */
     static int calculateDeviationStandardWeight(int height, int weight) {
-
         int standardWeight = height - 110;
-
+        
         return weight - standardWeight;
     }
 }
