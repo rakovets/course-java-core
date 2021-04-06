072adbc2fc626d655c92cc567fc93a2d23510a72
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-04-06 15:21:55 +0300

    solution: Task02 for theme `oop-classes-and-object'

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Point.java
@@ -0,0 +1,30 @@
+package com.rakovets.course.java.core.practice.oop.classes_and_objects;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double getDistance(Point point) {
+        double distancePoints;
+        distancePoints =
+        return distancePoints;
+    }
+}
+
+        * `getDistance(point)` – принимает объект типа `Point`, возвращает расстояние от текущей точки (экземпляра, для которого был вызван), до принимаемой точки
