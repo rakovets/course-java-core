821bd707114a0985e791af0582a398092fe11fc0
maksim_rudenko <akzby15@gmail.com>
2021-04-06 21:12:07 +0300

    String Task1 - Test1

---
+++ src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.string;
+
+public class StringUtil {
+
+    public String glue(String text1, String text2){
+        return text1.concat(text2);
+    }
+}
