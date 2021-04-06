086728e68cec445434afd73a6214e42a78f28569
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-04-06 16:00:31 +0300

    solution: Task01 Demo for theme `oop-classes-and-object'

--- src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/TimeDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/TimeDemo.java
@@ -2,6 +2,10 @@ package com.rakovets.course.java.core.practice.oop.classes_and_objects;
 
 public class TimeDemo {
     public static void main(String[] args) {
-        Time time = new Time(1,2,3);
+        Time time = new Time(5,15,45);
+        System.out.printf("%2d:%2d:2d", time.getHours(), time.getMinutes(), time.getSeconds())
     }
 }
+//NOTE: Для каждого созданного класса, нужно создать демонстрационные/тестовые классы, которые демонстрируют/тестирую работу ВСЕХ конструкторов и методов.
+
+  //      WARNING: ТОЛЬКО демонстрационные классы могут содержать вывод в *Standard Output* (консольный вывод).
