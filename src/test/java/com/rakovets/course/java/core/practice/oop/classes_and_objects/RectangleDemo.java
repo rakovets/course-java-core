05d4ef82d74ed41806c01a28bd5ab0ddff5dd61c
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-07 17:54:59 +0300

    Solution "Rectangle" for theme OOP.

--- src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/RectangleDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/RectangleDemo.java
@@ -3,7 +3,7 @@ package com.rakovets.course.java.core.practice.oop.classes_and_objects;
 public class RectangleDemo {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(new Point(2, 2), new Point(4, 4));
-        System.out.println(rectangle.getArea());
-        System.out.println(rectangle.getPerimeter());
+        System.out.println("Area = " + rectangle.getArea());
+        System.out.println("Perimeter = " + rectangle.getPerimeter());
     }
 }
