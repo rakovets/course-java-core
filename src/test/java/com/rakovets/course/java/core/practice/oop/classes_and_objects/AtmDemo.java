51268c4eba72aef91a592af04d4eb2805fbe2885
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-04-08 02:11:57 +0300

    solution: Task04 Demo  for theme `oop-classes-and-object'

--- src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/AtmDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/AtmDemo.java
@@ -1,4 +1,13 @@
 package com.rakovets.course.java.core.practice.oop.classes_and_objects;
 
 public class AtmDemo {
+    public static void main(String[] args) {
+        Atm atm = new Atm(10, 20 ,30);
+        System.out.printf("denomination 20: %d bills\ndenomination 50: %d bills\ndenomination 100: %d bills\n\n", atm.getNumberBanknotes20(), atm.getNumberBanknotes50(), atm.getNumberBanknotes100());
+
+        atm.addBanknotes20(30);
+        atm.addBanknotes50(55);
+        atm.addBanknotes100(100);
+        System.out.printf("denomination 20: %d bills\ndenomination 50: %d bills\ndenomination 100: %d bills\n", atm.getNumberBanknotes20(), atm.getNumberBanknotes50(), atm.getNumberBanknotes100());
+    }
 }
