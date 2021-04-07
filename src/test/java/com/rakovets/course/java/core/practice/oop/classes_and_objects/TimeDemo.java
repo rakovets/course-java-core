569356976c1f63562153ab3d093588cdace43608
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-04-08 01:18:53 +0300

    solution: Task01  for theme `oop-classes-and-object'

--- src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/TimeDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/TimeDemo.java
@@ -2,14 +2,13 @@ package com.rakovets.course.java.core.practice.oop.classes_and_objects;
 
 public class TimeDemo {
     public static void main(String[] args) {
-        Time time = new Time(5,15,45);
+        Time time = new Time(8,20,45);
         System.out.printf("%d:%d:%d\n", time.getHours(), time.getMinutes(), time.getSeconds());
-        Time totalSeconds = new Time(33333);
         System.out.printf("%d\n", time.getTotalSeconds());
 
-        time.setHours(11);
-        time.setMinutes(44);
-        time.setSeconds(51);
+        time.setHours(6);
+        time.setMinutes(10);
+        time.setSeconds(50);
         System.out.printf("%d:%d:%d\n", time.getHours(), time.getMinutes(), time.getSeconds());
         System.out.printf("%d\n", time.getTotalSeconds());
     }
