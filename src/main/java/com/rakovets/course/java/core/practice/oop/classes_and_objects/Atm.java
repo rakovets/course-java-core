ab07d7339fa9e748f5c8920008a97bcb154095b9
kiriller1990 <kiriller290890@gmail.com>
2021-04-07 22:46:46 +0300

    solution: task ATM for theme 'oop-classes-and-objects'

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Atm.java
@@ -0,0 +1,38 @@
+package com.rakovets.course.java.core.practice.oop.classes_and_objects;
+
+public class Atm {
+    public int numberBanknotes20;
+    public int numberBanknotes50;
+    public int numberBanknotes100;
+
+    public Atm(int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) {
+        this.numberBanknotes20 = numberBanknotes20;
+        this.numberBanknotes50 = numberBanknotes50;
+        this.numberBanknotes100 = numberBanknotes100;
+    }
+    public int getNumberBanknotes20() {
+        return numberBanknotes20;
+    }
+
+    public int getNumberBanknotes50() {
+        return numberBanknotes50;
+    }
+
+    public int getNumberBanknotes100() {
+        return numberBanknotes100;
+    }
+    public void addBanknotes20(int number) {
+        this.numberBanknotes20 += number;
+    }
+
+    public void addBanknotes50(int number) {
+        this.numberBanknotes50 += number;
+    }
+
+    public void addBanknotes100(int number) {
+        this.numberBanknotes100 += number;
+    }
+
+
+
+}
