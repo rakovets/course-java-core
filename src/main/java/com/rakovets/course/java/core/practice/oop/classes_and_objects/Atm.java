b968e3c46a03a44520c41dea46962deff207e014
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-04-08 02:14:39 +0300

    solution: Task04 Class  for theme `oop-classes-and-object'

--- src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Atm.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Atm.java
@@ -4,7 +4,6 @@ public class Atm {
     private int numberBanknotes20;
     private int numberBanknotes50;
     private int numberBanknotes100;
-
     public Atm(int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) {
         this.numberBanknotes20 = numberBanknotes20;
         this.numberBanknotes50 = numberBanknotes50;
