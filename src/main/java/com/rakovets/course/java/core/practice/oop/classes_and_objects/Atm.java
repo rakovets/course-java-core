5672760139bf6b8c148e79dca26b00dfb5fcfd39
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-07 19:44:30 +0300

    Solution "Atm" for theme OOP.

--- src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Atm.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Atm.java
@@ -1,11 +1,12 @@
 package com.rakovets.course.java.core.practice.oop.classes_and_objects;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Atm {
     private int numberBanknotes20;
     private int numberBanknotes50;
     private int numberBanknotes100;
+    private int[][] arrayWithOptions = new int[0][3];
 
     public Atm(int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) {
         this.numberBanknotes20 = numberBanknotes20;
@@ -26,30 +27,40 @@ public class Atm {
     }
 
     public boolean isPossibleIssue(int amount) {
-        return getOptionsCombinationBanknotes(amount).size() > 1;
+        return getOptionsCombinationBanknotes(amount).length >= 1;
     }
 
-    public ArrayList<Integer[]> getOptionsCombinationBanknotes(int amount) {
-        ArrayList<Integer[]> list = new ArrayList<>();
+    public int[][] createArray(int i, int j, int x) {
+        int[][] save = Arrays.copyOf(arrayWithOptions, arrayWithOptions.length);
+        arrayWithOptions = Arrays.copyOf(save, save.length + 1);
+        arrayWithOptions[arrayWithOptions.length - 1] = new int[]{i, j, x};
+        return arrayWithOptions;
+    }
+
+    public int[][] getOptionsCombinationBanknotes(int amount) {
+        arrayWithOptions = new int[0][3];
 
         for (int i = 0; i <= numberBanknotes20; i++) {
             for (int j = 0; j <= numberBanknotes50; j++) {
                 for (int x = 0; x <= numberBanknotes100; x++) {
 
                     if (i * 20 + j * 50 + x * 100 == amount) {
-                        list.add(new Integer[]{i, j, x});
+                        createArray(i, j, x);
                     }
                 }
             }
         }
-        return list;
+        return arrayWithOptions;
     }
 
-    public int getCash(Integer[] variant) {
-        numberBanknotes20 -= variant[0];
-        numberBanknotes50 -= variant[1];
-        numberBanknotes100 -= variant[2];
-        int sum = 20 * variant[0] + 50 * variant[1] + 100 * variant[2];
-        return sum;
+    public int getCash(int[] variant) {
+        int sumOfMoney = 20 * variant[0] + 50 * variant[1] + 100 * variant[2];
+
+        if (isPossibleIssue(sumOfMoney)) {
+            numberBanknotes20 -= variant[0];
+            numberBanknotes50 -= variant[1];
+            numberBanknotes100 -= variant[2];
+            return sumOfMoney;
+        } else return 0;
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/AtmDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/AtmDemo.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 
 public class AtmDemo {
     public static void main(String[] args) {
-        Atm atm = new Atm(15, 10, 100);
-        System.out.println(atm.isPossibleIssue(220));
-        ArrayList<Integer[]> list = atm.getOptionsCombinationBanknotes(220);
-        for (Integer[] l : list) {
-            System.out.println("20$ = "+l[0]+" 50$ = "+l[1]+" 100$ = "+l[2]);
+        Atm atm = new Atm(10, 5, 10);
+        int money = 210;
+
+        if (atm.isPossibleIssue(money)) {
+            System.out.printf("You can get %d$\n", money);
+            int[][] options = atm.getOptionsCombinationBanknotes(money);
+
+            for (int[] l : options) {
+                System.out.println("20$ = " + l[0] + " 50$ = " + l[1] + " 100$ = " + l[2]);
+            }
+
+            System.out.printf("The money was issued in the amount of %d$", atm.getCash(options[0]));
         }
-        System.out.println(atm.getCash(list.get(1)));
     }
 }
+
