ceed54aa245b6d1455223fbf5be19bc14f3b08c2
Aleksei <worcers@gmail.com>
2021-04-08 23:52:51 +0300

    feat: task StringUtil for theme "strings"

---
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -0,0 +1,26 @@
+package com.rakovets.course.java.core.practice.string;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class StringUtilTest {
+        static Stream<Arguments> glue() {
+            return Stream.of(
+                    Arguments.of("1", "", "1"),
+                    Arguments.of("", "5", "5"),
+                    Arguments.of("245", "312", "245312"),
+                    Arguments.of("", "", "")
+            );
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("glue")
+        void glueTest(String str1, String str2, String expected) {
+            StringUtil stringUtil = new StringUtil();
+            stringUtil.glue(str1, str2);
+        }
+
+}
