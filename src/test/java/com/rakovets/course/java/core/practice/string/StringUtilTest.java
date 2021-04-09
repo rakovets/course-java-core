c86d8185320f37772fd52e73fe7f8483c2dabbf9
andrewvolkov2021 <sangarskoy@gmail.com>
2021-04-10 02:58:56 +0300

    solution: task1-4 for theme "String"

--- src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -16,7 +16,7 @@ public class StringUtilTest {
        stringUtil = new StringUtil();
    }
 
-   static Stream<Arguments> provideArgumentsForUnion () {
+   static Stream<Arguments> provideArgumentsForUnion() {
        return Stream.of(
                Arguments.of("", "", ""),
                Arguments.of("123", "", "123"),
@@ -25,7 +25,7 @@ public class StringUtilTest {
        );
    }
 
-   static Stream<Arguments> provideArgumentsForIndexSearch () {
+   static Stream<Arguments> provideArgumentsForIndexSearch() {
        return Stream.of(
                Arguments.of("Welcome", 'e', "1"),
                Arguments.of("Hello Java", ' ', "5"),
@@ -34,22 +34,52 @@ public class StringUtilTest {
        );
    }
 
+   static Stream<Arguments> provideArgumentsCompareTwoString() {
+       return Stream.of(
+               Arguments.of("Hello Java","Hello Java", true),
+               Arguments.of("Welcome", "welcome", false),
+               Arguments.of(" Minsk","Minsk ", false),
+               Arguments.of("+375 29 999", "+375 29 999", true)
+       );
+   }
+
+   static Stream<Arguments> provideArgumentsChangeString() {
+       return Stream.of(
+               Arguments.of(" Hello Java ","HELLO JAVA"),
+               Arguments.of("Welcome", "WELCOME"),
+               Arguments.of(" Minsk","MINSK"),
+               Arguments.of(" a b c 1 ", "A B C 1")
+       );
+   }
+
+
     @ParameterizedTest(name = "Str1 - {0}, Str2 - {1}, Expected: {2}")
     @MethodSource("provideArgumentsForUnion")
-    void union(String str1, String str2, String expected) {
+    void unionTest(String str1, String str2, String expected) {
         String actual = stringUtil.union(str1, str2);
         Assertions.assertEquals(expected, actual);
     }
 
     @ParameterizedTest(name = "Str1 - {0}, char - {1}, Expected: {2}")
     @MethodSource("provideArgumentsForIndexSearch")
-    void indexSearch(String str1, char symbol, int expected) {
+    void indexSearchTest(String str1, char symbol, int expected) {
         int actual = stringUtil.indexSearch(str1, symbol);
         Assertions.assertEquals(expected, actual);
     }
 
+    @ParameterizedTest(name = "Str1 - {0}, Str2 - {1}, Expected: {2}")
+    @MethodSource("provideArgumentsCompareTwoString")
+    void compareTwoStringTest(String str1, String str2, boolean expected) {
+       boolean actual = stringUtil.compareTwoString(str1, str2);
+        Assertions.assertEquals(expected, actual);
+    }
 
-
+    @ParameterizedTest(name = "Str1 - {0}, Str2 - {1}, Expected: {2}")
+    @MethodSource("provideArgumentsChangeString")
+    void changeStringTest(String str1, String expected) {
+        String actual = stringUtil.changeString(str1);
+        Assertions.assertEquals(expected, actual);
+    }
 
 
 
