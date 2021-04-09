8a664218cdec90e6e13620ac8de89bc21a0e40c4
Aleksei <worcers@gmail.com>
2021-04-09 22:55:02 +0300

    feat: task StringUtil for theme "strings"

--- src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
@@ -1,5 +1,8 @@
 package com.rakovets.course.java.core.practice.string;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtil {
 
     public String glue(String str1, String str2) {
@@ -7,7 +10,63 @@ public class StringUtil {
     }
 
     public int symbolIndex(String x, String text) {
-        int index = text.indexOf(x);
-        return index;
+        return text.indexOf(x);
     }
+
+    public boolean stringEquals(String str1, String str2) {
+        return str1.equals(str2);
+    }
+
+    public String stringTrim(String str) {
+        return str.trim();
+    }
+
+    public String subString(String str, int beginIndex, int endIndex) {
+        return str.substring(beginIndex, endIndex);
+    }
+
+    public String replaceSymbol(String str, String oldSymbol, String newSymbol) {
+        return str.replace(oldSymbol, newSymbol);
+    }
+
+    public boolean startEndWord(String str, String word) {
+        boolean startWord = str.startsWith(word);
+        boolean endWord = str.endsWith(word);
+
+        return startWord && endWord;
+    }
+
+    public int vowel(String str) {
+        String vowels = "[AaEeIiOoUu]+";
+        int amountVowels = 0;
+
+        Pattern pattern = Pattern.compile(vowels, Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(str);
+
+        while (matcher.find()) {
+            amountVowels++;
+        }
+        return amountVowels;
+    }
+
+    public int punctuationMark(String str) {
+        String punctuationMarks = "[,.!?]+";
+        int amountPunctuationMarks = 0;
+
+        Pattern pattern = Pattern.compile(punctuationMarks, Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(str);
+
+        while (matcher.find()) {
+            amountPunctuationMarks++;
+        }
+        return amountPunctuationMarks;
+    }
+
+    public boolean palindrome(String str) {
+        String deleteSpaces = str.replaceAll("\\s", "");
+        StringBuilder sb = new StringBuilder(str.replaceAll("\\s", ""));
+        sb.reverse();
+        return deleteSpaces.equalsIgnoreCase(String.valueOf(sb));
+    }
+
 }
--- src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -28,6 +28,95 @@ public class StringUtilTest {
             );
         }
 
+        static Stream<Arguments> provideArgumentsForStringEquals() {
+            return Stream.of(
+                    Arguments.of("Hello World", "Hello World", true),
+                    Arguments.of(" ", " ", true),
+                    Arguments.of("", "", true),
+                    Arguments.of("Hello World", "HelloWorld", false),
+                    Arguments.of("", " ", false),
+                    Arguments.of("Java", "java", false)
+            );
+        }
+
+        static Stream<Arguments> provideArgumentsForStringTrim() {
+            return Stream.of(
+                    Arguments.of("  Hello World  ", "Hello World"),
+                    Arguments.of("  Hello  World  ", "Hello  World"),
+                    Arguments.of("   ", "")
+            );
+        }
+
+        static Stream<Arguments> provideArgumentsSubString() {
+            return Stream.of(
+                    Arguments.of("Hello World", 0, 5, "Hello"),
+                    Arguments.of("Hello World", 0, 6, "Hello "),
+                    Arguments.of("Hello World", 3, 8, "lo Wo"),
+                    Arguments.of("Hello  ", 0, 6, "Hello "),
+                    Arguments.of("", 0, 0, ""),
+                    Arguments.of("    ", 0, 4, "    ")
+            );
+        }
+
+        static Stream<Arguments> provideArgumentsForReplaceSymbol() {
+            return Stream.of(
+                    Arguments.of("Hello World", "l", "b", "Hebbo Worbd"),
+                    Arguments.of("Hello World", " ", "/", "Hello/World"),
+                    Arguments.of("Hello World", " ", "", "HelloWorld"),
+                    Arguments.of(":( :( :( :(", "(", ")", ":) :) :) :)")
+            );
+        }
+
+        static Stream<Arguments> provideArgumentsForStartEndWord() {
+            return Stream.of(
+                    Arguments.of("Hello World Hello", "Hello", true),
+                    Arguments.of("Java", "Java", true),
+                    Arguments.of("_Java_", "_", true),
+                    Arguments.of("Java", "java", false),
+                    Arguments.of("Hello World hello", "Hello", false),
+                    Arguments.of("", " ", false)
+            );
+        }
+
+        static Stream<Arguments> provideArgumentsForVowel() {
+            return Stream.of(
+                    Arguments.of("Helloo world", 3),
+                    Arguments.of("HEllO wOrld", 3),
+                    Arguments.of("AbcdefghIjklmnopqrstUvwxyz", 5),
+                    Arguments.of("", 0),
+                    Arguments.of("!%$", 0)
+            );
+        }
+
+        static Stream<Arguments> provideArgumentsForPunctuationMark() {
+            return Stream.of(
+                    Arguments.of(",.!?", 1),
+                    Arguments.of("What`s up?", 1),
+                    Arguments.of("", 0),
+                    Arguments.of(" ", 0),
+                    Arguments.of("!%$", 1)
+            );
+        }
+
+        static Stream<Arguments> provideArgumentsForPalindrome() {
+            return Stream.of(
+                    Arguments.of("deed", true),
+                    Arguments.of("Do geese see God", true),
+                    Arguments.of("1991", true),
+                    Arguments.of("Hello world", false),
+                    Arguments.of("Java", false)
+            );
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("provideArgumentsForGlue")
+        void glueTest(String str1, String str2, String expected) {
+            StringUtil string = new StringUtil();
+            String actual = string.glue(str1, str2);
+
+            assertEquals(expected, actual);
+        }
+
         @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
         @MethodSource("provideArgumentsForSymbolIndex")
         void symbolIndexTest(String x, String text, int expected) {
@@ -38,10 +127,73 @@ public class StringUtilTest {
         }
 
         @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
-        @MethodSource("provideArgumentsForGlue")
-        void glueTest(String str1, String str2, String expected) {
+        @MethodSource("provideArgumentsForStringEquals")
+        void stringEqualsTest(String str1, String str2, boolean expected) {
             StringUtil string = new StringUtil();
-            String actual = string.glue(str1, str2);
+            boolean actual = string.stringEquals(str1, str2);
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("provideArgumentsForStringTrim")
+        void stringTrimTest(String str, String expected) {
+            StringUtil string = new StringUtil();
+            String actual = string.stringTrim(str);
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("provideArgumentsSubString")
+        void subStringTest(String str, int beginIndex, int endIndex, String expected) {
+            StringUtil string = new StringUtil();
+            String actual = string.subString(str, beginIndex, endIndex);
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("provideArgumentsForReplaceSymbol")
+        void replaceSymbolTest(String str, String oldSymbol, String newSymbol, String expected) {
+            StringUtil string = new StringUtil();
+            String actual = string.replaceSymbol(str, oldSymbol, newSymbol);
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("provideArgumentsForStartEndWord")
+        void startEndWordTest(String str, String word, boolean expected) {
+            StringUtil string = new StringUtil();
+            boolean actual = string.startEndWord(str, word);
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("provideArgumentsForVowel")
+        void vowelTest(String str, int expected) {
+            StringUtil string = new StringUtil();
+            int actual = string.vowel(str);
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("provideArgumentsForPunctuationMark")
+        void punctuationMarkTest(String str, int expected) {
+            StringUtil string = new StringUtil();
+            int actual = string.punctuationMark(str);
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("provideArgumentsForPalindrome")
+        void palindromeTest(String str, boolean expected) {
+            StringUtil string = new StringUtil();
+            boolean actual = string.palindrome(str);
 
             assertEquals(expected, actual);
         }
