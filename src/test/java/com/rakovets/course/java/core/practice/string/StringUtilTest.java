b2d72791e4a45be7c450c3358e7afc860031d798
Aleksei <worcers@gmail.com>
2021-04-09 19:17:55 +0300

    feat: task StringUtil for theme "strings"

--- src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringUtilTest {
-        static Stream<Arguments> glue() {
+        static Stream<Arguments> provideArgumentsForGlue() {
             return Stream.of(
                     Arguments.of("1", "", "1"),
                     Arguments.of("", "5", "5"),
@@ -16,11 +18,32 @@ public class StringUtilTest {
             );
         }
 
+        static Stream<Arguments> provideArgumentsForSymbolIndex() {
+            return Stream.of(
+                    Arguments.of("l", "Hello world", 2),
+                    Arguments.of(" ", "Hello world", 5),
+                    Arguments.of("", "Java", 0),
+                    Arguments.of("b", "Java", -1),
+                    Arguments.of("V", "jaVa", 2)
+            );
+        }
+
         @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
-        @MethodSource("glue")
+        @MethodSource("provideArgumentsForSymbolIndex")
+        void symbolIndexTest(String x, String text, int expected) {
+            StringUtil string = new StringUtil();
+            int actual = string.symbolIndex(x, text);
+
+            assertEquals(expected, actual);
+        }
+
+        @ParameterizedTest(name = "Str1 {0}. Position player bu damage: {1}")
+        @MethodSource("provideArgumentsForGlue")
         void glueTest(String str1, String str2, String expected) {
             StringUtil string = new StringUtil();
-            string.glue(str1, str2);
+            String actual = string.glue(str1, str2);
+
+            assertEquals(expected, actual);
         }
 
 }
