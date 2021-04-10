769d215dfd37d1685c1d32d689f3d2668798c0f9
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-10 15:11:22 +0300

    Solution  for theme Strings.

--- src/test/java/com/rakovets/course/java/core/example/string/ManualAssuranceExample.java
+++
@@ -1,8 +0,0 @@
-package com.rakovets.course.java.core.example.string;
-
-public class ManualAssuranceExample {
-    public static void main(String[] args) {
-        String result = Example.concat("Hello", "Java");
-        System.out.println(result);
-    }
-}
