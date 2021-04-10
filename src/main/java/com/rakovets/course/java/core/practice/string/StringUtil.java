6312f6b3e3e2c5fea46adfdea9dd31c17b7794a9
andrewvolkov2021 <sangarskoy@gmail.com>
2021-04-10 03:41:58 +0300

    solution: task1-5 for theme "String"

--- src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
@@ -7,20 +7,25 @@ public class StringUtil {
         return str1.concat(str2);
     }
 
-    public int indexSearch (String str1, char symbol) {
+    public int indexSearch(String str1, char symbol) {
         return str1.indexOf(symbol);
     }
 
-    public boolean compareTwoString (String str1, String str2) {
+    public boolean compareTwoString(String str1, String str2) {
         return str1.equals(str2);
     }
 
-    public String changeString (String str1) {
+    public String changeString(String str1) {
         str1 = str1.trim();
         return str1.toUpperCase();
     }
 
-    public
+    public String retrievalNewString(String str1, int n, int m) {
+        return str1.substring(n - 1, m);
+    }
 
+    public String replaceEmoji(String str1) {
+        return str1.replace(":(",":)");
+    }
 
 }
--- src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -47,11 +47,28 @@ public class StringUtilTest {
        return Stream.of(
                Arguments.of(" Hello Java ","HELLO JAVA"),
                Arguments.of("Welcome", "WELCOME"),
-               Arguments.of(" Minsk","MINSK"),
+               Arguments.of(" Minsk", "MINSK"),
                Arguments.of(" a b c 1 ", "A B C 1")
        );
    }
 
+   static Stream<Arguments> provideArgumentsRetrievalNewString() {
+       return Stream.of(
+               Arguments.of("Hello world", 1, 5, "Hello"),
+               Arguments.of("Welcome to summer", 8, 11, " to "),
+               Arguments.of("Minsk 2021", 7, 10, "2021"),
+               Arguments.of("SunSet", 1, 6, "SunSet")
+       );
+   }
+
+   static Stream<Arguments> provideArgumentsReplaceEmoji(){
+       return Stream.of(
+               Arguments.of("Welcome:(", "Welcome:)"),
+               Arguments.of(":( Hello", ":) Hello"),
+               Arguments.of("Minsk :)","Minsk :)"),
+               Arguments.of(":(", ":)")
+       );
+   }
 
     @ParameterizedTest(name = "Str1 - {0}, Str2 - {1}, Expected: {2}")
     @MethodSource("provideArgumentsForUnion")
@@ -74,13 +91,25 @@ public class StringUtilTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @ParameterizedTest(name = "Str1 - {0}, Str2 - {1}, Expected: {2}")
+    @ParameterizedTest(name = "Str1 - {0}, Str2 - {1}")
     @MethodSource("provideArgumentsChangeString")
     void changeStringTest(String str1, String expected) {
         String actual = stringUtil.changeString(str1);
         Assertions.assertEquals(expected, actual);
     }
 
+    @ParameterizedTest(name = "Str1 - {0}, n = {1}, m = {2}, Expected: {3}")
+    @MethodSource("provideArgumentsRetrievalNewString")
+    void retrievalNewStringTest(String str1, int n, int m, String expected) {
+        String actual = stringUtil.retrievalNewString(str1, n, m);
+        Assertions.assertEquals(expected, actual);
+    }
 
+    @ParameterizedTest(name = "Str1 - {0}, Expected: {1}")
+    @MethodSource("provideArgumentsReplaceEmoji")
+    void replaceEmojiTest(String str1, String expected){
+        String actual = stringUtil.replaceEmoji(str1);
+        Assertions.assertEquals(expected, actual);
+    }
 
 }
