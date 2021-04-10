a3356bfa5b7ed5cee635a31e78c1612c646a0537
andrewvolkov2021 <sangarskoy@gmail.com>
2021-04-10 03:04:13 +0300

    solution: task1-4 for theme "String"

--- src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
@@ -1,5 +1,7 @@
 package com.rakovets.course.java.core.practice.string;
 
+import java.util.Locale;
+
 public class StringUtil {
     public String union(String str1, String str2){
         return str1.concat(str2);
@@ -8,4 +10,17 @@ public class StringUtil {
     public int indexSearch (String str1, char symbol) {
         return str1.indexOf(symbol);
     }
+
+    public boolean compareTwoString (String str1, String str2) {
+        return str1.equals(str2);
+    }
+
+    public String changeString (String str1) {
+        str1 = str1.trim();
+        return str1.toUpperCase();
+    }
+
+    public
+
+
 }
