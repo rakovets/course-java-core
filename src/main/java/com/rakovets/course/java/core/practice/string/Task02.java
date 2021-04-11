10f79029ecbe689ef70d310cf21b2493f7fe25e1
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-11 09:17:26 +0300

    Solution 2 for theme Strings.

--- src/main/java/com/rakovets/course/java/core/practice/string/Task02.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/Task02.java
@@ -1,4 +1,73 @@
 package com.rakovets.course.java.core.practice.string;
 
-public class Task02 {
+import java.util.Arrays;
+
+/**
+ * Разработать программу для разбора (parsing) банковских отчетов.
+ *
+ * @author Dmitry Rakovets
+ */
+@SuppressWarnings("unused")
+class Task02 {
+    /**
+     * The entry point of the task
+     *
+     * @param args entry arguments
+     */
+    public static void main(String[] args) {
+        //FIXME
+        // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
+        // аргументов. Типы данных изменять нельзя
+        String bankReport = "Remaining 10$  12$ essentially 13$ uncha12nged900$.       100$";
+
+        double[] moneyFromReport = getArrayMoneyFromReport(bankReport);
+        System.out.printf("Money from the report: %s\n", Arrays.toString(moneyFromReport));
+        double sumMoneyFromReport = getSumMoneyFromReport(bankReport);
+        System.out.printf("Sum money from the report: %s\n", sumMoneyFromReport);
+    }
+
+    /**
+     * Возвращает список сумм, которые встречаются в отчете.
+     *
+     * @param report в котором содержатся суммы в виде ' ${money}$ ', где ${money} - это сумма
+     * @return список сумм
+     */
+    static double[] getArrayMoneyFromReport(String report) {
+        String[] numbersInArray = report.replaceAll("[^0-9$]", " ").split(" ");
+        StringBuilder numbersInRow = new StringBuilder();
+
+        for (String word : numbersInArray) {
+            if (word.endsWith("$")) {
+                numbersInRow.append(word);
+            }
+        }
+
+        String[] numbersInArray2 = numbersInRow.toString().split("[$]");
+        double[] doubleNumbers = new double[numbersInArray2.length];
+        int index = 0;
+
+        for (String ss : numbersInArray2) {
+            doubleNumbers[index] = Double.parseDouble(ss);
+            index++;
+        }
+
+        return doubleNumbers;
+    }
+
+    /**
+     * Возвращает общую сумму всех денег, которые встречаются в отчете.
+     *
+     * @param report в котором содержатся суммы в виде ' ${money}$ ', где ${money} - это сумма
+     * @return общую сумму всех денег
+     */
+    static double getSumMoneyFromReport(String report) {
+        double sum = 0.0;
+        double[] doubleNumbers = getArrayMoneyFromReport(report);
+
+        for (double value : doubleNumbers) {
+            sum = sum + value;
+        }
+
+        return sum;
+    }
 }
