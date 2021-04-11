3e34ced6e7056c4ddeea06e4fbee4621c3b67373
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-11 09:16:02 +0300

    Solution 1 for theme Strings.

--- src/main/java/com/rakovets/course/java/core/practice/string/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/Task01.java
@@ -1,4 +1,66 @@
 package com.rakovets.course.java.core.practice.string;
 
-public class Task01 {
+/**
+ * Разработать программу для разбора (parsing) полного имени сотрудника компании.
+ *
+ * @author Dmitry Rakovets
+ */
+@SuppressWarnings("unused")
+class Task01 {
+    /**
+     * The entry point of the task
+     *
+     * @param args entry arguments
+     */
+    public static void main(String[] args) {
+        //FIXME
+        // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
+        // аргументов. Типы данных изменять нельзя
+        String fullName = "Nappie Lopez-gomez";
+
+        String name = getName(fullName);
+        System.out.printf("Name: %s\n", name);
+        String surname = getSurname(fullName);
+        System.out.printf("Surname: %s\n", surname);
+        String reverseFullName = reverseFullName(fullName);
+        System.out.printf("Reversed full name: %s\n", reverseFullName);
+    }
+
+    /**
+     * Возвращает имя сотрудника.
+     *
+     * @param fullName полное имя сотрудника согласно паттерну '${name} ${surname}'
+     * @return имя сотрудника
+     */
+    static String getName(String fullName) {
+        String[] name = fullName.split(" ");
+        return name[0];
+    }
+
+    /**
+     * Возвращает фамилию сотрудника.
+     *
+     * @param fullName полное имя сотрудника согласно паттерну '${name} ${surname}'
+     * @return фамилия сотрудника
+     */
+    static String getSurname(String fullName) {
+        String[] surname = fullName.split(" ");
+        return surname[1];
+    }
+
+    /**
+     * Возвращает полное имя сотрудника в обратном формате.
+     *
+     * @param fullName полное имя сотрудника согласно паттерну '${name} ${surname}'
+     * @return полное имя сотрудника в обратном формате
+     */
+    static String reverseFullName(String fullName) {
+        String fullNameReverse = "";
+
+        for (int i = fullName.length() - 1; i >= 0; i--) {
+            fullNameReverse = fullNameReverse + fullName.charAt(i);
+        }
+
+        return fullNameReverse;
+    }
 }
