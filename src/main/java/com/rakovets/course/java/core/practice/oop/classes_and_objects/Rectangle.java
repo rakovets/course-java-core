e93be5075195fa521fe1d58393ebfdc0a7205daf
Alex-210778 <Lukjanvich@gmail.com>
2021-04-12 22:53:09 +0300

    fix: task Rectangle for theme 'oop_classes_and_objects'

--- src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Rectangle.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Rectangle.java
@@ -8,21 +8,27 @@ public class Rectangle {
         this.firstPoint = firstPoint;
         this.secondPoint = secondPoint;
     }
+
     public Point getFirstPoint() {
         return firstPoint;
     }
+
     public void setFirstPoint(Point firstPoint) {
         this.firstPoint = firstPoint;
     }
+
     public Point getSecondPoint() {
         return secondPoint;
     }
+
     public void setSecondPoint(Point secondPoint) {
         this.secondPoint = secondPoint;
     }
+
     public double getPerimeter() {
         return 2 * (Math.abs(secondPoint.getX() - firstPoint.getX()) + Math.abs(secondPoint.getY() - firstPoint.getY()));
     }
+    
     public double getArea() {
         return Math.abs((secondPoint.getX() - firstPoint.getX()) * (secondPoint.getY() - firstPoint.getY()));
     }
