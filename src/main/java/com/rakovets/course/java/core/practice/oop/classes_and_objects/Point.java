4b0441b4b946d3cca26e3adfab936ba06bbb5300
Alex-210778 <Lukjanvich@gmail.com>
2021-04-12 22:40:12 +0300

    fix: task Point for theme 'oop_classes_and_objects'

--- src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Point.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classes_and_objects/Point.java
@@ -11,19 +11,24 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
     public double getX() {
         return x;
     }
+
     public void setX(double x) {
         this.x = x;
     }
+
     public double getY() {
         return y;
     }
+
     public void setY(double y) {
         this.y = y;
     }
+
     public double getDistance(Point point) {
-        return BigDecimal.valueOf(Math.sqrt(Math.pow((x - point.x), 2) + Math.pow((y - point.y), 2))).setScale(4, RoundingMode.HALF_UP).doubleValue();
+        return Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y,2));
     }
 }
