7c022a6c15595d50f3e08326f0ce5ab65c6c021d
Aleksandr <lukjanvich@gmail.com>
2021-04-13 21:27:46 +0300

    solution: Task1 for theme "exception_handling"

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task1.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+public class Task1 {
+    public static void throwSomeExeption() {
+        String someString = null;
+        someString.length();
+    }
+}
