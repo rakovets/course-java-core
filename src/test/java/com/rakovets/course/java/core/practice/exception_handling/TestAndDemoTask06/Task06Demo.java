dc83a618b709c09f6ef2ce44795b59200b6f8dc8
Aleksei <worcers@gmail.com>
2021-04-19 00:20:37 +0300

    fix: Task06 for theme "exception-handling"

--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/TestAndDemoTask06/Task06Demo.java
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/TestAndDemoTask06/Task06Demo.java
@@ -4,6 +4,7 @@ import com.rakovets.course.java.core.practice.exception_handling.Task06;
 
 public class Task06Demo {
     public static void main(String[] args) {
+
         try {
             Task06.throwException(null);
         } catch (Exception e) {
