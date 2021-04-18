79b2728ef80ab1dbc2dc7e1273ac75aead9c5d19
alex <adyuba@icloud.com>
2021-04-18 21:31:00 +0300

    solution: exception-handling MyException

---
+++ src/main/java/com/rakovets/course/java/core/example/exception_handling/exception/NullException.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.example.exception_handling.exception;
+
+public class NullException extends NullPointerException {
+    public NullException(String message) {
+        super(message);
+    }
+}
