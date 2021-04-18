59e114dc403d0c19ab036e48461cff612b2b53cf
Aleksei <worcers@gmail.com>
2021-04-19 00:26:46 +0300

    fix: Task06 for theme "exception-handling"

--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/TestAndDemoTask06/Task06Demo.java
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/TestAndDemoTask06/Task06Demo.java
@@ -3,11 +3,13 @@ package com.rakovets.course.java.core.practice.exception_handling.TestAndDemoTas
 import com.rakovets.course.java.core.practice.exception_handling.Task06;
 
 public class Task06Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         try {
             Task06.throwException(null);
-        } catch (Exception e) {
+        } catch (ArithmeticException | NullPointerException e) {
+            System.out.println(e.getMessage());
+        } catch (IndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         }
     }
