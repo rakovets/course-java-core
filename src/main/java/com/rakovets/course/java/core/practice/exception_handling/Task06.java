d2a55d85f1d92806e6f5eed42434f1e346e71cb2
alex <adyuba@icloud.com>
2021-04-18 21:30:20 +0300

    solution: exception-handling Task06

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task06.java
@@ -3,29 +3,14 @@ package com.rakovets.course.java.core.practice.exception_handling;
 import java.util.Random;
 
 public class Task06 {
-    public int randomException(String str, int[] array, int number) throws Exception {
-        Random random = new Random();
-        int choose = random.nextInt(5);
-        int digit = 9;
-        switch (choose) {
+    public void randomException() throws ArithmeticException, NullPointerException, RuntimeException {
+        switch ((int) (Math.random() * 3)) {
             case 1:
-                if (str == null) {
-                    throw new NullPointerException();
-                }
-                break;
+                throw new ArithmeticException();
             case 2:
-                int index = array.length + 1;
-                if (index > array.length) {
-                    throw new IndexOutOfBoundsException();
-                }
-                break;
-            case 3:
-                if (number == 0) {
-                   throw new ArithmeticException();
-                }
-                digit /= number;
-                break;
+                throw new NullPointerException();
+            default:
+                throw new RuntimeException();
         }
-        return 0;
     }
 }
