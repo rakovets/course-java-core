4d3767662bd62dad108284935d557ca3ce6e3f35
alex <adyuba@icloud.com>
2021-04-18 21:30:05 +0300

    solution: exception-handling Task05Demo

--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/Task05Demo.java
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/Task05Demo.java
@@ -1,6 +1,6 @@
 package com.rakovets.course.java.core.practice.exception_handling;
 
-import com.rakovets.course.java.core.example.exception_handling.exception.ExceptionFactorial;
+import com.rakovets.course.java.core.example.exception_handling.exception.NullException;
 
 public class Task05Demo {
     public static void main(String[] args) {
@@ -8,8 +8,7 @@ public class Task05Demo {
         try {
             System.out.println(task05.glue("Hello ", "Hi"));
         } catch (NullPointerException e) {
-            e.getMessage();
-            e.printStackTrace();
+            throw new NullException(e.getMessage());
         }
     }
 }
