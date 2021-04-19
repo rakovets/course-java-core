75849527e190bbbe60dd6794e48e9b40dadc1a96
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 10:05:06 +0300

    Task02 for theme "Exception-handling".

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task02.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task02.java
@@ -1,12 +1,8 @@
 package com.rakovets.course.java.core.practice.exception_handling;
 
 public class Task02 {
-    public static void main(String[] args) {
+    public static void throwArrayIndexOutOfBoundsException() {
         int[] array = new int[]{1, 2, 3, 4, 5};
-        try {
-            System.out.println(array[5]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ArrayIndexOutOfBoundsException has been caught.");
-        }
+        System.out.println(array[5]);
     }
 }
