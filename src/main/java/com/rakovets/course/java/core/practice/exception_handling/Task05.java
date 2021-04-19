f57c9f2d7ed43014b6d6ce4026ff74424048b7ad
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 11:38:19 +0300

    Task05 for theme "Exception-handling".

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task05.java
@@ -1,11 +1,10 @@
 package com.rakovets.course.java.core.practice.exception_handling;
 
 public class Task05 {
-    public static void printText() throws Exception {
-        String variableForException = null;
-        try {
-            System.out.println(variableForException.length());
-        } catch (NullPointerException e) {
-                throw new Exception("Exception in thread \"main\" java.lang.NullPointerException");
+    public static int lengthReturner(String text) {
+        if (text == "null") {
+            throw new NullPointerException();
         }
-    }}
+        return text.length();
+    }
+}
