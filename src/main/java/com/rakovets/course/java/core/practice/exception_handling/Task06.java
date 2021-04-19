f9b95f98ca9c526306ed46b3411325160539234e
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 10:19:29 +0300

    Task06 for theme "Exception-handling".

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task06.java
@@ -3,24 +3,7 @@ package com.rakovets.course.java.core.practice.exception_handling;
 import java.util.Random;
 
 public class Task06 {
-    /* создать метод случайным образом выбрасывающий одно из 3-х различных исключений
-     * вызвать этот метод в блоке `try ... catch`
-     * отловить и обработать эти исключения
-     ** два из них имею одинаковую обработку
-     ** обработка третьего должна отличаться от 2-х других
-     */
-
-    public static void main(String[] args) throws Exception {
-        try {
-            ramdom();
-        } catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
-            System.out.println("LOOOLZ");
-        } catch (ArithmeticException e) {
-            System.out.println("Ex.art");
-        }
-    }
-
-    public static void ramdom() throws Exception {
+    public static void randomException() {
         Random b = new Random();
         int a = b.nextInt(3);
         switch (a) {
