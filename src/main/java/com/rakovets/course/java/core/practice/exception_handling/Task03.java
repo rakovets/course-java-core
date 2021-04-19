b9ddb369ed949f460d66415b764d1ed0b3f484a7
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 10:05:18 +0300

    Task03 for theme "Exception-handling".

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task03.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task03.java
@@ -1,21 +1,9 @@
 package com.rakovets.course.java.core.practice.exception_handling;
 
-//* создать собственный класс-исключение - наследник класса `Exception`
-//* создать метод, выбрасывающий это исключение
-//* вызвать этот метод
-//* отловить это исключение
-//* вывести `stack trace` в Standard Output
 public class Task03 {
-    public static void main(String[] args) {
-        try {
-            exception();
-        } catch (ClassForTask03 e) {
-            System.out.println(e.getStackTrace());
+    public static void ageVerification(int age) throws AgeVerificationException {
+        if (age < 18) {
+            throw new AgeVerificationException("You are under 18 years old");
         }
-
-    }
-
-    public static void exception() throws ClassForTask03{
-        throw new ClassForTask03();
     }
 }
