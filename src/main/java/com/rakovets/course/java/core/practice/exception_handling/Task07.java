24d27e14eba0e5d62ce2d88ca6888d36993ae4b0
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 10:37:27 +0300

    Task07 for theme "Exception-handling".

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task07.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task07.java
@@ -3,30 +3,12 @@ package com.rakovets.course.java.core.practice.exception_handling;
 import java.util.Random;
 
 public class Task07 {
-    /*
-     * написать метод, который в 50% случаев бросает исключение
-     * вызвать этот метод в конструкции `try ... catch ... finally`
-     * протестировать работу блока `finally`
+    public static void random() throws Exception {
+        Random random = new Random();
+        int randomNumber = random.nextInt(2);
 
-     */
-
-
-    public static void main(String[] args) throws Exception {
-        try {
-            ramdom();
-        } catch (ArithmeticException e) {
-            System.out.println("Ex.art");
-        } finally {
-            System.out.println("Finally has been activated");
-        }
-    }
-
-    public static void ramdom() throws Exception {
-        Random b = new Random();
-        int a = b.nextInt(2);
-        switch (a) {
-            case 0:
-                throw new ArithmeticException();
+        if (randomNumber == 0) {
+            throw new ArithmeticException();
         }
     }
 }
