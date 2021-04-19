e259333e1f0488fc4db0bff4af7774e5bbf937e4
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 11:38:39 +0300

    Class for Task05, theme "Exception-handling".

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/MyNullPointerException.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+public class MyNullPointerException extends Exception {
+    public MyNullPointerException(String message) {
+        super(message);
+    }
+}
