6035c629e082c90faaee60a2b63cf6dab3ef573a
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 20:35:44 +0300

    PairTest for theme Generic.

---
+++ src/test/java/com/rakovets/course/java/core/practice/generic_types/PairTest.java
@@ -0,0 +1,24 @@
+package com.rakovets.course.java.core.practice.generic_types;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PairTest {
+
+    @Test
+    void swapTest() {
+        Pair<Integer, String> pair = new Pair<>(100, "Java");
+        Pair<String, Integer> swap = Pair.swap(pair);
+        Assertions.assertEquals("Java", swap.getObT());
+        Assertions.assertEquals(100, swap.getObV());
+
+    }
+
+    @Test
+    void getSwapped() {
+        Pair<Integer, String> firstPair = new Pair<>(1000, "Java");
+        Pair<String, Integer> secondPair = firstPair.getSwapped();
+        Assertions.assertEquals(1000, secondPair.getObV());
+        Assertions.assertEquals("Java", secondPair.getObT());
+    }
+}
