aada934b1116f0d57cd72d23152beb80862d4754
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 11:38:01 +0300

    Task05Demo for theme "Exception-handling".

--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/Task05Demo.java
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/Task05Demo.java
@@ -2,6 +2,10 @@ package com.rakovets.course.java.core.practice.exception_handling;
 
 public class Task05Demo {
     public static void main(String[] args) throws Exception {
-        Task05.printText();
+        try {
+            System.out.println(Task05.lengthReturner(null));
+        } catch (Exception e) {
+            throw new MyNullPointerException(e.getMessage());
+        }
     }
 }
