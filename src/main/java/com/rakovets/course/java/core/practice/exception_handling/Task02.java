11f0e6513a150b4fcf367b83a91c0e198852df2c
tatsianapuchkouskaya <tanyshkina@gmail.com>
2021-04-19 13:44:59 +0300

    solution: task2 for theme 'Exception-handling'

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task02.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+public class Task02 {
+    public static void throwArrayException() {
+        int[] array = new int[5];
+        array[5] = 7;
+    }
+}
