29d17b70f882767837fa05bd4b1c1809d799ea42
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 10:19:13 +0300

    Task05 for theme "Exception-handling".

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task05.java
@@ -1,13 +1,7 @@
 package com.rakovets.course.java.core.practice.exception_handling;
 
-
-      /* бросить одно из существующих в JDK исключений
-        * отловить это исключение
-        * выбросить своё собственное, указав в качестве причины сообщение отловленного `Exception`*/
-
-
 public class Task05 {
-    public static void main(String[] args) throws Exception {
+    public static void printText() throws Exception {
         String variableForException = null;
         try {
             System.out.println(variableForException.length());
