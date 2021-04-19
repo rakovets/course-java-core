c2bd46912e52fb702407c2c3576c552b48512852
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 10:19:46 +0300

    Task05Demo for theme "Exception-handling".

---
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/Task05Demo.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+public class Task05Demo {
+    public static void main(String[] args) throws Exception {
+        Task05.printText();
+    }
+}
