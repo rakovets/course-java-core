b4a217289e8d78ccc1529c835a3ad65bff49125a
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-19 10:55:08 +0300

    Task03 for theme "Exception-handling".

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task03.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task03.java
@@ -3,7 +3,7 @@ package com.rakovets.course.java.core.practice.exception_handling;
 public class Task03 {
     public static void ageVerification(int age) throws AgeVerificationException {
         if (age < 18) {
-            throw new AgeVerificationException("You are under 18 years old");
+            throw new AgeVerificationException("You are under 18 years old.");
         }
     }
 }
