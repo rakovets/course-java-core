f99b6aa66c0e8f983d206c6b93a7bf6962f59020
davydovbv <bogedavydov@gmail.com>
2021-04-20 14:18:45 +0300

    fixed tasks for theme "String"

--- src/test/java/com/rakovets/course/java/core/practice/string/CompareSpeedDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/string/CompareSpeedDemo.java
@@ -1,13 +1,9 @@
 package com.rakovets.course.java.core.practice.string;
 
-import java.util.Date;
-
 public class CompareSpeedDemo {
     public static void main(String[] args) {
         StringUtil stringUtil = new StringUtil();
         long[] results = stringUtil.compareSpeed("as");
-
         System.out.println("String concantination " + results[0] + "\nStringBuilder concantination "  + results[1]);
-
     }
 }
