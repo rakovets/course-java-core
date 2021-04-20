e4b2e39244e5db79635a34cd3fbbbcb5ec93951f
Alex-210778 <Lukjanvich@gmail.com>
2021-04-20 03:16:41 +0300

    solution: tasks MyExcepcion for theme "exception_handling"

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/MyException.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+public class MyException extends ArithmeticException {
+    public MyException(String message) {
+        super("secondNumber = 0");
+    }
+}
