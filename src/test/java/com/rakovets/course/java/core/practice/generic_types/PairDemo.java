0c99afdd92ef066440653265fa3be3a5629942af
Aleksei <worcers@gmail.com>
2021-04-20 03:24:33 +0300

    feat: Pair for theme "generic-types"

--- src/test/java/com/rakovets/course/java/core/practice/generic_types/PairDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/generic_types/PairDemo.java
@@ -4,5 +4,6 @@ public class PairDemo {
     public static void main(String[] args) {
         Pair pair = new Pair(20,10);
         System.out.println(Pair.swap(pair).getSwapped());
+        System.out.println(pair.toString());
     }
 }
