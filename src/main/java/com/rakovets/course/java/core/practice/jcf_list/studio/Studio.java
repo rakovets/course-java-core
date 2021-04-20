339b5c284f74f0c2e0a2685f834df12f9cd57bf5
Aleksei Alekseev <worcers@gmail.com>
2021-04-20 21:17:47 +0300

    feat: Studio for theme "jcf-collection-list"

---
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_list/studio/Studio.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.jcf_list.studio;
+
+import com.rakovets.course.java.core.practice.jcf_list.studio.model.Actor;
+
+public class Studio {
+
+    public void fire(Actor actor) {}
+}
