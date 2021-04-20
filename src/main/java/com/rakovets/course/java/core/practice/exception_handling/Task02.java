55da0ca1c912d9dcba1f8442e5d6f7eee32714aa
kiriller1990 <kiriller290890@gmail.com>
2021-04-21 00:54:42 +0300

    solution: task 1-3 for theme 'Exception-handling'

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task02.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+public class Task02 {
+    public static void throwArrayException() {
+        int[] array = new int[4];
+        array[4] = 10;
+    }
+}
---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task03.java
@@ -0,0 +1,6 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+public class Task03  {
+
+}
+
