07cd1e62d6cd9d36d2010c25176a75646bb4cc16
andrewvolkov2021 <sangarskoy@gmail.com>
2021-04-22 03:40:48 +0300

    solution: task1-3 for theme "Generic-types"

---
+++ src/main/java/com/rakovets/course/java/core/practice/generic_types/Math.java
@@ -0,0 +1,52 @@
+package com.rakovets.course.java.core.practice.generic_types;
+
+import static java.util.Arrays.sort;
+
+public abstract class Math {
+    public static Number getMaxValueOfThree(Number a, Number b, Number c) {
+        double da = a.doubleValue();
+        double db = b.doubleValue();
+        double dc = c.doubleValue();
+        if (da > db && da > db) {
+            return a;
+        } else if (db > da && db > dc) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+
+    public static Number getMinValueOfFive(Number a, Number b, Number c, Number d, Number f) {
+        double da = a.doubleValue();
+        double db = b.doubleValue();
+        double dc = c.doubleValue();
+        double dd = d.doubleValue();
+        double df = d.doubleValue();
+
+        double[] array = new double[]{da, db, dc, dd, df};
+        sort(array);
+        Number result = array[0];
+        return result;
+    }
+
+    public static Number getArithmeticMeanInArray(Number[] array) {
+        Double sum = 0.0;
+        for (Number item : array) {
+            sum += (item).doubleValue();
+        }
+        Number average = sum / array.length;
+        return average;
+    }
+
+    public static Number getMaxValueInArray(Number[] array) {
+        sort(array);
+        Number result = array[array.length - 1];
+        return result;
+    }
+
+    public static Number getMinValueInArray(Number[] array) {
+        sort(array);
+        Number result = array[0];
+        return result;
+    }
+}
---
+++ src/test/java/com/rakovets/course/java/core/practice/generic_types/MathDemo.java
@@ -0,0 +1,21 @@
+package com.rakovets.course.java.core.practice.generic_types;
+
+public class MathDemo {
+    public static void main(String[] args) {
+        Number max = Math.getMaxValueOfThree(0, 12, -12);
+        System.out.println("Max value is " + max);
+
+        Number min = Math.getMinValueOfFive(1.2, 0, -5, -5.1, 12);
+        System.out.println("Min value is " + min);
+
+        Number average = Math.getArithmeticMeanInArray(new Number[] {0.0, -1.0, 22.0, 1.1});
+        System.out.println("Average value of array is " + average);
+
+        Number maxValueInArray = Math.getMaxValueInArray(new Number[] {-3.2, 5.0, 0.0, 11.1, -6.0});
+        System.out.println("Max array value is " + maxValueInArray);
+
+        Number minValueInArray = Math.getMinValueInArray(new Number[] {-3.2, 5.0, 0.0, 11.1, -6.0});
+        System.out.println("Max array value is " + minValueInArray);
+    }
+}
+
