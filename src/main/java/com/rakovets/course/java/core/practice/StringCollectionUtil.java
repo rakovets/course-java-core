c4d36277753dbc36dc02e8a3eaa05971ac63551d
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-23 22:20:17 +0300

    StringCollectionUtil

--- src/main/java/com/rakovets/course/java/core/practice/StringCollectionUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/StringCollectionUtil.java
@@ -1,10 +1,27 @@
 package com.rakovets.course.java.core.practice;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class StringCollectionUtil {
-    public void resetWordsByLength(String []array,int lenght){
-        ArrayList<String> arrayList = new ArrayList<>();
-        arrayList = array;
+    public ArrayList<String> resetWordsByLength(Collection<String> collection, int lenght) {
+        ArrayList<String> arrayList = new ArrayList<>(collection);
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i).length() == lenght) {
+                arrayList.set(i,"*");
+            }
+        }
+        return arrayList;
+    }
+
+    public ArrayList<String> removeWordsByLength(List<String> list, int length) {
+        ArrayList<String> arrayList = new ArrayList<>(list);
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i).length() == length) {
+                arrayList.remove(i);
+            }
+        }
+        return arrayList;
     }
 }
