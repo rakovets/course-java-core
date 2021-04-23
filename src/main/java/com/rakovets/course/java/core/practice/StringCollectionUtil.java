a9091ebc3a63df4c0d6c062326367492a6056e2e
Ermakov_EV <evgeni_ermakov@mail.ru>
2021-04-23 17:10:19 +0300

    StringCollectionUtil

---
+++ src/main/java/com/rakovets/course/java/core/practice/StringCollectionUtil.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.java.core.practice;
+
+import java.util.ArrayList;
+
+public class StringCollectionUtil {
+    public void resetWordsByLength(String []array,int lenght){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList = array;
+    }
+}
