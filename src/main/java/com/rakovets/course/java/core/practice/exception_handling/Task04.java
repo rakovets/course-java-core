2d0810d473c5dfd869888413887a79b6f4ca78ef
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-04-25 11:34:35 +0300

    solution: Task-Demo-Test for theme `exception-handing'

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task04.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task04.java
@@ -3,7 +3,7 @@ package com.rakovets.course.java.core.practice.exception_handling;
 import com.rakovets.course.java.core.example.exception_handling.exception.OutOfRangeRuntimeException;
 
 public class Task04 {
-    public static void outOfRangeRuntimeException() throws OutOfRangeRuntimeException {
+    public static void outOfRangeRuntimeException() {
         throw new OutOfRangeRuntimeException("Time is out of range");
     }
 }
