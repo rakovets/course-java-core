9d28221fed427ff8f4261c56b49e3414accb7fa7
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-04-25 17:59:22 +0300

    solution: Task Obscure for theme `generic types'

---
+++ src/main/java/com/rakovets/course/java/core/practice/generic_types/pair/Pair.java
@@ -0,0 +1,35 @@
+package com.rakovets.course.java.core.practice.generic_types.pair;
+
+public class Pair<K, V> {
+    private K tapeFieldK;
+    private V tapeFieldV;
+
+    public Pair(K tapeFieldK, V tapeFieldV) {
+        this.tapeFieldK = tapeFieldK;
+        this.tapeFieldV = tapeFieldV;
+    }
+
+    public K getTapeFieldK() {
+        return tapeFieldK;
+    }
+
+    public V getTapeFieldV() {
+        return tapeFieldV;
+    }
+
+    public void setTapeFieldK(K tapeFieldK) {
+        this.tapeFieldK = tapeFieldK;
+    }
+
+    public void setTapeFieldV(V tapeFieldV) {
+        this.tapeFieldV = tapeFieldV;
+    }
+
+    public static<K, V> Pair<V, K> swap(Pair<K, V> pair) {
+        return new Pair(pair.getTapeFieldV(), pair.getTapeFieldK());
+    }
+
+    public Pair<V, K> getSwapped() {
+        return new Pair(getTapeFieldV(), getTapeFieldK());
+    }
+}
---
+++ src/main/java/com/rakovets/course/java/core/practice/generic_types/pair/PairDemo.java
@@ -0,0 +1,21 @@
+package com.rakovets.course.java.core.practice.generic_types.pair;
+
+public class PairDemo {
+    public static void main(String[] args) {
+        Pair pair = new Pair(55, 11);
+        System.out.println("K = " + pair.getTapeFieldK());
+        System.out.println("V = " + pair.getTapeFieldV());
+        pair.setTapeFieldK(10.10);
+        pair.setTapeFieldV(0.25);
+        System.out.println("New K = " + pair.getTapeFieldK());
+        System.out.println("New V = " + pair.getTapeFieldV());
+
+        Pair pair1 = Pair.swap(pair);
+        System.out.println("K after swap: " + pair1.getTapeFieldK());
+        System.out.println("V after swap: " + pair1.getTapeFieldV());
+
+        Pair pair2 = pair.getSwapped();
+        System.out.println("K after getSwapped: " + pair2.getTapeFieldK());
+        System.out.println("V after getSwapped: " + pair2.getTapeFieldV());
+    }
+}
