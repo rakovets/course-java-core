27d0ed8949b9f6660583eabc456ed36600038304
DealerSSil <evgeni_ermakov@mail.ru>
2021-04-25 17:04:00 +0300

    Task WebService for theme jcf.

---
+++ src/test/java/ClientControllerDemo.java
@@ -0,0 +1,10 @@
+import com.rakovets.course.java.core.practice.jcf_set.ClientController;
+
+import java.io.IOException;
+
+public class ClientControllerDemo {
+    public static void main(String[] args) throws IOException {
+        ClientController.control();
+    }
+}
+
