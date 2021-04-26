96ee922388bc7d5b17471e1af07a3aa0900df3f3
tatsianapuchkouskaya <tanyshkina@gmail.com>
2021-04-26 21:24:24 +0300

    solution: task_Studio_Comporator_LastName for theme 'Jcf-collection-list'

---
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_list/studio/ActorLastNameComporator.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.java.core.practice.jcf_list.studio;
+
+import java.util.Comparator;
+
+public class ActorLastNameComporator implements Comparator<Actor> {
+    @Override
+    public int compare(Actor o1, Actor o2) {
+        return o1.getLastName().compareTo(o2.getLastName());
+    }
+}
