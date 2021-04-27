e1cffa8191a0abcb75195e908d3d8c926136edc3
Alex-210778 <Lukjanvich@gmail.com>
2021-04-27 22:26:35 +0300

    fix: project 'Studio' for theme "jcf_collection"

--- src/main/java/com/rakovets/course/java/core/practice/jcf_list/studio/Studio.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_list/studio/Studio.java
@@ -17,39 +17,4 @@ public class Studio {
         actorsForStudio.sort(actorFeeComparator);
         actorsForStudio.remove(actorsForStudio.size() - 1);
     }
-
-
-
-
 }
-
-
-/*
-
-public static Collection<String> resetWordsByLength(Collection<String> arrayWords, int wordLength) {
-        Collection<String> arrayList = new ArrayList<>();
-        for (String element : arrayWords) {
-            if (element.length() != wordLength) {
-                arrayList.add(element);
-            } else {
-                arrayList.add("*");
-            }
-        }
-        return arrayList;
-    }
-
-
-
-Создать класс `Studio`.
-
-Поля:
-
-* `actors` - список актеров, которые работают в студии.
-
-Методы:
-
-* `fire()` - принимает список актеров и удаляющий из него актера с наибольшим гонораром.
-
-
-
- */
