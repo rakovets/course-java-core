b500f45cbbda37e13aa2e6830aeb2af3281da7fc
Alex-210778 <Lukjanvich@gmail.com>
2021-04-27 22:31:12 +0300

    fix: project 'class' for theme "jcf_collection"

--- src/main/java/com/rakovets/course/java/core/practice/jcf_list/сlass/SchoolClass.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_list/сlass/SchoolClass.java
@@ -8,13 +8,13 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SchoolClass {
-    public ArrayList<Student> students;
+    public List<Student> students;
 
-    public SchoolClass(ArrayList<Student> students) {
+    public SchoolClass(List<Student> students) {
         this.students = students;
     }
 
-    public Student getBestStudent(ArrayList<Student> students) {
+    public Student getBestStudent(List<Student> students) {
         Comparator<Student> studentAverageAnnualMarkComporator = new StudentAverageAnnualMarkComporator();
         students.sort(studentAverageAnnualMarkComporator);
         return students.get(students.size() - 1);
