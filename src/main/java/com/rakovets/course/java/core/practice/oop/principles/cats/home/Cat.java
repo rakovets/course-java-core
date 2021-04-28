aea19b5204a06f0de7afea45c3c64dbc9c3e5e38
Aleksei <worcers@gmail.com>
2021-04-28 19:52:40 +0300

    fix: CatsHome for theme "oop-principles"

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/cats/home/Cat.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/cats/home/Cat.java
@@ -30,6 +30,4 @@ public class Cat {
     public void setName(String name) {
         this.name = name;
     }
-
-
 }
--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/cats/home/Person.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/cats/home/Person.java
@@ -18,6 +18,4 @@ public class Person {
     public void setHappiness(int happiness) {
         this.happiness = happiness;
     }
-
-
 }
