59438526acfd9aefaba302b8fd2be432bc88fb26
Alex-210778 <Lukjanvich@gmail.com>
2021-04-28 23:55:52 +0300

    solusion: project 'TextMonitoring' for theme "jcf_collection"

---
+++ src/test/java/com/rakovets/course/java/core/practice/jcf_map/TextMonitoringDemo.java
@@ -0,0 +1,15 @@
+package com.rakovets.course.java.core.practice.jcf_map;
+
+import com.rakovets.course.java.core.practice.jcf_map.words_monitoring.TextMonitoring;
+
+public class TextMonitoringDemo {
+    public static void main(String[] args) {
+        String text = "hello, I like java, but I don't like java??? skript???.    hello hello, ,      hello";
+
+        System.out.println(TextMonitoring.researchText(text));
+        System.out.println(TextMonitoring.getCountUniqueWords(text));
+        System.out.println(TextMonitoring.getUniqueWords(text));
+        System.out.println(TextMonitoring.getFrequencyWord(text, "word"));
+        System.out.println(TextMonitoring.getFrequencyWords(text, true));
+        }
+    }
