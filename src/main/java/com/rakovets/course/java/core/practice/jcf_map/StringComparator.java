308a1c8f8cfa8b2cdd5e826cc32dc0f51fe268ee
tatsianapuchkouskaya <tanyshkina@gmail.com>
2021-04-29 08:38:00 +0300

    solution: task_TextMonitoring_Comparator1 for theme 'Jcf-queue-dequeue-set-map'

---
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_map/StringComparator.java
@@ -0,0 +1,11 @@
+package com.rakovets.course.java.core.practice.jcf_map;
+
+import java.util.Comparator;
+import java.util.Map;
+
+public class StringComparator implements Comparator<Map.Entry<String, Integer>> {
+    @Override
+    public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+        return o1.getValue() - o2.getValue();
+    }
+}
