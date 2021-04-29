16d920310ac2bcc9a34439feb2c81d512e7999be
Bozhen <bogedavydov@gmail.com>
2021-04-29 21:20:58 +0300

    Fixed tasks for theme "Lambda"

---
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/CityHelper.java
@@ -0,0 +1,40 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+public class CityHelper {
+
+    public static List<String> getUniqueCities(Collection<String> listOfCities){
+        List<String> uniqueCities = new ArrayList<>();
+        listOfCities.stream()
+                .distinct()
+                .forEach(uniqueCities::add);
+        return uniqueCities;
+    }
+
+    public static List<String> getCitiesWhitNameLength(Collection<String> listOfCities, int numberOfSymbols){
+        List<String> citiesWhitLength = new ArrayList<>();
+        listOfCities.stream()
+                .filter(p -> p.length() == numberOfSymbols)
+                .forEach(citiesWhitLength::add);
+        return citiesWhitLength;
+    }
+
+    public static List<String> getCitiesBeginningWhitLetter(List<String> cityList, String letter){
+        List<String> citiesBeginningWhitLetter = new ArrayList<>();
+        cityList.stream()
+                .filter(p -> p.startsWith(letter))
+                .forEach(citiesBeginningWhitLetter::add);
+        return citiesBeginningWhitLetter;
+    }
+
+    public static long getNumberOfCitiesUsage(List<String> cityList, String cityName){
+        long numberOfUsage = cityList.stream()
+                                    .filter(p -> p.equals(cityName))
+                                    .count();
+        return numberOfUsage;
+    }
+}
---
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/CityHelperTest.java
@@ -0,0 +1,24 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CityHelperTest {
+    public static void main(String[] args) {
+        List<String> cityList = new ArrayList<>();
+        cityList.add("New York");
+        cityList.add("Minsk");
+        cityList.add("Brest");
+        cityList.add("Dubai");
+        cityList.add("New York");
+        cityList.add("Minsk");
+        cityList.add("Brest");
+        cityList.add("Dubai");
+        System.out.println(CityHelper.getUniqueCities(cityList).toString());
+        System.out.println(CityHelper.getCitiesWhitNameLength(cityList, 5).toString());
+        System.out.println(CityHelper.getCitiesBeginningWhitLetter(cityList, "M"));
+        String minsk = "Minsk";
+        System.out.println(minsk + " " + CityHelper.getNumberOfCitiesUsage(cityList, minsk));
+
+    }
+}
