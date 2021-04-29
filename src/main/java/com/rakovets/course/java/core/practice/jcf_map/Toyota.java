a048527d0fcee8a492103cbe19f3b71a9176a384
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-04-29 08:43:19 +0300

    Update Toyota.java

--- src/main/java/com/rakovets/course/java/core/practice/jcf_map/Toyota.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_map/Toyota.java
@@ -2,7 +2,6 @@ package com.rakovets.course.java.core.practice.jcf_map;
 
 public class Toyota extends Car {
 
-
     public Toyota(String brand, String model, String color, int yearManufacture, String registrationNumber) {
         super(brand, model, color, yearManufacture, registrationNumber);
     }
