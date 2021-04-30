d7586353061e7c7627381071f0bf16e417f238b7
Aleksei <worcers@gmail.com>
2021-05-01 02:28:41 +0300

    update: CityHelper for theme "lambda-expressions"

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/CityHelper.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/CityHelper.java
@@ -5,13 +5,19 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class CityHelper {
-
     public static List<String> getUniqueCity(List<String> cityList) {
         return cityList.stream().distinct().collect(Collectors.toList());
     }
 
     public static List<String> getCityWithCertainCountSymbols(List<String> cityList, int amountSymbols) {
-        Predicate<String> predicate = cityName -> cityName.length() > amountSymbols;
-        return cityList.stream().filter(predicate).collect(Collectors.toList());
+        return cityList.stream().filter(cityName -> cityName.length() > amountSymbols).collect(Collectors.toList());
+    }
+
+    public static List<String> getCityByFirstSymbol(List<String> cityList, String symbol) {
+        return cityList.stream().filter(startSymbol -> startSymbol.startsWith(symbol)).collect(Collectors.toList());
+    }
+
+    public static int getAmountOfSameWords(List<String> cityList, String city) {
+        return (int)cityList.stream().filter(word -> word.equalsIgnoreCase(city)).count();
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/CityHelperDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/CityHelperDemo.java
@@ -5,14 +5,11 @@ import java.util.List;
 
 public class CityHelperDemo {
     public static void main(String[] args) {
-        List<String> cities = new ArrayList<>(List.of(
-                "Minsk",
-                "Gomel",
-                "Minsk",
-                "Grodno",
-                "Gomel"
-        ));
+        List<String> cities = new ArrayList<>(List.of("Minsk", "Gomel", "Minsk", "Grodno", "Gomel", "Brest", "Vitebsk",
+                "Minsk"));
         System.out.println(CityHelper.getUniqueCity(cities));
         System.out.println(CityHelper.getCityWithCertainCountSymbols(cities, 4));
+        System.out.println(CityHelper.getCityByFirstSymbol(cities, "M"));
+        System.out.println(CityHelper.getAmountOfSameWords(cities, "Minsk"));
     }
 }
