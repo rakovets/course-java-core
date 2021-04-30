b0d4dd1408de4ee0b415135447ebe09aedb82d60
alex <adyuba@icloud.com>
2021-04-30 20:12:34 +0300

    solution: Task1 for theme ENUM

---
+++ src/main/java/com/rakovets/course/java/core/practice/enum_types/Task1.java
@@ -0,0 +1,16 @@
+package com.rakovets.course.java.core.practice.enum_types;
+
+public enum Task1 {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
