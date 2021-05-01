afd065e0f4b39d1f2a82c24150e6b1fa7478b609
andrewvolkov2021 <sangarskoy@gmail.com>
2021-05-01 23:55:57 +0300

    solution: task "DateWrapper" for theme "Date-and-time"

---
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapper.java
@@ -0,0 +1,29 @@
+package com.rakovets.course.java.core.practice.date_and_time;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class DateWrapper {
+    public static LocalDate getLocalDate(int year, int month, int day) {
+        return LocalDate.of(year,month, day);
+    }
+
+    public static LocalDate getLocalDateWithChangedMonth(LocalDate date, int month) {
+        return date.plusMonths(month);
+    }
+
+    public static String getStringWithDate(LocalDate date, DateTimeFormatter pattern) {
+        return date.format(pattern);
+    }
+
+    public static LocalDate makeLocalDateFromString(String date, DateTimeFormatter formatter) {
+        return LocalDate.parse(date, formatter);
+    }
+
+    public static int getNumberDayBetweenTwoDates(LocalDate date1, LocalDate date2) {
+        Period period = Period.between(date1, date2);
+        return period.getDays();
+    }
+}
+
---
+++ src/test/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapperDemo.java
@@ -0,0 +1,26 @@
+package com.rakovets.course.java.core.practice.date_and_time;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateWrapperDemo {
+    public static void main(String[] args) {
+        LocalDate date1 = DateWrapper.getLocalDate(2021, 5, 1);
+        System.out.println(date1);
+
+        LocalDate date2 = DateWrapper.getLocalDateWithChangedMonth(date1, 8);
+        System.out.println(date2);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM, dd, yyyy", Locale.UK);
+        String dateString = DateWrapper.getStringWithDate(date2, formatter);
+        System.out.println(dateString);
+
+        String str = "May, 02, 2021";
+        LocalDate date3 = DateWrapper.makeLocalDateFromString(str, formatter);
+        System.out.println(date3);
+
+        int days = DateWrapper.getNumberDayBetweenTwoDates(date1, date3);
+        System.out.printf("Number days between two dates = %d", days);
+    }
+}
