4491bf313936f432637e0d4ae4bfe78454fdff36
DealerSSil <evgeni_ermakov@mail.ru>
2021-05-02 21:26:00 +0300

    Task Multiplication Table for theme lambda_expressions

---
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/multiplicationTable.java
@@ -0,0 +1,38 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+import java.util.stream.Stream;
+
+public class multiplicationTable {
+
+    public static void oneColumn() {
+        Stream.iterate(1, x -> x + 1)
+                .limit(10)
+                .forEach(x -> {
+                    Stream.iterate(1, y -> y + 1)
+                            .limit(10)
+                            .forEach(y -> System.out.println(x + " * " + y + " = " + x * y));
+                    System.out.println();
+                });
+    }
+
+    public static void fiveColumn() {
+        Stream.iterate(1, x -> x + 1)
+                .limit(10)
+                .forEach(x -> {
+                    Stream.iterate(1, y -> y + 1)
+                            .limit(5)
+                            .forEach(y -> System.out.print(x + " * " + y + " = " + x * y + "\t\t"));
+                    System.out.println();
+                });
+        System.out.println();
+
+        Stream.iterate(1, x -> x + 1)
+                .limit(10)
+                .forEach(x -> {
+                    Stream.iterate(6, y -> y + 1)
+                            .limit(5)
+                            .forEach(y -> System.out.print(x + " * " + y + " = " + x * y + "\t\t"));
+                    System.out.println();
+                });
+    }
+}
