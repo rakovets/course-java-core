e70605c01c96973c0c887906d17256d41bf67fc3
DealerSSil <evgeni_ermakov@mail.ru>
2021-05-03 22:21:44 +0300

    Task StudentsSorting for theme lambda.

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/StudentsSorting.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/StudentsSorting.java
@@ -1,14 +1,11 @@
 package com.rakovets.course.java.core.practice.lambda_expressions;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StudentsSorting {
-
     public static Map<Integer, List<Student>> groupingByCourse(List<Student> students) {
-
         return students.stream()
                 .collect(Collectors.groupingBy(
                         Student::getCourse));
@@ -23,28 +20,19 @@ public class StudentsSorting {
     }
 
     public static Map<String, Long> countStudentsInSpec(List<Student> students) {
-
         return students.stream()
                 .collect(Collectors.groupingBy(
                         Student::getSpec, Collectors.counting()));
     }
 
+    public boolean checkStudents(List<Student> students, int course, List<Student> spec) {
+        List<String> list = students.stream()
+                .filter(student -> student.getCourse() == course)
+                .map(Student::getSpec)
+                .collect(Collectors.toList());
+        list.removeAll(spec);
+        return list.size() > 0;
+    }
 }
 
-
-        /*
-    Дан список студентов. Каждый студент имеет:
-
-Написать utility class, который реализует следующие методы:
-
-групировка студентов по курсу
-
-получение списка специальностей (в алфавитном порядке), на которых учатся студенты
-
-получение количество учащихся на каждой из специальностей
-
-групировка студентов по специальностям (сохраняя алфавитный порядок специальности), а затем группировки по курсу
-
-проверка, есть ли среди учащихся кокретного курса, за исключение списка каких-то специальностей
-     */
 
