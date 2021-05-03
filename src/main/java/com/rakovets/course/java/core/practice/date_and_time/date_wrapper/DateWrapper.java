0742288ad6ea3d4304447afcea7126f74dfcb035
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-05-03 15:57:57 +0300

    solution: Task DateWrapper for theme `date-and-time'

---
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapper.java
@@ -0,0 +1,35 @@
+package com.rakovets.course.java.core.practice.date_and_time.date_wrapper;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+import java.util.Locale;
+
+public class DateWrapper implements TemporalAdjuster {
+    public static LocalDate getLocalDate(int year, int month, int day) {
+        return LocalDate.of(year, month, day);
+    }
+
+    public static LocalDate getDateAfterQuantityNMonth(LocalDate date, int quantityNMonth) {
+        return date.plusMonths(quantityNMonth);
+    }
+
+    public static String getDateFormatPattern(LocalDate date, String pattern) {
+        return date.format(DateTimeFormatter.ofPattern(pattern, Locale.UK));
+    }
+
+    public static LocalDate getLocalDateFormatPattern(String date, String pattern) {
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern(pattern, Locale.UK));
+    }
+
+    public static long getQuantityDaysBetweenDates(LocalDate firstDate, LocalDate secondDate) {
+        return ChronoUnit.DAYS.between(firstDate, secondDate);
+    }
+
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+        return null;
+    }
+}
---
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapperDemo.java
@@ -0,0 +1,17 @@
+package com.rakovets.course.java.core.practice.date_and_time.date_wrapper;
+
+import java.time.LocalDate;
+
+public class DateWrapperDemo {
+        public static void main(String[] args) {
+        DateWrapper dateWrapper = new DateWrapper();
+        String date = "11-01-20";
+        String pattern = "dd-MM-yy";
+        System.out.println(DateWrapper.getLocalDate(2020, 1, 11));
+        System.out.println(DateWrapper.getDateAfterQuantityNMonth(LocalDate.now(), 5));
+        System.out.println(DateWrapper.getDateFormatPattern(LocalDate.now(), pattern));
+        System.out.println(DateWrapper.getLocalDateFormatPattern(date, pattern));
+        System.out.println(DateWrapper.getQuantityDaysBetweenDates(LocalDate.of(2010, 11, 11),
+                LocalDate.of(2020, 11, 1)));
+        }
+}
