f9e7ea1bfe564d1c0e3942dbd2d9878d26e99488
Bozhen Davydov <bogedavydov@gmail.com>
2021-05-03 11:33:15 +0300

    Fixed task for theme "JCF"

--- src/main/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapper.java
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapper.java
@@ -1,34 +1,32 @@
 package com.rakovets.course.java.core.practice.date_and_time;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
 import java.time.Year;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.*;
 
 public class DateWrapper implements TemporalAdjuster {
 
-    public static LocalDate getLocalDate(int year, int month, int day){
+    public static LocalDate getLocalDate(int year, int month, int day) {
         return LocalDate.of(year, month, day);
     }
 
-    public static LocalDate changeLocalDate(LocalDate localDate, int numberOfMonths){
+    public static LocalDate changeLocalDate(LocalDate localDate, int numberOfMonths) {
         return localDate.plusMonths(numberOfMonths);
     }
 
-    public static String formatDateToString(LocalDate localDate, DateTimeFormatter formatter){
+    public static String formatDateToString(LocalDate localDate, DateTimeFormatter formatter) {
         return localDate.format(formatter);
     }
 
-    public static LocalDate stringFormatToLocalDate(String date, DateTimeFormatter formatter){
-        LocalDate localDate = LocalDate.parse(date,formatter);
+    public static LocalDate stringFormatToLocalDate(String date) {
+        LocalDate localDate = LocalDate.parse(date);
         return localDate;
     }
 
-    public static  long gapeBetweenDatas(LocalDate dateOne, LocalDate dateTwo){
+    public static  long gapeBetweenDatas(LocalDate dateOne, LocalDate dateTwo) {
         long difference;
-        if(dateOne.isAfter(dateTwo)) {
+        if (dateOne.isAfter(dateTwo)) {
             difference = ChronoUnit.DAYS.between(dateTwo, dateOne);
         } else {
             difference = ChronoUnit.DAYS.between(dateOne, dateTwo);
@@ -36,8 +34,26 @@ public class DateWrapper implements TemporalAdjuster {
         return difference;
     }
 
-    @Override
     public Temporal adjustInto(Temporal temporal, int numberOfDays) {
-        temporal.with(, numberOfDays);
+        return temporal.plus(numberOfDays, ChronoUnit.DAYS);
+
+    }
+
+
+    public Temporal adjustInto(Temporal temporal) {
+        Temporal nearestFirstOFJanuary;
+        int year = temporal.get(ChronoField.YEAR);
+        long period = ChronoUnit.DAYS.between(LocalDate.of(year, 1,1),temporal);
+        if (period > Year.of(year).length() / 2) {
+            nearestFirstOFJanuary = temporal
+                    .with(ChronoField.YEAR,year + 1)
+                    .with(ChronoField.MONTH_OF_YEAR, 1)
+                    .with(ChronoField.DAY_OF_YEAR, 1);
+        } else {
+            nearestFirstOFJanuary = temporal
+                    .with(ChronoField.MONTH_OF_YEAR, 1)
+                    .with(ChronoField.DAY_OF_YEAR, 1);
+        }
+        return nearestFirstOFJanuary;
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapperDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapperDemo.java
@@ -1,11 +1,21 @@
 package com.rakovets.course.java.core.practice.date_and_time;
 
+import javax.swing.text.DateFormatter;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class DateWrapperDemo {
     public static void main(String[] args) {
-        LocalDate dateOne = LocalDate.of(2001, 3,3);
+        DateWrapper dateWrapper = new DateWrapper();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("M d, yyyy");
+        LocalDate dateOne = LocalDate.of(2001, 7,3);
         LocalDate dateTwo = LocalDate.now();
+        System.out.println(DateWrapper.getLocalDate(2003,12,3));
+        System.out.println(DateWrapper.changeLocalDate(dateOne,4));
+        System.out.println(DateWrapper.formatDateToString(dateOne,dateTimeFormatter));
+        System.out.println(DateWrapper.stringFormatToLocalDate("2007-12-03"));
         System.out.println(DateWrapper.gapeBetweenDatas(dateTwo,dateOne));
+        System.out.println(dateWrapper.adjustInto(dateTwo,45));
+        System.out.println(dateWrapper.adjustInto(dateOne));
     }
 }
