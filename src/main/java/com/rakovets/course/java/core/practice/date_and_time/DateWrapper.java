2a831218c443af33a7f7de677ed7bbc268104ea4
busheyko <strash1111@gmai.com>
2021-05-03 18:43:17 +0300

    Task DateWrapper

--- src/main/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapper.java
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapper.java
@@ -1,8 +1,10 @@
 package com.rakovets.course.java.core.practice.date_and_time;
 
-import javax.swing.text.DateFormatter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
 
 public class DateWrapper {
     public static LocalDate localDate(int year, int months, int day) {
@@ -15,5 +17,13 @@ public class DateWrapper {
 
     public static String patternLocalDate(LocalDate localDate, String pattern) {
         return localDate.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public static LocalDate patternLocalDateString(String date, String pattern) {
+         return LocalDate.parse(date, DateTimeFormatter.ofPattern(pattern));
+    }
+
+    public static long differenceLocalDate(LocalDate firstLocalDate, LocalDate secondLocalDate) {
+        return Math.abs(ChronoUnit.DAYS.between(firstLocalDate, secondLocalDate));
     }
 }
