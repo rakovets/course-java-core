4134ba19e7bee508750b1986dab0a99f32a8e944
Aleksei <worcers@gmail.com>
2021-05-05 00:02:16 +0300

    feat: Task01 for theme "lambda-expressions"

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
@@ -1,7 +1,6 @@
 package com.rakovets.course.java.core.practice.lambda_expressions;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
