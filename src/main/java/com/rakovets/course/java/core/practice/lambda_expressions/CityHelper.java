b90bf3d28285f60f4b5c07fc9480f00a64cb82af
Aleksei <worcers@gmail.com>
2021-05-05 00:02:33 +0300

    fix: CityHelper for theme "lambda-expressions"

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/CityHelper.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/CityHelper.java
@@ -2,7 +2,6 @@ package com.rakovets.course.java.core.practice.lambda_expressions;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class CityHelper {
