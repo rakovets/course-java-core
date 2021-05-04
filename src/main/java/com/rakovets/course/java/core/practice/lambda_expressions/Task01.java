bf0042b90083c3b84e2ba8c7f31f733ed8b2f538
Aleksei <worcers@gmail.com>
2021-05-05 00:01:56 +0300

    feat: Task01 for theme "lambda-expressions"

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
@@ -1,4 +1,26 @@
 package com.rakovets.course.java.core.practice.lambda_expressions;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Task01 {
+    private static String[] keysAndValuesList = new String[] {
+            "-i", "in.txt", "--limit", "40", "-d", "1", "-o", "out.txt"
+    };
+
+    public static Map<String, String> convertArrayToMap() {
+        return IntStream.range(0, keysAndValuesList.length / 2)
+                .boxed()
+                .collect(Collectors.toMap(
+                        i -> keysAndValuesList[i * 2],
+                        i -> keysAndValuesList[i * 2 + 1]));
+    }
+
+    public static List<String> convertMapToList() {
+        return Arrays.stream(keysAndValuesList).collect(Collectors.toList());
+    }
 }
---
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01Demo.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+public class Task01Demo {
+    public static void main(String[] args) {
+        System.out.println(Task01.convertArrayToMap());
+        System.out.println(Task01.convertMapToList());
+    }
+}
