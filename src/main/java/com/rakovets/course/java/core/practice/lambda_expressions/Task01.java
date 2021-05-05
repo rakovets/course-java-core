3d1f06673e5bab5c5c9ca2478dc77d2947e12f48
Aleksei <worcers@gmail.com>
2021-05-06 00:28:57 +0300

    update: Task01 for theme "lambda-expressions"

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
@@ -11,24 +11,27 @@ public class Task01 {
             "-i", "in.txt", "--limit", "40", "-d", "1", "-o", "out.txt"
     };
 
-    public static HashMap<String, String> convertArrayToMap() {
+    public static Map<String, String> convertArrayToMap() {
         map = IntStream.range(0, keysAndValuesList.length / 2)
                 .boxed()
                 .collect(Collectors.toMap(
                         i -> keysAndValuesList[i * 2],
                         i -> keysAndValuesList[i * 2 + 1]));
-        return (HashMap<String, String>)map;
+        return map;
     }
 
     public static String[] convertMapToArray() {
-        //Так и не понял как реализовать данный метод через Stream.
-        String[] array = new String[map.size()];
+        /*String[] array = new String[map.size()];
         String[] keys = map.keySet().toArray(new String[0]);
         String[] values = map.values().toArray(new String[0]);
 
         for (int i = 0; i < array.length; i++) {
             array[i] = keys[i] + "=" + values[i];
         }
-        return array;
+        return array;*/
+        return map.entrySet()
+                .stream()
+                .map(e -> e.getKey() + "=" + e.getValue())
+                .toArray(String[]::new);
     }
 }
