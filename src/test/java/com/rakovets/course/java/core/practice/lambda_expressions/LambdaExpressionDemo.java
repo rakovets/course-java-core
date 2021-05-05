684823062df3fe6b039417c7d23d9dac6b39ed11
andrewvolkov2021 <sangarskoy@gmail.com>
2021-05-05 04:41:21 +0300

    solution: task1 for theme "Lambda-expressions"

---
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/LambdaExpressionDemo.java
@@ -0,0 +1,18 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+
+public class LambdaExpressionDemo {
+    public static void main(String[] args) {
+        String[] array = {"-i", "in.txt", "--limit", "40", "-d", "1", "-o", "out.txt"};
+        Map<String, String> map = LambdaExpressions.makeMapFromArray(array);
+        Set<Map.Entry<String, String>> argumentsWithValue = map.entrySet();
+        argumentsWithValue.stream().forEach(s -> System.out.printf("Argument: %s, value: %s\n", s.getKey(), s.getValue()));
+
+        String[] array1 = LambdaExpressions.makeArrayFromMap(map);
+        System.out.println("\nThis is array from Map:");
+        Arrays.stream(array1).forEach(System.out::println);
+    }
+}
