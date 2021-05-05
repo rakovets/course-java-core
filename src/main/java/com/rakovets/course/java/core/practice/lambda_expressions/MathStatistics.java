060d4c9268e3ac6a45259ba0174bb8d5ab5bf9aa
Aleksei <worcers@gmail.com>
2021-05-06 00:35:08 +0300

    update: MathStatistics for theme "lambda-expressions"

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/MathStatistics.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/MathStatistics.java
@@ -6,32 +6,32 @@ import java.util.Random;
 public class MathStatistics {
     public static String getRandomValues(int amountValues, int maxNumberValues) {
         Random random = new Random();
-        return Arrays.toString(Arrays.stream(new int[amountValues]).
-                map(n -> random.nextInt(maxNumberValues)).
-                toArray());
+        return Arrays.toString(Arrays.stream(new int[amountValues])
+                .map(n -> random.nextInt(maxNumberValues))
+                .toArray());
     }
 
     public static int getAmountEvenNumbers(int[] array) {
         return (int)Arrays.stream(array).
-                filter(n -> n % 2 == 0).
-                count();
+                filter(n -> n % 2 == 0)
+                .count();
     }
 
     public static int getAmountOddNumbers(int[] array) {
         return (int)Arrays.stream(array).
-                filter(n -> n % 2 != 0).
-                count();
+                filter(n -> n % 2 != 0)
+                .count();
     }
 
     public static int getAmountZeroNumbers(int[] array) {
         return (int)Arrays.stream(array).
-                filter(n -> n == 0).
-                count();
+                filter(n -> n == 0)
+                .count();
     }
 
     public static int getAmountEqualsNumbers(int[] array, int value) {
         return (int)Arrays.stream(array).
-                filter(n -> n == value).
-                count();
+                filter(n -> n == value)
+                .count();
     }
 }
