2eeaacccfdd5932cfcdbd0119fdc6a7f12c7727b
DealerSSil <evgeni_ermakov@mail.ru>
2021-05-05 20:40:04 +0300

    Task TV for theme lambda.

--- src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/TvTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/TvTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
