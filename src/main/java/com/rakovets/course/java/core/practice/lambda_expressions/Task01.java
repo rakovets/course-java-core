3105f050e59a72714f8141ff8df7db7a653f0821
Aleksei <worcers@gmail.com>
2021-05-06 00:10:41 +0300

    feat: Task01 for theme "lambda-expressions"

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
@@ -1,25 +1,34 @@
 package com.rakovets.course.java.core.practice.lambda_expressions;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Task01 {
+    private static Map<String, String> map = new HashMap<>();
     private static String[] keysAndValuesList = new String[] {
             "-i", "in.txt", "--limit", "40", "-d", "1", "-o", "out.txt"
     };
 
-    public static Map<String, String> convertArrayToMap() {
-        return IntStream.range(0, keysAndValuesList.length / 2)
+    public static HashMap<String, String> convertArrayToMap() {
+        map = IntStream.range(0, keysAndValuesList.length / 2)
                 .boxed()
                 .collect(Collectors.toMap(
                         i -> keysAndValuesList[i * 2],
                         i -> keysAndValuesList[i * 2 + 1]));
+        return (HashMap<String, String>)map;
     }
 
-    public static List<String> convertMapToList() {
-        return Arrays.stream(keysAndValuesList).collect(Collectors.toList());
+    public static String[] convertMapToArray() {
+        //Так и не понял как реализовать данный метод через Stream.
+        String[] array = new String[map.size()];
+        String[] keys = map.keySet().toArray(new String[0]);
+        String[] values = map.values().toArray(new String[0]);
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = keys[i] + "=" + values[i];
+        }
+        return array;
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01Demo.java
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01Demo.java
@@ -1,8 +1,10 @@
 package com.rakovets.course.java.core.practice.lambda_expressions;
 
+import java.util.Arrays;
+
 public class Task01Demo {
     public static void main(String[] args) {
         System.out.println(Task01.convertArrayToMap());
-        System.out.println(Task01.convertMapToList());
+        System.out.println(Arrays.toString(Task01.convertMapToArray()));
     }
 }
