c2e06cde47a4877c9f2b692868a30f1b7b332666
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-05-06 15:16:34 +0300

    solution: Task MultiplicationTable for theme `lambda-expressions'

---
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/MultiplicationTableDemo.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+public class MultiplicationTableDemo {
+    public static void main(String[] args) {
+        MultiplicationTable.printMultiplicationTableOneColumn();
+        MultiplicationTable.printMultiplicationTableFiveColumn();
+    }
+}
