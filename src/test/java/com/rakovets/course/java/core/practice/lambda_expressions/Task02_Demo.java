79103b6dfa1aa9b992473ee0f0f3f7bca87466fd
tatsianapuchkouskaya <tanyshkina@gmail.com>
2021-05-06 15:42:41 +0300

    solution: task_02_Demo for theme 'Lambda-expressions'

---
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/Task02_Demo.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+public class Task02_Demo {
+    public static void main(String[] args) {
+        Task02.multiplicationTableOneColumn();
+        Task02.multiplicationTableFiveColumn();
+    }
+}
