6acc63dbfe28595d673385f454081dea4cbf750b
DealerSSil <evgeni_ermakov@mail.ru>
2021-05-10 14:12:28 +0300

    FileAnalyze for theme java-io

--- src/main/java/com/rakovets/course/java/core/practice/io/FileAnalyzeUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/io/FileAnalyzeUtil.java
@@ -3,14 +3,15 @@ package com.rakovets.course.java.core.practice.io;
 import java.io.*;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class FileAnalyzeUtil {
 
-    public static List<String> getFileInList(String filePath) {
+    public static List<String> getTextInLinesList(Path filePath) {
         List<String> fileList = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(String.valueOf(filePath)))) {
             String currentLine;
             while ((currentLine = bufferedReader.readLine()) != null) {
                 fileList.add(currentLine);
@@ -18,23 +19,26 @@ public class FileAnalyzeUtil {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+        System.out.println(fileList);
         return fileList;
     }
 
-    public static List<String> getWordStartWithVowelList(String filePath) {
+    public static List<String> getWordStartWithVowelList(Path filePath) {
         List<String> wordList = new ArrayList<>();
-        String[] arrayWithWords = getFileInList(filePath).toString().split(" ");
+        String[] arrayWithWords = getTextInLinesList(filePath).toString()
+                .replaceAll("[,\\]\\[]","").split(" ");
         Arrays.stream(arrayWithWords)
                 .filter(x -> x.matches("^[aeiouyAEIOUY].*"))
                 .forEach(x -> wordList.add(x));
-
         return wordList;
     }
 
-    public static List<String> getWordLastCharEqualFirstCharNextWord(String filePath) {
+    public static List<String> getWordLastCharEqualFirstCharNextWord(Path filePath) {
         List<String> wordList = new ArrayList<>();
-        String[] arrayWithWord = getFileInList(filePath).toString().trim().split(" ");
+        String[] arrayWithWord = getTextInLinesList(filePath).toString().trim()
+                .replaceAll("[,\\]\\[]","").split(" ");
         for (int i = 0; i < arrayWithWord.length - 1; i++) {
+            System.out.println(arrayWithWord[i]);
             if (arrayWithWord[i].charAt(arrayWithWord[i].length() - 1) == arrayWithWord[i + 1].charAt(0)) {
                 wordList.add(arrayWithWord[i]);
             }
@@ -42,57 +46,26 @@ public class FileAnalyzeUtil {
         return wordList;
     }
 
-
-    public static Map<Character, Integer> getCharFrequency(String filePath) {
-        Map<Character, Integer> map = new HashMap<>();
-        String[] arrayWithWords = getFileInList(filePath).toString().toLowerCase().split(" ");
-        String text = arrayWithWords.toString().replaceAll("[^a-z]", "");
-        for (int i = 0; i < text.length(); i++) {
-            map.put(text.charAt(i), map.getOrDefault(text.charAt(i), 0) + 1);
-        }
-        return map;
-    }
-
-    public static Map<String, Integer> getStringFrequency(String filePath) {
-        Map<String, Integer> unsortedMap = new HashMap<>();
-        Map<String, Integer> sortedMap = new LinkedHashMap<>();
-        String[] arrayWithWords = getFileInList(filePath).toString().trim()
-                .replaceAll("[^a-zA-Z_0-9 ]", "").split(" ");
-
-        for (int i = 0; i < arrayWithWords.length; i++) {
-            unsortedMap.put(arrayWithWords[i], unsortedMap.getOrDefault(arrayWithWords[i], 0) + 1);
-        }
-
-        unsortedMap.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
-        return sortedMap;
-    }
-
-
-    public static List<String> listOfLargestNumberCombination(String filePath) {
-        List<String> arrayWithWords = getFileInList(filePath);
+    public static List<String> listOfLargestNumberCombination(Path filePath) {
+        List<String> arrayWithWords = getTextInLinesList(filePath);
         List<String> maxCombinations = new ArrayList<>();
-
+        List<Integer> numbersInArray = new ArrayList<>();
 
         for (String str : arrayWithWords) {
             String[] text = str.split(" ");
-            List<Integer> numbersInArray = new ArrayList<>();
             for (String number : text) {
                 numbersInArray.add(Integer.parseInt(number));
             }
-
             numbersInArray = numbersInArray.stream()
                     .sorted()
                     .distinct()
                     .collect(Collectors.toList());
-
             List<Integer> numbers = new ArrayList<>();
             List<Integer> tempArray = new ArrayList<>();
             tempArray.add(numbersInArray.get(0));
 
-
-            for (int i = 1; i < numbersInArray.size() - 1; i++) {
-                if (numbersInArray.get(i) + 1 == numbersInArray.get(i)) {
+            for (int i = 1; i < numbersInArray.size(); i++) {
+                if ((numbersInArray.get(i - 1) + 1) == numbersInArray.get(i)) {
                     tempArray.add(numbersInArray.get(i));
                 } else {
                     if (numbers.size() < tempArray.size()) {
@@ -100,17 +73,35 @@ public class FileAnalyzeUtil {
                         tempArray.stream().forEach(x -> numbers.add(x));
                     }
                     tempArray.clear();
-                    tempArray.add(i);
+                    tempArray.add(numbersInArray.get(i));
                 }
-
             }
-            maxCombinations.add(numbers.get(0).toString());
+
+            if (numbers.size() < tempArray.size()) {
+                numbers.clear();
+                tempArray.stream().forEach(x -> numbers.add(x));
+            }
+            maxCombinations.add(numbers.toString());
             numbers.clear();
         }
 
         return maxCombinations;
     }
 
+    public static Map<Character, Integer> getCharFrequency(Path filePath) {
+        Map<Character, Integer> map = new HashMap<>();
+        String[] arrayWithWords = getTextInLinesList(filePath).toString().toLowerCase().split(" ");
+        for(String s: arrayWithWords){
+            System.out.println(s);
+        }
+        String text = arrayWithWords.toString().replaceAll("[^a-z]", "");
+        System.out.println(text);
+        for (int i = 0; i < text.length(); i++) {
+            map.put(text.charAt(i), map.getOrDefault(text.charAt(i), 0) + 1);
+        }
+        return map;
+    }
+/*
     public static void writeSortedNumbersInFile(String filePath) {
         List<String> unsortedNumbers = getFileInList(filePath);
         String[] arrayWithNumbers = unsortedNumbers.toString()
@@ -129,6 +120,29 @@ public class FileAnalyzeUtil {
             System.out.println(ex.getMessage());
         }
     }
+
+
+
+
+/*
+
+
+    public static Map<String, Integer> getStringFrequency(String filePath) {
+        Map<String, Integer> unsortedMap = new HashMap<>();
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+        String[] arrayWithWords = getFileInList(filePath).toString().trim()
+                .replaceAll("[^a-zA-Z_0-9 ]", "").split(" ");
+
+        for (int i = 0; i < arrayWithWords.length; i++) {
+            unsortedMap.put(arrayWithWords[i], unsortedMap.getOrDefault(arrayWithWords[i], 0) + 1);
+        }
+
+        unsortedMap.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .forEachOrdered(x -> sortedMap.put(x.getKey(), x.getValue()));
+        return sortedMap;
+    }
+
+
 
     public static double studentProgress(String filePath) {
         String text;
@@ -167,4 +181,6 @@ public class FileAnalyzeUtil {
             e.printStackTrace();
         }
     }
+
+ */
 }
