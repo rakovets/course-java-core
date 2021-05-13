6f85a8d1d3204c2523da0ccda46d2dd5fb6647b0
Alex-210778 <Lukjanvich@gmail.com>
2021-05-13 03:15:58 +0300

    solution: task project `Text Monitoring` for theme "jcf_map"

---
+++ src/test/java/com/rakovets/course/java/core/practice/jcf_map/TextMonitoringTest.java
@@ -0,0 +1,34 @@
+package com.rakovets.course.java.core.practice.jcf_map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TextMonitoringTest {
+
+    @Test
+    void getCountUniqueWordsTest() {
+        String text = "hello, I like java, but I don't like java??? skript???.    hello hello, ,      hello";
+
+        int actualString = TextMonitoring.getCountUniqueWords(text);
+
+        Assertions.assertEquals(7, actualString);
+    }
+
+    @Test
+    void getFrequencyWordTest() {
+        String text = "hello, I like java, but I don't like java??? skript???.    hello hello, ,      hello";
+
+        int actualString = TextMonitoring.getFrequencyWord(text, "java");
+
+        Assertions.assertEquals(2, actualString);
+    }
+
+    @Test
+    void getFrequencyWordsText() {
+        String text = "hello, I like java, but I don't like java??? skript???.    hello hello, ,      hello";
+
+        String actualString = TextMonitoring.getFrequencyWords(text, true).toString();
+
+        Assertions.assertEquals("[but=1, don't=1, skript=1, java=2, like=2, I=2, hello=4]", actualString);
+    }
+}
