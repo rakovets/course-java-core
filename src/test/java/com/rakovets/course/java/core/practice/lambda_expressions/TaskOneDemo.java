796ff6b3bde436690b470cd4e1f8f787ecd96ecf
kiriller1990 <80848391+kiriller1990@users.noreply.github.com>
2021-05-13 15:04:04 +0200

    Update TaskOneDemo.java

--- src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/TaskOneDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/TaskOneDemo.java
@@ -6,7 +6,6 @@ import java.util.Map;
 public class TaskOneDemo {
     public static void main(String[] args) {
         String[] strings = new String[]{"-i", "in.txt", "--limit", "40", "-d", "1", "-o", "out.txt"};
-        Map<String, String> map = new HashMap<>();
         System.out.println(TaskOne.putArrayToMap(strings));
 
     }
