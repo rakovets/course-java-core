1fca5b360c704c39f69d3b19a58f8027d3f38d5f
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-05-13 16:16:16 +0300

    solution: Task 01 for theme `lambda-expressions'

---
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01.java
@@ -0,0 +1,25 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Task01 {
+    public static Map<String, String> getArrayToMap(String[] array) {
+        return Arrays.stream(getString(array))
+                .collect(Collectors.toMap(key -> key.split(" ")[0], value -> value.split(" ")[1]));
+    }
+
+    private static String[] getString(String[] array) {
+        String[] work = new String[array.length / 2];
+        int i = 0;
+        int j = 0;
+
+        while (i <= array.length - 1) {
+            work[j] = String.join(" ", array[i], array[i + 1]);
+            i += 2;
+            j++;
+        }
+        return work;
+    }
+}
---
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/Task01Demo.java
@@ -0,0 +1,12 @@
+package com.rakovets.course.java.core.practice.lambda_expressions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task01Demo {
+    public static void main(String[] args) {
+        String[] strings = new String[]{"-i", "in.txt", "--limit", "40", "-d", "1", "-o", "out.txt"};
+        Map<String, String> map = new HashMap<>();
+        System.out.println(Task01.getArrayToMap(strings));
+    }
+}
