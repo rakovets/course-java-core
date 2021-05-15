f6fe243ea77337cd7723632d96afddeb1d1adc60
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-05-15 12:35:12 +0300

    Update StudentAgeComparator.java

--- src/main/java/com/rakovets/course/java/core/example/jcf/comparator/StudentAgeComparator.java
+++ src/main/java/com/rakovets/course/java/core/example/jcf/comparator/StudentAgeComparator.java
@@ -5,7 +5,6 @@ import com.rakovets.course.java.core.practice.jcf_list.Student;
 import java.util.Comparator;
 
 public class StudentAgeComparator implements Comparator<Student> {
-
     @Override
     public int compare(Student o1, Student o2) {
         return Integer.compare(o1.getAge(), o2.getAge());
