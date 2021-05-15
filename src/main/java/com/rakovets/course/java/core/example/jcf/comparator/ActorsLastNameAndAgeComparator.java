795e79292189505f84294caea77aebda7f6c3b42
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-05-15 12:34:31 +0300

    Update ActorsLastNameAndAgeComparator.java

--- src/main/java/com/rakovets/course/java/core/example/jcf/comparator/ActorsLastNameAndAgeComparator.java
+++ src/main/java/com/rakovets/course/java/core/example/jcf/comparator/ActorsLastNameAndAgeComparator.java
@@ -5,7 +5,6 @@ import com.rakovets.course.java.core.practice.jcf_list.Actor;
 import java.util.Comparator;
 
 public class ActorsLastNameAndAgeComparator implements Comparator<Actor> {
-
     @Override
     public int compare(Actor o1, Actor o2) {
         var result = o1.getLastName().compareTo(o2.getLastName());
