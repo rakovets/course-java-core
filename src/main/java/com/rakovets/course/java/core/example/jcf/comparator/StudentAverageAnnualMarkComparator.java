0031d221210abb127d317e6cab27b19a708431b8
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-05-15 12:35:29 +0300

    Update StudentAverageAnnualMarkComparator.java

--- src/main/java/com/rakovets/course/java/core/example/jcf/comparator/StudentAverageAnnualMarkComparator.java
+++ src/main/java/com/rakovets/course/java/core/example/jcf/comparator/StudentAverageAnnualMarkComparator.java
@@ -5,11 +5,8 @@ import com.rakovets.course.java.core.practice.jcf_list.Student;
 import java.util.Comparator;
 
 public class StudentAverageAnnualMarkComparator implements Comparator<Student> {
-
     @Override
     public int compare(Student o1, Student o2) {
         return Double.compare(o1.getAverageAnnualMark(), o2.getAverageAnnualMark());
     }
 }
-
-
