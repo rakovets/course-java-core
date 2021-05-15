fbbf80de93b2dc2bda4c7d4b7d871bafd3fb49fa
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-05-15 12:34:47 +0300

    Update ClientPriorityComparator.java

--- src/main/java/com/rakovets/course/java/core/example/jcf/comparator/ClientPriorityComparator.java
+++ src/main/java/com/rakovets/course/java/core/example/jcf/comparator/ClientPriorityComparator.java
@@ -5,7 +5,6 @@ import com.rakovets.course.java.core.practice.jcf_set.Client;
 import java.util.Comparator;
 
 public class ClientPriorityComparator implements Comparator<Client> {
-
     @Override
     public int compare(Client o1, Client o2) {
         return Integer.compare(o1.getPriority(), o2.getPriority());
