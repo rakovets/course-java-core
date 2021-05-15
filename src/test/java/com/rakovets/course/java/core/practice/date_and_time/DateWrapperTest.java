67a699b19d70368535440e62cd2cf8f3e5895f68
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-05-15 12:23:40 +0300

    Update DateWrapperTest.java

--- src/test/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapperTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapperTest.java
@@ -19,19 +19,23 @@ public class DateWrapperTest {
     @Test
     void getDateTest() {
         LocalDate localDate1 = DateWrapper.getLocalDate(1994, 7, 12);
+        
         assertEquals("1994-07-12", localDate1.toString());
     }
 
     @Test
     void addMonthsTest() {
         LocalDate localDate1 = DateWrapper.getLocalDate(1994, 7, 12);
+        
         LocalDate localDate2 = DateWrapper.addMonths(localDate1, 3);
+        
         assertEquals("1994-10-12", localDate2.toString());
     }
 
     @Test
     void localDatePatternTest() {
         LocalDate localDate1 = DateWrapper.getLocalDate(1994, 7, 12);
+        
         assertEquals("12 July 1994", DateWrapper.localDatePattern(localDate1, "d MMMM yyyy"));
     }
 
@@ -43,17 +47,19 @@ public class DateWrapperTest {
     @Test
     void daysBetweenTest() {
         LocalDate localDate1 = DateWrapper.getLocalDate(1994, 7, 12);
+        
         LocalDate localDate2 = DateWrapper.getLocalDate(1995, 7, 13);
+        
         assertEquals(366, DateWrapper.daysBetweenLocalDates(localDate1, localDate2));
     }
 
     @Test
     void addDaysTest() {
         LocalDate localDate1 = DateWrapper.getLocalDate(1994, 7, 12);
+        
         assertEquals("1994-07-15", localDate1.with(DateWrapper.addDays(3)).toString());
     }
 
-    @Test
     static Stream<Arguments> adjustIntoTestArguments() {
         return Stream.of(
                 Arguments.of(LocalDate.of(1995, 01, 1), LocalDate.of(1994, 07, 12)),
@@ -70,26 +76,32 @@ public class DateWrapperTest {
     @Test
     void addMonthsDateTest() {
         Date date1 = new Date(1);
+        
         LocalDate date2 = DateWrapper.addMonthsDate(date1, 5);
+        
         assertEquals("1970-06-01", date2.toString());
     }
 
     @Test
     void datePatternTest() {
         Date date1 = new Date(1);
+        
         assertEquals("1 January 1970", DateWrapper.datePattern(date1, "d MMMM yyyy"));
     }
 
     @Test
     void calendarDateTest() {
         Calendar calendar = new GregorianCalendar(1994, Calendar.JULY, 12);
+        
         LocalDate localDate = DateWrapper.addMonthsCalendar(calendar, 3);
+        
         assertEquals("1994-10-12", localDate.toString());
     }
 
     @Test
     void calendarDatePatternTest() {
         Calendar calendar = new GregorianCalendar(1994, Calendar.JULY, 12);
+        
         assertEquals("12 July 1994", DateWrapper.calendarDatePattern(calendar, "d MMMM yyyy"));
     }
 }
