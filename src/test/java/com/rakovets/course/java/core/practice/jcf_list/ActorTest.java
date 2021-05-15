f58d4f4f151e58e598d55dbd430496a9f83be6e8
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-05-15 12:39:45 +0300

    Update ActorTest.java

--- src/test/java/com/rakovets/course/java/core/practice/jcf_list/ActorTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/jcf_list/ActorTest.java
@@ -17,7 +17,9 @@ public class ActorTest {
         actors.add(new Actor("Evgeni", "Ermakov", 5500, 20));
         actors.add(new Actor("Roman", "Romanich", 5300, 22));
         actors.add(new Actor("Uri", "Urevich", 5400, 23));
+        
         actors.sort(new ActorsLastNameComparator());
+        
         assertEquals("Ermakov", actors.get(0).getLastName());
         assertEquals("Urevich", actors.get(2).getLastName());
     }
@@ -28,7 +30,9 @@ public class ActorTest {
         actors.add(new Actor("Evgeni", "Ermakov", 5500, 20));
         actors.add(new Actor("Roman", "Romanich", 5300, 22));
         actors.add(new Actor("Uri", "Urevich", 5400, 23));
+        
         actors.sort(new ActorsAgeComparator());
+        
         assertEquals(20, actors.get(0).getAge());
         assertEquals(23, actors.get(2).getAge());
     }
@@ -39,7 +43,9 @@ public class ActorTest {
         actors.add(new Actor("Evgeni", "Ermakov", 5500, 20));
         actors.add(new Actor("Roman", "Romanich", 5300, 22));
         actors.add(new Actor("Uri", "Urevich", 5400, 23));
+        
         actors.sort(new ActorsFeeComparator());
+        
         assertEquals(5300, actors.get(0).getFee());
         assertEquals(5500, actors.get(2).getFee());
     }
@@ -50,7 +56,9 @@ public class ActorTest {
         actors.add(new Actor("Evgeni", "Ermakov", 5500, 20));
         actors.add(new Actor("Roman", "Ermakov", 5300, 22));
         actors.add(new Actor("Uri", "Urevich", 5400, 23));
+        
         actors.sort(new ActorsLastNameAndAgeComparator());
+        
         assertEquals("Ermakov", actors.get(0).getLastName());
         assertEquals(20, actors.get(0).getAge());
         assertEquals(23, actors.get(2).getAge());
@@ -62,7 +70,9 @@ public class ActorTest {
         actors.add(new Actor("Evgeni", "Ermakov", 5500, 20));
         actors.add(new Actor("Roman", "Albertikov", 5400, 22));
         actors.add(new Actor("Uri", "Urevich", 5400, 23));
+        
         actors.sort(new ActorsFeeAndLastNameComparator());
+        
         assertEquals(5400, actors.get(0).getFee());
         assertEquals("Albertikov", actors.get(0).getLastName());
         assertEquals(5500, actors.get(2).getFee());
@@ -75,9 +85,11 @@ public class ActorTest {
         actors.add(new Actor("Evgeni", "Ermakov", 5500, 20));
         actors.add(new Actor("Roman", "Albertikov", 5200, 22));
         actors.add(new Actor("Uri", "Urevich", 5300, 23));
+        
         Studio studio = new Studio(actors);
-        studio.fire(actors);
+        studio.fire(actors);   
         actors.sort(new ActorsFeeComparator());
+        
         assertEquals(2, studio.getActors().size());
         assertEquals(5300, actors.get(1).getFee());
     }
