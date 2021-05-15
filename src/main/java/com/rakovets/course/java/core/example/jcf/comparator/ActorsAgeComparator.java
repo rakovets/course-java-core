57484b97093869250c906560cbf897cff08ebaf1
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-05-15 12:33:27 +0300

    Update ActorsAgeComparator.java

--- src/main/java/com/rakovets/course/java/core/example/jcf/comparator/ActorsAgeComparator.java
+++ src/main/java/com/rakovets/course/java/core/example/jcf/comparator/ActorsAgeComparator.java
@@ -5,7 +5,6 @@ import com.rakovets.course.java.core.practice.jcf_list.Actor;
 import java.util.Comparator;
 
 public class ActorsAgeComparator implements Comparator<Actor> {
-
     @Override
     public int compare(Actor o1, Actor o2) {
         return Integer.compare(o1.getAge(), o2.getAge());
