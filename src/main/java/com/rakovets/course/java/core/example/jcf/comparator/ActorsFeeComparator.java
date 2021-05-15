5e8b7177a253ce714bb8607c7087f37851de0bc2
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-05-15 12:34:23 +0300

    Update ActorsFeeComparator.java

--- src/main/java/com/rakovets/course/java/core/example/jcf/comparator/ActorsFeeComparator.java
+++ src/main/java/com/rakovets/course/java/core/example/jcf/comparator/ActorsFeeComparator.java
@@ -5,7 +5,6 @@ import com.rakovets.course.java.core.practice.jcf_list.Actor;
 import java.util.Comparator;
 
 public class ActorsFeeComparator implements Comparator<Actor> {
-
     @Override
     public int compare(Actor o1, Actor o2) {
         return Double.compare(o1.getFee(), o2.getFee());
