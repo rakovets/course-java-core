3db114e2659b9ae03c74659aa12bf0cde7e44e09
DealerSSil <evgeni_ermakov@mail.ru>
2021-05-19 19:09:19 +0300

    Solution Producer-Queue-Consumer for theme concurrency.

--- src/main/java/com/rakovets/course/java/core/practice/concurrency/Consumer.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/Consumer.java
@@ -7,36 +7,42 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import java.sql.Timestamp;
-import java.util.NoSuchElementException;
 
 public class Consumer implements Runnable {
-    private QueueDelay queueDelay;
-
-    public Consumer(QueueDelay queue) {
-        this.queueDelay = queue;
-    }
+    private static boolean status = true;
 
     Path filePath = Paths.get("src", "main", "resources", "practice", "MasterWorker.txt");
 
     @Override
     public void run() {
-        while (true) {
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(filePath), true));
-                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-                if (queueDelay.queue.size() != 0) {
-                    int delay = queueDelay.queue.poll();
-                    Thread.sleep(delay * 1000);
-                    writer.write(timestamp + " " + Thread.currentThread().getName() + " - I slept " + delay + " seconds");
-                } else {
-                    writer.write(timestamp + " " + Thread.currentThread().getName() + " - I slept 1  seconds");
-                    Thread.sleep(1000);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(String.valueOf(filePath)))) {
+            while (isStatus()) {
+                try {
+                    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                    if (Producer.getQueue().peek() != null) {
+                        int delay = Producer.getQueue().poll();
+                        Thread.sleep(delay * 1000);
+                        writer.write(timestamp + " " + Thread.currentThread().getName() + " - I slept " + delay + " seconds");
+                    } else {
+                        writer.write(timestamp + " " + Thread.currentThread().getName() + " - I slept 1  seconds");
+                        Thread.sleep(1000);
+                    }
+                    writer.newLine();
+                    writer.flush();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
                 }
-                writer.newLine();
-                writer.flush();
-            } catch (IOException | InterruptedException | NullPointerException e) {
-                e.printStackTrace();
             }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
+    }
+
+    public static void setStatus(boolean status) {
+        Consumer.status = status;
+    }
+
+    public static boolean isStatus() {
+        return status;
     }
 }
--- src/main/java/com/rakovets/course/java/core/practice/concurrency/Producer.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/Producer.java
@@ -3,33 +3,39 @@ package com.rakovets.course.java.core.practice.concurrency;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 
 public class Producer implements Runnable {
-    private QueueDelay queueDelay;
-
-    public Producer(QueueDelay queue) {
-        this.queueDelay = queue;
-    }
+    private static LinkedList<Integer> queue = new LinkedList<>();
 
     @Override
     public void run() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String line = "";
-        while ((!line.equals("-1"))) {
+        while (!line.equals("-1")) {
             try {
                 line = reader.readLine();
                 int delay = Integer.parseInt(line);
                 if (delay == -1) {
+                    Consumer.setStatus(false);
                     break;
                 } else if (delay < -1) {
                     throw new UserInputException();
                 } else {
-                    queueDelay.queue.add(delay);
+                    queue.addLast(delay);
                 }
             } catch (UserInputException | NumberFormatException | IOException exception) {
                 exception.printStackTrace();
             }
         }
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static LinkedList<Integer> getQueue() {
+        return queue;
     }
 }
-
