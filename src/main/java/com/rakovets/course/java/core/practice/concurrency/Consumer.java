8d852739bc2ab5cd7e13deb9ec66c0e5e58ba9b4
andrewvolkov2021 <sangarskoy@gmail.com>
2021-05-19 21:43:43 +0300

    solution: task "Producer-Queue-Consumer" for theme "Concurrency"

--- src/main/java/com/rakovets/course/java/core/practice/concurrency/Consumer.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/Consumer.java
@@ -1,27 +1,29 @@
 package com.rakovets.course.java.core.practice.concurrency;
 
 import com.rakovets.course.java.core.util.AnsiColorCode;
+import com.rakovets.course.java.core.util.StandardOutputUtil;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Properties;
 import java.util.Queue;
 
 public class Consumer implements Runnable {
-
     private Queue<Integer> queue;
     private Thread thread;
+    private String filePath;
 
-    public Consumer(Queue<Integer> queue, Thread thread) {
+    public Consumer(Queue<Integer> queue, Thread thread, String filePath) {
         this.queue = queue;
         this.thread = thread;
+        this.filePath = filePath;
     }
 
     @Override
     public void run() {
+
         while (true) {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("txt"))) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(""))) {
                 if (!thread.isAlive()) {
                     break;
                 }
@@ -31,16 +33,15 @@ public class Consumer implements Runnable {
                 } else {
                     int i = queue.remove();
                     Thread.sleep(i * 1000);
-
-
-
+                    writer.write(i);
+                    writer.flush();
+                    StandardOutputUtil.printlnWithTime("Main thread: started", AnsiColorCode.FG_RED_BOLD);
                 }
             } catch (InterruptedException ex) {
                 System.out.printf("%s%s%s\n", AnsiColorCode.FG_RED_BOLD, "Thread has been interrupted", AnsiColorCode.RESET);
             } catch (IOException ioEx) {
                 System.out.printf("%s%s%s\n", AnsiColorCode.FG_RED_BOLD, "There is a problem with writing", AnsiColorCode.RESET);
             }
-
         }
     }
 }
--- src/main/java/com/rakovets/course/java/core/practice/concurrency/Program.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/Program.java
@@ -2,18 +2,38 @@ package com.rakovets.course.java.core.practice.concurrency;
 
 import com.rakovets.course.java.core.util.AnsiColorCode;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayDeque;
+import java.util.Properties;
 import java.util.Queue;
 
 public class Program {
     public static void main(String[] args) {
+        Path userPropertiesPath = Paths.get("src", "main", "java", "com.rakovets.course.java.core",
+                "practice.concurrency", "users.properties");
+        Properties p = new Properties();
+        try (FileReader reader = new FileReader(userPropertiesPath.toFile())) {
+            p.load(reader);
+        } catch (IOException ex) {
+            System.out.printf("%s%s%s\n", AnsiColorCode.FG_RED_BOLD,
+                    "There is a problem with writing path for properties", AnsiColorCode.RESET);
+        }
+
+        String filePath = p.getProperty("filePath");
+
         Queue<Integer> queue = new ArrayDeque<>();
         System.out.printf("%s%s%s\n", AnsiColorCode.FG_BLUE_BOLD, "THE PROGRAM HAS STARTED:", AnsiColorCode.RESET);
         Thread thread1 = new Thread(new Producer(queue), "Producer");
+        Thread thread2 = new Thread(new Consumer(queue, thread1, filePath), "Consumer");
 
         thread1.start();
+        thread2.start();
         try {
             thread1.join();
+            thread2.join();
 
         } catch (InterruptedException ex) {
             System.out.printf("%s%s%s\n", AnsiColorCode.FG_RED_BOLD, "Thread has been interrupted", AnsiColorCode.RESET);
