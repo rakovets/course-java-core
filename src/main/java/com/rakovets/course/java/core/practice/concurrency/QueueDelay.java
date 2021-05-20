272a0ceccb0d6ed29d6cdd11618025c383bc7fcf
EvgeniErmakov <evgeni_ermakov@mail.ru>
2021-05-20 08:49:49 +0300

    Delete QueueDelay.java

--- src/main/java/com/rakovets/course/java/core/practice/concurrency/QueueDelay.java
+++
@@ -1,8 +0,0 @@
-package com.rakovets.course.java.core.practice.concurrency;
-
-import java.util.PriorityQueue;
-import java.util.Queue;
-
-public class QueueDelay {
-    Queue<Integer> queue = new PriorityQueue();
-}
