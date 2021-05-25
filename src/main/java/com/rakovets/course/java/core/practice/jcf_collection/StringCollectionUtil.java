1933a4a616a5fdc8616bb3fc33b0cd09e959633f
Aleksei <worcers@gmail.com>
2021-05-25 23:20:38 +0300

    fix: StringCollectionUtil for theme "jcf-collection-list"

--- src/main/java/com/rakovets/course/java/core/practice/jcf_collection/StringCollectionUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_collection/StringCollectionUtil.java
@@ -1,22 +1,24 @@
 package com.rakovets.course.java.core.practice.jcf_collection;
 
+import java.util.Collection;
 import java.util.List;
 
 public class StringCollectionUtil {
-
-    public void resetWordsByLength(List<String> textList, int textLength) {
+    public Collection<String> resetWordsByLength(List<String> textList, int textLength) {
         for (String i : textList) {
             if (i.length() == textLength) {
                 textList.set(textList.indexOf(i), "*");
             }
         }
+        return textList;
     }
 
-    public void removeWordsByLength(List<String> textList, int textLength) {
+    public Collection<String> removeWordsByLength(List<String> textList, int textLength) {
         for (int i = 0; i < textList.size(); i ++) {
             if (textList.get(i).length() == textLength) {
                 textList.remove(i);
             }
         }
+        return textList;
     }
 }
