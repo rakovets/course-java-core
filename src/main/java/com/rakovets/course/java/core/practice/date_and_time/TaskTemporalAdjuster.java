c62ed69018c02d0270515d1f0367568a96d6c7a9
tatsianapuchkouskaya <tanyshkina@gmail.com>
2021-05-26 21:46:41 +0300

    solution: task_TemporalAdjuster for theme 'Date-and-time'

---
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/TaskTemporalAdjuster.java
@@ -0,0 +1,17 @@
+package com.rakovets.course.java.core.practice.date_and_time;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+public class TaskTemporalAdjuster implements TemporalAdjuster{
+    private static int days;
+
+    public TaskTemporalAdjuster(int days) {
+        this.days = days;
+    }
+
+    @Override
+    public Temporal adjustInto(Temporal temporal) {
+        return temporal.plus(this.days, ChronoUnit.DAYS);
+    }
+}
