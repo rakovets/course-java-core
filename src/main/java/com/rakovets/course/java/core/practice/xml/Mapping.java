b89d310b4a005c69fa28dbd1f07ee344e0208532
DealerSSil <evgeni_ermakov@mail.ru>
2021-05-26 20:01:58 +0300

    Add new Classes "Property" and "Mapping" for Hibernate.

---
+++ src/main/java/com/rakovets/course/java/core/practice/xml/Mapping.java
@@ -0,0 +1,23 @@
+package com.rakovets.course.java.core.practice.xml;
+
+public class Mapping {
+
+    private String name;
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/rakovets/course/java/core/practice/xml/Property.java
@@ -0,0 +1,22 @@
+package com.rakovets.course.java.core.practice.xml;
+
+public class Property {
+    private String name;
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
