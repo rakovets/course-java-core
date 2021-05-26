c8170422879ba1e10bc3aaae2374a87a1d363394
Paul <psyhocat@gmail.com>
2021-05-26 22:17:17 +0300

    completed tasks for Theme JAVA Time and Enum

--- src/main/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapper.java
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/DateWrapper.java
@@ -9,41 +9,24 @@ import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 
 public class DateWrapper {
-
-    public static LocalDate dMY(int year,int month, int day){
-        return LocalDate.of(year,month,day);
+    public static LocalDate dMY(int year, int month, int day) {
+        return LocalDate.of(year, month, day);
     }
 
-    public static LocalDate monthLocal(LocalDate date, int amountmonth){
+    public static LocalDate monthLocal(LocalDate date, int amountmonth) {
         return date.plusMonths(amountmonth);
     }
 
-    public static String patternOfDate(LocalDate date, String pattern){
+    public static String patternOfDate(LocalDate date, String pattern) {
         return date.format(DateTimeFormatter.ofPattern(pattern));
     }
 
-    public static LocalDate stringPattern(String time, String pattern){
-        return LocalDate.parse(time, DateTimeFormatter.ofPattern(pattern,Locale.UK));
+    public static LocalDate stringPattern(String time, String pattern) {
+        return LocalDate.parse(time, DateTimeFormatter.ofPattern(pattern, Locale.UK));
     }
 
-    public static long daysBetweenDates(LocalDate first, LocalDate second){
-        return ChronoUnit.DAYS.between(first,second);
+    public static long daysBetweenDates(LocalDate first, LocalDate second) {
+        return ChronoUnit.DAYS.between(first, second);
     }
 
-    public static Temporal addDays(Temporal temporal, int days){
-        return temporal.plus(days, ChronoUnit.DAYS);
-    }
-
-    public static Temporal closerToJan(Temporal temporal){
-    boolean leap = temporal.get(ChronoField.YEAR) % 4 == 0;
-    final int HALF_OF_LEAP_YEAR = 366/2;
-    final int HALF_OF_DEFAULT_YEAR = 365/2;
-    int quantityOfDays = (int)ChronoUnit.DAYS.between(temporal,LocalDate.of(temporal.get(ChronoField.YEAR)+1,1,1));
-    if(leap && quantityOfDays < HALF_OF_DEFAULT_YEAR || !leap && quantityOfDays <= HALF_OF_LEAP_YEAR){
-        temporal = temporal.with(ChronoField.YEAR, temporal.get(ChronoField.YEAR)+1);
-        return temporal .with(ChronoField.MONTH_OF_YEAR, 1).with(ChronoField.DAY_OF_MONTH,1);
-    }
-    else
-        return temporal.with(ChronoField.MONTH_OF_YEAR,1).with(ChronoField.DAY_OF_MONTH,1);
-    }
 }
--- src/test/java/com/rakovets/course/java/core/practice/date_and_time/dateWrapperDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/date_and_time/dateWrapperDemo.java
@@ -5,13 +5,15 @@ import java.time.LocalDate;
 public class dateWrapperDemo {
     public static void main(String[] args) {
         DateWrapper dateWrapper = new DateWrapper();
+        addDays addDays = new addDays(20);
+        CloserToJun closerToJun = new CloserToJun();
 
         System.out.println(DateWrapper.dMY(1,2,3));
         System.out.println(DateWrapper.monthLocal(LocalDate.now(),10));
         System.out.println(DateWrapper.patternOfDate(LocalDate.now(),"MM.dd.YY"));
         System.out.println(DateWrapper.stringPattern("04-28-21","MM-dd-yy"));
         System.out.println(DateWrapper.daysBetweenDates(LocalDate.of(2002,12,1),LocalDate.of(2001,1,1)));
-        System.out.println(DateWrapper.addDays(LocalDate.now(),22));
-        System.out.println(DateWrapper.closerToJan(LocalDate.of(2021,7,2)));
+        System.out.println(addDays.adjustInto(LocalDate.now()));
+        System.out.println(closerToJun.adjustInto(LocalDate.of(2021,7,2)));
     }
 }
