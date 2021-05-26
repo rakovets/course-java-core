50374eafc328adb706d9a426b360f70096bf027a
DealerSSil <evgeni_ermakov@mail.ru>
2021-05-26 20:38:20 +0300

    Add HibernateReader and "Mapping" for Hibernate.

---
+++ src/main/java/com/rakovets/course/java/core/practice/xml/HibernateConfiguration.java
@@ -0,0 +1,33 @@
+package com.rakovets.course.java.core.practice.xml;
+
+import java.util.List;
+
+public class HibernateConfiguration {
+    private List<Property> propertyList;
+    private List<Mapping> mappingList;
+
+    public HibernateConfiguration() {
+
+    }
+
+    public HibernateConfiguration(List<Property> propertyList, List<Mapping> mappingList) {
+        this.propertyList = propertyList;
+        this.mappingList = mappingList;
+    }
+
+    public void setPropertyList(List<Property> propertyList) {
+        this.propertyList = propertyList;
+    }
+
+    public void setMappingList(List<Mapping> mappingList) {
+        this.mappingList = mappingList;
+    }
+
+    public List<Property> getPropertyList() {
+        return propertyList;
+    }
+
+    public List<Mapping> getMappingList() {
+        return mappingList;
+    }
+}
---
+++ src/main/java/com/rakovets/course/java/core/practice/xml/HibernateReader.java
@@ -0,0 +1,89 @@
+package com.rakovets.course.java.core.practice.xml;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class HibernateReader {
+    public HibernateConfiguration readConfig(String pathToXML) {
+        HibernateConfiguration Hibernate = new HibernateConfiguration();
+        Mapping mapping = new Mapping();
+        Property property = new Property();
+        List<Mapping> mappingList = new ArrayList<>();
+        List<Property> propertyList = new ArrayList<>();
+        try {
+            XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+            XMLEventReader eventReader = inputFactory.createXMLEventReader(new FileInputStream(pathToXML));
+
+            while (eventReader.hasNext()) {
+                XMLEvent event = eventReader.nextEvent();
+                if (event.isStartElement()) {
+                    StartElement startElement = event.asStartElement();
+                    String elementName = startElement.getName().getLocalPart();
+                    switch (elementName) {
+                        case HibernateField.PROPERTY:
+                            property = new Property();
+                            String name = getAttributeValueByName(startElement, HibernateField.NAME);
+                            event = eventReader.nextEvent();
+                            String value = event.asCharacters().getData();
+                            if (name != null) {
+                            }
+                            property.setName(name);
+                            property.setValue(value);
+                            break;
+
+                        case HibernateField.MAPPING:
+                            mapping = new Mapping();
+                            String classValue = getAttributeValueByName(startElement, HibernateField.NAME);
+                            event = eventReader.nextEvent();
+                            if (classValue != null) {
+                                mapping.setName(HibernateField.NAME);
+                                mapping.setValue(classValue);
+                            } else {
+                                String packageValue = getAttributeValueByName(startElement, HibernateField.NAME);
+                                if (packageValue != null) {
+                                    mapping.setName(HibernateField.NAME);
+                                    mapping.setValue(packageValue);
+                                }
+                            }
+                    }
+                }
+
+                if (event.isEndElement()) {
+                    EndElement endElement = event.asEndElement();
+                    if (endElement.getName().getLocalPart().equals(HibernateField.PROPERTY)) {
+                        propertyList.add(property);
+                    } else if (endElement.getName().getLocalPart().equals(HibernateField.MAPPING)){
+                        mappingList.add(mapping);
+                    }
+                }
+            }
+            Hibernate.setMappingList(mappingList);
+            Hibernate.setPropertyList(propertyList);
+
+        } catch (FileNotFoundException | XMLStreamException e) {
+            e.printStackTrace();
+        }
+        return Hibernate;
+    }
+
+    private static String getAttributeValueByName(StartElement startElement, String attributeName) {
+        Iterator<Attribute> attributes = startElement.getAttributes();
+        while (attributes.hasNext()) {
+            Attribute attribute = attributes.next();
+            if (attribute.getName().toString().equals(attributeName)) {
+                return attribute.getValue();
+            }
+        }
+        return null;
+    }
+}
