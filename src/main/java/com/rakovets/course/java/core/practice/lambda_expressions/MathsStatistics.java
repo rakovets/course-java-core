b5bac99e2ae5657801decd05efe8f2febb22cecd
tatsianapuchkouskaya <tanyshkina@gmail.com>
2021-05-27 13:03:35 +0300

    fix: task_MathsStatistics for theme 'Lambda-expressions'

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/MathsStatistics.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/MathsStatistics.java
@@ -1,32 +1,38 @@
 package com.rakovets.course.java.core.practice.lambda_expressions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class MathsStatistics {
-    static int[] randomArray = new Random().ints(new Random().nextInt(50), -10, 70).toArray();
-
-    public static long getCountEvenNumber() {
-        return Arrays.stream(randomArray)
+    public static int[] getRandomNumber (int size, int firstNumber, int lastNumber) {
+        return new Random()
+                .ints(size, firstNumber, lastNumber)
+                .toArray();
+    }
+    public static long getCountEvenNumber(int[] arrayNumbers) {
+        return Arrays.stream(arrayNumbers)
                 .filter(number -> number % 2 == 0)
                 .count();
     }
 
-    public static long getCountOddNumber() {
-        return Arrays.stream(randomArray)
+    public static long getCountOddNumber(int[] arrayNumbers) {
+        return Arrays.stream(arrayNumbers)
                 .filter(number -> number % 2 != 0)
                 .count();
     }
 
-    public static long getCountZeroNumber() {
-        return Arrays.stream(randomArray)
+    public static long getCountZeroNumber(int[] arrayNumbers) {
+        return Arrays.stream(arrayNumbers)
                 .filter(number -> number == 0)
                 .count();
     }
 
-    public static long equalToValue(int value) {
-        return Arrays.stream(randomArray)
+    public static long equalToValue(int[] arrayNumbers, int value) {
+        return Arrays.stream(arrayNumbers)
                 .filter(number -> number == value)
                 .count();
     }
 }
+
