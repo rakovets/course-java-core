c52f4bf2240d308756b6f2be1b1580b7d6339a87
tatsianapuchkouskaya <tanyshkina@gmail.com>
2021-05-27 15:07:40 +0300

    solution: HibernateField for theme 'xml'

---
+++ src/main/java/com/rakovets/course/java/core/practice/xml/HibernateField.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.java.core.practice.xml;
+
+public class HibernateField {
+    public static final String HIBERNATE_CONFIGURATION = "hibernate-configuration";
+    public static final String SESSION_FACTORY = "session-factory";
+    public static final String PROPERTY = "property";
+    public static final String MAPPING = "mapping";
+    public static final String NAME = "name";
+    public static final String CLASS = "class";
+}
