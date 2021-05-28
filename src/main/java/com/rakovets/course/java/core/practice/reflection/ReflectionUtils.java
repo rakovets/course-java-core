673bc20c9fadab22b51b3baffee419a68331abce
Aleksei <worcers@gmail.com>
2021-05-28 21:00:13 +0300

    feat: ReflectionUtil for theme "reflection"

--- src/main/java/com/rakovets/course/java/core/practice/reflection/ReflectionUtils.java
+++ src/main/java/com/rakovets/course/java/core/practice/reflection/ReflectionUtils.java
@@ -5,10 +5,9 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ReflectionUtils {
-    public static Field getFieldFromObject(String name, Class clazz) throws IllegalAccessException, NoSuchFieldException {
-        Field modelFields;
-        clazz = Class.class;
-        modelFields = clazz.getDeclaredField(name);
+
+    public static Field getFieldFromObject(String name, Class<?> clazz) throws IllegalAccessException, NoSuchFieldException {
+        Field modelFields = clazz.getDeclaredField(name);
         modelFields.setAccessible(true);
         return (Field) modelFields.get(clazz);
     }
--- src/test/java/com/rakovets/course/java/core/practice/reflection/ReflectionUtilDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/reflection/ReflectionUtilDemo.java
@@ -1,12 +1,14 @@
 package com.rakovets.course.java.core.practice.reflection;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.NoSuchElementException;
-
 public class ReflectionUtilDemo {
     public static void main(String[] args) {
-        Model model = new Model("Tokamo", 22);
-        ReflectionUtils.getFieldFromObject("name", model);
+        String name = "name";
+        Model model = new Model("Tokamo");
+        Class<Model> modelClass = Model.class;
+        try {
+            System.out.println(ReflectionUtils.getFieldFromObject(name, modelClass));
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
     }
 }
