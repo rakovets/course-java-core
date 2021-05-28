039cc11fbc86ac66673b2ee91c0d6a3246f75604
Oleg Busheyko <80833044+busheyko@users.noreply.github.com>
2021-05-28 13:06:15 +0300

    Update src/main/java/com/rakovets/course/java/core/example/exception_handling/task5/CustomNulException.java
    Co-authored-by: Dmitry Rakovets <dmitryrakovets@gmail.com>

--- src/main/java/com/rakovets/course/java/core/example/exception_handling/task5/CustomNulException.java
+++ src/main/java/com/rakovets/course/java/core/example/exception_handling/task5/CustomNulException.java
@@ -1,6 +1,6 @@
 package com.rakovets.course.java.core.example.exception_handling.task5;
 
-public class CustomNulException extends NullPointerException{
+public class CustomException {
     public CustomNulException(NullPointerException e,String message) {
         super(message);
     }
