b9d96606b643b374d15280e5a07b3cb48258052c
DealerSSil <evgeni_ermakov@mail.ru>
2021-05-30 19:08:01 +0300

    Project 'MasterWorks' for theme Concurrency

--- src/main/java/com/rakovets/course/java/core/practice/concurrency/UserInputException.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/UserInputException.java
@@ -1,6 +1,5 @@
 package com.rakovets.course.java.core.practice.concurrency;
 
-
 public class UserInputException extends Exception {
     public String toString() {
         return "The number is negative or you entered not a number";
