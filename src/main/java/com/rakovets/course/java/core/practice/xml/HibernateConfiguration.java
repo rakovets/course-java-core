d35d07157c4289ab00b8d2f8b9f8eb5b333b6b03
vitovtbel <vitaliyeliseyenka@gmail.com>
2021-05-31 13:28:17 +0300

    commit xml

---
+++ src/main/java/com/rakovets/course/java/core/practice/xml/HibernateConfiguration.java
@@ -0,0 +1,32 @@
+package com.rakovets.course.java.core.practice.xml;
+
+import java.util.LinkedHashMap;
+
+public class HibernateConfiguration {
+    private LinkedHashMap<String, String> property;
+    private LinkedHashMap<String, String> mapping;
+
+    public HibernateConfiguration() {
+    }
+
+    public HibernateConfiguration(LinkedHashMap<String, String> property, LinkedHashMap<String, String> mapping) {
+        this.property = property;
+        this.mapping = mapping;
+    }
+
+    public LinkedHashMap<String, String> getProperty() {
+        return property;
+    }
+
+    public void setProperty(LinkedHashMap<String, String> property) {
+        this.property = property;
+    }
+
+    public LinkedHashMap<String, String> getMapping() {
+        return mapping;
+    }
+
+    public void setMapping(LinkedHashMap<String, String> mapping) {
+        this.mapping = mapping;
+    }
+}
---
+++ src/main/java/com/rakovets/course/java/core/practice/xml/HibernateDemo.java
@@ -0,0 +1,23 @@
+package com.rakovets.course.java.core.practice.xml;
+
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class HibernateDemo {
+    public static void main(String[] args) {
+        HibernateReader read = new HibernateReader();
+        HibernateConfiguration readConfig = read.readConfig(Paths.get("src", "main", "resources", "practice", "xml", "hibernate.cfg.xml").toString());
+        LinkedHashMap<String, String> propertys = readConfig.getProperty();
+        LinkedHashMap<String, String> mapping = readConfig.getMapping();
+        for (Map.Entry<String, String> property : propertys.entrySet()) {
+            System.out.println("Name = " + property.getKey() + "; Value = " + property.getValue());
+        }
+        for (Map.Entry<String, String> map : mapping.entrySet()) {
+            System.out.println("Class = " + map.getKey() + "; Value = " + map.getValue());
+        }
+        HibernateConfiguration hibernateConfiguration = new HibernateConfiguration(propertys, mapping);
+        HibernateWriter configFile = new HibernateWriter();
+        configFile.saveConfigFile(Paths.get("src", "main", "resources", "practice", "xml", "hibernate-writer.xml").toString(),hibernateConfiguration);
+    }
+}
