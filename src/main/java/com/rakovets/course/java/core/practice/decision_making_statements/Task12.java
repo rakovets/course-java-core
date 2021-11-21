23e3cc05b3000b4c4cdb54f1f19b97de6d94dc45
Nasta2305 <juniorwind23@gmail.com>
2021-11-21 12:58:25 +0300

    Task 12 for theme "Decision-making statements."

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task12.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task12.java
@@ -28,7 +28,7 @@ class Task12 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int day = 12;
+        int day = 22;
         int month = 12;
 
         String zodiacSign = getZodiacSign(day, month);
@@ -43,8 +43,60 @@ class Task12 {
      * @return знак зодиака
      */
     static String getZodiacSign(int day, int month) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        switch (month) {
+            case 1:
+                if (day <=20) {
+                    return "Goat";
+                } else return "Water-bearer";
+            case 2:
+                if (day <=19) {
+                    return "Water-bearer";
+                } else return "Fish";
+            case 3:
+                if (day <=20) {
+                    return "Fish";
+                } else return "Ram";
+            case 4:
+                if (day <=20) {
+                    return "Ram";
+                } else return "Bull";
+            case 5:
+                if (day <=20) {
+                    return "Bull";
+                } else return "Twins";
+            case 6:
+                if (day <=21) {
+                    return "Twins";
+                } else return "Crab";
+            case 7:
+                if (day <=22)  {
+                    return "Crab";
+                } else return "Lion";
+            case 8:
+                if (day <=22) {
+                    return "Lion";
+                } else return "Maiden";
+            case 9:
+                if (day <=21) {
+                    return "Maiden";
+                } else return "Scales";
+            case 10:
+                if (day <=22) {
+                    return "Scales";
+                } else return "Scorpion";
+            case 11:
+                if (day <=22) {
+                    return "Scorpion";
+                } else return "Archer";
+            case 12:
+                if (day <=21) {
+                    return "Archer";
+                } else return "Goat";
+            default:
+                return null;
+        }
+
+
+
     }
 }
