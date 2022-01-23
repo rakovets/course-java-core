0cc559935e5f0befde755e4eb3fd5b1173794666
Nasta2305 <juniorwind23@gmail.com>
2022-01-23 11:52:32 +0300

    Task 03 for theme "Looping_statements"

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task03.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task03.java
@@ -17,8 +17,8 @@ class Task03 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int healthPoints = 1000;
-        double regenerationPercentFromCurrentHealth = 100.0;
+        int healthPoints = 100;
+        double regenerationPercentFromCurrentHealth = 5.0;
         int hoursAfterRespawn = 10;
 
         int featureHealthPoint = calculateHealthPointsByTime(healthPoints, regenerationPercentFromCurrentHealth, hoursAfterRespawn);
@@ -34,8 +34,11 @@ class Task03 {
      * @return количество HP
      */
     static int calculateHealthPointsByTime(int healthPoints, double regenerationPercentFromCurrentHealth, int hoursAfterRespawn) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int featureHealthPoint = healthPoints;
+        regenerationPercentFromCurrentHealth = regenerationPercentFromCurrentHealth / 100;
+        for (int i=1; i <= hoursAfterRespawn; i++){
+            featureHealthPoint += (int) Math.round(featureHealthPoint * regenerationPercentFromCurrentHealth);
+        }
+        return featureHealthPoint;
     }
 }
