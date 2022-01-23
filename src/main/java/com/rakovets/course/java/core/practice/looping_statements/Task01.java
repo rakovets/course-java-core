300efcee45da765bc4d794417db4137621506057
Nasta2305 <juniorwind23@gmail.com>
2022-01-23 11:51:31 +0300

    Task 01 for theme "Looping_statements"

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task01.java
@@ -16,7 +16,7 @@ class Task01 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int rows = 10;
+        int rows = 9;
 
         String numbersColumn = generateNumbersColumn(rows);
         System.out.printf("Result:\n%s", numbersColumn);
@@ -29,8 +29,13 @@ class Task01 {
      * @return текст, который содержит графа с порядковыми номерами записей, где каждый номер на новой строке
      */
     static String generateNumbersColumn(int numberRows) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String generateNumbersColumn = "";
+        for (int i = 1; i <= numberRows; i++){
+            generateNumbersColumn += i;
+            if (i < numberRows) {
+                generateNumbersColumn += "\n";
+            }
+        }
+        return generateNumbersColumn;
     }
 }
