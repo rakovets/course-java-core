c03b83cf8eb2382eb487b7a7d7e6fbe2fa34b80b
Nasta2305 <juniorwind23@gmail.com>
2022-01-23 11:52:45 +0300

    Task 04 for theme "Looping_statements"

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task04.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task04.java
@@ -16,12 +16,12 @@ class Task04 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int healthPoints = 1000;
-        double regenerationPercentFromCurrentHealth = 10.0;
-        int averageDamagePerHour = 200;
+        int healthPoints = 10000;
+        double regenerationPercentFromCurrentHealth = 5.0;
+        int averageDamagePerHour = 917;
 
         double raidTime = calculateRaidTime(healthPoints, regenerationPercentFromCurrentHealth, averageDamagePerHour);
-        System.out.printf("Result: %f", raidTime);
+        System.out.printf("Result: %.0f", raidTime);
     }
 
     /**
@@ -33,8 +33,18 @@ class Task04 {
      * @return время для убийства RaidBoss (когда party не справляется за 24 часа, то вывести -1)
      */
     static int calculateRaidTime(int healthPoints, double regenerationPercentPerHour, int averageDamagePerHour) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int raidTime = 0;
+        regenerationPercentPerHour = regenerationPercentPerHour / 100;
+
+        while (healthPoints > 0 && raidTime <= 23) {
+            healthPoints += (int) Math.round(healthPoints * regenerationPercentPerHour);
+            healthPoints -= averageDamagePerHour;
+            raidTime +=1;
+        }
+        if (healthPoints < 0){
+            return raidTime;
+        } else {
+            return -1;
+        }
     }
 }
