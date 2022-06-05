2e7c32e8607723eb5140911629c54f3088d1a396
Evgeniy Antonovich <gnoptswbn5kj@mail.ru>
2022-06-05 23:41:07 +0300

    feat: complete operators Task01

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task01.java
@@ -31,6 +31,6 @@ class Task01 {
     static int getNumberFullTrucks(int cargo, int carryingCapacity) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        return cargo / carryingCapacity;
     }
 }
