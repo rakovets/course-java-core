dee5a0775c6e244eb5f4bd62ff0db7b7727ae4da
Yegor Kharitonchik <egorkhary@mail.ru>
2022-06-06 17:05:11 +0300

    feat: completed the task06

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task06.java
@@ -28,8 +28,36 @@ class Task06 {
      * @return время в формате 'D H:m:s', где D - дни, H - часы, m - минуты, s - секунды
      */
     static String getPlayingTime(int playingTimeInSeconds) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        int Days = 0, Hours = 0, Min = 0, Sec = 0;
+
+        Days = playingTimeInSeconds / 86400;
+        Hours = playingTimeInSeconds % 86400;
+        Min = Hours % 3600;
+        Hours /= 3600;
+        Sec = Min % 60;
+        Min /= 60;
+
+        /**boolean Agreem = true;
+        while(Agreem) {
+            if(playingTimeInSeconds >= 60) {
+                playingTimeInSeconds -= 60;
+                Min++;
+            }
+            else {
+                Sec = playingTimeInSeconds;
+                Agreem = false;
+            }
+            if(Min == 60) {
+                Hours++;
+                Min = 0;
+            }
+            if(Hours == 24) {
+                Days++;
+                Hours = 0;
+            }
+        }**/
+
+        String Time = Days + " " + Hours + ":" + Min + ":" + Sec;
+        return Time;
     }
 }
