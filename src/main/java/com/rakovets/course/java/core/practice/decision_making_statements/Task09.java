7e0b23b0cc9f817dba0ab4b85da8e50dcc37d3c8
Yegor Kharitonchik <egorkhary@mail.ru>
2022-06-07 00:33:21 +0300

    feat: complete Task09

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task09.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task09.java
@@ -34,6 +34,7 @@ class Task09 {
     static int getPorchNumber(int numberFloors, int numberApartmentsPerFloor, int apartmentNumber) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
+
         int commonCountOfApartaments = numberFloors * numberApartmentsPerFloor;
         float numberOfPorch = (float) apartmentNumber / commonCountOfApartaments;
         int result = Math.round(numberOfPorch);
