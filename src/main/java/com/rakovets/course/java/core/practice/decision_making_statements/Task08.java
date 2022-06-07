d43abea4989dda4834f906f31fcae56c23f4346a
Evgeniy Antonovich <gnoptswbn5kj@mail.ru>
2022-06-07 22:49:55 +0300

    feat: complete decision_making_statements Task08

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task08.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task08.java
@@ -40,6 +40,19 @@ class Task08 {
     static int getTotalDamage(int damage, String typeMob, boolean hasHolyAttribute) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        double damageMultiplier=1;
+        if (hasHolyAttribute) {
+            switch (typeMob) {
+                case "UNDEAD":
+                case "ZOMBIE":
+                    damageMultiplier = 1.5;
+                    break;
+                case "SAINT":
+                    damageMultiplier = 0.5;
+                    break;
+            }
+        }
+        int totalDamage = (int) (damage * damageMultiplier);
+        return  totalDamage;
     }
 }
