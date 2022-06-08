18b5fa8e939acf247e2db251d17400e6b167056b
AlenaTrafimovich <trofimovich_elena@mail.ru>
2022-06-08 19:11:57 +0300

    feat: task 6

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task06.java
@@ -10,7 +10,8 @@ class Task06 {
     private final static int MIN_IN_HOUR = 60;
     private final static int SEC_IN_MIN = 60;
     private final static int SEC_IN_DAY = HOURS_IN_DAY * MIN_IN_HOUR * SEC_IN_MIN;
-    private final static int SEC_N_HOUR = MIN_IN_HOUR * SEC_IN_MIN;
+
+    private final static int SEC_IN_HOUR = MIN_IN_HOUR * SEC_IN_MIN;
 
     /**
      * The entry point of the task
@@ -36,10 +37,10 @@ class Task06 {
     static String getPlayingTime(int playingTimeInSeconds) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        int d = playingTimeInSeconds / (SEC_IN_DAY);
-        int h = playingTimeInSeconds % (SEC_IN_DAY) / (SEC_N_HOUR);
-        int m = (playingTimeInSeconds % (SEC_IN_DAY) % (SEC_N_HOUR)) / MIN_IN_HOUR;
-        int s = (playingTimeInSeconds % (SEC_IN_DAY) % (SEC_N_HOUR)) % MIN_IN_HOUR;
+        int d = playingTimeInSeconds / SEC_IN_DAY;
+        int h = (playingTimeInSeconds % SEC_IN_DAY) / SEC_IN_HOUR;
+        int m = (playingTimeInSeconds % SEC_IN_DAY % SEC_IN_HOUR) / MIN_IN_HOUR;
+        int s = (playingTimeInSeconds % SEC_IN_DAY % SEC_IN_HOUR) % MIN_IN_HOUR;
         return d + " " + h + ":" + m + ":" + s;
     }
 }
