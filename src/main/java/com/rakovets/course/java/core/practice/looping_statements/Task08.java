e18588fa581a0bb83091be29393770f2ad3d0675
Yegor Kharitonchik <egorkhary@mail.ru>
2022-06-09 17:06:08 +0300

    feat: complete Task08

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task08.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task08.java
@@ -1,5 +1,7 @@
 package com.rakovets.course.java.core.practice.looping_statements;
 
+import com.rakovets.course.java.core.util.NumberUtil;
+
 /**
  * Разработать программу для сети оптовых гипермаркетов.
  * Необходимо сформировать список цен для некоторого продукта.
@@ -24,11 +26,11 @@ class Task08 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int startNumberItems = 4;
-        double startPriceAllItems = 3.0;
-        int differentialNumberItems = 5;
-        double differentialSell = 4.0;
-        int sizeTotalPrice = 6;
+        int startNumberItems = 5;
+        double startPriceAllItems = 11.0;
+        int differentialNumberItems = 4;
+        double differentialSell = 1.0;
+        int sizeTotalPrice = 8;
 
         String totalPriceList = generateTotalPriceList(startNumberItems, startPriceAllItems, differentialNumberItems, differentialSell, sizeTotalPrice);
         System.out.printf("Result:\n%s", totalPriceList);
@@ -51,6 +53,22 @@ class Task08 {
     static String generateTotalPriceList(int startNumberItems, double startPriceAllItems, int differentialNumberItems, double differentialSell, int sizeTotalPrice) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String text = "";
+        double onePrice = startPriceAllItems / startNumberItems;
+        if (startNumberItems < 0 || startPriceAllItems < 0) {
+            throw new Error();
+        } else {
+            for (int i = 0; i < sizeTotalPrice; i++) {
+                double differentialPrice = onePrice * (startNumberItems + differentialNumberItems * i);
+                double percentPrice = differentialPrice * (differentialSell * i / 100);
+                differentialPrice -= percentPrice;
+                int newCount = startNumberItems + differentialNumberItems * i;
+                double newPercent = differentialSell * i;
+                double result = NumberUtil.roundValueToTwoDigitsForMantissa(differentialPrice);
+                text += newCount + " - " + result + " with sell " + newPercent + "%";
+                text += '\n';
+            }
+        }
+        return text;
     }
 }
