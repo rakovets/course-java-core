e2d0e36dc9fcd9e2b97832dc02324b36b71c8789
Yegor Kharitonchik <egorkhary@mail.ru>
2022-06-09 17:05:43 +0300

    feat: complete Task03

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task03.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task03.java
@@ -17,9 +17,9 @@ class Task03 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int healthPoints = 1000;
-        double regenerationPercentFromCurrentHealth = 100.0;
-        int hoursAfterRespawn = 10;
+        int healthPoints = 100;
+        double regenerationPercentFromCurrentHealth = -4.0;
+        int hoursAfterRespawn = 6;
 
         int featureHealthPoint = calculateHealthPointsByTime(healthPoints, regenerationPercentFromCurrentHealth, hoursAfterRespawn);
         System.out.printf("Result: %d", featureHealthPoint);
@@ -36,6 +36,15 @@ class Task03 {
     static int calculateHealthPointsByTime(int healthPoints, double regenerationPercentFromCurrentHealth, int hoursAfterRespawn) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        double newHealthPoint = healthPoints;
+        if (healthPoints < 0 || hoursAfterRespawn < 0) {
+            throw new Error();
+        } else {
+            for (int i = 0; i < hoursAfterRespawn; i++) {
+                double regenHealth = newHealthPoint * (regenerationPercentFromCurrentHealth / 100);
+                newHealthPoint += regenHealth;
+            }
+        }
+        return (int) newHealthPoint;
     }
 }
