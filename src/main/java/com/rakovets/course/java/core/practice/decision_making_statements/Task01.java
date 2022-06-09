1ab4eefa9614188c1ba7c5d98348049e9c397af5
Yegor Kharitonchik <egorkhary@mail.ru>
2022-06-09 17:17:00 +0300

    feat: complete Task01

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task01.java
@@ -38,6 +38,14 @@ class Task01 {
     static boolean isTopPlayer(int sizeTop, int positionPlayerByDamage) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return false;
+        if (positionPlayerByDamage <= 0) {
+            throw new Error();
+        } else {
+            if (positionPlayerByDamage <= sizeTop) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
