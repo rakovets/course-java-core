4c5abc905be432ecfc3a5ab3a33c36ae0fb4d736
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-06-09 21:38:14 +0200

    feat: add solution for Task 12

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task12.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task12.java
@@ -45,6 +45,35 @@ class Task12 {
     static String getZodiacSign(int day, int month) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String zodiacSign;
+
+        if (month == 3 && day >= 21 || month == 4 && day <= 20) {
+            zodiacSign = "Ram";
+        } else if (month == 4 || month == 5 && day <= 20) {
+            zodiacSign = "Bull";
+        } else if (month == 5 || month == 6 && day <= 21) {
+            zodiacSign = "Twins";
+        } else if (month == 6 || month == 7 && day <= 22) {
+            zodiacSign = "Crab";
+        } else if (month == 7 || month == 8 && day <= 22) {
+            zodiacSign = "Lion";
+        } else if (month == 8 || month == 9 && day <= 21) {
+            zodiacSign = "Maiden";
+        } else if (month == 9 || month == 10 && day <= 22) {
+            zodiacSign = "Scales";
+        } else if (month == 10 || month == 11 && day <= 22) {
+            zodiacSign = "Scorpion";
+        } else if (month == 11 || month == 12 && day <= 21) {
+            zodiacSign = "Archer";
+        } else if (month == 12 || month == 1 && day <= 20) {
+            zodiacSign = "Goat";
+        } else if (month == 1 || month == 2 && day <= 19) {
+            zodiacSign = "Water-bearer";
+        } else if (month == 2 || month == 3) {
+            zodiacSign = "Fish";
+        } else {
+            return null;
+        }
+        return zodiacSign;
     }
 }
