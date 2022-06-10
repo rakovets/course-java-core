7348ffdb1a8853c1d1dcc0223532bd89a9d07745
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-06-10 15:38:22 +0200

    feat: add final const BrocksIndex for task 5

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task05.java
@@ -36,6 +36,8 @@ class Task05 {
     static int calculateDeviationStandardWeight(int height, int weight) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return weight - (height - 110);
+        final int BrocksIndex = 110;
+        
+        return weight - (height - BrocksIndex);
     }
 }
