29d8aa407bb43f96b37f857310eeabe83f753cd8
gastuxa1ne <gastuxa1ne@gmail.com>
2022-06-10 11:02:52 +0300

    feat: add solution for Task 1

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task01.java
@@ -38,6 +38,10 @@ class Task01 {
     static boolean isTopPlayer(int sizeTop, int positionPlayerByDamage) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return false;
+        if (positionPlayerByDamage > 0 && positionPlayerByDamage <= sizeTop) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
