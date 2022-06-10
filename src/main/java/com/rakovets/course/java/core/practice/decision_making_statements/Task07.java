1b48d8f2d7bd6f3a1c83abbd4f5a40819a9a1ead
gastuxa1ne <gastuxa1ne@gmail.com>
2022-06-10 11:09:49 +0300

    feat: add solution for Task 7

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task07.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task07.java
@@ -36,8 +36,17 @@ class Task07 {
      * @return <code>false</code>/<code>true</code>
      */
     static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return false;
     }
 }
