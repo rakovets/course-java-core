3756371b4f1678017d937ac9e6040b041dc81566
gastuxa1ne <gastuxa1ne@gmail.com>
2022-06-10 10:55:10 +0300

    feat: add solution for Task 5

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task05.java
@@ -36,6 +36,7 @@ class Task05 {
     static int calculateDeviationStandardWeight(int height, int weight) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int currentHeight = 110;
+        return weight - (height - currentHeight);
     }
 }
