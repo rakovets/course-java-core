79d66f4f8871cd8fa4fb292861afcb81a8706126
Googgy <nl5kcrash@gmail.com>
2022-06-13 00:16:16 +0300

    feat: add solution for task 1

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task01.java
@@ -31,6 +31,17 @@ class Task01 {
     static String generateNumbersColumn(int numberRows) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String textNumberColumns = "";
+
+        for (int i = 1; i <= numberRows; i++) {
+
+            if (i == numberRows) {
+                textNumberColumns += i;
+                continue;
+            }
+
+            textNumberColumns = textNumberColumns + i + "\n";
+        }
+        return textNumberColumns;
     }
 }
