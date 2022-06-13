2cc5038dba563cbfccc981fddc963f21fde1ee13
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-06-13 15:20:30 +0200

    feat: renamed variable for Task 4

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task04.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task04.java
@@ -35,10 +35,10 @@ class Task04 {
     static float getDistance(float milliseconds) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        final int METERS_TO_KILOMETRS = 1000;
+        final int METERS_TO_KILOMETRES = 1000;
         final int SECONDS_TO_HOURS = 3600;
         final int MILES_TO_SECONDS = 1000;
 
-        return SOUND_SPEED * METERS_TO_KILOMETRS / SECONDS_TO_HOURS * milliseconds / MILES_TO_SECONDS;
+        return SOUND_SPEED * METERS_TO_KILOMETRES / SECONDS_TO_HOURS * milliseconds / MILES_TO_SECONDS;
     }
 }
