8bb0bbc050ca27756286633907fa25f017934510
Alina Dubrovina <dubrovinaa389@gmail.com>
2022-06-13 20:28:26 +0300

    feat: add tasks 7

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task07.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task07.java
@@ -1,5 +1,7 @@
 package com.rakovets.course.java.core.practice.looping_statements;
 
+import com.rakovets.course.java.core.util.NumberUtil;
+
 /**
  * Разработать программу для сети оптовых гипермаркетов.
  * Необходимо сформировать список цен для некоторого продукта.
@@ -47,6 +49,17 @@ class Task07 {
     static String generateTotalPriceList(int startNumberItems, double startPriceAllItems, int differentialNumberItems, int sizeTotalPrice) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String totalPriceList = "";
+        double pricePerItem = startPriceAllItems / startNumberItems;
+        for (int i = 0, currentNumberItem = startNumberItems;
+             i < sizeTotalPrice;
+             i++, currentNumberItem += differentialNumberItems){
+            totalPriceList += currentNumberItem + " - "
+                    + NumberUtil.roundValueToTwoDigitsForMantissa(currentNumberItem * pricePerItem) ;
+            if (i != sizeTotalPrice - 1){
+                totalPriceList += "\n";
+            }
+        }
+        return totalPriceList;
     }
 }
