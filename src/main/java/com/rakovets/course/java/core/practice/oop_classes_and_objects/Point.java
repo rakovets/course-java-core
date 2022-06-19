5229bbb0200a9888773d8d532e5dec75ceddcbe2
Yegor Kharitonchik <egorkhary@mail.ru>
2022-06-19 19:57:00 +0300

    feat: complete solution Task02

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Point.java
@@ -0,0 +1,33 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
+public class Point {
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getDistande(Point point) {
+        double distance;
+        distance = Math.pow(point.getX() - getX(), 2) + Math.pow(point.getY() - getY(), 2);
+        return Math.pow(distance, 0.5);
+    }
+
+    private double x;
+    private double y;
+}
