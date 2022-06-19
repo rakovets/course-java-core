79463f66e837a9404483611ec4a111b9e3325112
Yegor Kharitonchik <egorkhary@mail.ru>
2022-06-19 19:57:06 +0300

    feat: complete solution Task03

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Rectangle.java
@@ -0,0 +1,39 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
+public class Rectangle {
+    public Rectangle(Point topLeftPoint, Point bottomRightPoint) {
+        this.topLeftPoint = topLeftPoint;
+        this.bottomRightPoint = bottomRightPoint;
+    }
+
+    public Point getTopLeftPoint() {
+        return topLeftPoint;
+    }
+
+    public Point getBottomRightPoint() {
+        return bottomRightPoint;
+    }
+
+    public void setTopLeftPoint(Point topLeftPoint) {
+        this.topLeftPoint = topLeftPoint;
+    }
+
+    public void setBottomRightPoint(Point bottomRightPoint) {
+        this.bottomRightPoint = bottomRightPoint;
+    }
+
+    public double getPerimeter() {
+        double firstSide = Math.abs(bottomRightPoint.getX() - topLeftPoint.getX());
+        double secondSide = Math.abs(bottomRightPoint.getY() - topLeftPoint.getY());
+        return 2 * (firstSide + secondSide);
+    }
+
+    public double getArea() {
+        double firstSide = Math.abs(bottomRightPoint.getX() - topLeftPoint.getX());
+        double secondSide = Math.abs(bottomRightPoint.getY() - topLeftPoint.getY());
+        return firstSide * secondSide;
+    }
+
+    private Point topLeftPoint;
+    private Point bottomRightPoint;
+}
