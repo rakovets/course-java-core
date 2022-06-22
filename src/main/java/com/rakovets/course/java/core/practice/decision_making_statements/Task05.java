d0be7a2274a74d82c4effab9bc1ba0e32c6467eb
Svetlana Zhukova <zhukova678@gmail.com>
2022-06-22 22:14:49 +0300

    feat: solution task 5

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task05.java
@@ -33,6 +33,33 @@ class Task05 {
     static String getMonthName(byte monthNumber) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        switch (monthNumber) {
+            case (1):
+                return "JANUARY";
+            case (2):
+                return "FEBRUARY";
+            case (3):
+                return "MARCH";
+            case (4):
+                return "APRIL";
+            case (5):
+                return "MAY";
+            case (6):
+                return "JUNE";
+            case (7):
+                return "JULY";
+            case (8):
+                return "AUGUST";
+            case (9):
+                return "SEPTEMBER";
+            case (10):
+                return "OCTOBER";
+            case (11):
+                return "NOVEMBER";
+            case (12):
+                return "DECEMBER";
+            default:
+                return null;
+        }
     }
 }
