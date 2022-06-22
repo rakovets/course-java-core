f258ddc614822aa94adc1627fbb220f63b2cb7d4
Yegor Kharitonchik <egorkhary@mail.ru>
2022-06-23 00:48:36 +0300

    feat: fixed test04

--- src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Atm.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Atm.java
@@ -1,58 +1,172 @@
 package com.rakovets.course.java.core.practice.oop_classes_and_objects;
 
 public class Atm {
-    public Atm(int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) {
-        this.numberBanknotes20 = numberBanknotes20;
-        this.numberBanknotes50 = numberBanknotes50;
-        this.numberBanknotes100 = numberBanknotes100;
+    private int numberBanknotes20 = 0;
+    private int numberBanknotes50 = 0;
+    private int numberBanknotes100 = 0;
+    private int[] arrDigit20 = new int[100];
+    private int[] arrDigit50 = new int[100];
+    private int[] arrDigit100 = new int[100];
+
+    public Atm(int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) throws IllegalArgumentException {
+        if(numberBanknotes20 < 0 || numberBanknotes50 < 0 || numberBanknotes100 < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.numberBanknotes20 = numberBanknotes20;
+            this.numberBanknotes50 = numberBanknotes50;
+            this.numberBanknotes100 = numberBanknotes100;
+        }
     }
 
     public boolean isPossibleIssue(int amount) {
-        int newDigit = amount;
-        int abc = newDigit / 20;
-        for (int i = abc; i != -1; i--) {
-            newDigit -= 20 * i;
-            if (newDigit % 50 == 0 && numberBanknotes50 >= newDigit / 50 && numberBanknotes20 >= i) {
-                return true;
+        int newDigit100 = 0;
+        int countDigit100 = 0;
+        int countDigit50 = 0;
+        int countDigit20 = 0;
+        while (newDigit100 < amount) {
+            if (newDigit100 > amount) {
+                break;
             }
-            if (newDigit % 100 == 0 && newDigit != 0 && numberBanknotes20 >= i && numberBanknotes100 >= newDigit / 100) {
-                return true;
+            int newDigit50 = newDigit100;
+            while (newDigit50 < amount) {
+                newDigit50 += 50;
+                if (newDigit50 > amount) {
+                    break;
+                }
+                if (numberBanknotes50 < countDigit50) {
+                    break;
+                }
+                countDigit50++;
+                int newDigit20 = newDigit50;
+                while (newDigit20 % 20 != 0) {
+                    newDigit20 += 20;
+                    if (newDigit20 > amount) {
+                        break;
+                    }
+                    if (newDigit20 == amount) {
+                        countDigit20 = (newDigit20 - newDigit50) / 20;
+                        break;
+                    }
+                    if (numberBanknotes20 < countDigit20) {
+                        break;
+                    }
+                }
+                if (newDigit20 % 20 == 0) {
+                    countDigit20 = (amount - newDigit20) / 20;
+                }
+                if (numberBanknotes20 < countDigit20) {
+                    break;
+                }
+                int result = countDigit20 * 20 + countDigit50 * 50 + countDigit100 * 100;
+                if (result == amount) {
+                    return true;
+                }
+                countDigit20 = 0;
             }
-            newDigit = amount;
+            countDigit50 = 0;
+            int newDigit20 = newDigit100;
+            while (newDigit20 < amount) {
+                if ((amount - newDigit20) % 20 == 0) {
+                    countDigit20 = (amount - newDigit20) / 20;
+                }
+                if (numberBanknotes20 < countDigit20) {
+                    break;
+                }
+                int result = countDigit20 * 20 + countDigit50 * 50 + countDigit100 * 100;
+                if (result == amount) {
+                    return true;
+                }
+                newDigit20 += 20;
+                if (newDigit20 > amount) {
+                    break;
+                }
+            }
+            newDigit100 += 100;
+            countDigit100++;
+            if (numberBanknotes100 < countDigit100) {
+                break;
+            }
         }
         return false;
     }
 
     public void getOptionsCombinationBanknotes(int amount) {
-        int newDigit = amount;
-        int digit20 = 0;
-        int digit50 = 0;
-        int digit100 = 0;
+        int newDigit100 = 0;
+        int countDigit100 = 0;
+        int countDigit50 = 0;
+        int countDigit20 = 0;
         int count = 0;
-        int abc = newDigit / 20;
-        for (int i = abc; i != -1; i--) {
-            newDigit -= 20 * i;
-            if (newDigit % 50 == 0 && numberBanknotes50 >= newDigit / 50 && numberBanknotes20 >= i) {
-                digit20 = i;
-                digit50 = newDigit / 50;
-                arrDigit20[count] = digit20;
-                arrDigit50[count] = digit50;
-                arrDigit100[count] = digit100;
-                System.out.println(digit20 + " 20$ " + digit50 + " 50$ " + digit100 + " 100$ ");
-                count++;
-                digit50 = 0;
+        while (newDigit100 < amount) {
+            if (newDigit100 > amount) {
+                break;
             }
-            if (newDigit % 100 == 0 && newDigit != 0 && numberBanknotes20 >= i && numberBanknotes100 >= newDigit / 100) {
-                digit20 = i;
-                digit100 = newDigit / 100;
-                arrDigit20[count] = digit20;
-                arrDigit50[count] = digit50;
-                arrDigit100[count] = digit100;
-                System.out.println(digit20 + " 20$ " + digit50 + " 50$ " + digit100 + " 100$ ");
-                count++;
-                digit100 = 0;
+            int newDigit50 = newDigit100;
+            while (newDigit50 < amount) {
+                newDigit50 += 50;
+                if (newDigit50 > amount) {
+                    break;
+                }
+                if (numberBanknotes50 < countDigit50) {
+                    break;
+                }
+                countDigit50++;
+                int newDigit20 = newDigit50;
+                while (newDigit20 % 20 != 0) {
+                    newDigit20 += 20;
+                    if (newDigit20 > amount) {
+                        break;
+                    }
+                    if (newDigit20 == amount) {
+                        countDigit20 = (newDigit20 - newDigit50) / 20;
+                        break;
+                    }
+                    if (numberBanknotes20 < countDigit20) {
+                        break;
+                    }
+                }
+                if (newDigit20 % 20 == 0) {
+                    countDigit20 = (amount - newDigit20) / 20;
+                }
+                if (numberBanknotes20 < countDigit20) {
+                    break;
+                }
+                int result = countDigit20 * 20 + countDigit50 * 50 + countDigit100 * 100;
+                if (result == amount) {
+                    System.out.println(countDigit20 + " 20$ " + countDigit50 + " 50$ " + countDigit100 + " 100$ ");
+                    arrDigit20[count] = countDigit20;
+                    arrDigit50[count] = countDigit50;
+                    arrDigit100[count] = countDigit100;
+                    count++;
+                }
+                countDigit20 = 0;
             }
-            newDigit = amount;
+            countDigit50 = 0;
+            int newDigit20 = newDigit100;
+            while (newDigit20 < amount) {
+                if ((amount - newDigit20) % 20 == 0) {
+                    countDigit20 = (amount - newDigit20) / 20;
+                }
+                if (numberBanknotes20 < countDigit20) {
+                    break;
+                }
+                int result = countDigit20 * 20 + countDigit50 * 50 + countDigit100 * 100;
+                if (result == amount) {
+                    System.out.println(countDigit20 + " 20$ " + countDigit50 + " 50$ " + countDigit100 + " 100$ ");
+                    arrDigit20[count] = countDigit20;
+                    arrDigit50[count] = countDigit50;
+                    arrDigit100[count] = countDigit100;
+                    count++;
+                }
+                newDigit20 += 20;
+                if (newDigit20 > amount) {
+                    break;
+                }
+            }
+            newDigit100 += 100;
+            countDigit100++;
+            if (numberBanknotes100 < countDigit100) {
+                break;
+            }
         }
     }
 
@@ -78,11 +192,4 @@ public class Atm {
     public void addBanknotes100(int number) {
         this.numberBanknotes100 += number;
     }
-
-    private int numberBanknotes20 = 0;
-    private int numberBanknotes50 = 0;
-    private int numberBanknotes100 = 0;
-    private int[] arrDigit20 = new int[100];
-    private int[] arrDigit50 = new int[100];
-    private int[] arrDigit100 = new int[100];
 }
--- src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/AtmApplication.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/AtmApplication.java
@@ -1,18 +1,20 @@
 package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
 import java.util.Scanner;
+
 public class AtmApplication {
     public static void main(String[] args) {
+        int amount = 170;
         Scanner scanner = new Scanner(System.in);
-        Atm firstAtm = new Atm(6, 1,1);
-        if (firstAtm.isPossibleIssue(120)) {
+        Atm firstAtm = new Atm(1, 1, 1);
+        if (firstAtm.isPossibleIssue(amount)) {
             System.out.println("Yes, we can give you money.");
-            firstAtm.getOptionsCombinationBanknotes(120);
-        }
-        else {
+            firstAtm.getOptionsCombinationBanknotes(amount);
+            System.out.println("Choose one of the option:");
+            int input = scanner.nextInt();
+            firstAtm.getCash(input);
+        } else {
             System.out.println("Sorry, but we cant give you such an amount");
         }
-        System.out.println("Choose one of the option:");
-        int input = scanner.nextInt();
-        firstAtm.getCash(input);
     }
 }
