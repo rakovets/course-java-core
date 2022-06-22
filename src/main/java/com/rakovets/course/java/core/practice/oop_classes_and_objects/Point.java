27af6293c682aaedc770f5add1c09315ad301d1c
Yegor Kharitonchik <egorkhary@mail.ru>
2022-06-23 00:48:15 +0300

    feat: fixed test02

--- src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Point.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Point.java
@@ -1,9 +1,18 @@
 package com.rakovets.course.java.core.practice.oop_classes_and_objects;
 
 public class Point {
+    private double x;
+    private double y;
+
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double getDistande(Point point) {
+        double distance;
+        distance = Math.pow(point.getX() - getX(), 2) + Math.pow(point.getY() - getY(), 2);
+        return Math.pow(distance, 0.5);
     }
 
     public double getX() {
@@ -21,13 +30,4 @@ public class Point {
     public void setY(double y) {
         this.y = y;
     }
-
-    public double getDistande(Point point) {
-        double distance;
-        distance = Math.pow(point.getX() - getX(), 2) + Math.pow(point.getY() - getY(), 2);
-        return Math.pow(distance, 0.5);
-    }
-
-    private double x;
-    private double y;
 }
--- src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/PointApplication.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/PointApplication.java
@@ -3,7 +3,7 @@ package com.rakovets.course.java.core.practice.oop_classes_and_objects;
 public class PointApplication {
     public static void main(String[] args) {
         Point a = new Point(5, 10);
-        Point b = new Point ( 4, 5 );
+        Point b = new Point(4, 5);
         System.out.println(a.getX());
         System.out.println(a.getY());
         System.out.println(a.getDistande(b));
