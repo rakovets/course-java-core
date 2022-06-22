54255f5f6463ee0f5c8d05a01851fb0894b20580
Borodkin Nikita <Shneppi111@gmail.com>
2022-06-22 18:28:47 +0300

    Complete tasks 1-3 OOP

--- src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/TimeApplication.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/TimeApplication.java
@@ -2,7 +2,7 @@ package com.rakovets.course.java.core.practice.oop_classes_and_objects;
 
 public class TimeApplication {
     public static void main(String[] args) {
-        Time time = new Time(3,32,43);
+        Time time = new Time(3,32,42);
         System.out.print("This time is: " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds());
 
     }
