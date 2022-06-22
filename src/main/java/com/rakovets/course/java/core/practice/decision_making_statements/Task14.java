fa38f049a712d424ea885fd49df4a409d8ee8706
Svetlana Zhukova <zhukova678@gmail.com>
2022-06-22 22:41:59 +0300

    feat: solution task 14

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task14.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task14.java
@@ -44,6 +44,45 @@ class Task14 {
     static String getZodiacYearName(int year) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String zodiacYearName = null;
+        switch (year % 12) {
+            case 0:
+                zodiacYearName = "Monkey";
+                break;
+            case 1:
+                zodiacYearName = "Rooster";
+                break;
+            case 2:
+                zodiacYearName = "Dog";
+                break;
+            case 3:
+                zodiacYearName = "Pig";
+                break;
+            case 4:
+                zodiacYearName = "Rat";
+                break;
+            case 5:
+                zodiacYearName = "Ox";
+                break;
+            case 6:
+                zodiacYearName = "Tiger";
+                break;
+            case 7:
+                zodiacYearName = "Rabbit";
+                break;
+            case 8:
+                zodiacYearName = "Dragon";
+                break;
+            case 9:
+                zodiacYearName = "Snake";
+                break;
+            case 10:
+                zodiacYearName = "Horse";
+                break;
+            case 11:
+                zodiacYearName = "Ram";
+                break;
+        }
+        return zodiacYearName;
     }
 }
