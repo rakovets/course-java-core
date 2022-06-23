ea30ea6b59c79bbce1cfeb9cc86776264639feff
Svetlana Zhukova <zhukova678@gmail.com>
2022-06-23 13:16:06 +0300

    feat: fixed task 6

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task06.java
@@ -41,16 +41,12 @@ class Task06 {
     static String getResult(int tanksKilledFirstPlayer, int tanksKilledSecondPlayer) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
+        int bonusPoint = BONUS_POINT / 100;
         if (tanksKilledFirstPlayer > tanksKilledSecondPlayer) {
-            tanksKilledFirstPlayer = tanksKilledFirstPlayer * KILL_POINT + BONUS_POINT;
-            tanksKilledSecondPlayer = tanksKilledSecondPlayer * KILL_POINT;
+            tanksKilledFirstPlayer += bonusPoint;
         } else if (tanksKilledFirstPlayer < tanksKilledSecondPlayer) {
-            tanksKilledFirstPlayer = tanksKilledFirstPlayer * KILL_POINT;
-            tanksKilledSecondPlayer = tanksKilledSecondPlayer * KILL_POINT + BONUS_POINT;
-        } else {
-            tanksKilledFirstPlayer = tanksKilledFirstPlayer * KILL_POINT;
-            tanksKilledSecondPlayer = tanksKilledSecondPlayer * KILL_POINT;
+            tanksKilledSecondPlayer += bonusPoint;
         }
-        return "Player 1: " + tanksKilledFirstPlayer + ". Player 2: " + tanksKilledSecondPlayer;
+        return "Player 1: " + KILL_POINT * tanksKilledFirstPlayer + ". Player 2: " + KILL_POINT * tanksKilledSecondPlayer;
     }
 }
