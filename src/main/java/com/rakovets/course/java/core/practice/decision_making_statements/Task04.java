3931fac5b11324e28c2d17f63885d4ba3645b567
Svetlana Zhukova <zhukova678@gmail.com>
2022-06-23 13:05:27 +0300

    feat: fixed task 4

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task04.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task04.java
@@ -32,19 +32,19 @@ class Task04 {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
         switch (dayOfWeekNumber) {
-            case (1):
+            case 1 :
                 return "MONDAY";
-            case (2):
+            case 2 :
                 return "TUESDAY";
-            case (3):
+            case 3 :
                 return "WEDNESDAY";
-            case (4):
+            case 4 :
                 return "THURSDAY";
-            case (5):
+            case 5 :
                 return "FRIDAY";
-            case (6):
+            case 6 :
                 return "SATURDAY";
-            case (7):
+            case 7 :
                 return "SUNDAY";
             default:
                 return null;
---
+++ src/main/java/com/rakovets/course/java/core/practice/strings/StringUtil.java
@@ -0,0 +1,9 @@
+package com.rakovets.course.java.core.practice.strings;
+
+public class StringUtil {
+    private String stringUtil;
+
+    public String concatenate(String string1, String string2) {
+        return string1 + string2;
+    }
+}
