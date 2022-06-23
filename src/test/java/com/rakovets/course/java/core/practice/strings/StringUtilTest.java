1476598df6ab303527b0412f6d2ee567ee78cc06
AlenaTrafimovich <trofimovich_elena@mail.ru>
2022-06-23 18:59:10 +0300

    tests for tasks 3-5

--- src/test/java/com/rakovets/course/java/core/practice/strings/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/strings/StringUtilTest.java
@@ -18,11 +18,11 @@ public class StringUtilTest {
 
     @Test
     void testConcat(String input1, String input2, String expected) {
-        String actual = StringUtil.concat(input1, input2);
+        String actual = stringUtil.concat(input1, input2);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
+
     void testIndexOf(String input1, char x, int expected) {
         int actual = stringUtil.indexOf(input1, x);
         Assertions.assertEquals(expected, actual);
@@ -36,6 +36,51 @@ public class StringUtilTest {
     @Test
     void testIndexOf2() {
         testIndexOf("", 'e', -1);
+    }
+
+    void testEquals(String input1, String input2, boolean expected) {
+        boolean actual = stringUtil.equals(input1, input2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testEquals1() {
+        testEquals("Hello", "Hello", true);
+    }
+
+    @Test
+    void testEquals2() {
+        testEquals("Java", "java", false);
+    }
+
+    void testTrimSpaceLowerCase(String input1, String expected) {
+        String actual = stringUtil.trimSpaceLowerCase(input1);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testTrimSpaceLowerCase1() {
+        testTrimSpaceLowerCase("   Hello    ", "hello");
+    }
+
+    @Test
+    void testTrimSpaceLowerCase2() {
+        testTrimSpaceLowerCase("    JA VA   ", "ja va");
+    }
+
+    void testSubstringSymbol(String input1, char n, char m, String expected) {
+        String actual = stringUtil.substringSymbol(input1, n, m);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubstringSymbol1() {
+        testSubstringSymbol("Hello Java", 'J', 'a', "Ja");
+    }
+
+    @Test
+    void testSubstringSymbol2() {
+        testSubstringSymbol("gGit", 'G', 't', "Git");
     }
 
 }
