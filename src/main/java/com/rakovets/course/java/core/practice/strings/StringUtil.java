433f077eb5066261d0e72c1d327d3684f4874bd6
Voropai Vadim <vab18071981@gmail.com>
2022-06-23 09:08:54 +0300

    feat: add task String

---
+++ src/main/java/com/rakovets/course/java/core/practice/strings/StringUtil.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.strings;
+
+public class StringUtil {
+    public StringUtil(String str1, String str2) {
+
+    }
+
+}
---
+++ src/test/java/com/rakovets/course/java/core/practice/strings/StringUtilTest.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.strings;
+
+public class StringUtilTest {
+    StringUtil st = new StringUtil("w","dd");
+
+
+}
