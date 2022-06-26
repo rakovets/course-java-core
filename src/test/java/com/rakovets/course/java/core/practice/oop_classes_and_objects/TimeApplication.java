312a7b6702a476679b09900929558ac47a7a70dc
Googgy <nl5kcrash@gmail.com>
2022-06-26 21:42:20 +0300

    feat: add test TimeApplication

--- src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/TimeApplication.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/TimeApplication.java
@@ -3,9 +3,37 @@ package com.rakovets.course.java.core.practice.oop_classes_and_objects;
 public class TimeApplication {
     public static void main(String[] args) {
         Time time = new Time(10, 33, 34);
-        Time totalSeconds = new Time (566);
-        Point point = new Point(55, 3);
+        Time timeSeconds = new Time(5656444);
 
+        System.out.println("Hours: " + time.getHours());
+        System.out.println("Minutes: " + time.getMinutes());
+        System.out.println("Seconds: " + time.getSeconds());
+        System.out.println ("Total seconds: " + time.getTotalSeconds());
+        System.out.println();
 
+        time.setHours(8);
+        time.setMinutes(59);
+        time.setSeconds(37);
+
+        System.out.println("Set hours: " + time.getHours());
+        System.out.println("Set minutes: " + time.getMinutes());
+        System.out.println("Set seconds: " + time.getSeconds());
+        System.out.println("Total seconds: " + time.getTotalSeconds());
+        System.out.println();
+
+        System.out.println("Set hours: " + timeSeconds.getHours());
+        System.out.println("Set minutes: " + timeSeconds.getMinutes());
+        System.out.println("Set seconds: " + timeSeconds.getSeconds());
+        System.out.println("Total seconds: " + timeSeconds.getTotalSeconds());
+        System.out.println();
+
+        timeSeconds.setHours(55);
+        timeSeconds.setMinutes(26);
+        timeSeconds.setSeconds(45);
+
+        System.out.println("Set hours: " + timeSeconds.getHours());
+        System.out.println("Set minutes: " + timeSeconds.getMinutes());
+        System.out.println("Set seconds: " + timeSeconds.getSeconds());
+        System.out.println("Total seconds: " + timeSeconds.getTotalSeconds());
     }
 }
