8e764412bc291f4ac804f49ef13115495a18b717
Googgy <nl5kcrash@gmail.com>
2022-06-26 22:20:53 +0300

    feat: add test PointApplication

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/PointApplication.java
@@ -0,0 +1,21 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
+public class PointApplication {
+    public static void main(String[] args) {
+        Point pointFirst = new Point(5, 10);
+        Point pointSecond = new Point(2, -15);
+
+        System.out.println("First point: X = " + pointFirst.getX() + "; Y = " + pointFirst.getY());
+        System.out.println("Second point: X = " + pointSecond.getX() + "; Y = " + pointSecond.getY());
+        System.out.println("Distance between points: " + pointFirst.getDistance(pointSecond));
+
+        pointFirst.setX(33);
+        pointFirst.setY(-2);
+        pointSecond.setX(1);
+        pointSecond.setY(-23);
+
+        System.out.println("First point: X = " + pointFirst.getX() + "; Y = " + pointFirst.getY());
+        System.out.println("Second point: X = " + pointSecond.getX() + "; Y = " + pointSecond.getY());
+        System.out.println("Distance between points: " + pointFirst.getDistance(pointSecond));
+    }
+}
