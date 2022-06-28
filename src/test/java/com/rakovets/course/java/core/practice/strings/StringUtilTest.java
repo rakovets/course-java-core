f084cb8c8d917d243aafc95509a51b6c697e4366
AlenaTrafimovich <trofimovich_elena@mail.ru>
2022-06-28 10:54:11 +0300

    feat: fixed tasks

--- src/test/java/com/rakovets/course/java/core/practice/strings/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/strings/StringUtilTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 public class StringUtilTest {
     private final StringUtil stringUtil = new StringUtil();
-    String str;
 
     @Test
     void testConcatWithTwoValues() {
