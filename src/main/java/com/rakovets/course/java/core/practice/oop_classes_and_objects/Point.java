913b822b8fdb071e3609daf0680b7498afa0c347
Googgy <nl5kcrash@gmail.com>
2022-07-01 12:01:03 +0300

    feat: changing the int data type Point to double

--- src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Point.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Point.java
@@ -1,33 +1,31 @@
 package com.rakovets.course.java.core.practice.oop_classes_and_objects;
 
 public class Point {
-    private int x;
-    private int y;
+    private double x;
+    private double y;
 
-    public Point(int x, int y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
     public double getDistance(Point secondPoint) {
-        double distanceX = Math.pow(this.x - secondPoint.getX(), 2);
-        double distanceY = Math.pow(this.y - secondPoint.getY(), 2);
-        return Math.sqrt(distanceX + distanceY);
+        return Math.sqrt(Math.pow(this.x - secondPoint.getX(), 2) + Math.pow(this.y - secondPoint.getY(), 2));
     }
 
-    public int getX() {
+    public double getX() {
         return this.x;
     }
 
-    public void setX(int x) {
+    public void setX(double x) {
         this.x = x;
     }
 
-    public int getY() {
+    public double getY() {
         return this.y;
     }
 
-    public void setY(int y) {
+    public void setY(double y) {
         this.y = y;
     }
 }
