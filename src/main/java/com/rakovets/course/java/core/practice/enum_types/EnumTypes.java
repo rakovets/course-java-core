18954b4d18d951a270dfbd79dc3a387d5068d508
Yegor Kharitonchik <egorkhary@mail.ru>
2022-07-01 22:41:36 +0300

    feat: make solution for EnumTypes

---
+++ src/main/java/com/rakovets/course/java/core/practice/enum_types/EnumTypes.java
@@ -0,0 +1,56 @@
+package com.rakovets.course.java.core.practice.enum_types;
+
+public class EnumTypes {
+    public enum Seasons {
+        AUTUMN(10),
+        WINTER(-20),
+        SPRING(15),
+        SUMMER(25) {
+            @Override
+            public String getDescription() {
+                return "Warm season";
+            }
+        };
+
+        private String myFavouriteSeason;
+        private final int averageTemperature;
+
+        Seasons(int averageTemperature) {
+            this.averageTemperature = averageTemperature;
+        }
+
+        public String getMyFavouriteSeason() {
+            return Seasons.SUMMER + " with temperature " + Seasons.SUMMER.averageTemperature + "C";
+        }
+
+        public String findingFavouriteSeason(Seasons seasons) {
+            String text = "";
+            switch (seasons) {
+                case AUTUMN:
+                    text = "I like " + Seasons.AUTUMN;
+                case WINTER:
+                    text = "I like " + Seasons.WINTER;
+                case SPRING:
+                    text = "I like " + Seasons.SPRING;
+                case SUMMER:
+                    text = "I like " + Seasons.SUMMER;
+                    break;
+            }
+            return text;
+        }
+
+        public void getInfoAboutSeasons() {
+            for (Seasons seasons: Seasons.values()) {
+                System.out.println(seasons + " with temperature " + seasons.averageTemperature + "C " + seasons.getDescription());
+            }
+        }
+
+        public String getDescription() {
+            return "Cold season";
+        }
+
+        public int getAverageTemperature() {
+            return averageTemperature;
+        }
+    }
+}
