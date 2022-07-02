2f295617ddfebd2e5a80c006b11b35908b1fa363
Yegor Kharitonchik <egorkhary@mail.ru>
2022-07-02 13:25:02 +0300

    feat: fixed Task11

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task11.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task11.java
@@ -40,16 +40,15 @@ class Task11 {
     static String getSymbolType(char symbol) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        String text;
-        int num = (int) symbol;
-        if (num >= 32 && num <= 47 || num >= 58 && num <= 64 || num >= 91 && num <= 96 || num >= 123 && num <= 127) {
-            text = "undefined";
-        } else if (num >= 48 && num <= 57) {
+        String text = "";
+        if (symbol >= '0' && symbol <= '9') {
             text = "digit";
-        } else if (num >= 65 && num <= 90 || num >= 97 && num <= 122) {
+        } else if (symbol >= 'A' && symbol <= 'Z' || symbol >= 'a' && symbol <= 'z') {
             text = "latin";
-        } else {
+        } else if (symbol >= 'А' && symbol <= 'Я' || symbol >= 'а' && symbol <= 'я') {
             text = "cyrillic";
+        } else {
+            text = "undefined";
         }
         return text;
     }
