2dd33cfbf77edb929ffcb92bac43a7c7fa8e39e2
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-04 03:41:42 +0200

    feat: add class Archer

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/hero/Archer.java
@@ -0,0 +1,40 @@
+package com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.hero;
+
+import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.enemy.Enemy;
+
+public class Archer extends Hero {
+
+    private int DAMAGES_ARCHER = 20;
+    final int BOOSTER_SHOT = 40;
+
+    public Archer(String name, int health) {
+        super(name, health);
+    }
+
+    /**
+     * Deals archer damage to an enemy.
+     *
+     * @param enemy type enemy.
+     */
+    @Override
+    public void attackEnemy(Enemy enemy) {
+        enemy.takingDamage(DAMAGES_ARCHER);
+    }
+
+    /**
+     * Empowered Shot deals double Archer damage to an enemy.
+     *
+     * @param enemy type enemy.
+     */
+    protected void boostedShot(Enemy enemy) {
+        enemy.takingDamage(BOOSTER_SHOT);
+    }
+
+    public void setDamageArcher(int damageArcher) {
+        this.DAMAGES_ARCHER = damageArcher;
+    }
+
+    public int getDAMAGES_ARCHER() {
+        return DAMAGES_ARCHER;
+    }
+}
