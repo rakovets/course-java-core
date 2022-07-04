e70f45eac3abe67c744d757a2b504f9a70bc4253
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-04 03:47:23 +0200

    feat: deleted documentation interface Mortal

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/Mortal.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/Mortal.java
@@ -1,9 +1,6 @@
 package com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground;
 
 public interface Mortal {
-    /**
-     * Interface requiring an implementation.
-     */
 
     boolean isAlive();
 }
