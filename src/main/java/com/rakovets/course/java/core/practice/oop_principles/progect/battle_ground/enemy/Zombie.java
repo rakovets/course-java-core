e8a521be2aa2f6aadd6ecd9c79301dc570972bad
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-04 03:43:01 +0200

    feat: add class Zombie

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/enemy/Zombie.java
@@ -0,0 +1,60 @@
+package com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.enemy;
+
+import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.hero.Hero;
+
+import java.util.Random;
+
+public class Zombie extends Enemy {
+    final int DAMAGE_ZOMBIE = 12;
+
+    Random random = new Random();
+
+    /**
+     * Constructor.
+     *
+     * @param health indicator of health.
+     */
+    public Zombie(int health) {
+        super(health);
+    }
+
+    /**
+     * Deals damage to the hero.
+     *
+     * @param hero type hero.
+     */
+    @Override
+    protected void attackEnemy(Hero hero) {
+        hero.takingDamage(DAMAGE_ZOMBIE);
+    }
+
+    /**
+     * Subtracts damage taken from current health.
+     * <p>
+     * Passive Ability: Can randomly resurrect after death.
+     *
+     * @param damage damage taken.
+     */
+    public void takingDamage(int damage) {
+        if (isAlive()) {
+            if (getHealth() - damage < 0) {
+                setHealth(0);
+            } else if (getHealth() - damage == 0) {
+                boolean live = random.nextBoolean();
+
+                if (live) {
+                    setHealth(super.getHealth());
+                } else {
+                    setHealth(getHealth() - damage);
+                }
+            } else {
+                setHealth(getHealth() - damage);
+
+            }
+        }
+    }
+
+    public int getDAMAGE_ZOMBIE() {
+        return DAMAGE_ZOMBIE;
+    }
+}
