09bb7b2ebb604aba428dd82e0d1f08b000206fcf
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-04 03:46:14 +0200

    feat: add interface Mortal

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/Mortal.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/Mortal.java
@@ -1,5 +1,9 @@
 package com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground;
 
 public interface Mortal {
+    /**
+     * Interface requiring an implementation.
+     */
+
     boolean isAlive();
 }
