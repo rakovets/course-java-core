2daa8b4a0a70a9a8514e7454cce390ae4b259cd4
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-04 15:51:03 +0200

    feat: add new test for ZombieResurrectionTest

--- src/test/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/enemy/ZombieResurrectionTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/enemy/ZombieResurrectionTest.java
@@ -1,6 +1,8 @@
 package com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.enemy;
 
 import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.hero.Archer;
+import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.hero.Mag;
+import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.hero.Warrior;
 
 public class ZombieResurrectionTest {
     public static void main(String[] args) {
@@ -18,5 +20,58 @@ public class ZombieResurrectionTest {
         System.out.println(zombie.getHealth());
         archer.attackEnemy(zombie);
         System.out.println(zombie.getHealth());
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        zombie.setHealth(100);
+        archer.setBoostShot(100);
+
+        System.out.println(zombie.getHealth());
+        archer.boostedShot(zombie);
+        System.out.println(zombie.getHealth());
+        archer.boostedShot(zombie);
+        System.out.println(zombie.getHealth());
+        archer.boostedShot(zombie);
+        System.out.println(zombie.getHealth());
+        archer.boostedShot(zombie);
+        System.out.println(zombie.getHealth());
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        Mag mag = new Mag("", 0);
+
+        mag.setDamageMag(100);
+        zombie.setHealth(100);
+
+        System.out.println(zombie.getHealth());
+        mag.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        mag.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        mag.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        mag.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        mag.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        mag.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        Warrior warrior = new Warrior("", 0);
+
+        warrior.setDamageWarrior(100);
+        zombie.setHealth(100);
+
+        System.out.println(zombie.getHealth());
+        warrior.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        warrior.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        warrior.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        warrior.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        warrior.attackEnemy(zombie);
+        System.out.println(zombie.getHealth());
+        System.out.println("-----------------------------------------------------------------------------------------");
     }
 }
