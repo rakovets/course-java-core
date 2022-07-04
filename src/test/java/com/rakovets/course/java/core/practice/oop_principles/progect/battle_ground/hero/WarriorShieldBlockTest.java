565ab38309b7bf9258e75c7ba209c66af67cbcce
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-04 15:51:49 +0200

    feat: add test for WarriorShieldBlockTest

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/hero/WarriorShieldBlockTest.java
@@ -0,0 +1,91 @@
+package com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.hero;
+
+import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.enemy.Vampire;
+import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.enemy.Werewolf;
+import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.enemy.Zombie;
+
+public class WarriorShieldBlockTest {
+    public static void main(String[] args) {
+        Warrior warrior = new Warrior("", 100);
+        Vampire vampire = new Vampire(100);
+
+        System.out.println(warrior.getHealth());
+        vampire.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        vampire.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        vampire.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        vampire.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        vampire.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        Werewolf werewolf = new Werewolf(100, 7);
+
+        warrior.setHealth(100);
+
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        werewolf.setTime(3);
+        warrior.setHealth(100);
+
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        werewolf.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        System.out.println("-----------------------------------------------------------------------------------------");
+
+        Zombie zombie = new Zombie(100);
+
+        warrior.setHealth(100);
+
+        System.out.println(warrior.getHealth());
+        zombie.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        zombie.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        zombie.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        zombie.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        zombie.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        zombie.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        zombie.attackHero(warrior);
+        System.out.println(warrior.getHealth());
+        System.out.println("-----------------------------------------------------------------------------------------");
+    }
+}
