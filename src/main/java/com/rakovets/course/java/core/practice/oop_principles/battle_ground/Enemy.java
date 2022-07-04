8c5df91d0774f57585252b7b67f3484d64df39dd
Svetlana Zhukova <zhukova678@gmail.com>
2022-07-04 16:49:35 +0300

    feat: solution battle_ground task 5

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Enemy.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Enemy.java
@@ -1,6 +1,6 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
 
-public class Enemy {
+public class Enemy implements Mortal {
     private int health;
 
     Enemy (int health) {
@@ -9,6 +9,12 @@ public class Enemy {
     public int takeDamage(int damage) {
         return health - damage;
     }
+
+    @Override
+    public boolean isAlive(int health) {
+      return health > 0 ;
+    }
+
     public void setHealth(){
         this.health = health;
     }
