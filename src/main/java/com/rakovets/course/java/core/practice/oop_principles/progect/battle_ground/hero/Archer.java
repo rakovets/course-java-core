5db208b5398fe0691c4e05a1cdfb2b1aec996863
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-04 13:33:08 +0200

    feat: deleted empty lines and comments for class Archer

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/hero/Archer.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/hero/Archer.java
@@ -3,7 +3,6 @@ package com.rakovets.course.java.core.practice.oop_principles.progect.battle_gro
 import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.enemy.Enemy;
 
 public class Archer extends Hero {
-
     private int DAMAGES_ARCHER = 20;
     final int BOOSTER_SHOT = 40;
 
