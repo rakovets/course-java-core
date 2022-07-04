d66f25ad0b998993e26ddfaeeda059fcac2e8a01
Voropai Vadim <vab18071981@gmail.com>
2022-07-04 19:06:09 +0300

    feat: add solutionfor theme  Task

--- src/test/java/com/rakovets/course/java/core/practice/oop_principles/cats_home/CatsHomeApplication.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_principles/cats_home/CatsHomeApplication.java
@@ -1,5 +1,4 @@
 package com.rakovets.course.java.core.practice.oop_principles.cats_home;
-
 public class CatsHomeApplication {
     public static void main(String[] args) {
         Cat cat = new Cat("Murka");
