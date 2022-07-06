384b28d2e75c597a6838b877c93d6ed27eb18128
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-06 16:20:48 +0200

    feat: text output fixed for ZombieResurrectionDemo

--- src/test/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/enemy/ZombieResurrectionDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/enemy/ZombieResurrectionDemo.java
@@ -6,72 +6,77 @@ import com.rakovets.course.java.core.practice.oop_principles.progect.battle_grou
 
 public class ZombieResurrectionDemo {
     public static void main(String[] args) {
-        Zombie zombie = new Zombie(100);
-        Archer archer = new Archer("", 100);
+        Zombie zombie = new Zombie(100, 0);
+        Archer archer = new Archer("", 100, 0);
 
-        archer.setDamageArcher(100);
+        archer.setDamageHero(100);
 
-        System.out.println(zombie.getHealth());
+        System.out.print("-------------------------------------------------------------------------------------------");
+        System.out.println("\nThe archer attacks the zombies.");
+        System.out.println(zombie.getHealthEnemy());
         archer.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         archer.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         archer.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         archer.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
-        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println(zombie.getHealthEnemy());
+        System.out.print("-------------------------------------------------------------------------------------------");
 
-        zombie.setHealth(100);
-        archer.setBoostShot(100);
+        zombie.setHealthEnemy(100);
+        archer.setDamageHero(50);
 
-        System.out.println(zombie.getHealth());
-        archer.boostedShot(zombie);
-        System.out.println(zombie.getHealth());
-        archer.boostedShot(zombie);
-        System.out.println(zombie.getHealth());
-        archer.boostedShot(zombie);
-        System.out.println(zombie.getHealth());
-        archer.boostedShot(zombie);
-        System.out.println(zombie.getHealth());
-        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println("\nThe archer attacks the zombies using an enhanced shot.");
+        System.out.println(zombie.getHealthEnemy());
+        archer.boostedShot();
+        System.out.println(zombie.getHealthEnemy());
+        archer.boostedShot();
+        System.out.println(zombie.getHealthEnemy());
+        archer.boostedShot();
+        System.out.println(zombie.getHealthEnemy());
+        archer.boostedShot();
+        System.out.println(zombie.getHealthEnemy());
+        System.out.print("-------------------------------------------------------------------------------------------");
 
-        Mag mag = new Mag("", 0);
+        Mag mag = new Mag("", 0, 0);
 
-        mag.setDamageMag(100);
-        zombie.setHealth(100);
+        mag.setDamageHero(100);
+        zombie.setHealthEnemy(100);
 
-        System.out.println(zombie.getHealth());
+        System.out.println("\nThe magician attacks the zombies.");
+        System.out.println(zombie.getHealthEnemy());
         mag.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         mag.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         mag.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         mag.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         mag.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         mag.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
-        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println(zombie.getHealthEnemy());
+        System.out.print("-------------------------------------------------------------------------------------------");
 
-        Warrior warrior = new Warrior("", 0);
+        Warrior warrior = new Warrior("", 0, 0);
 
-        warrior.setDamageWarrior(100);
-        zombie.setHealth(100);
+        warrior.setDamageHero(100);
+        zombie.setHealthEnemy(100);
 
-        System.out.println(zombie.getHealth());
+        System.out.println("\nWarrior attacks zombies.");
+        System.out.println(zombie.getHealthEnemy());
         warrior.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         warrior.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         warrior.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         warrior.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
+        System.out.println(zombie.getHealthEnemy());
         warrior.attackEnemy(zombie);
-        System.out.println(zombie.getHealth());
-        System.out.println("-----------------------------------------------------------------------------------------");
+        System.out.println(zombie.getHealthEnemy());
+        System.out.print("-------------------------------------------------------------------------------------------");
     }
 }
