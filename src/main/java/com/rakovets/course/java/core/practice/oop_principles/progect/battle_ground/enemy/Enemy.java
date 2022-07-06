aea2c0cc60dd4da7663b7fd0334aec03f79c4514
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-06 16:11:52 +0200

    feat: many bugs fixed and documentation corrected for Enemy.

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/enemy/Enemy.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/enemy/Enemy.java
@@ -4,43 +4,79 @@ import com.rakovets.course.java.core.practice.oop_principles.progect.battle_grou
 import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.Mortal;
 
 public abstract class Enemy implements Mortal {
-    final int MIN_HP = 0;
-    private int healthEnemy;
+    final int MINIMAL_HP = 0;
+    final int MINIMAL_DAMAGE = 0;
 
-    protected Enemy(int health) {
-        this.healthEnemy = health;
+    private int healthEnemy;
+    private int damageEnemy;
+
+    /**
+     * Constructor.
+     *
+     * @param healthEnemy maximum enemy health.
+     * @param damageEnemy maximum enemy damage.
+     */
+    protected Enemy(int healthEnemy, int damageEnemy) {
+        this.healthEnemy = healthEnemy;
+        this.damageEnemy = damageEnemy;
+
     }
 
+    /**
+     * The enemy attacks the hero.
+     *
+     * @param hero attacked hero.
+     */
     protected abstract void attackHero(Hero hero);
 
     /**
-     * Subtracts damage taken from current health.
+     * The enemy takes damage.
      *
-     * @param damage damage taken.
+     * @param damage the damage it takes.
      */
-    public void takingDamage(int damage) {
-        if (this.healthEnemy - damage <= MIN_HP) {
-            this.healthEnemy = MIN_HP;
+    public void takeDamage(int damage) {
+        if (this.healthEnemy - damage <= MINIMAL_HP) {
+            this.healthEnemy = MINIMAL_HP;
         } else {
             this.healthEnemy -= damage;
         }
     }
 
     /**
-     * Checks if the enemy is alive.
+     * Current state.
      *
-     * @return the result of a boolean comparison of the enemy's health with the damage received.
+     * @return true if alive, false if dead.
      */
     @Override
     public boolean isAlive() {
-        return getHealth() > MIN_HP;
+        return getHealthEnemy() > MINIMAL_HP;
     }
 
-    public int getHealth() {
+    public int getHealthEnemy() {
         return this.healthEnemy;
     }
 
-    public void setHealth(int health) {
-        this.healthEnemy = health;
+    /**
+     * Makes a check when setting current health.
+     * <p>If the value received is less than zero, sets the current value to 0.
+     *
+     * @param health current health.
+     */
+    public void setHealthEnemy(int health) {
+        this.healthEnemy = Math.max(health, MINIMAL_HP);
+    }
+
+    public int getDamageEnemy() {
+        return damageEnemy;
+    }
+
+    /**
+     * Makes a check when setting the current damage value.
+     * <p>If the value received is less than zero, sets the current value to 0.
+     *
+     * @param damageEnemy current damage.
+     */
+    public void setDamageEnemy(int damageEnemy) {
+        this.damageEnemy = Math.max(damageEnemy, MINIMAL_DAMAGE);
     }
 }
