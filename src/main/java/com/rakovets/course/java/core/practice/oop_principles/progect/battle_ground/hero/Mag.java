86492a67b5399ca9bea2089722e5bf3cad58b351
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-06 16:12:07 +0200

    feat: many bugs fixed and documentation corrected for Mag.

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/hero/Mag.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/progect/battle_ground/hero/Mag.java
@@ -3,55 +3,59 @@ package com.rakovets.course.java.core.practice.oop_principles.progect.battle_gro
 import com.rakovets.course.java.core.practice.oop_principles.progect.battle_ground.enemy.Enemy;
 
 public class Mag extends Hero {
-    final int HEALING = 25;
-    final int MINIMAL_HEALING = 50;
-    final int PERCENTAGE = 100;
-    private int damageMag = 17;
-
-    public Mag(String name, int health) {
-        super(name, health);
+    /**
+     * Constructor.
+     *
+     * @param nameMag   magician's name.
+     * @param healthMag mage's maximum health.
+     * @param damageMag mage max damage.
+     */
+    public Mag(String nameMag, int healthMag, int damageMag) {
+        super(nameMag, healthMag, damageMag);
     }
 
     /**
-     * Mage attacks the enemy.
+     * Magician attacks the enemy.
      *
-     * @param enemy type enemy.
+     * @param enemy attacked enemy.
      */
     @Override
     public void attackEnemy(Enemy enemy) {
-        enemy.takingDamage(damageMag);
+        enemy.takeDamage(getDamageHero());
     }
 
     /**
-     * Healing HP.
-     * <p>
-     * Ability active.
+     * Mage takes damage.
+     * <p>Passive ability.
+     * <p>If the mage takes damage and his health drops below zero, then his current health is 0.
+     * <p> If the mage takes damage and his health does not fall below zero, then the mage can heal
+     * himself if the mage's health after taking damage is less than 50.
+     * <p>When healed, the mage cannot restore more health to himself than his maximum health.
+     * <p> If the mage takes damage and his health has not dropped to 50, then the healing does not work.
      *
-     * @param damage damage taken.
+     * @param damage damage received.
+     * @return current health after taking damage.
      */
     @Override
-    public int takingDamage(int damage) {
-        int health = MIN_HP;
+    public int takeDamage(int damage) {
+        final int HEALING = 25;
+        final int MINIMAL_HEALING = 50;
+        final int PERCENTAGE = 100;
+
+        int health = getHealthHero();
 
         if (isAlive()) {
-            if (getHealth() - damage <= MIN_HP) {
-                setHealth(MIN_HP);
-            } else if (getHealth() * MINIMAL_HEALING / PERCENTAGE <= MINIMAL_HEALING) {
-                setHealth(getHealth() - damage + HEALING);
-                health = getHealth();
+            if (getHealthHero() - damage <= MINIMAL_HP) {
+                setHealthHero(MINIMAL_HP);
+                health = getHealthHero();
+            } else if (getHealthHero() * MINIMAL_HEALING / PERCENTAGE <= MINIMAL_HEALING) {
+                setHealthHero(Math.min(getHealthHero() - damage + HEALING, getHealthHero()));
+                health = getHealthHero();
             } else {
-                setHealth(getHealth() - damage);
-                health = getHealth();
+                setHealthHero(getHealthHero() - damage);
+                health = getHealthHero();
             }
         }
         return health;
-    }
-
-    public int getDamageMag() {
-        return damageMag;
-    }
-
-    public void setDamageMag(int damageMag) {
-        this.damageMag = damageMag;
     }
 }
