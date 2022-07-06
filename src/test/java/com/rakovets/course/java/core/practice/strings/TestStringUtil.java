97ede78a658e3a57e6aa1d95e3af10a22afc7c9d
Alina Dubrovina <dubrovinaa389@gmail.com>
2022-07-06 08:07:59 +0300

    fix: add tests

--- src/test/java/com/rakovets/course/java/core/practice/strings/TestStringUtil.java
+++ src/test/java/com/rakovets/course/java/core/practice/strings/TestStringUtil.java
@@ -5,18 +5,22 @@ import org.junit.jupiter.api.Test;
 
 public class TestStringUtil {
     private final StringUtil stringUtilTest = new StringUtil();
-    //Task 1 tests
+
     @Test
     void testConcatWithTwoValues() {
         String expected = "123456";
+
         String actual = StringUtil.concatTwoStrings("123", "456");
+
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
     void testConcatWithEmptyValues() {
         String expected = " 1234";
+
         String actual = StringUtil.concatTwoStrings(" ", "1234");
+
         Assertions.assertEquals(expected, actual);
     }
 
@@ -24,106 +28,230 @@ public class TestStringUtil {
     @Test
     void testGetIndexOfSymbol() {
         int actual1 = StringUtil.getIndexOfSymbol("Hello", "l");
+
         int expected1 = 2;
+
         Assertions.assertEquals(expected1, actual1);
+
         int expected2 = -1;
+
         int actual2 = StringUtil.getIndexOfSymbol("Hello", "b");
+
         Assertions.assertEquals(expected2, actual2);
     }
 
-    //Task 3 test
     @Test
     void testCompareStringCaseSensitive() {
         boolean expected = true;
+
         boolean actual = StringUtil.compareStringCaseSensitive("Hello", "Hello");
+
         Assertions.assertEquals(expected, actual);
     }
 
-    //Task 4 test
+    @Test
+    void testCompareStringCaseSensitiveWithDifferentCase() {
+        boolean expected = false;
+
+        boolean actual = StringUtil.compareStringCaseSensitive("hello", "Hello");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void testRemoveSpaceAndUppercase() {
        String expected = "GOOD MORNING";
+
        String actual = StringUtil.removeSpaceAndUppercase(" Good morning ");
+
        Assertions.assertEquals(expected, actual);
     }
 
-    //Task 5 test
+    @Test
+    void testRemoveSpaceAndUppercaseDifferentCase() {
+        String expected = "GOOD MORNING";
+
+        String actual = StringUtil.removeSpaceAndUppercase("gOOd mOrNiNg ");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void testExtractSubstring() {
         String expected = "morning";
+
         String actual= StringUtil.extractSubstring("Good morning", 5, 12);
+
         Assertions.assertEquals(expected, actual);
     }
 
-    //Task 6 test
+    @Test
+    void testExtractSubstringWithEndingAndBeginning0() {
+        String expected = "";
+
+        String actual= StringUtil.extractSubstring("HELLO PEOPLE", 0, 0);
+
+        Assertions.assertEquals(expected, actual);}
+
     @Test
     void  testReplaceEmoji() {
         String expected = "Good morning :)";
+
         String actual = StringUtil.replaceEmoji("Good morning :(", ":(", ":)");
+
         Assertions.assertEquals(expected, actual);
     }
 
-    //Task 7 test
+    @Test
+    void  testReplaceEmojiWithoutEmoji() {
+        String expected = "Good morning";
+
+        String actual = StringUtil.replaceEmoji("Good morning", ":(", ":)");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void testReceiveIfEndAndBeginningIsWord() {
         boolean expected = true;
+
         boolean actual = StringUtil.receiveIfEndAndBeginningIsWord("good morning good", "good");
+
         Assertions.assertEquals(expected, actual);
     }
 
-    //Task 8 test
+    @Test
+    void testReceiveIfEndAndBeginningIsWordWithDifferentCase() {
+        boolean expected = false;
+
+        boolean actual = StringUtil.receiveIfEndAndBeginningIsWord("Good morning good", "good");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void testGetNumberOfVowels() {
         int actual = StringUtil.getNumberOfVowels("Good morning");
+
         int expected = 4;
+
         Assertions.assertEquals(expected, actual);
     }
 
-    //Task 9 test
     @Test
-    void testgetNumberOfPunctuationMarks() {
-        int actual = StringUtil.getNumberOfPunctuationMarks("Добрый вечер! Как твои дела?");
-        int expected = 2;
+    void testGetNumberOfVowelsWithoutVowels() {
+        int actual = StringUtil.getNumberOfVowels("GBTRDSRT");
+
+        int expected = 0;
+
         Assertions.assertEquals(expected, actual);
     }
 
-    //Task 10 test
+    @Test
+    void testGetNumberOfPunctuationMarks() {
+        int actual = StringUtil.getNumberOfPunctuationMarks("Добрый вечер! Как твои дела?");
+
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetNumberOfPunctuationMarksWithoutPunctuationMarks() {
+        int actual = StringUtil.getNumberOfPunctuationMarks("Добрый вечер");
+
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void testisPalindrome() {
         boolean actual = StringUtil.isPalindrome("Do geese see God?");
+
         boolean expected = true;
+
         Assertions.assertEquals(expected, actual);
     }
 
-    //Task 11 test
+    @Test
+    void testisNotPalindrome() {
+        boolean actual = StringUtil.isPalindrome("Do geese see?");
+
+        boolean expected = false;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void testsSplitToEqualParts() {
         String[] expected = {"Go", "od", " m", "or", "ni", "ng"};
+
         String[] actual = StringUtil.splitToEqualParts("Good morning", 2);
+
         Assertions.assertArrayEquals(actual, expected);
     }
 
-    //Task 12 test
+    @Test
+    void testsSplitToEqualPartsWithAllSymbols() {
+        String[] expected = {"Good morning"};
+
+        String[] actual = StringUtil.splitToEqualParts("Good morning", 12);
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
     @Test
     void testCountWordsNumber() {
         int actual = StringUtil.countWordsNumber("Good  morning");
+
         int expected = 2;
+
         Assertions.assertEquals(expected, actual);
     }
 
-    //Task 13 test
+    @Test
+    void testCountWordsNumberEmpty() {
+        int actual = StringUtil.countWordsNumber("");
+
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void testGetInitials() {
         String actual = StringUtil.getInitials("Alina dubrovina");
+
         String expected = "AD";
+
         Assertions.assertEquals(expected, actual);
     }
 
-    //Task 14 test
+    @Test
+    void testGetInitialsWithEmpty() {
+        String actual = StringUtil.getInitials(" ");
+
+        String expected = "";
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     @Test
     void testGetNumbers() {
         String expected = "123";
+
         String actual = StringUtil.getNumbers("Hello123");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetNumbersWithoutNumbers() {
+        String expected = "";
+
+        String actual = StringUtil.getNumbers("Hello");
+
         Assertions.assertEquals(expected, actual);
     }
 }
