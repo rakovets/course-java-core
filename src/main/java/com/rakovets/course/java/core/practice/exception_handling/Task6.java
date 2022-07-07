d09edd1073728af5b21eab8ae5453e5770ddf5e3
Evgeniy Antonovich <gnoptswbn5kj@mail.ru>
2022-07-07 23:22:07 +0300

    refactor: update exception_handling Task6

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task6.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task6.java
@@ -1,11 +1,19 @@
 package com.rakovets.course.java.core.practice.exception_handling;
 
 public class Task6 {
-    public void getThreeExceptions() {
-        int a = 5 / 0;
-        int[] b = new int[3];
-        b[4] = 10;
-        String n = null;
-        n.length();
+    public void getOneOfThreeExceptions(int variant) throws ArithmeticException, ArrayIndexOutOfBoundsException, NullPointerException {
+        switch (variant) {
+            case 1:
+                int a = 5 / 0;
+                break;
+            case 2:
+                int[] b = new int[3];
+                b[4] = 10;
+                break;
+            case 3:
+                String n = null;
+                n.length();
+                break;
+        }
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/Task6Demo.java
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/Task6Demo.java
@@ -5,7 +5,23 @@ public class Task6Demo {
         Task6 testMultipleExceptions = new Task6();
 
         try {
-            testMultipleExceptions.getThreeExceptions();
+            testMultipleExceptions.getOneOfThreeExceptions(1);
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            testMultipleExceptions.getOneOfThreeExceptions(2);
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            testMultipleExceptions.getOneOfThreeExceptions(3);
         } catch (ArithmeticException | ArrayIndexOutOfBoundsException e) {
             System.out.println(e.getMessage());
         } catch (NullPointerException e) {
