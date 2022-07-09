57e6a905736892bf2addad328079f46b0ccd784f
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-10 00:06:02 +0200

    feat: add demo for Task03

---
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/demo/Task03Demo.java
@@ -0,0 +1,19 @@
+package com.rakovets.course.java.core.practice.exception_handling.demo;
+
+import com.rakovets.course.java.core.practice.exception_handling.tasks.Task03;
+import com.rakovets.course.java.core.practice.exception_handling.exception.AgeException;
+
+public class Task03Demo {
+    public static void main(String[] args) {
+        String welcomeToClub;
+        int age = 17;
+
+        try {
+            welcomeToClub = Task03.checkAge(age);
+        } catch (AgeException e) {
+            welcomeToClub = e.getMessage();
+            e.printStackTrace();
+        }
+        System.out.println(welcomeToClub);
+    }
+}
