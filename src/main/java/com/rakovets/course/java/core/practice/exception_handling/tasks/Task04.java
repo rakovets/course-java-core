9c4e12b2e19054ca54ee827e917c7822e851335e
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-10 00:07:03 +0200

    feat: add test for Task04

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/tasks/Task04.java
@@ -0,0 +1,22 @@
+package com.rakovets.course.java.core.practice.exception_handling.tasks;
+
+import com.rakovets.course.java.core.practice.exception_handling.exception.IndexOutOfBoundsException;
+
+public class Task04 {
+    /**
+     * Gets the value from the array at its index.
+     * <p> If the value is 1, an error is returned.
+     *
+     * @param index index of the number in the array.
+     * @return the number value of the received index.
+     * @throws IndexOutOfBoundsException returns an error if the index is invalid.
+     */
+    public static String getIndexArray(int index, int[] array) throws IndexOutOfBoundsException {
+        final int EXCEPTION = 1;
+
+        if (index == EXCEPTION) {
+            throw new IndexOutOfBoundsException();
+        }
+        return "numbers[" + index + "] = " + array[index];
+    }
+}
