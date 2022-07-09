ecd26bb739c6f6616d4a753e6edc5c2be8518b24
Anastasia Melnikova <aam.cheshire@gmail.com>
2022-07-10 00:06:09 +0200

    feat: add demo for Task04

---
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/demo/Task04Demo.java
@@ -0,0 +1,20 @@
+package com.rakovets.course.java.core.practice.exception_handling.demo;
+
+import com.rakovets.course.java.core.practice.exception_handling.tasks.Task04;
+import com.rakovets.course.java.core.practice.exception_handling.exception.IndexOutOfBoundsException;
+
+public class Task04Demo {
+    public static void main(String[] args) {
+        int[] array = {10, 11, 12, 13, 14, 15};
+        int index = 1;
+        String indexArray;
+
+        try {
+            indexArray = Task04.getIndexArray(index, array);
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            indexArray = e.getMessage();
+        }
+        System.out.println(indexArray);
+    }
+}
