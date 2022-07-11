9de02bb28561fd7f57aee28ee1e5899ed66b8b57
Yegor Kharitonchik <egorkhary@mail.ru>
2022-07-11 21:10:25 +0300

    feat: add solution to Task01

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/tasks/Task01.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.exception_handling.tasks;
+
+public class Task01 {
+    public String getStringLenght(String string) {
+        return String.valueOf(string.length());
+    }
+}
