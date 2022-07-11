dfd0fe29d6f6d885ef1c41f98ce9f90c91d79c95
Yegor Kharitonchik <egorkhary@mail.ru>
2022-07-11 21:10:30 +0300

    feat: add solution to Task02

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/tasks/Task02.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.exception_handling.tasks;
+
+public class Task02 {
+    public String getIndexOfElement(int index, int[] array) {
+        return String.valueOf(array[index]);
+    }
+}
