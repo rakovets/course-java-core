22980108b11fe664959ca41a3e5254bd715a2d7d
AlenaTrafimovich <trofimovich_elena@mail.ru>
2022-07-13 14:45:35 +0300

    feat:task1-2

--- src/main/java/com/rakovets/course/java/core/practice/jcf_list/Studio.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_list/Studio.java
@@ -2,6 +2,7 @@ package com.rakovets.course.java.core.practice.jcf_list;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Objects;
 
 public class Studio {
     ArrayList<Actor> actors = new ArrayList<>();
@@ -33,7 +34,35 @@ public class Studio {
         }
     };
 
-    public void addActor(Actor actor) {
+    Comparator<Actor> byFeeSurname = new Comparator<>() {
+        @Override
+        public int compare(Actor a1, Actor a2) {
+            String firstSurname = a1.getLastName();
+            String secondSurname = a2.getLastName();
+            Float firstFee = a1.getFee();
+            Float secondFee = a2.getFee();
+            if (!firstFee.equals(secondFee)) {
+                return firstFee.compareTo(secondFee);
+            }
+            return firstSurname.compareTo(secondSurname);
+        }
+    };
+
+    Comparator<Actor> bySurnameAge = new Comparator<>() {
+        @Override
+        public int compare(Actor a1, Actor a2) {
+            String firstSurname = a1.getLastName();
+            String secondSurname = a2.getLastName();
+            Integer firstAge = a1.getAge();
+            Integer secondAge = a2.getAge();
+            if (!Objects.equals(firstSurname, secondSurname)) {
+                return firstSurname.compareTo(secondSurname);
+            }
+            return firstAge.compareTo(secondAge);
+        }
+    };
+
+    public void addActors(Actor actor) {
         actors.add(actor);
     }
 
@@ -54,6 +83,16 @@ public class Studio {
 
     public ArrayList<Actor> sortBySurname(ArrayList<Actor> actors) {
         actors.sort(bySurname);
+        return actors;
+    }
+
+    public ArrayList<Actor> sortBySurnameAge(ArrayList<Actor> actors) {
+        actors.sort(bySurnameAge);
+        return actors;
+    }
+
+    public ArrayList<Actor> sortByFeeSurname(ArrayList<Actor> actors) {
+        actors.sort(byFeeSurname);
         return actors;
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/jcf_list/StudioTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/jcf_list/StudioTest.java
@@ -11,33 +11,46 @@ public class StudioTest {
     Actor actor1 = new Actor("Ivan", "Ivanov", 5000, 32);
     Actor actor2 = new Actor("Peter", "Petrov", 500, 25);
     Actor actor3 = new Actor("Inna", "Innova", 1000, 67);
-
-    ArrayList<Actor> list = new ArrayList<>(Arrays.asList(actor1, actor2, actor3));
-    ArrayList<Actor> listChangedFee = new ArrayList<>(Arrays.asList(actor2, actor3));
-    ArrayList<Actor> listChangedSurname = new ArrayList<>(Arrays.asList(actor3, actor1, actor2));
-    ArrayList<Actor> listChangedAge = new ArrayList<>(Arrays.asList(actor2, actor1, actor3));
+    Actor actor4 = new Actor("Yana", "Innova", 1000, 65);
     Studio studio = new Studio();
+    ArrayList<Actor> list = new ArrayList<>(Arrays.asList(actor1, actor2, actor3, actor4));
+    ArrayList<Actor> listSortedByFee = new ArrayList<>(Arrays.asList(actor2, actor3, actor4));
+    ArrayList<Actor> listSortedBySurname = new ArrayList<>(Arrays.asList(actor3, actor4, actor1, actor2));
+    ArrayList<Actor> listSortedByAge = new ArrayList<>(Arrays.asList(actor2, actor1, actor4, actor3));
+    ArrayList<Actor> listSortedByFeeSurname = new ArrayList<>(Arrays.asList(actor2, actor3, actor4, actor1));
+    ArrayList<Actor> listSortedBySurnameAge = new ArrayList<>(Arrays.asList(actor4, actor3, actor1, actor2));
 
     @Test
     public void testAddActor() {
-        studio.addActor(actor1);
-        studio.addActor(actor2);
-        studio.addActor(actor3);
+        studio.addActors(actor1);
+        studio.addActors(actor2);
+        studio.addActors(actor3);
+        studio.addActors(actor4);
         Assertions.assertEquals(list, studio.getActors());
     }
 
     @Test
     public void testFire() {
-        Assertions.assertEquals(listChangedFee, studio.fire(list));
+        Assertions.assertEquals(listSortedByFee, studio.fire(list));
     }
 
     @Test
     public void testSurnameComparator() {
-        Assertions.assertEquals(listChangedSurname, studio.sortBySurname(list));
+        Assertions.assertEquals(listSortedBySurname, studio.sortBySurname(list));
     }
 
     @Test
     public void testAgeComparator() {
-        Assertions.assertEquals(listChangedAge, studio.sortByAge(list));
+        Assertions.assertEquals(listSortedByAge, studio.sortByAge(list));
+    }
+
+    @Test
+    public void testSurnameAgeComparator() {
+        Assertions.assertEquals(listSortedBySurnameAge, studio.sortBySurnameAge(list));
+    }
+
+    @Test
+    public void testFeeSurnameComparator() {
+        Assertions.assertEquals(listSortedByFeeSurname, studio.sortByFeeSurname(list));
     }
 }
