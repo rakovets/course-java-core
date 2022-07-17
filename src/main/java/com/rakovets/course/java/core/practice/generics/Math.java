1429fe7ce2046f2b0ad125367ac20c8ec0b2273e
Evgeniy Antonovich <gnoptswbn5kj@mail.ru>
2022-07-17 16:28:21 +0300

    feat: complete generics Math

--- src/main/java/com/rakovets/course/java/core/practice/generics/Math.java
+++ src/main/java/com/rakovets/course/java/core/practice/generics/Math.java
@@ -3,8 +3,31 @@ package com.rakovets.course.java.core.practice.generics;
 import java.util.Arrays;
 
 public abstract class Math<T extends Number> {
-    public static <T> T getMaxOfThree(T a, T b, T c) {
-       // T[] array = new T[10];
-        return a;
+    public static <T> T getMaxValueOfThree(T a, T b, T c) {
+        T[] arr = (T[]) new Object[]{a, b, c};
+        return getMaxValueInArray(arr);
+    }
+
+    public static <T> T getMinValueOfFive(T a, T b, T c, T d, T e) {
+        T[] arr = (T[]) new Object[]{a, b, c, d, e};
+        return getMinValueInArray(arr);
+    }
+
+    public static <T extends Number> double getAverageValueInArray(T[] arr) {
+        double sum = 0.0;
+        for (T value : arr) {
+            sum += value.doubleValue();
+        }
+        return sum / arr.length;
+    }
+
+    public static <T> T getMaxValueInArray(T[] arr) {
+        Arrays.sort(arr);
+        return arr[arr.length - 1];
+    }
+
+    public static <T> T getMinValueInArray(T[] arr) {
+        Arrays.sort(arr);
+        return arr[0];
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/generics/MathDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/generics/MathDemo.java
@@ -1,4 +1,20 @@
 package com.rakovets.course.java.core.practice.generics;
 
 public class MathDemo {
+    public static void main(String[] args) {
+        System.out.println("Maximum of three double parameters: " + Math.getMaxValueOfThree(3.0, 5.2, 10.0));
+        System.out.println("Maximum of three integer parameters: " + Math.getMaxValueOfThree(31, 52, 13));
+
+        System.out.println("Minimum of five double parameters: " + Math.getMinValueOfFive(1.2,20.5,50.1,-4.0,0.0));
+        System.out.println("Minimum of five integer parameters: " + Math.getMinValueOfFive(10,2,5,4,8));
+
+        System.out.println("Average from double array: " + Math.getAverageValueInArray(new Double[] {51.0, 0.0, 10.0, 10.0, 10.0}));
+        System.out.println("Average from integer array: " + Math.getAverageValueInArray(new Integer[] {51, 0, 10, 10, 10}));
+
+        System.out.println("Minimum number from double array: " + Math.getMinValueInArray(new Double[] {0.0, -1.0, 22.0, 1.1}));
+        System.out.println("Minimum number from integer array: " + Math.getMinValueInArray(new Integer[] {1000, -1, 22, 1, 0}));
+
+        System.out.println("Maximum number from double array: " + Math.getMaxValueInArray(new Double[] {0.0, -1.0, 22.0, 1.1}));
+        System.out.println("Maximum number from integer array: " + Math.getMaxValueInArray(new Integer[] {1000, -1, 22, 1, 0}));
+    }
 }
