fdbb339497735706fd0c4da776af6f60997ddd35
AlenaTrafimovich <trofimovich_elena@mail.ru>
2022-07-26 17:49:20 +0300

    fixed tasks

---
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_set/exception/PenaltyAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.jcf_set.exception;
+
+public class PenaltyAlreadyExistException extends RuntimeException{
+    public PenaltyAlreadyExistException(String message) {
+        super(message);
+    }
+}
