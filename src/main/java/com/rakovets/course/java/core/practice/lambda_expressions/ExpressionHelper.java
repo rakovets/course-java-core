28df8ff92524c04216072a6210872402a686ab8a
AlenaTrafimovich <trofimovich_elena@mail.ru>
2022-07-27 22:17:56 +0300

    fixed

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/ExpressionHelper.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/ExpressionHelper.java
@@ -1,6 +1,7 @@
 package com.rakovets.course.java.core.practice.lambda_expressions;
 
 public class ExpressionHelper {
+
     static boolean isEven(int number) {
         return number % 2 == 0;
     }
--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/MathsStatistics.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/MathsStatistics.java
@@ -1,6 +1,5 @@
 package com.rakovets.course.java.core.practice.lambda_expressions;
 
-
 import java.util.Arrays;
 import java.util.Random;
 
