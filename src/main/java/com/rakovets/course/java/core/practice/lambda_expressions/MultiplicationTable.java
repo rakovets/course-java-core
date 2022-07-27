bea58d7ebc70aaad658794205bc2777af00ef402
AlenaTrafimovich <trofimovich_elena@mail.ru>
2022-07-27 21:13:21 +0300

    task 2

--- src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/MultiplicationTable.java
+++ src/main/java/com/rakovets/course/java/core/practice/lambda_expressions/MultiplicationTable.java
@@ -6,18 +6,18 @@ public class MultiplicationTable {
 
     public void getMultiplicationTableInOneColumn(int first, int second) {
         IntStream.rangeClosed(first, second)
-                .forEach(col -> IntStream.rangeClosed(first, second)
+                .forEach(col -> IntStream.range(first, second)
                         .forEach(row -> System.out.println(col + " * " + row + " = " + (col * row)))
                 );
     }
 
-    public void getMultiplicationTableInFiveColumns(int first, int second, int column) {
+    public void getMultiplicationTableInFiveColumns(int first, int second) {
         IntStream.range(first, second).forEach(i -> {
-            IntStream.rangeClosed(first, column).forEach(j -> {
+            IntStream.rangeClosed(first, 5).forEach(j -> {
                 System.out.print(j + " x " + i + " = " + (i * j) + "\t");
             });
             System.out.println();
-            IntStream.rangeClosed(column+1, second).forEach(j -> {
+            IntStream.rangeClosed(6, second).forEach(j -> {
                 System.out.print(j + " x " + i + " = " + (i * j) + "\t");
             });
             System.out.println();
--- src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/MultiplicationTableDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/MultiplicationTableDemo.java
@@ -2,17 +2,17 @@ package com.rakovets.course.java.core.practice.lambda_expressions;
 
 import org.junit.jupiter.api.Test;
 
-public class MultiplicationTableTest {
+public class MultiplicationTableDemo {
     MultiplicationTable multiplicationTable = new MultiplicationTable();
 
    @Test
-    public void test () {
+    public void demoGetMultiplicationTableInOneColumn () {
         multiplicationTable.getMultiplicationTableInOneColumn(1,10);
     }
 
     @Test
-    public void test1 () {
-        multiplicationTable.getMultiplicationTableInFiveColumns(1,10,5);
+    public void demoGetMultiplicationTableInFiveColumns () {
+        multiplicationTable.getMultiplicationTableInFiveColumns(1,10);
     }
 
 }
