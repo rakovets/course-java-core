4f3ea834fc9093f244b704a3a893b95f82f8e080
Anastasia <taa.cheshire@gmail.com>
2022-07-28 17:30:09 +0200

    feat: deleted util*

--- src/main/java/com/rakovets/course/java/core/practice/jcf_map/TextMonitoring.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_map/TextMonitoring.java
@@ -1,6 +1,13 @@
 package com.rakovets.course.java.core.practice.jcf_map;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class TextMonitoring {
     private Map<String, Integer> map;
