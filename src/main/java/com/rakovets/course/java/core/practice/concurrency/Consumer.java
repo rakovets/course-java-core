a1f9cf86fd09711fac0dca6ac41ad409a2f7ac6c
AlenaTrafimovich <trofimovich_elena@mail.ru>
2022-08-04 00:04:30 +0300

    feat:done tasks 6-8

--- src/main/java/com/rakovets/course/java/core/practice/concurrency/Consumer.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/Consumer.java
@@ -1,31 +1,52 @@
 package com.rakovets.course.java.core.practice.concurrency;
 
-import java.util.ArrayDeque;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Queue;
+import java.util.concurrent.TimeUnit;
 
-public class Consumer extends Thread{
-    private Queue <Integer> queue;
+public class Consumer extends Thread {
+    private final Queue<Integer> queue;
+    private final BufferedWriter writer;
 
-    public Consumer(Queue queue) {
-        this.queue=queue;
+    public Consumer(String name, Queue<Integer> queue, BufferedWriter writer) {
+        super(name);
+        this.queue = queue;
+        this.writer = writer;
     }
 
     @Override
     public void run() {
-        getNumbersFromProducerThread();
+        try {
+            getNumbersFromProducerThread();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public void getNumbersFromProducerThread () {
-      while (true) {
-          if (!queue.isEmpty()) {
-              try {
-                  System.out.println("I will sleep for" + queue.poll());
-                  Thread.sleep(queue.poll()*1000);
-                  System.out.println("I wake up)");
-              } catch (InterruptedException e) {
-                  throw new RuntimeException(e);
-              }
-          }
-      }
+    public void getNumbersFromProducerThread() throws IOException {
+        while (!Thread.currentThread().isInterrupted()) {
+            if (!queue.isEmpty()) {
+                int sec = queue.poll();
+                try {
+                    Thread.sleep(TimeUnit.SECONDS.toMillis(sec));
+                    writer.write(LocalDateTime.now() + " " + Thread.currentThread().getName() +
+                            " I slept " + sec + " seconds\n");
+                    writer.flush();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                writer.write(LocalDateTime.now() + " " + Thread.currentThread().getName() + "\n");
+                writer.flush();
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
--- src/main/java/com/rakovets/course/java/core/practice/concurrency/Producer.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/Producer.java
@@ -3,15 +3,14 @@ package com.rakovets.course.java.core.practice.concurrency;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Objects;
 import java.util.Queue;
 
 public class Producer extends Thread {
-    private Queue <Integer> queue;
+    private final Queue<Integer> queue;
 
-    public Producer (Queue queue) {
-       this.queue=queue;
+    public Producer(Queue <Integer> queue) {
+        this.queue = queue;
     }
 
     @Override
@@ -26,18 +25,17 @@ public class Producer extends Thread {
             try {
                 System.out.print("Enter Integer Numbers: ");
                 inputString = bufferRead.readLine();
-                    if (!inputString.matches("-?[\\d]+")) {
-                        throw new UserInputException("Enter Integer Numbers!");
-                    }
+                if (!inputString.matches("-?[\\d]+")) {
+                    throw new UserInputException("Enter Integer Numbers!");
+                }
                 if (Objects.equals(inputString, "-1")) {
                     return;
                 }
                 queue.add(Integer.valueOf(inputString));
                 System.out.println(queue);
-            }catch (UserInputException e){
+            } catch (UserInputException e) {
                 System.out.println(e.getMessage());
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
