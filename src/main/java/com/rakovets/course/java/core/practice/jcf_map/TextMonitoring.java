ac83bbcd0963c506f94a9e3d3b7cebb0b2c38b09
AmosovichA <pvaganich@gmail.com>
2022-08-13 15:38:39 +0300

    feat: add solutions for jcf_map

---
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_map/TextMonitoring.java
@@ -0,0 +1,46 @@
+package com.rakovets.course.java.core.practice.jcf_map;
+
+import java.util.*;
+
+public class TextMonitoring {
+    private Map<String, Integer> map;
+
+    public void researchText(String text) {
+        String[] item = text.split(" ");
+        map = new HashMap<>();
+        for (String word : item) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+    }
+    public int getCountUniqueWords() {
+        return map.size();
+    }
+
+    public Set<String> getUniqueWords() {
+        return map.keySet();
+    }
+
+    public int getFrequencyWord(String word) {
+        return map.get(word);
+    }
+
+    public Map<String, Integer> getFrequencyWords(boolean isAscendingFrequency) {
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+
+        if (isAscendingFrequency) {
+            list.sort(Map.Entry.comparingByValue());
+        } else {
+            list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        }
+
+        Map<String, Integer> result = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+}
