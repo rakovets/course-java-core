f0ff73bba64152c426c1afe05d119ffea6da2b75
Evgeniy Antonovich <gnoptswbn5kj@mail.ru>
2022-08-17 23:25:45 +0300

    feat: complete part concurrency_synchronization SkyNet

---
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/sky_net/Factory.java
@@ -0,0 +1,25 @@
+package com.rakovets.course.java.core.practice.concurrency_thread_synchronization.sky_net;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Factory {
+    private final int maxSizeOfPartsStorage = 10;
+    private List<String> partsList = new ArrayList<>(Arrays.asList("head", "torso", "hand", "feet"));
+    private List<String> partsStorage= new ArrayList<>();
+
+    private void produce() {
+        for (int i = 0; i < getDailyPartsPlan(); i++) {
+            partsStorage.add(partsList.get(new Random().nextInt(partsList.size())));
+        }
+    }
+
+    private int getDailyPartsPlan() {
+        return new Random().nextInt(maxSizeOfPartsStorage + 1);
+    }
+
+
+
+}
---
+++ src/test/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/sky_net/SkyNetDemo.java
@@ -0,0 +1,24 @@
+package com.rakovets.course.java.core.practice.concurrency_thread_synchronization.sky_net;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class SkyNetDemo {
+    private static final int sizeOfPartsStorage = 10;
+    private static List<String> partsList = new ArrayList<>(Arrays.asList("head", "torso", "hand", "feet"));
+    private static List<String> partsStorage = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < sizeOfPartsStorage; i++) {
+            partsStorage.add(partsList.get(new Random().nextInt(partsList.size())));
+        }
+        System.out.println(partsStorage);
+
+    }
+
+
+}
+
