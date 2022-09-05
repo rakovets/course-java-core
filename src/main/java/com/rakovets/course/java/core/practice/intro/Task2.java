14a41f786fe35dad4479c3de916a0dc6da271823
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-06 00:31:07 +0300

    feat: add feature HW for Lesson 1 - intro

---
+++ src/main/java/com/rakovets/course/java/core/practice/intro/Task2.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.intro;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Java: version 11.");
+        System.out.println("JVM: HotSpot.");
+    }
+}
