13d2c3a0fbf9b92f7a62d68f7babc5414ee06468
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-06 00:26:56 +0300

    feat: add feature HW for Lesson 1 - intro

---
+++ src/main/java/com/rakovets/course/java/core/practice/intro/Task1.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.intro;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Java: version 11.");
+    }
+}
