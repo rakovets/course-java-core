a41eca47f8903ecee4ea9b4203b1d013c27bdbff
Vict Pol <vpolik10@gmail.com>
2022-09-11 21:25:38 +0200

    feat: add solution for Task01.java theme Operaters

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task01.java
@@ -31,7 +31,6 @@ class Task01 {
     static int getNumberFullTrucks(int cargo, int carryingCapacity) {
         //TODO
         // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        int result = cargo / carryingCapacity;
-        return result;
+        return cargo / carryingCapacity;
     }
 }
