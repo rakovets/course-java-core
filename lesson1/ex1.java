d20836cabb083f6914a16348c792026c3149af1f
Dmitry Sharshokov <reguldgreate@gmail.com>
2022-09-12 04:11:22 +0300

    Add files via upload

---
+++ lesson1/ex1.java
@@ -0,0 +1,5 @@
+public class ex1{
+public static void main (String[] args){
+System.out.println("Java: version 11.");
+}
+}
---
+++ lesson1/ex2.java
@@ -0,0 +1,5 @@
+public class ex2{
+public static void main(String[] args){
+System.out.println("Java: version 11. \nJVM: HotSpot.");
+}
+}
