f14464dfb1711e9f2b0241d819faedb1f33bd842
Vict Pol <vpolik10@gmail.com>
2022-09-15 19:31:37 +0200

    feat: update file Task02.java

--- src/main/java/com/rakovets/course/java/core/practice/intro/Task02.java
+++ src/main/java/com/rakovets/course/java/core/practice/intro/Task02.java
@@ -1,4 +1,7 @@
 package com.rakovets.course.java.core.practice.intro;
 
 public class Task02 {
+    public static void main(String[] args) {
+        System.out.println("Java: version 11.\nJava: HotSpot.");
+    }
 }
