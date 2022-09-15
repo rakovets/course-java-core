d4421e129183ea839b647572c2b1aae0ece92032
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-15 19:01:30 +0300

    feat: add solution for Task08

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task08.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task08.java
@@ -1,5 +1,7 @@
 package com.rakovets.course.java.core.practice.looping_statements;
 
+import com.rakovets.course.java.core.util.NumberUtil;
+
 /**
  * Разработать программу для сети оптовых гипермаркетов.
  * Необходимо сформировать список цен для некоторого продукта.
@@ -24,11 +26,11 @@ class Task08 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int startNumberItems = 4;
-        double startPriceAllItems = 3.0;
-        int differentialNumberItems = 5;
-        double differentialSell = 4.0;
-        int sizeTotalPrice = 6;
+        int startNumberItems = 100;
+        double startPriceAllItems = 1.15;
+        int differentialNumberItems = 10;
+        double differentialSell = 1.0;
+        int sizeTotalPrice = 5;
 
         String totalPriceList = generateTotalPriceList(startNumberItems, startPriceAllItems, differentialNumberItems, differentialSell, sizeTotalPrice);
         System.out.printf("Result:\n%s", totalPriceList);
@@ -49,8 +51,23 @@ class Task08 {
      * <code>NumberUtil.roundValueToTwoDigitsForMantissa(value)</code>
      */
     static String generateTotalPriceList(int startNumberItems, double startPriceAllItems, int differentialNumberItems, double differentialSell, int sizeTotalPrice) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        double sell = 0.0;
+        int numberOfItems = startNumberItems;
+        double priceAllItems;
+        double totalDiscount;
+        String totalPriceList = startNumberItems + " - " + startPriceAllItems + " with sell " +  sell + "%";
+
+        for (int i = 1; i < sizeTotalPrice; i++) {
+            totalPriceList += "\n";
+            sell += differentialSell;
+            numberOfItems += differentialNumberItems;
+            priceAllItems = numberOfItems * (startPriceAllItems / startNumberItems);
+            totalDiscount = priceAllItems * sell / 100;
+            totalPriceList += numberOfItems + " - "
+                    + NumberUtil.roundValueToTwoDigitsForMantissa(priceAllItems - totalDiscount)
+                    + " with sell " +  sell + "%";
+        }
+
+        return totalPriceList;
     }
 }
