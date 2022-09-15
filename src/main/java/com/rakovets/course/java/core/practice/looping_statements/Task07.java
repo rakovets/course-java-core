f6a2753f0a8567c4b1063fe841f2bf7b492dc86e
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-15 18:28:01 +0300

    feat: add solution for Task07

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task07.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task07.java
@@ -1,5 +1,7 @@
 package com.rakovets.course.java.core.practice.looping_statements;
 
+import com.rakovets.course.java.core.util.NumberUtil;
+
 /**
  * Разработать программу для сети оптовых гипермаркетов.
  * Необходимо сформировать список цен для некоторого продукта.
@@ -45,8 +47,18 @@ class Task07 {
      * <code>NumberUtil.roundValueToTwoDigitsForMantissa(value)</code>
      */
     static String generateTotalPriceList(int startNumberItems, double startPriceAllItems, int differentialNumberItems, int sizeTotalPrice) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String totalPriceList = startNumberItems + " - " + startPriceAllItems;
+        double priceAllItems;
+        int numberOfItems = startNumberItems;
+
+        for (int i = 1; i < sizeTotalPrice; i++) {
+            totalPriceList += "\n";
+            numberOfItems += differentialNumberItems;
+            priceAllItems = NumberUtil.roundValueToTwoDigitsForMantissa(numberOfItems
+                    * (startPriceAllItems / startNumberItems));
+            totalPriceList += numberOfItems + " - " + priceAllItems;
+        }
+
+        return totalPriceList;
     }
 }
