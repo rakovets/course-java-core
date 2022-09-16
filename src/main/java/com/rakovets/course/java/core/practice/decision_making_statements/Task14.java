b366060eb979775e54eee65c927c4f42f51678cc
Hanna Kavaliova <a.kovaleva@21vek.by>
2022-09-16 21:30:40 +0300

    feat: done Task14

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task14.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task14.java
@@ -42,8 +42,45 @@ class Task14 {
      * @return название года по китайскому календарю
      */
     static String getZodiacYearName(int year) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String animal = "";
+        switch (year % 12) {
+            case 0:
+                animal = "Monkey";
+                break;
+            case 1:
+                animal = "Rooster";
+                break;
+            case 2:
+                animal = "Dog";
+                break;
+            case 3:
+                animal = "Pig";
+                break;
+            case 4:
+                animal = "Rat";
+                break;
+            case 5:
+                animal = "Ox";
+                break;
+            case 6:
+                animal = "Tiger";
+                break;
+            case 7:
+                animal = "Rabbit";
+                break;
+            case 8:
+                animal = "Dragon";
+                break;
+            case 9:
+                animal = "Snake";
+                break;
+            case 10:
+                animal = "Horse";
+                break;
+            case 11:
+                animal = "Ram";
+                break;
+        }
+        return animal;
     }
 }
