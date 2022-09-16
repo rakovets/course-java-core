e9d06e4d47f22472a08dab07059b562b3bd6629a
Hanna Kavaliova <a.kovaleva@21vek.by>
2022-09-16 12:09:24 +0300

    feat: done Task06

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task06.java
@@ -39,8 +39,13 @@ class Task06 {
      * @return 'Player 1: ${0}. Player 2: ${1}', где ${0} - очки первого игрока, ${1} - очки второго игрока.
      */
     static String getResult(int tanksKilledFirstPlayer, int tanksKilledSecondPlayer) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        int resultFirstPlayer = tanksKilledFirstPlayer * KILL_POINT;
+        int resultSecondPlayer = tanksKilledSecondPlayer * KILL_POINT;
+        if (tanksKilledFirstPlayer > tanksKilledSecondPlayer) {
+            resultFirstPlayer += BONUS_POINT;
+        } else if (tanksKilledFirstPlayer < tanksKilledSecondPlayer) {
+            resultSecondPlayer += BONUS_POINT;
+        }
+        return ("Player 1: " + resultFirstPlayer + ". Player 2: " + resultSecondPlayer);
     }
 }
