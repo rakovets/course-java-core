af97c02844784b220797354e4a60794c03a7932d
Hanna Kavaliova <a.kovaleva@21vek.by>
2022-09-16 15:34:30 +0300

    feat: done Task08

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task08.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task08.java
@@ -38,8 +38,14 @@ class Task08 {
      * @return итоговый урон по данному типу моба
      */
     static int getTotalDamage(int damage, String typeMob, boolean hasHolyAttribute) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int totalDamage;
+        if (hasHolyAttribute == true && typeMob == "SAINT") {
+            totalDamage = damage / 2;
+        } else if (hasHolyAttribute == true && (typeMob == "UNDEAD" || typeMob == "ZOMBIE")) {
+            totalDamage = damage + damage / 2;
+        } else {
+            totalDamage = damage;
+        }
+        return totalDamage;
     }
 }
