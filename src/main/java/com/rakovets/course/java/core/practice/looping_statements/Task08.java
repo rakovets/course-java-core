035eb1f24effd356d1f66bc22f610ae0532d76c9
Hanna Kavaliova <a.kovaleva@21vek.by>
2022-09-17 21:13:19 +0300

    feat: done Task08

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task08.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task08.java
@@ -1,5 +1,7 @@
 package com.rakovets.course.java.core.practice.looping_statements;
 
+import com.rakovets.course.java.core.util.NumberUtil;
+
 /**
  * Разработать программу для сети оптовых гипермаркетов.
  * Необходимо сформировать список цен для некоторого продукта.
@@ -49,8 +51,15 @@ class Task08 {
      * <code>NumberUtil.roundValueToTwoDigitsForMantissa(value)</code>
      */
     static String generateTotalPriceList(int startNumberItems, double startPriceAllItems, int differentialNumberItems, double differentialSell, int sizeTotalPrice) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String totalPriceList = "";
+        totalPriceList += startNumberItems + " - " + startPriceAllItems + " with sell " + "0.0%";
+        for (int i = 1; i < sizeTotalPrice; i++) {
+            int numberItem = differentialNumberItems * i + startNumberItems;
+            double priceAllItems = startPriceAllItems / startNumberItems * numberItem;
+            double sale = i * priceAllItems * differentialSell / 100;
+            double priceWithSale = NumberUtil.roundValueToTwoDigitsForMantissa(priceAllItems - sale);
+            totalPriceList += "\n" + numberItem + " - " + priceWithSale + " with sell " + i * differentialSell + "%";
+        }
+        return totalPriceList;
     }
 }
