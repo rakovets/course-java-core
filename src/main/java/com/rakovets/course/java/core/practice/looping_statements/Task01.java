5668a7a92a391d45b641290c848853eee1d1d6db
Aliaksei Kharko <AliakseiKharko@gmail.com>
2022-09-17 19:34:15 +0300

    feat: add done Task01 from Looping statements

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task01.java
@@ -29,8 +29,11 @@ class Task01 {
      * @return текст, который содержит графа с порядковыми номерами записей, где каждый номер на новой строке
      */
     static String generateNumbersColumn(int numberRows) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String ordinalNumbers = "";
+
+        for (int i = 1; i < numberRows; i++) {
+            ordinalNumbers += i + "\n";
+        }
+        return ordinalNumbers + numberRows;
     }
 }
