dd0ee8b2a47967f58fdd8b3fe0023aa6715dde9b
Hanna Kavaliova <a.kovaleva@21vek.by>
2022-09-17 23:11:57 +0300

    feat: done Task09

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task09.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task09.java
@@ -20,7 +20,7 @@ class Task09 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        long amount = 1500L;
+        long amount = -1500L;
 
         String amountWithAccountingFormat = convertToAccountingFormat(amount);
         System.out.printf("Result: %s", amountWithAccountingFormat);
@@ -34,12 +34,25 @@ class Task09 {
      * @return сумма в бухгалтерском формате
      */
     static String convertToAccountingFormat(long amount) {
-        String amountWithAccountingFormat = "";
-        String start = "";
-        while (amount/1000 != 0){
-
-            amountWithAccountingFormat += amount%1000 + " ";
+        String first = "";
+        StringBuffer amountWithFormat = new StringBuffer("");
+        if (amount < 0) {
+            amount = -amount;
+            first = "-";
         }
-        return null;
+        while (amount / 1000 != 0) {
+            if (amount % 1000 == 0) {
+                amountWithFormat.insert(0, " 000");
+            } else {
+                amountWithFormat.insert(0, (" " + amount % 1000));
+            }
+            amount = amount / 1000;
+        }
+        if (amount / 1000 == 0) {
+            amountWithFormat.insert(0, "" + amount);
+        } else if (amount / 100 == 0) {
+            amountWithFormat.insert(0, "" + amount);
+        }
+        return first + amountWithFormat.toString();
     }
 }
