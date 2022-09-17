334a51bc4a9d8aeb4ab1e7c3cb8a6458d94b1f48
Aliaksei Kharko <AliakseiKharko@gmail.com>
2022-09-17 19:12:47 +0300

    feat: add done Task06 from Decision-making statements

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task06.java
@@ -39,8 +39,15 @@ class Task06 {
      * @return 'Player 1: ${0}. Player 2: ${1}', где ${0} - очки первого игрока, ${1} - очки второго игрока.
      */
     static String getResult(int tanksKilledFirstPlayer, int tanksKilledSecondPlayer) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        int basicPointsFirstPlayer = tanksKilledFirstPlayer * KILL_POINT;
+        int basicPointsSecondPlayer = tanksKilledSecondPlayer * KILL_POINT;
+
+        if (tanksKilledFirstPlayer > tanksKilledSecondPlayer) {
+            return "Player 1: " + (basicPointsFirstPlayer + BONUS_POINT) + ". Player 2: " + basicPointsSecondPlayer;
+        } else if (tanksKilledFirstPlayer < tanksKilledSecondPlayer) {
+            return "Player 1: " + basicPointsFirstPlayer + ". Player 2: " + (basicPointsSecondPlayer + BONUS_POINT);
+        } else {
+            return "Player 1: " + basicPointsFirstPlayer + ". Player 2: " + basicPointsSecondPlayer;
+        }
     }
 }
