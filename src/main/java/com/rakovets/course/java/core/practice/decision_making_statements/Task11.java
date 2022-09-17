1fbd6a87ea989ce10e914aad81c99a9976c2cc19
Aliaksei Kharko <AliakseiKharko@gmail.com>
2022-09-17 19:18:07 +0300

    feat: add done Task11 from Decision-making statements

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task11.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task11.java
@@ -38,8 +38,14 @@ class Task11 {
      * @return <code>latin</code>/<code>cyrillic</code>/<code>digit</code>/<code>undefined</code>
      */
     static String getSymbolType(char symbol) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        if ('A' <= symbol && symbol <= 'Z' || 'a' <= symbol && symbol <= 'z') {
+            return "latin";
+        } else if ('А' <= symbol && symbol <= 'я') {
+            return "cyrillic";
+        } else if ('0' <= symbol && symbol <= '9') {
+            return "digit";
+        } else {
+            return "undefined";
+        }
     }
 }
