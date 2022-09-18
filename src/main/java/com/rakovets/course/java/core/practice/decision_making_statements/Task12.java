0c99b465a1bf2ce653ef67343f019c75335ee7e8
Vict Pol <vpolik10@gmail.com>
2022-09-18 02:00:05 +0200

    feat: update file Task12.java

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task12.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task12.java
@@ -28,8 +28,8 @@ class Task12 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int day = 12;
-        int month = 12;
+        int day = 20;
+        int month = 1;
 
         String zodiacSign = getZodiacSign(day, month);
         System.out.printf("Result: %s", zodiacSign);
@@ -43,8 +43,48 @@ class Task12 {
      * @return знак зодиака
      */
     static String getZodiacSign(int day, int month) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String _zodiacSign = "Неправильно введена дата рождения";
+        float dateMonthDay = month + (float) day / 100;
+        if (1.01f <= dateMonthDay && dateMonthDay <= 1.20f) {
+            _zodiacSign = "Goat";
+            return _zodiacSign;
+        } else if (1.21f <= dateMonthDay && dateMonthDay <= 2.19f) {
+            _zodiacSign = "Water-bearer";
+            return _zodiacSign;
+        } else if (2.20f <= dateMonthDay && dateMonthDay <= 3.20f) {
+            _zodiacSign = "Fish";
+            return _zodiacSign;
+        } else if (3.21f <= dateMonthDay && dateMonthDay <= 4.20f) {
+            _zodiacSign = "Ram";
+            return _zodiacSign;
+        } else if (4.21f <= dateMonthDay && dateMonthDay <= 5.20f) {
+            _zodiacSign = "Bull";
+            return _zodiacSign;
+        } else if (5.21f <= dateMonthDay && dateMonthDay <= 6.21f) {
+            _zodiacSign = "Twins";
+            return _zodiacSign;
+        } else if (6.22f <= dateMonthDay && dateMonthDay <= 7.22f) {
+            _zodiacSign = "Crab";
+            return _zodiacSign;
+        } else if (7.22f <= dateMonthDay && dateMonthDay <= 8.22f) {
+            _zodiacSign = "Lion";
+            return _zodiacSign;
+        } else if (8.23f <= dateMonthDay && dateMonthDay <= 9.21f) {
+            _zodiacSign = "Maiden";
+            return _zodiacSign;
+        } else if (9.22f <= dateMonthDay && dateMonthDay <= 10.22f) {
+            _zodiacSign = "Scales";
+            return _zodiacSign;
+        } else if (10.23f <= dateMonthDay && dateMonthDay <= 11.22f) {
+            _zodiacSign = "Scorpion";
+            return _zodiacSign;
+        } else if (11.23f <= dateMonthDay && dateMonthDay <= 12.21f) {
+            _zodiacSign = "Archer";
+            return _zodiacSign;
+        } else if (12.22f <= dateMonthDay && dateMonthDay <= 12.31f) {
+            _zodiacSign = "Goat";
+            return _zodiacSign;
+        }
+        return _zodiacSign;
     }
 }
