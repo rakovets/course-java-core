cca305fa9012faccf13da1807069d55d1c6b028c
Vict Pol <vpolik10@gmail.com>
2022-09-18 23:38:57 +0200

    feat: update file Task05.java

--- src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/looping_statements/Task05.java
@@ -32,8 +32,17 @@ class Task05 {
      * @return текст, который содержит графа с порядковыми номерами записей, где каждый номер на новой строке
      */
     static String generateNumbersColumn(int numberRows, boolean isEnableHeaderRow) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String _numbersColumns = "";
+        if (isEnableHeaderRow) {
+            for (int i = 1; i < numberRows; i++) {
+            _numbersColumns += "\n" + i;
+            }
+        }  else {
+            for (int i = 1; i < numberRows; i++) {
+                _numbersColumns += i + "\n";
+            }
+            _numbersColumns += numberRows;
+        }
+        return _numbersColumns;
     }
 }
