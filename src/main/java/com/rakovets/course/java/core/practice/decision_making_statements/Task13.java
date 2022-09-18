b939d3e366e4341f69a98621ab422609b37fd09d
Vict Pol <vpolik10@gmail.com>
2022-09-18 15:45:09 +0200

    feat: update file Task13.java

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task13.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task13.java
@@ -17,7 +17,7 @@ class Task13 {
         // аргументов. Типы данных изменять нельзя
         int numberFloors = 9;
         int numberApartmentsPerFloor = 4;
-        int apartmentNumber = 68;
+        int apartmentNumber = 37;
 
         int floorNumber = getFloorNumber(numberFloors, numberApartmentsPerFloor, apartmentNumber);
         System.out.printf("Result: %s", floorNumber);
@@ -32,8 +32,21 @@ class Task13 {
      * @return номер этажа
      */
     static int getFloorNumber(int numberFloors, int numberApartmentsPerFloor, int apartmentNumber) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int _floorNumber = 0;
+        int apartmentQuantityInOnePorch = numberFloors * numberApartmentsPerFloor;
+        if (apartmentNumber > apartmentQuantityInOnePorch) {
+            if (apartmentNumber % numberApartmentsPerFloor >=1) {
+                _floorNumber = ((apartmentNumber / numberApartmentsPerFloor) + 1) - ((apartmentNumber / apartmentQuantityInOnePorch) * numberFloors);
+            } else {
+                _floorNumber = (apartmentNumber / numberApartmentsPerFloor - (apartmentNumber / apartmentQuantityInOnePorch) * numberFloors);
+            }
+        } else {
+            if (apartmentNumber % numberApartmentsPerFloor >=1) {
+                _floorNumber = apartmentNumber / numberApartmentsPerFloor +1;
+            } else {
+                _floorNumber = apartmentNumber / numberApartmentsPerFloor;
+            }
+        }
+        return _floorNumber;
     }
 }
