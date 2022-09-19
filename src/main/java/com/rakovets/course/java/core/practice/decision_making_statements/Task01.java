17397a2f35b4a7e4ec56b2c6331aa6f16ea5616b
10 <reguldgreate@gmail.com>
2022-09-19 21:17:05 +0300

    feat: add Lesson 3, task 1

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task01.java
@@ -36,8 +36,12 @@ class Task01 {
      * </ul>
      */
     static boolean isTopPlayer(int sizeTop, int positionPlayerByDamage) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return false;
+        boolean top = false;
+        if (sizeTop >= positionPlayerByDamage) {
+            top =true;
+        } else {
+            top = false;
+        }
+        return top;
     }
 }
