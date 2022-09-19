80ec34e969ff05b3f24775579e3c69143ed52927
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-19 20:10:55 +0300

    feat: refactor solution for Task07

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task07.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task07.java
@@ -36,7 +36,11 @@ class Task07 {
      * @return <code>false</code>/<code>true</code>
      */
     static boolean isLeapYear(int year) {
-        boolean isYearLeap = year % 4 == 0;
+        boolean isYearLeap = false;
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ) {
+            isYearLeap = true;
+        }
 
         return isYearLeap;
     }
