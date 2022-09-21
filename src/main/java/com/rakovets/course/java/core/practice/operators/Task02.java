73a3b9124d722118707ff82b360fc8547965a9b5
Aliaksei Kharko <aliakseikharko@gmail.com>
2022-09-21 15:40:19 +0300

    feat: add done Task02 from Operators

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task02.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task02.java
@@ -30,8 +30,6 @@ class Task02 {
      * @param carryingCapacity грузоподъемность грузовиков
      */
     static int getAmountRemainingCargo(int cargo, int carryingCapacity) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        return cargo % carryingCapacity;
     }
 }
