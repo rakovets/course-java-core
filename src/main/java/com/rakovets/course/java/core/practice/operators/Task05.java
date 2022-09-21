f2fdc3ea5ebeb0e26ca92aae53434af40a5703c4
Aliaksei Kharko <aliakseikharko@gmail.com>
2022-09-21 15:42:48 +0300

    feat: add done Task05 from Operators

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task05.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task05.java
@@ -34,8 +34,7 @@ class Task05 {
      * @param weight - вес
      */
     static int calculateDeviationStandardWeight(int height, int weight) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        final int VALUE_FOR_CALCULATION_OF_IDEAL_WEIGHT = 110;
+        return weight - (height - VALUE_FOR_CALCULATION_OF_IDEAL_WEIGHT);
     }
 }
