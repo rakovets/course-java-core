4a771042363231772fef5293e820ae06f1cbf561
Oleg Aleksashov <Fairp87@gmail.com>
2022-09-21 20:19:05 +0300

    feat: add solutions for all tasks operators

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task03.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task03.java
@@ -35,8 +35,7 @@ class Task03 {
      * @param depositAnnualPercentage ежегодный простой процент
      */
     static float getTotalDepositAmount(int depositAmount, int depositYears, int depositAnnualPercentage) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0.0f;
+        float percent = (float) depositAmount / 100 * depositAnnualPercentage * depositYears;
+        return depositAmount + percent;
     }
 }
