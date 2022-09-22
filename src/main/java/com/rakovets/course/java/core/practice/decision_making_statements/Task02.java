210ecf1190f54e9fc403f7134c6141f9e04fdd19
Hanna Matveyeva <jessikane4ka@gmail.com>
2022-09-23 00:17:42 +0300

    feature: update solution task02 lesson 3

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task02.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task02.java
@@ -16,7 +16,7 @@ class Task02 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int hour = 10;
+        int hour = 20;
 
         String greeting = getGreetingByHour(hour);
         System.out.printf("Result: %s", greeting);
@@ -35,13 +35,14 @@ class Task02 {
      * </ul>
      */
     static String getGreetingByHour(int hour) {
-        if (hour >= 6 && (hour < 12)) {
+        int hour1 = 6, hour2 = 12, hour3 = 18, hour4 = 24, hour5 = 0;
+        if (hour >= hour1 && (hour < hour2)) {
             return "Good morning";
-        } else if (hour >= 12 && hour <= 17) {
+        } else if (hour >= hour2 && hour < hour3) {
             return "Good day";
-        } else if (hour >= 18 && hour <= 23) {
+        } else if (hour >= hour3 && hour < hour4) {
             return "Good evening";
-        } else if (hour >= 0 && hour < 6) {
+        } else if (hour >= hour5 && hour < hour1) {
             return "Good night";
         } else {
             return "Invalid data";
