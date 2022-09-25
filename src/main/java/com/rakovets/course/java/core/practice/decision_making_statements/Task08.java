eb7da9aaef158a0dacb7e1984792b71ad7e1713d
Vict Pol <vpolik10@gmail.com>
2022-09-25 20:19:02 +0200

    feat: update file Task08.java

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task08.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task08.java
@@ -38,28 +38,28 @@ class Task08 {
      * @return итоговый урон по данному типу моба
      */
     static int getTotalDamage(int damage, String typeMob, boolean hasHolyAttribute) {
-        int _totalDamage;
+        int totalDamage;
         if (hasHolyAttribute) {
             switch (typeMob) {
                 case "UNDEAD":
                 case "ZOMBIE":
-                    _totalDamage = (int) (damage * 1.5f);
+                    totalDamage = (int) (damage * 1.5f);
                     break;
                 case "SAINT":
-                    _totalDamage = (int) (damage / 2);
+                    totalDamage = (int) (damage / 2);
                     break;
                 case "ANIMAL":
                 case "HUMANOID":
                 case "PLANT":
                 case "GHOST":
-                    _totalDamage = damage * 1;
+                    totalDamage = damage * 1;
                     break;
                 default:
-                    _totalDamage = 0;
+                    totalDamage = 0;
             }
         } else {
-            _totalDamage = 0;
+            totalDamage = 0;
         }
-        return _totalDamage;
+        return totalDamage;
     }
 }
