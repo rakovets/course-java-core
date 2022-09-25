b73ea974872113756310a792563b884eefb90c0a
Vict Pol <vpolik10@gmail.com>
2022-09-25 19:38:37 +0200

    feat: update file Task02.java

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task02.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task02.java
@@ -35,14 +35,25 @@ class Task02 {
      * </ul>
      */
     static String getGreetingByHour(int hour) {
-        if (hour >= 6 && hour <=11) {
-            return "Good morning";
-        } else if (hour >= 12 && hour <= 17) {
-            return "Good day";
-        } else if (hour >= 18 && hour <= 23) {
-            return "Good evening";
-        } else if (hour >= 0 && hour <= 5 || hour == 24) {
-            return "Good night";
-        } return null;
+        final int beginningOfMorningHour = 6;
+        final int beginningOfDayHour = 12;
+        final int beginningOfEveningHour = 18;
+        final int midnightHour = 24;
+        String greetingByHour = "";
+
+        if (hour >= (midnightHour - midnightHour) && hour <= midnightHour) {
+            if (hour < beginningOfMorningHour || hour == midnightHour) {
+                greetingByHour = "Good night";
+            } else if (hour < beginningOfDayHour) {
+                greetingByHour = "Good morning";
+            } else if (hour < beginningOfEveningHour) {
+                greetingByHour = "Good day";
+            } else if (hour < midnightHour) {
+                greetingByHour = "Good evening";
+            }
+            return greetingByHour;
+        } else {
+            return null;
+        }
     }
 }
