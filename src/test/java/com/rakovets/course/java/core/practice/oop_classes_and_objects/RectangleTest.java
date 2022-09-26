aad41e97a86e4060cd3ffa45233b29e536fcb569
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-26 23:39:45 +0300

    feat: refactor RectangleTest.java

--- src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/RectangleTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/RectangleTest.java
@@ -4,18 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RectangleTest {
     public static void main(String[] args) {
-        Point topLeftPoint = new Point(1,1);
-        Point bottomRightPoint = new Point(4,4);
-        Rectangle testRectangle = new Rectangle(topLeftPoint, bottomRightPoint);
+        Point testLeftPoint = new Point(1,1);
+        Point testBottomRightPoint = new Point(4,4);
+        Rectangle testRectangle = new Rectangle(testLeftPoint, testBottomRightPoint);
 
-        topLeftPoint.setX(2);
-        topLeftPoint.setY(2);
-        bottomRightPoint.setX(5);
-        bottomRightPoint.setY(5);
+        testLeftPoint.setX(2);
+        testLeftPoint.setY(2);
+        testBottomRightPoint.setX(5);
+        testBottomRightPoint.setY(5);
 
 
-        assertEquals(topLeftPoint, testRectangle.getTopLeftPoint());
-        assertEquals(bottomRightPoint, testRectangle.getBottomRightPoint());
+        assertEquals(testLeftPoint, testRectangle.getTopLeftPoint());
+        assertEquals(testBottomRightPoint, testRectangle.getBottomRightPoint());
         assertEquals(9, testRectangle.getArea());
         assertEquals(12, testRectangle.getPerimeter());
     }
