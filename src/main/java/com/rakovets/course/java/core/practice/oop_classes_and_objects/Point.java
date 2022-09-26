840c278d8584e4f029566bb4dc98d2937c0c8116
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-26 22:45:45 +0300

    feat: add Point.java

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Point.java
@@ -0,0 +1,35 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
+import com.rakovets.course.java.core.util.NumberUtil;
+
+public class Point {
+    private int x;
+    private int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getDistance(Point targetPoint, int x, int y) {
+        double distance = Math.sqrt(Math.pow((targetPoint.y - y), 2) + Math.pow((targetPoint.x - x), 2));
+
+        return NumberUtil.roundValueToTwoDigitsForMantissa(distance);
+    }
+}
