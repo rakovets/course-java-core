7ebe39bb08684580f34ed1893acdf3cb1727c7cf
Dzmitriy Krasiuk <strixmymail@gmail.com>
2022-09-26 22:45:54 +0300

    feat: add PointTest.java

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/PointTest.java
@@ -0,0 +1,14 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point test1 = new Point(2, 3);
+
+        assertEquals(2, test1.getX());
+        assertEquals(3, test1.getY());
+        assertEquals(2.83, test1.getDistance(test1, 4, 5));
+
+    }
+}
