d382e0762ea70eaecc24a5cb2a74407203a863ec
Aliaksei Kharko <aliakseikharko@gmail.com>
2022-09-29 23:47:03 +0300

    feat: add Task04 from OOP classes and objects

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Atm.java
@@ -0,0 +1,37 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
+public class Atm {
+    private int numberBanknotes20;
+    private int numberBanknotes50;
+    private int numberBanknotes100;
+
+    public Atm(int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) {
+        this.numberBanknotes20 = numberBanknotes20;
+        this.numberBanknotes50 = numberBanknotes50;
+        this.numberBanknotes100 = numberBanknotes100;
+    }
+
+    public int addBanknotes20(int number) {
+        return this.numberBanknotes20 += number;
+    }
+
+    public int addBanknotes50(int number) {
+        return this.numberBanknotes50 += number;
+    }
+
+    public int addBanknotes100(int number) {
+        return this.numberBanknotes100 += number;
+    }
+
+    public int getNumberBanknotes20() {
+        return numberBanknotes20;
+    }
+
+    public int getNumberBanknotes50() {
+        return numberBanknotes50;
+    }
+
+    public int getNumberBanknotes100() {
+        return numberBanknotes100;
+    }
+}
