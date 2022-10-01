96ac1b83d18698b560f020bd5f757bf57d0f1b2a
Vict Pol <vpolik10@gmail.com>
2022-10-01 12:55:30 +0200

    feat: add 2 files with Atm task

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Atm.java
@@ -0,0 +1,111 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
+public class Atm {
+    private int numberBanknotes20;
+    private int numberBanknotes50;
+    private int numberBanknotes100;
+    private int currentIssueAmount;
+    {
+        currentIssueAmount = 0;
+    }
+    final int VALUE_BANKNOTES_20 = 20;
+    final int VALUE_BANKNOTES_50 = 50;
+    final int VALUE_BANKNOTES_100 = 100;
+
+    public Atm(int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) {
+        this.numberBanknotes20 = numberBanknotes20;
+        this.numberBanknotes50 = numberBanknotes50;
+        this.numberBanknotes100 = numberBanknotes100;
+    }
+    public void addBanknotes20(int numberBanknotes20) {
+        this.numberBanknotes20 += numberBanknotes20;
+    }
+    public void addBanknotes50(int numberBanknotes50) {
+        this.numberBanknotes50 += numberBanknotes50;
+    }
+    public void addBanknotes100(int numberBanknotes100) {
+        this.numberBanknotes100 += numberBanknotes100;
+    }
+    public int getNumberBanknotes20() {
+        return this.numberBanknotes20;
+    }
+    public int getNumberBanknotes50() {
+        return this.numberBanknotes50;
+    }
+    public int getNumberBanknotes100() {
+        return this.numberBanknotes100;
+    }
+    public int getCurrentIssueAmount() {
+        return this.currentIssueAmount;
+    }
+    public boolean isPossibleIssue(int amount) {
+        boolean isPossibleIssue = false;
+        int totalAmountInAtm = numberBanknotes20 * VALUE_BANKNOTES_20 + numberBanknotes50 * VALUE_BANKNOTES_50 + numberBanknotes100 * VALUE_BANKNOTES_100;
+        if (amount > 0 && amount < totalAmountInAtm) {
+            for (int i = 0; i <= (amount / VALUE_BANKNOTES_100) && i <= numberBanknotes100; i++) {
+                for (int j = 0; j <= (amount / VALUE_BANKNOTES_50) && j <= numberBanknotes50; j++) {
+                    for (int k = 0; k <= (amount / VALUE_BANKNOTES_20) && k <= numberBanknotes20; k++) {
+                        if (amount == k * VALUE_BANKNOTES_20 + j * VALUE_BANKNOTES_50 + i * VALUE_BANKNOTES_100) {
+                            isPossibleIssue = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        } else if (amount == totalAmountInAtm) {
+            isPossibleIssue = true;
+        }
+        if (isPossibleIssue) {
+            currentIssueAmount = amount;
+        }
+        return isPossibleIssue;
+    }
+
+    public int[][] getOptionsCombinationBanknotes(int amount) {
+        int totalNumberOfOptions = 0;
+        int[][] optionsCombinationBanknotes;
+        /* it's much better to use dynamic list of arrays in this case, but we
+        haven't learned this type of objects yet and therefor I use static 2D array */
+        for (int i = 0; i <= amount / VALUE_BANKNOTES_100 && i <= numberBanknotes100; i++) {
+            for (int j = 0; j <= amount / VALUE_BANKNOTES_50 && j <= numberBanknotes50; j++) {
+                for (int k = 0; k <= amount / VALUE_BANKNOTES_20 && k <= numberBanknotes20; k++) {
+                    if (amount == k * VALUE_BANKNOTES_20 + j * VALUE_BANKNOTES_50 + i * VALUE_BANKNOTES_100) {
+                            totalNumberOfOptions += 1;
+                    }
+                }
+            }
+        }
+        if (totalNumberOfOptions > 0) {
+            optionsCombinationBanknotes = new int[totalNumberOfOptions][3];
+            int numberOfOptions = 0;
+            for (int i = 0; i <= amount / VALUE_BANKNOTES_100 && i <= numberBanknotes100; i++) {
+                for (int j = 0; j <= amount / VALUE_BANKNOTES_50 && j <= numberBanknotes50; j++) {
+                    for (int k = 0; k <= amount / VALUE_BANKNOTES_20 && k <= numberBanknotes20; k++) {
+                        if (amount == k * VALUE_BANKNOTES_20 + j * VALUE_BANKNOTES_50 + i * VALUE_BANKNOTES_100) {
+                            optionsCombinationBanknotes[numberOfOptions][0] = k;
+                            optionsCombinationBanknotes[numberOfOptions][1] = j;
+                            optionsCombinationBanknotes[numberOfOptions][2] = i;
+                            numberOfOptions++;
+                        }
+                    }
+                }
+            }
+        } else {
+            optionsCombinationBanknotes = new int[1][3];
+        }
+        return optionsCombinationBanknotes;
+    }
+    public int[] getCash(int number) {
+        int[][] optionsOfCombinations = this.getOptionsCombinationBanknotes(currentIssueAmount);
+        int[] chosenOption = new int[optionsOfCombinations[0].length];
+        for (int i = 0; i < optionsOfCombinations[0].length; i++) {
+            chosenOption[i] = optionsOfCombinations[number-1][i];
+        }
+        numberBanknotes20 -= chosenOption[0];
+        numberBanknotes50 -= chosenOption[1];
+        numberBanknotes100 -= chosenOption[2];
+        currentIssueAmount = 0;
+        return chosenOption;
+    }
+}
+
---
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/AtmTests.java
@@ -0,0 +1,90 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AtmTests {
+    @Test
+    public void testAtm() {
+        Atm atm = new Atm(15, 10, 5);
+        Assertions.assertEquals(15, atm.getNumberBanknotes20());
+        Assertions.assertEquals(10, atm.getNumberBanknotes50());
+        Assertions.assertEquals(5, atm.getNumberBanknotes100());
+        Assertions.assertEquals(0, atm.getCurrentIssueAmount());
+    }
+    @Test
+    public void testAddBanknotes20() {
+        Atm atm = new Atm(15, 10, 5);
+        atm.addBanknotes20(1);
+        Assertions.assertEquals(16, atm.getNumberBanknotes20());
+        atm.addBanknotes20(10);
+        Assertions.assertEquals(26, atm.getNumberBanknotes20());
+    }
+    @Test
+    public void testAddBanknotes50() {
+        Atm atm = new Atm(15, 10, 5);
+        atm.addBanknotes50(1);
+        Assertions.assertEquals(11, atm.getNumberBanknotes50());
+        atm.addBanknotes50(10);
+        Assertions.assertEquals(21, atm.getNumberBanknotes50());
+    }
+    @Test
+    public void testAddBanknotes100() {
+        Atm atm = new Atm(15, 10, 5);
+        atm.addBanknotes100(1);
+        Assertions.assertEquals(6, atm.getNumberBanknotes100());
+        atm.addBanknotes100(10);
+        Assertions.assertEquals(16, atm.getNumberBanknotes100());
+    }
+    @Test
+    public void testIsPossibleIssueFalse() {
+        Atm atm = new Atm(15, 10, 5);
+        Assertions.assertEquals(false, atm.isPossibleIssue(1320));
+        Assertions.assertEquals(false, atm.isPossibleIssue(1290));
+        Assertions.assertEquals(false, atm.isPossibleIssue(30));
+        Assertions.assertEquals(false, atm.isPossibleIssue(0));
+    }
+    @Test
+    public void testIsPossibleIssueTrue() {
+        Atm atm = new Atm(15, 10, 5);
+        Assertions.assertEquals(true, atm.isPossibleIssue(1300));
+        Assertions.assertEquals(true, atm.isPossibleIssue(1280));
+        Assertions.assertEquals(true, atm.isPossibleIssue(280));
+        Assertions.assertEquals(true, atm.isPossibleIssue(20));
+    }
+    @Test
+    public void testGetOptionsCombinationBanknotes() {
+        Atm atm = new Atm(15, 10, 5);
+        int[][] optionsCombinationBanknotes280 = atm.getOptionsCombinationBanknotes(280);
+        for (int i = 0; i < optionsCombinationBanknotes280.length; i++) {
+            System.out.print("Вариант " + (i + 1) + ": ");
+            System.out.print(optionsCombinationBanknotes280[i][0] + " банкнот(ы) по 20; ");
+            System.out.print(optionsCombinationBanknotes280[i][1] + " банкнот(ы) по 50; ");
+            System.out.print(optionsCombinationBanknotes280[i][2] + " банкнот(ы) по 100. ");
+            System.out.println("");
+        }
+        Assertions.assertEquals(6, optionsCombinationBanknotes280.length);
+        int[][] optionsCombinationBanknotes1300 = atm.getOptionsCombinationBanknotes(1300);
+        Assertions.assertEquals(1, optionsCombinationBanknotes1300.length);
+        int[][] optionsCombinationBanknotes20 = atm.getOptionsCombinationBanknotes(20);
+        Assertions.assertEquals(1, optionsCombinationBanknotes20.length);
+        int[][] optionsCombinationBanknotes100 = atm.getOptionsCombinationBanknotes(100);
+        Assertions.assertEquals(3, optionsCombinationBanknotes100.length);
+    }
+    @Test
+    public void testGetCash() {
+        Atm atm = new Atm(15, 10, 5);
+        if (atm.isPossibleIssue(280)) {
+            int[] chosenOption = atm.getCash(6);
+            System.out.println("Получите деньги по выбранному Вами варианту: ");
+            System.out.print(chosenOption[0] + " банкнот(ы) по 20; ");
+            System.out.print(chosenOption[1] + " банкнот(ы) по 50; ");
+            System.out.println(chosenOption[2] + " банкнот(ы) по 100; ");
+            System.out.println("Всего к выдаче: " + (chosenOption[0] * atm.VALUE_BANKNOTES_20 + chosenOption[1] * atm.VALUE_BANKNOTES_50 + chosenOption[2] * atm.VALUE_BANKNOTES_100) + "." );
+            Assertions.assertEquals(11, atm.getNumberBanknotes20());
+            Assertions.assertEquals(10, atm.getNumberBanknotes50());
+            Assertions.assertEquals(3, atm.getNumberBanknotes100());
+            Assertions.assertEquals(0, atm.getCurrentIssueAmount());
+        }
+    }
+}
