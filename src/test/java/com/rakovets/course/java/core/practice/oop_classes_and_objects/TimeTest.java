4399aff3e46b327809413727d0baf44be90f1ee9
Dzmitry_Highlander <strixmymail@gmail.com>
2022-10-02 07:15:01 +0300

    feat: refactor TimeTest

--- src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/TimeTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/TimeTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 @DisplayName("Time")
 public class TimeTest {
-    static Stream<Arguments> provideArguments() {
+    static Stream<Arguments> provideArguments1() {
         return Stream.of(
                 Arguments.of(11416, 11416),
                 Arguments.of(9000, 9000),
@@ -22,12 +22,30 @@ public class TimeTest {
         );
     }
 
+    static Stream<Arguments> provideArguments2() {
+        return Stream.of(
+                Arguments.of(1, 1, 1, 3661),
+                Arguments.of(2, 2, 2, 7322),
+                Arguments.of(3, 3, 3, 10983)
+        );
+    }
+
     @ParameterizedTest(name = "Total seconds: {0}")
-    @MethodSource("provideArguments")
-    @DisplayName("Total seconds")
+    @MethodSource("provideArguments1")
+    @DisplayName("Total seconds #1")
     void totalSecondsTestViaIntTotalSeconds(int totalSeconds, int expected) {
-        Time test = new Time(totalSeconds);
-        int actual = test.getTotalSeconds();
+        Time test1 = new Time(totalSeconds);
+        int actual = test1.getTotalSeconds();
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest(name = "Total seconds: {0}")
+    @MethodSource("provideArguments2")
+    @DisplayName("Total seconds #2")
+    void totalSecondsTest(int hours, int minutes, int seconds, int expected) {
+        Time test2 = new Time(hours, minutes, seconds);
+        int actual = test2.getTotalSeconds();
 
         assertEquals(expected, actual);
     }
