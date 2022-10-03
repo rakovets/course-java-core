4675b049ec8179e387a1f4b3089b9f036d8c5a4f
Hanna Matveyeva <jessikane4ka@gmail.com>
2022-10-03 15:18:04 +0300

    feature: add solution task03 lesson 7

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/Atm.java
@@ -0,0 +1,25 @@
+package com.rakovets.course.java.core.practice.oop_principles;
+
+public class Atm {
+    public int numberBanknotes20;
+    public int numberBanknotes50;
+    public int numberBanknotes100;
+
+    public Atm(int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) {
+        this.numberBanknotes20 = numberBanknotes20;
+        this.numberBanknotes50 = numberBanknotes50;
+        this.numberBanknotes100 = numberBanknotes100;
+    }
+
+    public int addNumberBanknotes20() {
+        return numberBanknotes20;
+    }
+
+    public int addNumberBanknotes50() {
+        return numberBanknotes50;
+    }
+
+    public int addNumberBanknotes100() {
+        return numberBanknotes100;
+    }
+}
