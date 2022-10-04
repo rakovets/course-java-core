7f134a4122e414c975bf8c77983db1cc5dd73fa0
Oleg Aleksashov <Fairp87@gmail.com>
2022-10-04 20:13:57 +0300

    fix: Class Sphynx in  Project: 'Cat's home' OOP. Principles

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/Sphynx.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/Sphynx.java
@@ -1,6 +1,6 @@
 package com.rakovets.course.java.core.practice.oop_principles;
 
-public class Sphynx {
+public class Sphynx extends Cat {
 
     public String mew() {
         return "My name Sphynx!";
