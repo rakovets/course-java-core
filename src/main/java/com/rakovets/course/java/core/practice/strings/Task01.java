bd4763ffc609bcadf39ac309caf877c457caa372
Hanna Kavaliova <a.kovaleva@21vek.by>
2022-10-05 17:56:03 +0300

    feat: add task01

--- src/main/java/com/rakovets/course/java/core/practice/strings/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/strings/Task01.java
@@ -33,9 +33,8 @@ class Task01 {
      * @return имя сотрудника
      */
     static String getName(String fullName) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String[] nameLastname = fullName.split(" ");
+        return nameLastname[0];
     }
 
     /**
@@ -45,9 +44,8 @@ class Task01 {
      * @return фамилия сотрудника
      */
     static String getSurname(String fullName) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String[] nameLastname = fullName.split(" ");
+        return nameLastname[1];
     }
 
     /**
@@ -57,8 +55,7 @@ class Task01 {
      * @return полное имя сотрудника в обратном формате, т.е. '${surname} ${name}'
      */
     static String reverseFullName(String fullName) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String[] nameLastname = fullName.split(" ");
+        return nameLastname[1] + " " + nameLastname[0];
     }
 }
