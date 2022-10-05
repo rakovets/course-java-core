7c650fae57dfafb688da387d942c2c6a3503f6c9
Oleg Aleksashov <Fairp87@gmail.com>
2022-10-05 18:32:06 +0300

    fix: Class Cat in  Project: 'Cat's home' OOP. Principles

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/Cat.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/Cat.java
@@ -1,62 +1,63 @@
 package com.rakovets.course.java.core.practice.oop_principles;
 
-/*
-Note
-Для каждого созданного класса, нужно создать тестовые классы,
-которые тестирую работу ВСЕХ конструкторов и методов.
-Warning
-ТОЛЬКО тестовые классы могут содержать вывод в Standard Output (консольный вывод).
-Note
-В процессе разработки код постоянно модифицируется (удаляется, добавляется, изменяется).
-Note
-При разработке иерархии держать в уме принцип инкапсуляции,
-выбирать корректные имена классов и методов, пользоваться преимуществами полиморфизма.
-Note
-Отдавайте предпочтение интерфейсам, а не абстрактным классам.
-Project: 'Cat’s home'
-
-Specification of task 1
-Создать класс Cat.
-Создать Fields:
-name – кличка кота
-Создать Constructors:
-Cat(name)
-Создать Methods:
-mew() – кот мяукает (т.е возвращает "текст" мяуканья)
-purr() – кот мурлычет (т.е возвращает "текст" мурчания)
-getName() – получить кличку кота
-setName(name) – задать кличку кота
-
-Specification of task 2
-Создать классы Siamese, Persian, Sphynx наследуемые от класса Cat.
-Переопределить в них methods:
-mew() – кот мяукает (каждый по разному)
-purr() – кот мурлычет (каждый по разному)
-
-Specification of task 3
-Создать класс Person.
-Создать Fields:
-happiness - количество счастья у человека (в процентах)
-Создать Constructors:
-Person(happiness) - принимает количество счастья
-Создать Methods:
-changeHappiness(percentHappiness) - изменение счастья на percentHappiness
-(может быть как положительное, так и отрицательное)
-getHappiness() - получить количество счастья
-setHappiness(happiness) - задать количество счастья
-
-Specification of task 4
-Перегрузить в классе Cat методы:
-mew(Person) - кот мяукает для User, вызывает у него метод changeHappiness(percentHappiness),
-где percentHappiness отрицательное число
-purr(Person) - кот мурлычет для User, вызывает у него метод changeHappiness(percentHappiness),
-где percentHappiness положительное число
-
-Specification of task 5
-Переопределить в классах Siamese, Persian, Sphynx методы:
-mew(Person) - каждый кот по разному воздействует на счастье человека мяуканьем
-purr(Person) - каждый кот по разному воздействует на счастье человека когда мурлыкает
+/**
+ * Note
+ * Для каждого созданного класса, нужно создать тестовые классы,
+ * которые тестирую работу ВСЕХ конструкторов и методов.
+ * Warning
+ * ТОЛЬКО тестовые классы могут содержать вывод в Standard Output (консольный вывод).
+ * Note
+ * В процессе разработки код постоянно модифицируется (удаляется, добавляется, изменяется).
+ * Note
+ * При разработке иерархии держать в уме принцип инкапсуляции,
+ * выбирать корректные имена классов и методов, пользоваться преимуществами полиморфизма.
+ * Note
+ * Отдавайте предпочтение интерфейсам, а не абстрактным классам.
+ * Project: 'Cat’s home'
+ *
+ * Specification of task 1
+ * Создать класс Cat.
+ * Создать Fields:
+ * name – кличка кота
+ * Создать Constructors:
+ * Cat(name)
+ * Создать Methods:
+ * mew() – кот мяукает (т.е возвращает "текст" мяуканья)
+ * purr() – кот мурлычет (т.е возвращает "текст" мурчания)
+ * getName() – получить кличку кота
+ * setName(name) – задать кличку кота
+ *
+ * Specification of task 2
+ * Создать классы Siamese, Persian, Sphynx наследуемые от класса Cat.
+ * Переопределить в них methods:
+ * mew() – кот мяукает (каждый по разному)
+ * purr() – кот мурлычет (каждый по разному)
+ *
+ * Specification of task 3
+ * Создать класс Person.
+ * Создать Fields:
+ * happiness - количество счастья у человека (в процентах)
+ * Создать Constructors:
+ * Person(happiness) - принимает количество счастья
+ * Создать Methods:
+ * changeHappiness(percentHappiness) - изменение счастья на percentHappiness
+ * (может быть как положительное, так и отрицательное)
+ * getHappiness() - получить количество счастья
+ * setHappiness(happiness) - задать количество счастья
+ *
+ * Specification of task 4
+ * Перегрузить в классе Cat методы:
+ * mew(Person) - кот мяукает для User, вызывает у него метод changeHappiness(percentHappiness),
+ * где percentHappiness отрицательное число
+ * purr(Person) - кот мурлычет для User, вызывает у него метод changeHappiness(percentHappiness),
+ * где percentHappiness положительное число
+ *
+ * Specification of task 5
+ * Переопределить в классах Siamese, Persian, Sphynx методы:
+ * mew(Person) - каждый кот по разному воздействует на счастье человека мяуканьем
+ * purr(Person) - каждый кот по разному воздействует на счастье человека когда мурлыкает
  */
+
 public class Cat {
     public String name;
 
