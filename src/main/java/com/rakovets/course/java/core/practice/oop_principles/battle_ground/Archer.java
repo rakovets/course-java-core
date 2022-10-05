32edf80db3d3aa958d1d607c7f9a88abe262c92f
Kate Komleva <komlevakate99@gmail.com>
2022-10-05 19:08:12 +0300

    feat: add classes for Battle ground

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Archer.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
+
+public class Archer extends Hero {
+    public Archer(String name) {
+        super(name);
+    }
+}
---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Hero.java
@@ -0,0 +1,19 @@
+package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
+
+public class Hero {
+    private String name;
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public String attackEnemy(Enemy enemy) {
+        int damage = 5;
+        enemy.takeDamage(damage);
+        return "Attacks enemy!";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
