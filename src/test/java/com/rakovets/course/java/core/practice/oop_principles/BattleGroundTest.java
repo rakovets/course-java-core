c01d779498e993cb289ce18b65e1597f542da5d1
Kate Komleva <komlevakate99@gmail.com>
2022-10-07 14:04:18 +0300

    feat: add "Battle ground" test

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop_principles/BattleGroundTest.java
@@ -0,0 +1,197 @@
+package com.rakovets.course.java.core.practice.oop_principles;
+
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.enemies.Demon;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.enemies.Enemy;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.enemies.Werewolf;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.enemies.Zombie;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.heroes.Archer;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.heroes.Hero;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.heroes.Mag;
+import com.rakovets.course.java.core.practice.oop_principles.battle_ground.heroes.Warrior;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BattleGroundTest {
+    public static void main(String[] args) {
+        Warrior warrior = new Warrior("Gingo", 300, 50);
+        Mag wizard = new Mag("Herald", 250, 30);
+        Archer archer = new Archer("Sam", 300, 5);
+        Zombie zombie = new Zombie("Groat", 200, 2);
+        Demon demon = new Demon("Baltazar", 400, 2);
+        Werewolf werewolf = new Werewolf("Jakob", 250);
+
+        System.out.println("Let's attack begin!");
+        System.out.println(warrior.getName() + " " + warrior.attackEnemy(demon));
+        System.out.println(demon.getName() + " health: " + demon.getHealth());
+        System.out.println(demon.getName() + " " + demon.attackHero(warrior));
+        System.out.println(warrior.getName() + " Hero health: " + warrior.getHealth());
+        System.out.println("Hero armor: " + warrior.getArmor());
+        System.out.println(wizard.getName() + " " + wizard.attackEnemy(zombie));
+        System.out.println(zombie.getName() + " health: " + zombie.getHealth());
+        System.out.println(zombie.getName() + " " + zombie.attackHero(wizard));
+        System.out.println(wizard.getName() + " health: " + wizard.getHealth());
+        System.out.println(archer.getName() + " " + archer.attackEnemy(werewolf));
+        System.out.println(werewolf.getName() + " health: " + werewolf.getHealth());
+        System.out.println(werewolf.getName() + " " + werewolf.attackHero(archer));
+        System.out.println(archer.getName() + " health: " + archer.getHealth());
+        System.out.println(warrior.getName() + " " + warrior.attackEnemy(werewolf));
+        System.out.println(werewolf.getName() + " health: " + werewolf.getHealth());
+        System.out.println(werewolf.getName() + " " + werewolf.clawedHit(warrior));
+        System.out.println(warrior.getName() + " health: " + warrior.getHealth());
+        System.out.println("Hero armor: " + warrior.getArmor());
+        System.out.println(wizard.getName() + " " + wizard.attackEnemy(demon));
+        System.out.println(demon.getName() + " health: " + demon.getHealth());
+        System.out.println(demon.getName() + " " + demon.attackWithHellfire(wizard));
+        System.out.println(wizard.getName() + " health: " + wizard.getHealth());
+        wizard.makeHealingSpell();
+        wizard.makeHealingSpell(warrior);
+        System.out.println(wizard.getName() + " health after healing spell: " + wizard.getHealth());
+        System.out.println(warrior.getName() + " health after healing spell: " + warrior.getHealth());
+        System.out.println(wizard.getName() + " mana after healing spell: " + wizard.getMana());
+        System.out.println(archer.getName() + " " + archer.fireArrowAttack(zombie));
+        System.out.println(zombie.getName() + " health: " + zombie.getHealth());
+    }
+
+    static Stream<Arguments> provideArgumentsForAttackEnemy() {
+        return Stream.of(
+                Arguments.of(new Warrior("Gingo", 300, 50), new Demon("Baltazar", 400, 2), 380.0),
+                Arguments.of(new Archer("Sam", 300, 5), new Zombie("Groat", 200, 2), 187.0),
+                Arguments.of(new Mag("Herald", 250, 30), new Werewolf("Jakob", 250), 240.0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForAttackEnemy")
+    void testForAttackEnemy(Hero hero, Enemy enemy, double expected) {
+        hero.attackEnemy(enemy);
+        double actual = enemy.getHealth();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsForAttackHero() {
+        return Stream.of(
+                Arguments.of(new Warrior("Gingo", 300, 50), new Demon("Baltazar", 380, 2), 285.0),
+                Arguments.of(new Warrior("Gingo", 300, 10), new Demon("Baltazar", 380, 2), 285.0),
+                Arguments.of(new Warrior("Gingo", 300, 0), new Demon("Baltazar", 380, 2), 270.0),
+                Arguments.of(new Archer("Sam", 300, 5), new Zombie("Groat", 187, 2), 285.0),
+                Arguments.of(new Mag("Herald", 250, 30), new Werewolf("Jakob", 240), 235.0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForAttackHero")
+    void testForAttackHero(Hero hero, Enemy enemy, double expected) {
+        enemy.attackHero(hero);
+        double actual = hero.getHealth();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsForHealing() {
+        return Stream.of(
+                Arguments.of(new Mag("Herald", 250, 30), 260.0),
+                Arguments.of(new Mag("Herald", 250, 0), 250.0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsForHealing")
+    void testForHealing(Mag mag, double expected) {
+        mag.makeHealingSpell();
+        double actual = mag.getHealth();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsFireArrow() {
+        return Stream.of(
+                Arguments.of(new Archer("Sam", 300, 5), new Zombie("Groat", 187, 2), 167.0),
+                Arguments.of(new Archer("Sam", 300, 3), new Zombie("Groat", 167, 2), 147.0),
+                Arguments.of(new Archer("Sam", 300, 0), new Zombie("Groat", 187, 2), 187.0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsFireArrow")
+    void testForFireArrow(Archer arch, Enemy enemy, double expected) {
+        arch.fireArrowAttack(enemy);
+        double actual = enemy.getHealth();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsZombie() {
+        return Stream.of(
+                Arguments.of(new Zombie("Groam", 250, 2), 150.0),
+                Arguments.of(new Zombie("Groam", 100, 2), 50.0),
+                Arguments.of(new Zombie("Groam", 50, 2), 25.0),
+                Arguments.of(new Zombie("Groam", 50, 0), -50.0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsZombie")
+    void testZombieGetAlive(Zombie zombie, double expected) {
+        zombie.takeDamage(100);
+        zombie.getAlive();
+        double actual = zombie.getHealth();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsAttackWithHellfire() {
+        return Stream.of(
+                Arguments.of(new Demon("Devil", 400, 2), new Archer("Sam", 300, 5), 240.0),
+                Arguments.of(new Demon("Devil", 400, 1), new Archer("Sam", 240, 5), 180.0),
+                Arguments.of(new Demon("Devil", 400, 0), new Archer("Sam", 180, 5), 180.0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsAttackWithHellfire")
+    void testAttackWithHellfire(Demon demon, Hero hero, double expected) {
+        demon.attackWithHellfire(hero);
+        double actual = hero.getHealth();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsWerewolfHealth() {
+        return Stream.of(
+                Arguments.of(new Werewolf("Jake", 250), 150.0),
+                Arguments.of(new Werewolf("Jake", 120), 25.0),
+                Arguments.of(new Werewolf("Jake", 0), 0.0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsWerewolfHealth")
+    void testWerewolfHealth(Werewolf werewolf, double expected) {
+        werewolf.takeDamage(100);
+        double actual = werewolf.getHealth();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideArgumentsClawedHit() {
+        return Stream.of(
+                Arguments.of(new Werewolf("Jake", 250), new Mag("Herald", 250, 30), 230.0),
+                Arguments.of(new Werewolf("Jake", 250), new Mag("Herald", 100, 30), 80.0),
+                Arguments.of(new Werewolf("Jake", 250), new Mag("Herald", 20, 30), 0.0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideArgumentsClawedHit")
+    void testClawedHit(Werewolf werewolf, Hero hero, double expected) {
+        werewolf.clawedHit(hero);
+        double actual = hero.getHealth();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
