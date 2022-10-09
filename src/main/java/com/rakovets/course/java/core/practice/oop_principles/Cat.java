f9fc574158aeb9fdfb7b59d7f286476698877130
Vict Pol <vpolik10@gmail.com>
2022-10-09 17:49:27 +0200

    feat: update Cat.java file

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/Cat.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/Cat.java
@@ -1,4 +1,25 @@
 package com.rakovets.course.java.core.practice.oop_principles;
 
 public class Cat {
+    private String name;
+
+    Cat(String name) {
+        this.name = name;
+    }
+
+    public void mew() {
+        System.out.println("mjau");
+    }
+
+    public void purr() {
+        System.out.println("murr");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
