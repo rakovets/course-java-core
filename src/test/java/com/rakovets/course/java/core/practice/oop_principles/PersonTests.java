10f822b64e76cac462962cba59ca55b15eff3e77
Vict Pol <vpolik10@gmail.com>
2022-10-09 20:01:34 +0200

    feat: update test file

--- src/test/java/com/rakovets/course/java/core/practice/oop_principles/PersonTests.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_principles/PersonTests.java
@@ -1,4 +1,19 @@
 package com.rakovets.course.java.core.practice.oop_principles;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PersonTests {
+    Person person = new Person(200);
+
+    @Test
+    public void testPerson() {
+        Assertions.assertEquals(200, person.getHappiness());
+    }
+
+    @Test
+    public void testChangeHappiness() {
+        person.changeHappiness(-10);
+        Assertions.assertEquals(180, person.getHappiness());
+    }
 }
