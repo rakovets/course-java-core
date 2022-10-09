0d1639d83c5dc14a6e078a26e70c253ec880213b
Vict Pol <vpolik10@gmail.com>
2022-10-09 18:16:33 +0200

    feat: update CatTests.java file

--- src/test/java/com/rakovets/course/java/core/practice/oop_principles/CatTests.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_principles/CatTests.java
@@ -1,4 +1,29 @@
 package com.rakovets.course.java.core.practice.oop_principles;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CatTests {
+    Cat cat = new Cat("Tosha");
+
+    @Test
+    public void testCat() {
+        Assertions.assertEquals("Tosha", cat.getName());
+    }
+
+    @Test
+    public void testMew() {
+        System.out.println("mjau");
+    }
+
+    @Test
+    public void testPurr() {
+        System.out.println("murr");
+    }
+
+    @Test
+    public void testSetName() {
+        cat.setName("Kosha");
+        Assertions.assertEquals("Kosha", cat.getName());
+    }
 }
