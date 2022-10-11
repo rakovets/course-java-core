05768aa6dde63f2f1ce31667b2dbb456b1775ad4
Kate Komleva <komlevakate99@gmail.com>
2022-10-11 21:10:25 +0300

    feat: fixed Cats

--- src/test/java/com/rakovets/course/java/core/practice/oop_principles/CatTests.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_principles/CatTests.java
@@ -6,7 +6,6 @@ import com.rakovets.course.java.core.practice.oop_principles.cat.cats.Persian;
 import com.rakovets.course.java.core.practice.oop_principles.cat.cats.Siamese;
 import com.rakovets.course.java.core.practice.oop_principles.cat.cats.Sphynx;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
