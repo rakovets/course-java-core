d83707b52b745c9408c82c18587cc9ef4f025435
Oleg Aleksashov <Fairp88@tut.by>
2022-10-11 23:59:40 +0300

    Update src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Alligator.java
    Co-authored-by: Dmitry Rakovets <dmitryrakovets@gmail.com>

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Alligator.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Alligator.java
@@ -1,7 +1,6 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
 
 public class Alligator extends Enemy {
-
     public Alligator(int health) {
         super(health);
     }
