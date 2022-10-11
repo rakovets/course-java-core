29a26bbb9057bac17fc924b87f206baf53cd0543
Oleg Aleksashov <Fairp88@tut.by>
2022-10-11 23:46:42 +0300

    Update src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Warrior.java
    Co-authored-by: Dmitry Rakovets <dmitryrakovets@gmail.com>

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Warrior.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Warrior.java
@@ -1,7 +1,6 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
 
 public class Warrior extends Hero {
-
     public Warrior(String name, int health) {
         super(name, health);
     }
