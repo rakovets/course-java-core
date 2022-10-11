45e5b0afe3e5427a81a98695be957734571b5733
Oleg Aleksashov <Fairp88@tut.by>
2022-10-11 23:56:45 +0300

    Update src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Mag.java
    Co-authored-by: Dmitry Rakovets <dmitryrakovets@gmail.com>

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Mag.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Mag.java
@@ -1,7 +1,6 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
 
 public class Mag extends Hero {
-
     public Mag(String name, int health) {
         super(name, health);
     }
