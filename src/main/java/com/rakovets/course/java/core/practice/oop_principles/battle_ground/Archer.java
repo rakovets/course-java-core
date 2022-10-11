a0b468430e41b899a870c610b981024b1b1482d0
Oleg Aleksashov <Fairp88@tut.by>
2022-10-12 00:04:30 +0300

    Update src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Archer.java
    Co-authored-by: Dmitry Rakovets <dmitryrakovets@gmail.com>

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Archer.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Archer.java
@@ -1,7 +1,6 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
 
 public class Archer extends Hero {
-
     public Archer(String name, int health) {
         super(name, health);
     }
