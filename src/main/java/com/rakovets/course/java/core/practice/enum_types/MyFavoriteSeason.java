0784ea570952c1b755332bbd803c7f7608060565
Nikita Egorov <egorovnikita2020@gmail.com>
2022-10-14 15:20:12 +0700

    feat: add all solutions for enum

---
+++ src/main/java/com/rakovets/course/java/core/practice/enum_types/MyFavoriteSeason.java
@@ -0,0 +1,9 @@
+package com.rakovets.course.java.core.practice.enum_types;
+
+public class MyFavoriteSeason {
+    public static void main(String[] args) {
+        Season myFavSeason = Season.SPRING;
+        System.out.println("My favorite season is " + myFavSeason + " because it is quite warm and sunny");
+    }
+
+}
