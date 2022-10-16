4c7a144705e351b9ad99825266f7dd736278778d
Hanna Kavaliova <a.kovaleva@21vek.by>
2022-10-16 14:10:23 +0300

    feat: add solution Task07 and TaskDemo07

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task07.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task07.java
@@ -1,4 +1,13 @@
 package com.rakovets.course.java.core.practice.exception_handling;
 
 public class Task07 {
+    public int divisionTwoNumbers(int firstNumber, int secondNumber) throws Exception {
+        if (secondNumber == 0) {
+            throw new ArithmeticException("Деление на ноль недопустимо");
+        }
+        if (firstNumber == 0) {
+            throw new Exception("Ответ всегда равен 0");
+        }
+        return firstNumber / secondNumber;
+    }
 }
--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/Task07Demo.java
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/Task07Demo.java
@@ -1,4 +1,15 @@
 package com.rakovets.course.java.core.practice.exception_handling;
 
 public class Task07Demo {
+    public static void main(String[] args) {
+        Task07 task07 = new Task07();
+        try {
+            task07.divisionTwoNumbers(9, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.print("");
+        }
+    }
 }
