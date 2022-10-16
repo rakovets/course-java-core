ecf85806b2a7cc89bd78cc7edffe0e82edd266e7
Aliaksei Kharko <aliakseikharko@gmail.com>
2022-10-16 16:13:24 +0300

    feat: add done Task04 from Exception handling

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task04.java
@@ -0,0 +1,13 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+import com.rakovets.course.java.core.practice.exception_handling.exception.ZeroCheckException;
+
+public class Task04 {
+    public int calculatorDividing(int dividend, int divisor) throws ZeroCheckException {
+        if (divisor == 0) {
+            throw new ZeroCheckException("You cannot divide by zero!");
+        } else {
+            return dividend / divisor;
+        }
+    }
+}
