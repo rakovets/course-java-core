08ca538d036c570979a79d2a744020b608769ea6
Aliaksei Kharko <aliakseikharko@gmail.com>
2022-10-16 16:18:33 +0300

    feat: add JavaUtilLogging from Exception handling

---
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/JavaUtilLogging/JavaUtilLogging.java
@@ -0,0 +1,7 @@
+package com.rakovets.course.java.core.practice.exception_handling.JavaUtilLogging;
+
+import java.util.logging.Logger;
+
+public class JavaUtilLogging {
+    public static final Logger logger = Logger.getLogger(JavaUtilLogging.class.getName());
+}
