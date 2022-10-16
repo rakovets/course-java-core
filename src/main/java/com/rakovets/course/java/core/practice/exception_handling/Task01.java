37abcb23ef116029cae5314d0bc59d95b16491d5
Aliaksei Kharko <aliakseikharko@gmail.com>
2022-10-16 16:12:48 +0300

    feat: add done Task01 from Exception handling

---
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/Task01.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.exception_handling;
+
+public class Task01 {
+    public int countNumberCharactersInString() {
+        String str = null;
+        return str.length();
+    }
+}
