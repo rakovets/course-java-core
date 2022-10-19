22f03dff80e0bb8077de7f3708d16566f9e7bdad
KateKoml <komlevakate99@gmail.com>
2022-10-19 08:40:30 +0300

    Update src/main/java/com/rakovets/course/java/core/practice/exception_handling/task3/MarkException.java
    fix: code convention
    
    Co-authored-by: Dmitry Rakovets <dmitryrakovets@gmail.com>

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/task3/MarkException.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/task3/MarkException.java
@@ -1,6 +1,6 @@
 package com.rakovets.course.java.core.practice.exception_handling.task3;
 
-public class MarkException extends Exception{
+public class MarkException extends Exception {
     public MarkException(String massage) {
         super(massage);
     }
