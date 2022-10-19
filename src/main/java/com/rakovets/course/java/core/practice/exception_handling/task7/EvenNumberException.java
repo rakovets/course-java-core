929ef432579f798cdb7c120a4c94ee797bc93373
KateKoml <komlevakate99@gmail.com>
2022-10-19 08:42:20 +0300

    Update src/main/java/com/rakovets/course/java/core/practice/exception_handling/task7/EvenNumberException.java
    fix: code convention
    
    Co-authored-by: Dmitry Rakovets <dmitryrakovets@gmail.com>

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/task7/EvenNumberException.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/task7/EvenNumberException.java
@@ -1,6 +1,6 @@
 package com.rakovets.course.java.core.practice.exception_handling.task7;
 
-public class EvenNumberException extends Exception{
+public class EvenNumberException extends Exception {
     public EvenNumberException(String message) {
         super(message);
     }
