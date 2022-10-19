aae9c222d39cd14bcc6d905ce02cc2e6164fa7e3
KateKoml <komlevakate99@gmail.com>
2022-10-19 08:42:07 +0300

    Update src/main/java/com/rakovets/course/java/core/practice/exception_handling/task5/ArrayException.java
    fix: code convention
    
    Co-authored-by: Dmitry Rakovets <dmitryrakovets@gmail.com>

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/task5/ArrayException.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/task5/ArrayException.java
@@ -1,6 +1,6 @@
 package com.rakovets.course.java.core.practice.exception_handling.task5;
 
-public class ArrayException extends Exception{
+public class ArrayException extends Exception {
     public ArrayException(String message) {
         super(message);
     }
