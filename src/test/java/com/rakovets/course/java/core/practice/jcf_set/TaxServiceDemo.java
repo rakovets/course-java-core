774e092b5f8687802ebc604a89a4cb47997230f7
Kate Komleva <komlevakate99@gmail.com>
2022-10-24 18:18:07 +0300

    fix: modified TaxServiceDemo

--- src/test/java/com/rakovets/course/java/core/practice/jcf_set/TaxServiceDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/jcf_set/TaxServiceDemo.java
@@ -6,9 +6,9 @@ public class TaxServiceDemo {
     public static void main(String[] args) {
         LinkedHashSet<Person> taxPayer = new LinkedHashSet<>();
         TaxService taxService = new TaxService(taxPayer);
-        Person kate = new Person("Kate Komleva", City.Minsk, List.of(FineType.FINE181, FineType.FINE1811), 10091999);
-        Person liza = new Person("Liza Benitsrvich", City.Minsk, List.of(FineType.FINE1812), 8022000);
-        Person georg = new Person("Georg Shpendel", City.Mogilev, List.of(FineType.FINE181, FineType.FINE1815, FineType.FINE1814), 23041985);
+        Person kate = new Person("Kate Komleva", City.Minsk, new ArrayList<>(Arrays.asList(FineType.FINE181, FineType.FINE1811)), 10091999);
+        Person liza = new Person("Liza Benitsrvich", City.Minsk, new ArrayList<>(Arrays.asList(FineType.FINE1815)), 8022000);
+        Person georg = new Person("Georg Shpendel", City.Mogilev, new ArrayList<>(Arrays.asList(FineType.FINE1815, FineType.FINE1811)), 23041985);
         taxService.addNewTaxPayer(kate);
         taxService.addNewTaxPayer(liza);
         taxService.addNewTaxPayer(georg);
@@ -22,6 +22,8 @@ public class TaxServiceDemo {
         System.out.println("Show by city");
         System.out.println(taxService.showByCity(City.Minsk));
         System.out.println(taxService.getInfoAboutTaxPayer(liza));
+        taxService.deleteFineType(FineType.FINE181, 10091999);
+        System.out.println(kate);
         taxService.addFineType(FineType.FINE1814, 8022000);
         System.out.println(liza);
     }
