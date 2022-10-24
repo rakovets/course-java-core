bc89d0a40ddadee9dbf8cf10e5a3b8af52cefb72
Kate Komleva <komlevakate99@gmail.com>
2022-10-24 09:15:47 +0300

    fix: solution for project Text Monitoring

---
+++ src/test/java/com/rakovets/course/java/core/practice/jcf_set/TaxServiceDemo.java
@@ -0,0 +1,23 @@
+package com.rakovets.course.java.core.practice.jcf_set;
+
+import com.rakovets.course.java.core.practice.jcf_list.City;
+import com.rakovets.course.java.core.practice.jcf_list.FineType;
+import com.rakovets.course.java.core.practice.jcf_list.Person;
+import com.rakovets.course.java.core.practice.jcf_list.TaxService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class TaxServiceDemo {
+    public static void main(String[] args) {
+        TaxService taxService = new TaxService();
+        Person kate = new Person("Kate Komleva", City.Minsk, List.of(FineType.FINE181, FineType.FINE1811),10091999);
+        Person liza = new Person("Liza Benitsrvich", City.Grodno, List.of(FineType.FINE1812),8022000);
+        Person georg = new Person("Georg Shpendel", City.Mogilev, List.of(FineType.FINE181, FineType.FINE1815, FineType.FINE1814),23041985);
+        taxService.addNewTaxPayer(kate);
+        taxService.addNewTaxPayer(liza);
+        taxService.addNewTaxPayer(georg);
+        System.out.println(taxService.getDatabase());
+    }
+}
