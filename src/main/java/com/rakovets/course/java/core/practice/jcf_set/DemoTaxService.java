359a09494af3ceac54b9d1443ccbda5eed6d4f05
Vict Pol <vpolik10@gmail.com>
2022-10-25 15:46:42 +0200

    feat: update Demo file with done tasks for Tax service project

--- src/main/java/com/rakovets/course/java/core/practice/jcf_set/DemoTaxService.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_set/DemoTaxService.java
@@ -7,20 +7,9 @@ import com.rakovets.course.java.core.practice.jcf_set.models.Payer;
 import com.rakovets.course.java.core.practice.jcf_set.models.TaxBase;
 
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class DemoTaxService {
     public static void main(String[] args) {
-        Set<Payer> payers = new HashSet<>();
-        payers.add(new Payer("0001AA7", "Victor Polonnikov", CityName.MINSK, new HashSet<>(List.of(new Fine("24102022_7_001", FineType.FINE_1)))));
-        System.out.println(payers);
-        for (Payer p : payers) {
-            if (p.getPayerId() == "0001AA7") {
-                p.getFines().add(new Fine("24102022_7_002", FineType.FINE_2));
-                p.getFines().add(new Fine("24102022_7_003", FineType.FINE_3));
-            }
-        }
         TaxBase taxBase = new TaxBase(new HashSet<Payer>());
         boolean addPayer = taxBase.addPayer("0001AB5", "Andrew Belov", CityName.MINSK_REGION, "25102022_5_001", FineType.FINE_4);
         System.out.println(taxBase.getTaxBase());
