41947a6605bcc1e4bba493eb250087d4b7e6f4c3
Oleg Aleksashov <Fairp87@gmail.com>
2022-10-30 21:18:19 +0300

    feat: adding solutions for Date Wrapper & TestDateWrapper

---
+++ src/main/java/com/rakovets/course/java/core/practice/date_and_time/date_wrapper/DateWrapper.java
@@ -0,0 +1,30 @@
+package com.rakovets.course.java.core.practice.date_and_time.date_wrapper;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+public class DateWrapper {
+    public LocalDate getLocalDate(Integer year, Integer month, Integer day) {
+        return LocalDate.of(year, month, day);
+    }
+
+    public LocalDate getLocalDateAfterSeveralMonth(LocalDate now, Integer severalMonth) {
+        return now.plusMonths(severalMonth);
+    }
+
+    public String getFormatPattern(LocalDate localDate, DateTimeFormatter formatter){
+        return localDate.format(formatter);
+    }
+
+    public LocalDate getStringFormat(String time, DateTimeFormatter formatter){
+        return LocalDate.parse(time, formatter);
+    }
+
+    public int getAmountDay(LocalDate localDate, LocalDate localDate1){
+        return Math.toIntExact(DAYS.between(localDate, localDate1));
+    }
+}
---
+++ src/test/java/com/rakovets/course/java/core/practice/date_and_time/TestDateWrapper.java
@@ -0,0 +1,98 @@
+package com.rakovets.course.java.core.practice.date_and_time;
+
+import com.rakovets.course.java.core.practice.date_and_time.date_wrapper.DateWrapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Stream;
+
+public class TestDateWrapper {
+    static Stream<Arguments> provideFromGetLocalDate() {
+        return Stream.of(Arguments.of(2014, 4, 25, LocalDate.of(2014, 4, 25)),
+                Arguments.of(2017, 5, 12, LocalDate.of(2017, 5, 12)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFromGetLocalDate")
+    public void getLocalDate(Integer year, Integer month, Integer day, LocalDate expected) {
+        DateWrapper dateWrapper = new DateWrapper();
+
+        LocalDate actual = dateWrapper.getLocalDate(year, month, day);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideFromGetAmountDay() {
+        return Stream.of(Arguments.of(LocalDate.of(2014, 4, 25)
+                        , LocalDate.of(2017, 5, 12), 1113),
+                Arguments.of(LocalDate.of(2014, 4, 25)
+                        , LocalDate.of(2014, 4, 27), 2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFromGetAmountDay")
+    public void getLocalDate(LocalDate localDate, LocalDate localDate1, int expected) {
+        DateWrapper dateWrapper = new DateWrapper();
+
+        int actual = dateWrapper.getAmountDay(localDate, localDate1);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideFromGetLocalDateAfterSeveralMonth() {
+        return Stream.of(Arguments.of(LocalDate.of(2014, 4, 25),
+                        5, LocalDate.of(2014, 9, 25)),
+                Arguments.of(LocalDate.of(2014, 4, 25),
+                        3, LocalDate.of(2014, 7, 25)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFromGetLocalDateAfterSeveralMonth")
+    public void getLocalDateAfterSeveralMonth(LocalDate localDate, Integer month, LocalDate expected) {
+        DateWrapper dateWrapper = new DateWrapper();
+
+        LocalDate actual = dateWrapper.getLocalDateAfterSeveralMonth(localDate, month);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideFromGetFormatPattern() {
+        return Stream.of(Arguments.of(LocalDate.of(2014, 4, 25),
+                        DateTimeFormatter.ofPattern("MMMM, dd, yyyy"), "апреля, 25, 2014"),
+                Arguments.of(LocalDate.of(2017, 5, 12),
+                        DateTimeFormatter.ofPattern("dd, M, yyyy"), "12, 5, 2017"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFromGetFormatPattern")
+    public void getFormatPattern(LocalDate localDate, DateTimeFormatter dateTimeFormatter, String expected) {
+        DateWrapper dateWrapper = new DateWrapper();
+
+        String actual = dateWrapper.getFormatPattern(localDate, dateTimeFormatter);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> provideFromGetStringFormat() {
+        return Stream.of(Arguments.of("апреля, 25, 2014",
+                        DateTimeFormatter.ofPattern("MMMM, dd, yyyy"),
+                        LocalDate.of(2014, 4, 25)),
+                Arguments.of("12, 5, 2017",
+                        DateTimeFormatter.ofPattern("dd, M, yyyy"),
+                        LocalDate.of(2017, 5, 12)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFromGetStringFormat")
+    public void getStringFormat(String localDate, DateTimeFormatter dateTimeFormatter, LocalDate expected) {
+        DateWrapper dateWrapper = new DateWrapper();
+
+        LocalDate actual = dateWrapper.getStringFormat(localDate, dateTimeFormatter);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
