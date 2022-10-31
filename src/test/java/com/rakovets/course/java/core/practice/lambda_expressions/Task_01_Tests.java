6ea556224d6b5b0b31a72eccf554f1fbf2f3fc0d
Vict Pol <vpolik10@gmail.com>
2022-10-31 13:46:29 +0100

    feat: update test file for Task01

--- src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/Task_01_Tests.java
+++ src/test/java/com/rakovets/course/java/core/practice/lambda_expressions/Task_01_Tests.java
@@ -10,16 +10,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Task_01_Tests {
-    /*final String TEXT = " a a a aa? word  aa .aaa, b d bb! bbb word word c cc ccc d d d dd& ddd/ ddd! d.";
-    TextMonitoring textMonitoring = new TextMonitoring();
-
-    @Test
-    public void testResearchText() {
-        Map<String, Integer> mapWords = new HashMap<>(textMonitoring.researchText(TEXT));
-        String stringExpected = "[aa=2, aaa=1, bb=1, cc=1, dd=1, a=3, b=1, ccc=1, bbb=1, c=1, d=5, ddd=2, word=3]";
-
-        Assertions.assertEquals(mapWords.entrySet().toString(), stringExpected);
-    }*/
     String[] arrayArguments = {"-i", "in.txt", "--limit", "40", "-d", "1", "-o", "out.txt"};
     ArgumentsConverter argumentsConverter = new ArgumentsConverter();
 
