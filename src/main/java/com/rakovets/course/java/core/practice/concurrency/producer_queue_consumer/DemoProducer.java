c2a78740c96a99413a0cecc2834cd38b54faf903
Oleg Aleksashov <Fairp87@gmail.com>
2022-11-01 20:23:37 +0300

    feat: first project

---
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/producer_queue_consumer/DemoProducer.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.concurrency.producer_queue_consumer;
+
+public class DemoProducer {
+    public static void main(String[] args) {
+        Producer producer = new Producer();
+        producer.start();
+    }
+}
---
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/producer_queue_consumer/Producer.java
@@ -0,0 +1,21 @@
+package com.rakovets.course.java.core.practice.concurrency.producer_queue_consumer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Producer extends Thread {
+    public void run() {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> x = new LinkedList<>();
+        while (true) {
+
+            int l = sc.nextInt();
+            if (l == -1) {
+                break;
+            }
+            x.add(l);
+            System.out.println(l);
+        }
+    }
+}
