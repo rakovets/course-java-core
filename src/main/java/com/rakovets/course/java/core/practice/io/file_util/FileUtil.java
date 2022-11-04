be9b5e9c38d0d4f5337cf7ac2d65a07d90fcb8c0
Kate Komleva <komlevakate99@gmail.com>
2022-11-04 14:35:33 +0300

    feat: crated FileUtil

---
+++ src/main/java/com/rakovets/course/java/core/practice/io/file_util/FileUtil.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.java.core.practice.io.file_util;
+
+import java.io.File;
+
+public class FileUtil {
+
+    public void rewriteOneFileToAnother(File file1, File file2){
+
+}
+}
