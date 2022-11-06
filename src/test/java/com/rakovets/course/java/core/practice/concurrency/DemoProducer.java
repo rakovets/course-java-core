9e120faa352a2460ea4bb31adb6031b5703853b0
Oleg Aleksashov <Fairp87@gmail.com>
2022-11-07 00:12:04 +0300

    fix: fixed Demo

--- src/test/java/com/rakovets/course/java/core/practice/concurrency/DemoProducer.java
+++ src/test/java/com/rakovets/course/java/core/practice/concurrency/DemoProducer.java
@@ -5,7 +5,7 @@ import com.rakovets.course.java.core.practice.concurrency.producer_queue_consume
 import com.rakovets.course.java.core.practice.concurrency.producer_queue_consumer.Producer;
 
 public class DemoProducer {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Container queue1 = new Container();
         Thread producer = new Producer(queue1);
         Thread consumer = new Consumer(queue1);
@@ -13,13 +13,13 @@ public class DemoProducer {
         Thread consumer2 = new Consumer(queue1);
 
         producer.start();
+        try {
+            producer.join();
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
         consumer.start();
         consumer1.start();
         consumer2.start();
-
-        producer.join();
-        consumer.join();
-        consumer1.join();
-        consumer2.join();
     }
 }
