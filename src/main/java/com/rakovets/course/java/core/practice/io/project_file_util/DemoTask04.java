77da9a951c3944226a718096f06d16480cdac1d2
Vict Pol <vpolik10@gmail.com>
2022-11-08 09:41:59 +0100

    feat: add DemoTask05 file for project_file_util

---
+++ src/main/java/com/rakovets/course/java/core/practice/io/project_file_util/DemoTask04.java
@@ -0,0 +1,27 @@
+package com.rakovets.course.java.core.practice.io.project_file_util;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class DemoTask04 {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(com.rakovets.course.java.core.practice.io.DemoTask04.class.getName());
+        FileUtil fileUtil = new FileUtil();
+        String pathToFile = "C:\\MyDir\\task04.txt";
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathToFile))) {
+            String text1 = "Any yo any.    text 1.\n";
+            String text2 = "ooy ide? ust text 2.\n";
+            String text3 = "gy ako   op text 3.\n";
+            bw.write(text1);
+            bw.write(text2);
+            bw.write(text3);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        List<String> stringList = fileUtil.getStringListOfWordsEndsWithLetterNextWordStatsFromFile(pathToFile);
+        logger.info(stringList.toString());
+    }
+}
--- src/main/java/com/rakovets/course/java/core/practice/io/project_file_util/DemoTask05.java
+++ src/main/java/com/rakovets/course/java/core/practice/io/project_file_util/DemoTask05.java
@@ -1,4 +1,7 @@
 package com.rakovets.course.java.core.practice.io.project_file_util;
 
 public class DemoTask05 {
+    public static void main(String[] args) {
+
+    }
 }
