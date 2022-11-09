48a88d079b8aac51c1171831b6395b5c0d76afef
Oleg Aleksashov <Fairp87@gmail.com>
2022-11-09 10:46:54 +0300

    fix: fixed DemoStringUtil

--- src/test/java/com/rakovets/course/java/core/practice/io/DemoStringUtil.java
+++ src/test/java/com/rakovets/course/java/core/practice/io/DemoStringUtil.java
@@ -3,7 +3,6 @@ package com.rakovets.course.java.core.practice.io;
 import com.rakovets.course.java.core.practice.io.file_util.FileUtil;
 
 public class DemoStringUtil {
-    //private static String text = "src/test/java/com/rakovets/course/java/core/practice/io/text.txt"
     private static String text = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\java\\com\\rakovets\\course\\java\\core\\practice\\io\\text.txt";
     private static String text2 = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\java\\com\\rakovets\\course\\java\\core\\practice\\io\\text2.txt";
     public static void main(String[] args) {
