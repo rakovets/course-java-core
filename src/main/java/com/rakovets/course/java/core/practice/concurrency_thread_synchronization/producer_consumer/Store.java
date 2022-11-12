f6d04aa3915a2912972c12af99c302f38781f63b
Kate Komleva <komlevakate99@gmail.com>
2022-11-12 12:43:41 +0300

    feat: add method consume

--- src/main/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/producer_consumer/Store.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/producer_consumer/Store.java
@@ -1,16 +1,25 @@
 package com.rakovets.course.java.core.practice.concurrency_thread_synchronization.producer_consumer;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Store {
     private final Logger logger = Logger.getLogger(Store.class.getName());
-    private List<Integer> numbers;
+    private final Queue<Integer> numbers = new LinkedList<>();
 
-    public void produce() {
+    public synchronized void produce() {
         logger.info("Enter 10 positive numbers: ");
         Scanner sc = new Scanner(System.in);
+        while (numbers.size() >= 10) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         int number;
         int count = 0;
         while (count <= 10) {
@@ -19,7 +28,7 @@ public class Store {
                 if (number < 0) {
                     throw new UserInputException("Try only positive numbers");
                 } else {
-                    numbers.add(number);
+                    numbers.offer(number);
                     count++;
                 }
             } catch (UserInputException | IllegalArgumentException e) {
@@ -30,7 +39,20 @@ public class Store {
         sc.close();
     }
 
-    public List<Integer> getNumbers() {
+    public synchronized int consume() {
+        while (numbers.isEmpty()) {
+            logger.info(Thread.currentThread().getName() + ", store is empty!");
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                logger.severe(e.getMessage());
+            }
+        }
+        notify();
+        return numbers.poll();
+    }
+
+    public Queue<Integer> getNumbers() {
         return numbers;
     }
 }
