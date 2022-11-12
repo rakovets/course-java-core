44fb6a93ffbb92eb447fcaab56dc1560318b2df1
Kate Komleva <komlevakate99@gmail.com>
2022-11-12 14:17:12 +0300

    feat: add Producer and Consumer Threads

---
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/producer_consumer/ProducerThread.java
@@ -0,0 +1,15 @@
+package com.rakovets.course.java.core.practice.concurrency_thread_synchronization.producer_consumer;
+
+import java.util.Random;
+
+public class ProducerThread implements Runnable {
+    private final Store store;
+
+    public ProducerThread(Store store) {
+        this.store = store;
+    }
+
+    public void run() {
+            store.produce();
+    }
+}
--- src/main/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/producer_consumer/Store.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/producer_consumer/Store.java
@@ -1,45 +1,30 @@
 package com.rakovets.course.java.core.practice.concurrency_thread_synchronization.producer_consumer;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class Store {
     private final Logger logger = Logger.getLogger(Store.class.getName());
     private final Queue<Integer> numbers = new LinkedList<>();
+    private Random rnd = new Random();
 
     public synchronized void produce() {
-        logger.info("Enter 10 positive numbers: ");
-        Scanner sc = new Scanner(System.in);
         while (numbers.size() >= 10) {
+            logger.info(Thread.currentThread().getName() + ", store is full!");
             try {
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        int number;
-        int count = 0;
-        while (count <= 10) {
-            try {
-                number = Integer.parseInt(sc.nextLine());
-                if (number < 0) {
-                    throw new UserInputException("Try only positive numbers");
-                } else {
-                    numbers.offer(number);
-                    count++;
-                }
-            } catch (UserInputException | IllegalArgumentException e) {
-                logger.severe("Error: Incorrect input. " + e.getMessage());
-                number = 0;
-            }
-        }
-        sc.close();
+        int addedNumber = rnd.nextInt(100);
+        numbers.offer(addedNumber);
+        logger.info(Thread.currentThread().getName() + " number " + addedNumber + " was added.");
+        logger.info("Store size is " + numbers.size());
+        notify();
     }
 
-    public synchronized int consume() {
+    public synchronized void consume() {
         while (numbers.isEmpty()) {
             logger.info(Thread.currentThread().getName() + ", store is empty!");
             try {
@@ -48,8 +33,9 @@ public class Store {
                 logger.severe(e.getMessage());
             }
         }
+        int removedNumber = numbers.poll();
+        logger.info(Thread.currentThread().getName() + " number " + removedNumber + " was used.");
         notify();
-        return numbers.poll();
     }
 
     public Queue<Integer> getNumbers() {
