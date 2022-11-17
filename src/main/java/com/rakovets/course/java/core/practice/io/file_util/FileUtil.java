ee8073d06c0246eae18e07d3991c722d0dbc71b1
Oleg Aleksashov <Fairp87@gmail.com>
2022-11-17 18:55:28 +0300

    feat: ninth method

--- src/main/java/com/rakovets/course/java/core/practice/io/file_util/FileUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/io/file_util/FileUtil.java
@@ -1,9 +1,20 @@
 package com.rakovets.course.java.core.practice.io.file_util;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -103,7 +114,7 @@ public class FileUtil {
     public Map<Character, Integer> getCountCharFrequency(String fileName) throws IOException {
         try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
             return lines
-                    .flatMapToInt((String s) -> s.chars())
+                    .flatMapToInt(String::chars)
                     .mapToObj(c -> (char) c)
                     .filter(Character::isLetter)
                     .collect(Collectors.groupingBy(
@@ -130,27 +141,40 @@ public class FileUtil {
                 }
             }
         }
-
         return map
                 .entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue())
-                .collect(Collectors
-                        .toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (e1, e2) -> e1, LinkedHashMap::new));
     }
 
-    public void getSortInAscendingOrder(String way) {
+    public void getSortInAscendingOrder(String way, String pattern) {
         List<String> list = getListStringFromFile(way);
         List<Integer> integerList = list.stream()
                 .flatMap(line -> Arrays.stream(line.split(" ")))
                 .map(Integer::valueOf)
                 .sorted()
                 .collect(Collectors.toList());
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(way + "_"))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(way.concat(pattern)))) {
             bufferedWriter.write(integerList.toString());
             bufferedWriter.flush();
         } catch (IOException e) {
             logger.log(Level.WARNING, e.getMessage());
         }
+    }
+
+    public List<String> getStudentPerformance(String students) {
+        List<String> list = getListStringFromFile(students);
+        Map<String, Integer> map = new HashMap<>();
+        for (String lineWithMarks : list) {
+            String[] stringsOfStudent = lineWithMarks.replace("\\D", "").trim().split(",");
+            for (String mark : stringsOfStudent) {
+
+            }
+        }
+        return list;
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/io/DemoStringUtil.java
+++ src/test/java/com/rakovets/course/java/core/practice/io/DemoStringUtil.java
@@ -5,11 +5,13 @@ import com.rakovets.course.java.core.practice.io.file_util.FileUtil;
 import java.io.IOException;
 
 public class DemoStringUtil {
-    private static String text = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\text.txt";
-    private static String text2 = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\text2.txt";
-    private static String wayNumber = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\way.txt";
-    private static String text3 = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\text3.txt";
-    private static String numbers = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\numbers.txt";
+    private static final String text = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\text.txt";
+    private static final String text2 = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\text2.txt";
+    private static final String wayNumber = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\way.txt";
+    private static final String text3 = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\text3.txt";
+    private static final String numbers = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\numbers.txt";
+    private static final String studentsMarks = "C:\\Users\\User\\IdeaProjects\\untitled\\course-java-core\\src\\test\\resources\\practice.io\\studentsMarks.txt";
+    private static final String pattern = "_";
 
     public static void main(String[] args) throws IOException {
         FileUtil fileUtil = new FileUtil();
@@ -22,6 +24,10 @@ public class DemoStringUtil {
         System.out.println(fileUtil.getLargestCombinationNumbers(wayNumber));
         System.out.println(fileUtil.getCountCharFrequency(text));
         System.out.println(fileUtil.getCountOfWordsCharFrequency(text3));
-        fileUtil.getSortInAscendingOrder(numbers);
+        fileUtil.getSortInAscendingOrder(numbers, pattern);
+        System.out.println(fileUtil.getStudentPerformance(studentsMarks));
     }
 }
+//Smirnov,4,6,8,9,9,2
+//        Sobovev,5,2,5,8,9,8
+//        Koba,2,5,4,5,3,5
