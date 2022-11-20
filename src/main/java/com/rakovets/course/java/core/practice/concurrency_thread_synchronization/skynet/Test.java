543ba8386970c3e0e47485e51775c284c52f6290
Oleg Aleksashov <Fairp87@gmail.com>
2022-11-20 20:43:36 +0300

    refactor: deleting something classes

--- src/main/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/skynet/Test.java
+++
@@ -1,7 +0,0 @@
-package com.rakovets.course.java.core.practice.concurrency_thread_synchronization.skynet;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(PathOfBody.values().length);
-    }
-}
