774ef8fa4939109a49a4e5b599d3dd410e875029
Vict Pol <vpolik10@gmail.com>
2022-11-21 09:37:33 +0100

    feat: update project_producer_consumer move to one package and add test file

---
+++ src/test/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/testDemoProjevtConsumerProducer.java
@@ -0,0 +1,28 @@
+package com.rakovets.course.java.core.practice.concurrency_thread_synchronization;
+
+import com.rakovets.course.java.core.practice.concurrency_thread_synchronization.project_producer_consumer.Store;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class testDemoProjevtConsumerProducer {
+
+    @Test
+    public void testProjectConsumerProducer() {
+        final int PRODUCER_THREAD_NUMBERS = 3;
+        final int CONSUMER_THREAD_NUMBERS = 3;
+        final int MAX_DEQUE_SIZE = 10;
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        Store store = new Store(deque, MAX_DEQUE_SIZE);
+        for (int i = 0; i < PRODUCER_THREAD_NUMBERS; i++) {
+            Thread producerThread = new com.rakovets.course.java.core.practice.concurrency_thread_synchronization.project_producer_consumer.ProducerThread("Producer_" + i, store);
+            producerThread.start();
+        }
+        for (int i = 0; i < CONSUMER_THREAD_NUMBERS; i++) {
+            Thread consumerThread = new com.rakovets.course.java.core.practice.concurrency_thread_synchronization.project_producer_consumer.ConsumerThread("Consumer_" + i, store);
+            consumerThread.start();
+        }
+    }
+}
