fb3019dcedc81530d2d60c1c59dc64ed909960fc
Vict Pol <vpolik10@gmail.com>
2022-11-21 08:32:50 +0100

    feat: update Demo file and add Test file with project_sky_net

--- src/test/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/TestDemoProjectSkyNet.java
+++ src/test/java/com/rakovets/course/java/core/practice/concurrency_thread_synchronization/TestDemoProjectSkyNet.java
@@ -1,11 +1,19 @@
-package com.rakovets.course.java.core.practice.concurrency_thread_synchronization.project_sky_net.task_01;
+package com.rakovets.course.java.core.practice.concurrency_thread_synchronization;
+
+import com.rakovets.course.java.core.practice.concurrency_thread_synchronization.project_sky_net.task_01.FactoryStarter;
+import com.rakovets.course.java.core.practice.concurrency_thread_synchronization.project_sky_net.task_01.FactoryStore;
+import com.rakovets.course.java.core.practice.concurrency_thread_synchronization.project_sky_net.task_01.FractionOfRobotsStarter;
+import com.rakovets.course.java.core.practice.concurrency_thread_synchronization.project_sky_net.task_01.SpeedType;
+import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class Demo {
-    public static void main(String[] args) {
-        final int TIME_OF_WORK_IN_DAYS = 10;
+public class TestDemoProjectSkyNet {
+
+    @Test
+    public void testDemoProject(){
+        final int TIME_OF_WORK_IN_MINUTES = 2;
         final int NUMBER_OF_PERMISSIONS_TO_PRODUCE_SPEARS_FOR_FACTORY = 10;
 
         String NAME_OF_FACTORY_STARTER_THREAD = "FactoryStarter";
@@ -16,7 +24,7 @@ public class Demo {
         namesOfRobotsThreadAndNumberOfSpearsAllowedToTakePerDay.put("World", 5);
         namesOfRobotsThreadAndNumberOfSpearsAllowedToTakePerDay.put("Wednesday", 5);
 
-        FactoryStore factoryStore = new FactoryStore(TIME_OF_WORK_IN_DAYS, NUMBER_OF_PERMISSIONS_TO_PRODUCE_SPEARS_FOR_FACTORY, namesOfRobotsThreadAndNumberOfSpearsAllowedToTakePerDay);
+        FactoryStore factoryStore = new FactoryStore(TIME_OF_WORK_IN_MINUTES, NUMBER_OF_PERMISSIONS_TO_PRODUCE_SPEARS_FOR_FACTORY, namesOfRobotsThreadAndNumberOfSpearsAllowedToTakePerDay);
         Thread factoryStarter = new FactoryStarter(NAME_OF_FACTORY_STARTER_THREAD, NAME_OF_FACTORY_THREAD, factoryStore, SpeedType.HIGH);
         Thread fractionOfRobotsStarter = new FractionOfRobotsStarter(NAME_OF_FRACTION_OF_ROBOTS_STARTER_THREAD, factoryStore, SpeedType.MIDDLE);
         factoryStarter.start();
