8338d405f80287edc470e85a3a32473ed2369f12
Aliaksei Kharko <AliakseiKharko@gmail.com>
2022-11-25 17:25:39 +0300

    Delete JavaUtilLogging.java

--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/JavaUtilLogging/JavaUtilLogging.java
+++
@@ -1,7 +0,0 @@
-package com.rakovets.course.java.core.practice.exception_handling.JavaUtilLogging;
-
-import java.util.logging.Logger;
-
-public class JavaUtilLogging {
-    public static final Logger logger = Logger.getLogger(JavaUtilLogging.class.getName());
-}
