3379175e83a09e622295dc3674499472260eab07
akavity <basilakavity@gmail.com>
2022-12-23 20:07:32 +0300

    feat: add solution for Task14 'decision-making statements'

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task14.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task14.java
@@ -42,8 +42,33 @@ class Task14 {
      * @return название года по китайскому календарю
      */
     static String getZodiacYearName(int year) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        final int cycle = 12;
+        String zodiac = " ";
+            if (year % cycle == 0) {
+                zodiac = "Monkey";
+            } else if (year % cycle == 1) {
+                zodiac = "Rooster";
+            } else if (year % cycle == 2) {
+                zodiac = "Dog";
+            } else if (year % cycle == 3) {
+                zodiac = "Pig";
+            } else if (year % cycle == 4) {
+                zodiac = "Rat";
+            } else if (year % cycle == 5) {
+                zodiac = "Ox";
+            } else if (year % cycle == 6) {
+                zodiac = "Tiger";
+            } else if (year % cycle == 7) {
+                zodiac = "Rabbit";
+            } else if (year % cycle == 8) {
+                zodiac = "Dragon";
+            } else if (year % cycle == 9) {
+                zodiac = "Snake";
+            } else if (year % cycle == 10) {
+                zodiac = "Horse";
+            } else if (year % cycle == 11) {
+                zodiac = "Ram";
+            }
+            return zodiac;
+        }
     }
-}
