c4296618df2a46d281d9747bf30a100af7ddc988
Vladislav Mitskevich <mitskevichvlm@gmail.com>
2022-12-25 17:42:24 +0300

    feat: add solution with final for Task06 on operators

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task06.java
@@ -6,6 +6,9 @@ package com.rakovets.course.java.core.practice.operators;
  * @author Dmitry Rakovets
  */
 class Task06 {
+    private static final int SECOND_IN_DAY = 24 * 60 * 60;
+    private static final int SECOND_IN_HOUR = 60 * 60;
+    private static final int SECOND_IN_MINIT = 60;
     /**
      * The entry point of the task
      *
@@ -28,8 +31,10 @@ class Task06 {
      * @return время в формате 'D H:m:s', где D - дни, H - часы, m - минуты, s - секунды
      */
     static String getPlayingTime(int playingTimeInSeconds) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        int playingDays = playingTimeInSeconds / SECOND_IN_DAY;
+        int playingHours = (playingTimeInSeconds % SECOND_IN_DAY) / SECOND_IN_HOUR;
+        int playingMinints = (playingTimeInSeconds % SECOND_IN_HOUR) / SECOND_IN_MINIT;
+        int playingSeconds = playingTimeInSeconds % SECOND_IN_MINIT;
+        return playingDays + " " + playingHours + ":" + playingMinints + ":" + playingSeconds;
     }
 }
