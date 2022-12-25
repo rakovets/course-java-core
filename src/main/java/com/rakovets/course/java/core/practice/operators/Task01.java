2513f173e8b35698f0f47f361177da849072f5c8
Vladislav Mitskevich <mitskevichvlm@gmail.com>
2022-12-25 03:37:07 +0300

    feat: add solution for Task01 on operators

--- src/main/java/com/rakovets/course/java/core/practice/operators/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/operators/Task01.java
@@ -29,8 +29,6 @@ class Task01 {
      * @param carryingCapacity грузоподъемность грузовиков
      */
     static int getNumberFullTrucks(int cargo, int carryingCapacity) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        return cargo/carryingCapacity;
     }
 }
