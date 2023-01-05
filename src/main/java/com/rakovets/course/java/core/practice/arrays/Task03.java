c177d5a126d4965f58d5ba9cf4cf577f2c002c8e
Nataliia Levak <nataliialevak@gmail.com>
2023-01-05 04:54:24 +0300

    feat: add arrays task 3

--- src/main/java/com/rakovets/course/java/core/practice/arrays/Task03.java
+++ src/main/java/com/rakovets/course/java/core/practice/arrays/Task03.java
@@ -2,6 +2,8 @@ package com.rakovets.course.java.core.practice.arrays;
 
 import java.util.Arrays;
 
+import com.rakovets.course.java.core.util.NumberUtil;
+
 /**
  * Разработать программу для электронного дневника, которая работает с отметками по каждому предмету.
  *
@@ -33,9 +35,17 @@ class Task03 {
      * @return средняя арифметическая отметка
      */
     static double[] getAverageMarks(int[][] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+
+        double[] averageLineMark = new double[marks.length];  // массив для сред.арифметического строк
+        for (int i = 0; i < marks.length; i++) {  // перебор строк в массиве
+            int sumLineMarks = 0;
+            for (int j = 0; j < marks[i].length; j++) {  // перебор элементов в строке
+                sumLineMarks += marks[i][j];  // сумма элементов строки
+            }
+            // присвоение сред.арифм. в массив для каждой строки при переборе строк
+            averageLineMark[i] = NumberUtil.roundValueToTwoDigitsForMantissa(sumLineMarks * 1.0 / marks[i].length);
+        }
+        return averageLineMark;
     }
 
     /**
@@ -45,9 +55,18 @@ class Task03 {
      * @return минимальная отметка
      */
     static int[] getMinMarks(int[][] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+
+        int[] minLineMarks = new int[marks.length]; // массив для мин.значения каждой строки
+        for (int i = 0; i < marks.length; i++) {  // перебор строк в массиве
+            int min = marks[i][0];
+            for (int j = 0; j < marks[i].length; j++) {  // перебор элементов в строке
+                if (min > marks[i][j]) { // поиск мин.элемента в данной строке
+                    min = marks[i][j];
+                }
+            }
+            minLineMarks[i] = min; //присвоение мин. для каждой строки
+        }
+        return minLineMarks;
     }
 
     /**
@@ -57,8 +76,17 @@ class Task03 {
      * @return максимальная отметка
      */
     static int[] getMaxMarks(int[][] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+
+        int[] maxLineMarks = new int[marks.length]; // массив для макс.значения каждой строки
+        for (int i = 0; i < marks.length; i++) { // перебор строк в массиве
+            int max = marks[i][0];
+            for (int j = 0; j < marks[i].length; j++) { // перебор элементов в строке
+                if (max < marks[i][j]) { // поиск макс.элемента в данной строке
+                    max = marks[i][j];
+                }
+            }
+            maxLineMarks[i] = max; // присвоение макс. для каждой строки
+        }
+        return maxLineMarks;
     }
 }
