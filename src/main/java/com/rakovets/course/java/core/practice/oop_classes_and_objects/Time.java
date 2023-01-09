b0cd14f8b705baa021772430b9ff8c916cf4c3e3
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-09 22:37:28 +0300

    add: Task 1

--- src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Time.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Time.java
@@ -1,47 +1,56 @@
 package com.rakovets.course.java.core.practice.oop_classes_and_objects;
 
 public class Time {
-    private static int hours;
-    private static int minutes;
-    private static int seconds;
+    private int hours;
+    private int minutes;
+    private int seconds;
 
-    Time(int totalSeconds) {
-        hours = totalSeconds / 3600;
-        minutes = totalSeconds % 3600 / 60;
-        seconds = totalSeconds % 3600 % 60 ;
+    Time (int totalSeconds) {
+        if (totalSeconds >= 0) {
+            this.hours = totalSeconds / 3600;
+            this.minutes = (totalSeconds - hours * 3600) / 60;
+            this.seconds = totalSeconds - hours * 3600 - minutes * 60;
+        } else {
+            System. exit(404);
+        }
     }
-
-    Time(int hours, int minutes, int seconds) {
-            Time.hours = hours;
-            Time.minutes = minutes;
-            Time.seconds = seconds;
+    Time (int hours, int minutes, int seconds) {
+        if (hours >= 0) {
+            this.hours = hours;
+        } else {
+            System. exit(404);
+        }
+        if (minutes >= 0 && minutes <= 59) {
+            this.minutes = minutes;
+        } else {
+            System. exit(404);
+        }
+        if (seconds >= 0 && seconds <= 59) {
+            this.seconds = seconds;
+        } else {
+            System. exit(404);
+        }
     }
 
     public int getHours() {
         return hours;
     }
-
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
     public int getMinutes() {
         return minutes;
     }
-
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
     public int getSeconds() {
         return seconds;
     }
-
-    public void setHours(int hours) {
-        Time.hours = hours;
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
     }
-
-    public void setMinutes(int minutes) {
-        Time.minutes = minutes;
-    }
-
-    public static void setSeconds(int seconds) {
-        Time.seconds = seconds;
-    }
-
-    public static int getTotalSeconds() {
-        return (hours * 3600 + minutes * 60 + seconds);
+    public int getTotalSeconds() {
+        return hours * 3600 + minutes * 60 + seconds;
     }
 }
