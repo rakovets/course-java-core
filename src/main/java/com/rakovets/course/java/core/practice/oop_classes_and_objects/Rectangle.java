038f0d1dcd04cacafc655d88a8113a89416e980d
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-11 19:44:23 +0300

    add: Task 3

--- src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Rectangle.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Rectangle.java
@@ -1,2 +1,33 @@
 package com.rakovets.course.java.core.practice.oop_classes_and_objects;
 
+public class Rectangle {
+    Point topLeftPoint;
+    Point bottomRightPoint;
+
+    Rectangle (Point topLeftPoint, Point bottomRightPoint) {
+        this.topLeftPoint = topLeftPoint;
+        this.bottomRightPoint = bottomRightPoint;
+    }
+
+    public Point getTopLeftPoint() {
+        return topLeftPoint;
+    }
+
+    public Point getBottomRightPoint() {
+        return bottomRightPoint;
+    }
+
+    public void setTopLeftPoint(Point topLeftPoint) {
+        this.topLeftPoint = topLeftPoint;
+    }
+
+    public void setBottomRightPoint(Point bottomRightPoint) {
+        this.bottomRightPoint = bottomRightPoint;
+    }
+    public int getPerimeter() {
+        return ((topLeftPoint.getX() - bottomRightPoint.getX()) + (topLeftPoint.getY() - bottomRightPoint.getY())) * 2;
+    }
+    public int getArea() {
+        return (topLeftPoint.getX() - bottomRightPoint.getX()) * (topLeftPoint.getY() - bottomRightPoint.getY());
+    }
+}
