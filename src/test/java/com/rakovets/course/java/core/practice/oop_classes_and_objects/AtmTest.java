27b9e566287ab071a0e72d227cabea11f6f8f48a
Ira Protasevich <irina.m.protasevich@gmail.com>
2023-01-11 17:17:02 +0400

    feat:add solutions to task4 of 'OOP.Classes and Objects'

---
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/AtmTest.java
@@ -0,0 +1,31 @@
+package com.rakovets.course.java.core.practice.oop_classes_and_objects;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AtmTest {
+
+    @Test
+    void testAddBanknotes20() {
+        Atm atm = new Atm(10,0,0);
+
+        atm.addBanknotes20(7);
+        Assertions.assertEquals(3, atm.getNumberBanknotes20());
+    }
+
+    @Test
+    void testAddBanknotes50() {
+        Atm atm = new Atm(0,10,0);
+
+        atm.addBanknotes50(7);
+        Assertions.assertEquals(3, atm.getNumberBanknotes50());
+    }
+
+    @Test
+    void testAddBanknotes100() {
+        Atm atm = new Atm(0,0,10);
+
+        atm.addBanknotes100(7);
+        Assertions.assertEquals(3,atm.getNumberBanknotes100());
+    }
+}
