2f553f29b4d60eaba947e46372d5401d7961c997
Dmitry Chernuho <d.chernuho@gmail.com>
2023-01-13 13:34:28 -0800

    Delete task classes from test directory

--- src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.rakovets.course.java.core.practice.oop_classes_and_objects;
-
-public class Utils {
-    public static int getSquareOfNumber(int number) {
-        return (int) Math.pow(number, 2);
-    }
-
-}
