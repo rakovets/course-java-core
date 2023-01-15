3978c697a9ef9bfd31fd1b6526189d96e9dca556
akavity <basilakavity@gmail.com>
2023-01-15 12:13:00 +0300

    feat: add 4 Tests for Task02 'oop_classes_and_objects'

--- src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/TimeTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/TimeTest.java
@@ -5,25 +5,57 @@ import org.junit.jupiter.api.Test;
 
 public class TimeTest {
     @Test
-    void convertTime1() {
+    void convertSecondToHoursMinutesSecondsTest1() {
         Time time = new Time(3661);
         Assertions.assertEquals(1, time.getHours());
         Assertions.assertEquals(1, time.getMinutes());
-        Assertions.assertEquals(1,time.getSeconds());
+        Assertions.assertEquals(1, time.getSeconds());
     }
+
     @Test
-    void convertTime2() {
+    void convertSecondToHoursMinutesSecondsTest2() {
         Time time = new Time(20715);
         Assertions.assertEquals(5, time.getHours());
         Assertions.assertEquals(45, time.getMinutes());
-        Assertions.assertEquals(15,time.getSeconds());
+        Assertions.assertEquals(15, time.getSeconds());
     }
+
     @Test
-    void convertTime3() {
+    void convertSecondToHoursMinutesSecondsTest3() {
         Time time = new Time(11601);
         Assertions.assertEquals(3, time.getHours());
         Assertions.assertEquals(13, time.getMinutes());
-        Assertions.assertEquals(21,time.getSeconds());
+        Assertions.assertEquals(21, time.getSeconds());
     }
 
+    @Test
+    void convertSecondToHoursMinutesSecondsTest4() {
+        Time time = new Time(3601);
+        Assertions.assertEquals(1, time.getHours());
+        Assertions.assertEquals(0, time.getMinutes());
+        Assertions.assertEquals(1, time.getSeconds());
+    }
+
+    @Test
+    void convertSecondToHoursMinutesSecondsTest5() {
+        Time time = new Time(0);
+        Assertions.assertEquals(0, time.getHours());
+        Assertions.assertEquals(0, time.getMinutes());
+        Assertions.assertEquals(0, time.getSeconds());
+    }
+
+    @Test
+    void convertSecondToHoursMinutesSecondsTest6() {
+        Time time = new Time(621);
+        Assertions.assertEquals(0, time.getHours());
+        Assertions.assertEquals(10, time.getMinutes());
+        Assertions.assertEquals(21, time.getSeconds());
+    }
+    @Test
+    void convertSecondToHoursMinutesSecondsTest7() {
+        Time time = new Time(7860);
+        Assertions.assertEquals(2, time.getHours());
+        Assertions.assertEquals(11, time.getMinutes());
+        Assertions.assertEquals(0, time.getSeconds());
+    }
 }
