bbb3d7591da6f27d5ed83c32a279de2f8935a87b
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-15 18:23:15 +0300

    feat: refactor some code

--- src/main/java/com/rakovets/course/java/core/practice/strings/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/strings/StringUtil.java
@@ -2,39 +2,42 @@ package com.rakovets.course.java.core.practice.strings;
 
 public class StringUtil {
 
-    public static String concatenation(String str1, String str2) {
+    public String glue(String str1, String str2) {
         return str1.concat(str2);
     }
 
-    public static int charAt(int x, String str1) {
+    public int charAt(int x, String str1) {
         return str1.indexOf(x);
     }
 
-    public static boolean equals(String str1, String str2) {
+    public boolean equals(String str1, String str2) {
         return str1.equals(str2);
     }
 
-    public static String trim(String str1) {
+    public String trim(String str1) {
         return str1.trim();
     }
 
-    public static String symbolsExtraction(char n, char m, String str1) {
+    public String symbolsExtraction(char n, char m, String str1) {
         return str1.substring(n - 1, m - 1); // возможно ошибка, пользователь задает символ по счёту или по имени?
     }
 
-    public static String emojiReplacement(String str1) {
+    public String emojiReplacement(String str1) {
         return str1.replaceAll("\\(", ":)");
     }
 
-    public static boolean startsAndEndsWithWord(String text, String word) {
+    public boolean startsAndEndsWithWord(String text, String word) {
         boolean isTrue = false;
-        if (text.startsWith(word) && text.endsWith(word)) {
+        String str1 = text.toLowerCase();
+
+        if (str1.startsWith(word) && str1.endsWith(word)) {
             isTrue = true;
         }
+
         return isTrue;
     }
 
-    public static int vowelsCount(String str1) {
+    public int vowelsCount(String str1) {
         int vowelsCounter = 0;
         String str1LowerCase = str1.toLowerCase();
 
@@ -50,7 +53,7 @@ public class StringUtil {
         return vowelsCounter;
     }
 
-    public static int punctuationMarksCount(String str1) {
+    public int punctuationMarksCount(String str1) {
         int punctuationMarksCounter = 0;
         String str1LowerCase = str1.toLowerCase();
 
@@ -65,14 +68,14 @@ public class StringUtil {
         return punctuationMarksCounter;
     }
 
-    public static boolean IsPalindrome(String str1) {
+    public boolean IsPalindrome(String str1) {
         String str1Modified = str1.toLowerCase().replaceAll("\\W", "");
         String str1ModifiedReversed = new StringBuilder(str1Modified).reverse().toString();
 
         return str1Modified.equals(str1ModifiedReversed);
     }
 
-    public static String[] nGetArray(int n, String str1) {
+    public String[] nGetArray(int n, String str1) {
         int stringsAmount = str1.length() / n;
         String[] arrayOfStrings = new String[stringsAmount];
         int substringBeginIndex = 0;
@@ -85,4 +88,8 @@ public class StringUtil {
 
         return arrayOfStrings;
     }
+
+   // public static int howManyWordsInText(String str1) {
+
+    //}
 }
