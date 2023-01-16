1a224aac3b79e83de0b441ac326c236360a64d99
Maxim Yarmosh <maxinc636@gmail.com>
2023-01-16 22:06:24 +0300

    Refactor

--- src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Time.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Time.java
@@ -1,56 +1,56 @@
 package com.rakovets.course.java.core.practice.oop_classes_and_objects;
 
-    public class Time {
-        private final int secondConvertForHour = 3600;
-        private final int secondConvertForMinutes = 60;
+public class Time {
+    private final int secondConvertForHour = 3600;
+    private final int secondConvertForMinutes = 60;
 
-        private int hours;
-        private int minutes;
-        private int seconds;
+    private int hours;
+    private int minutes;
+    private int seconds;
 
 
-        public Time(int totalTimeInSeconds) {
-            this.hours = totalTimeInSeconds / secondConvertForHour;
-            int secondsLeft = totalTimeInSeconds % secondConvertForHour;
-            this.minutes = secondsLeft / secondConvertForMinutes;
-            secondsLeft = secondsLeft % secondConvertForMinutes;
-            this.seconds = secondsLeft;
-        }
-
-        public Time(int hours, int minutes, int seconds) {
-            this.hours = hours;
-            this.minutes = minutes;
-            this.seconds = seconds;
-        }
-
-        public int getHours() {
-            return hours;
-        }
-
-        public void setHours(int hours) {
-            this.hours = hours;
-        }
-
-        public int getMinutes() {
-            return minutes;
-        }
-
-        public void setMinutes(int minutes) {
-            this.minutes = minutes;
-        }
-
-        public int getSeconds() {
-            return seconds;
-        }
-
-        public void setSeconds(int seconds) {
-            this.seconds = seconds;
-        }
-
-        public int totalTimeInSeconds() {
-            return this.seconds + (this. minutes * secondConvertForMinutes) + (this.hours * secondConvertForHour);
-        }
+    public Time(int totalTimeInSeconds) {
+        this.hours = totalTimeInSeconds / secondConvertForHour;
+        int secondsLeft = totalTimeInSeconds % secondConvertForHour;
+        this.minutes = secondsLeft / secondConvertForMinutes;
+        secondsLeft = secondsLeft % secondConvertForMinutes;
+        this.seconds = secondsLeft;
     }
+
+    public Time(int hours, int minutes, int seconds) {
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public int totalTimeInSeconds() {
+        return this.seconds + (this.minutes * secondConvertForMinutes) + (this.hours * secondConvertForHour);
+    }
+}
 
 
 
