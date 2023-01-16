aac29204a006547be888b63ebb39cedce25dadd7
akavity <basilakavity@gmail.com>
2023-01-16 11:04:24 +0300

    add solution for Task06 'decision-making statements'

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task06.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task06.java
@@ -39,8 +39,13 @@ class Task06 {
      * @return 'Player 1: ${0}. Player 2: ${1}', где ${0} - очки первого игрока, ${1} - очки второго игрока.
      */
     static String getResult(int tanksKilledFirstPlayer, int tanksKilledSecondPlayer) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        int pointFirstGamer = tanksKilledFirstPlayer * KILL_POINT;
+        int pointSecondGamer = tanksKilledSecondPlayer * KILL_POINT;
+        if (tanksKilledFirstPlayer > tanksKilledSecondPlayer) {
+            pointFirstGamer += BONUS_POINT;
+        } else if (tanksKilledFirstPlayer < tanksKilledSecondPlayer) {
+            pointSecondGamer += BONUS_POINT;
+        }
+        return "Player 1: " + pointFirstGamer + "." + " Player 2: " + pointSecondGamer;
     }
 }
