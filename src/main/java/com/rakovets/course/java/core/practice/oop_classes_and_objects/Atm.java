38f390ac6a6f3aa1536a1c59839c5ed226c51ea3
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-16 18:41:18 +0300

    refactor: some code

--- src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Atm.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/Atm.java
@@ -5,7 +5,7 @@ public class Atm {
     public int numberBanknotes50;
     public int numberBanknotes100;
 
-    Atm (int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) {
+    public Atm (int numberBanknotes20, int numberBanknotes50, int numberBanknotes100) {
         this.numberBanknotes20 = numberBanknotes20;
         this.numberBanknotes50 = numberBanknotes50;
         this.numberBanknotes100 = numberBanknotes100;
