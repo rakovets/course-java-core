4356928c205d1ef5b3aa790b899dacc819dd81cb
akavity <basilakavity@gmail.com>
2023-01-16 11:40:33 +0300

    add solution for Task08 'decision-making statements'

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task08.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task08.java
@@ -12,6 +12,9 @@ package com.rakovets.course.java.core.practice.decision_making_statements;
  * Определить сколько урона нанесет оружие по данному типу моба (моб - персонаж в игре управляемый компьютером).
  */
 class Task08 {
+    private static final double DAMAGE_COEFFICIENT_DEAD = 1.5;
+    private static final double DAMAGE_COEFFICIENT_SAINT = 0.5;
+
     /**
      * The entry point of the task
      *
@@ -24,7 +27,6 @@ class Task08 {
         int damage = 100;
         String typeMob = "SAINT";
         boolean hasHolyAttribute = true;
-
         int totalDamage = getTotalDamage(damage, typeMob, hasHolyAttribute);
         System.out.printf("Result: %s", totalDamage);
     }
@@ -38,8 +40,24 @@ class Task08 {
      * @return итоговый урон по данному типу моба
      */
     static int getTotalDamage(int damage, String typeMob, boolean hasHolyAttribute) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int totalDamage;
+        int holyDamageDead = (int) Math.round(damage * DAMAGE_COEFFICIENT_DEAD);
+        int holyDamageSaint = (int) Math.round(damage * DAMAGE_COEFFICIENT_SAINT);
+        if (hasHolyAttribute == true) {
+            switch (typeMob) {
+                case "UNDEAD":
+                case "ZOMBIE":
+                    totalDamage = holyDamageDead;
+                    break;
+                case "SAINT":
+                    totalDamage = holyDamageSaint;
+                    break;
+                default:
+                    totalDamage = damage;
+            }
+        } else {
+            totalDamage = damage;
+        }
+        return totalDamage;
     }
 }
