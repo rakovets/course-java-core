a6b2bf99db9ada4d64a47c4ded7603d23e0dfc1f
akavity <basilakavity@gmail.com>
2023-01-16 13:33:13 +0300

    add solution for Task09 'decision-making statements'

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task09.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task09.java
@@ -32,8 +32,10 @@ class Task09 {
      * @return номер подъезда
      */
     static int getPorchNumber(int numberFloors, int numberApartmentsPerFloor, int apartmentNumber) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int numberApartmentsInPorch = numberFloors * numberApartmentsPerFloor;
+        double howManyPorchInApartmentNumber = (double) apartmentNumber / numberApartmentsInPorch;
+        int integerPartOfNumber = (int) howManyPorchInApartmentNumber;
+        double fractionalPartOfNumber = howManyPorchInApartmentNumber % 1;
+        return fractionalPartOfNumber == 0 ? integerPartOfNumber : integerPartOfNumber + 1;
     }
 }
