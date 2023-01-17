517231c5589a3a93e58ef001f7c9290a00309142
akavity <basilakavity@gmail.com>
2023-01-18 00:04:39 +0300

    tests  for Task04 'oop_classes_and_objects'

--- src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/AtmTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop_classes_and_objects/AtmTest.java
@@ -9,46 +9,52 @@ public class AtmTest {
     //    Atm atm = new Atm(10, 5, 10);
     //    Assertions.assertEquals(true, atm.isPossibleIssue(1270));
     // }
-
     @Test
     void withdrawMoneyTest2() {
         Atm atm = new Atm(10, 5, 10);
+
         Assertions.assertFalse(atm.isPossibleIssue(1300));
     }
 
     @Test
     void withdrawMoneyTest3() {
         Atm atm = new Atm(0, 5, 10);
+
         Assertions.assertFalse(atm.isPossibleIssue(1240));
     }
 
     @Test
     void withdrawMoneyTest4() {
         Atm atm = new Atm(0, 0, 15);
+
         Assertions.assertFalse(atm.isPossibleIssue(1240));
     }
 
     @Test
     void withdrawMoneyTest5() {
         Atm atm = new Atm(100, 0, 1);
+
         Assertions.assertTrue(atm.isPossibleIssue(1240));
     }
 
     @Test
     void checkMoneyInAtmTest1() {
         Atm atm = new Atm(1, 5, 10);
+
         Assertions.assertEquals(1270, atm.getTotalMoneyInAtm());
     }
 
     @Test
     void checkMoneyTest2() {
         Atm atm = new Atm(10, 50, 100);
+
         Assertions.assertEquals(12700, atm.getTotalMoneyInAtm());
     }
 
     @Test
     void checkMoneyTest3() {
         Atm atm = new Atm(43, 33, 347);
+
         Assertions.assertEquals(37210, atm.getTotalMoneyInAtm());
     }
 }
