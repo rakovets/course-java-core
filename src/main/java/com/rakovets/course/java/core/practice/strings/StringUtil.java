1c7aa51e412c52e6d08978a6842cbcaedc7b1220
Nataliia Levak <nataliialevak@gmail.com>
2023-01-19 01:21:53 +0300

    feat: add solution of task4 + 4 tests

--- src/main/java/com/rakovets/course/java/core/practice/strings/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/strings/StringUtil.java
@@ -1,13 +1,35 @@
 package com.rakovets.course.java.core.practice.strings;
 
 public class StringUtil {
+    //task 1
     public String glue(String str1, String str2) {
-        return (str1 == null || str2 == null) ? "" : str1.concat(str2);
+        return str1 == null || str2 == null ? "" : str1.concat(str2);
     }
-
-
-    public int FindSymbolIndex(String str, char x) {
+    //task 2
+    public int findSymbolIndex(String str, char x) {
         return str == null ? -1 : str.indexOf(x);
-
     }
+    //task 3
+    public boolean checkEqual(String str1, String str2) {
+        return str1 == null || str2 == null ? false : str1.equals(str2);
+    }
+    //task 4
+    public String removeSpaces(String str) {
+        return str == null ? "String is empty" : str.trim();
+    }
+    //task 5
+    //task 6
+    //task 7
+    //task 8
+    //task 9
+    //task 10
+    //task 11
+    //task 12
+    //task 13
+    //task 14
+    //task 15
+    //task 16
+    //task 17
+    //task 18
+    //task 19
 }
--- src/test/java/com/rakovets/course/java/core/practice/strings/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/strings/StringUtilTest.java
@@ -13,7 +13,6 @@ public class StringUtilTest {
 
         Assertions.assertEquals("123456", actual);
     }
-
     @Test
     void glueTest2() {
 
@@ -23,7 +22,6 @@ public class StringUtilTest {
 
         Assertions.assertEquals("", actual);
     }
-
     @Test
     void glueTest3() {
 
@@ -33,7 +31,6 @@ public class StringUtilTest {
 
         Assertions.assertEquals("", actual);
     }
-
     @Test
     void glueTest4() {
 
@@ -43,7 +40,6 @@ public class StringUtilTest {
 
         Assertions.assertEquals("", actual);
     }
-
     @Test
     void glueTest5() {
 
@@ -53,74 +49,159 @@ public class StringUtilTest {
 
         Assertions.assertEquals("", actual);
     }
-
     @Test
-    void FindSymbolIndexTest1() {
+    void findSymbolIndexTest1() {
 
         StringUtil stringUtil = new StringUtil();
 
-        int result = stringUtil.FindSymbolIndex("Hello world", 'x');
+        int index = stringUtil.findSymbolIndex("Hello world", 'x');
 
-        Assertions.assertEquals(-1, result);
+        Assertions.assertEquals(-1, index);
+    }
+    @Test
+    void findSymbolIndexTest2() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        int index = stringUtil.findSymbolIndex("Hello world", 'H');
+
+        Assertions.assertEquals(0, index);
+    }
+    @Test
+    void findSymbolIndexTest3() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        int index = stringUtil.findSymbolIndex("Hello world", 'h');
+
+        Assertions.assertEquals(-1, index);
+    }
+    @Test
+    void findSymbolIndexTest4() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        int index = stringUtil.findSymbolIndex("Hello world", 'l');
+
+        Assertions.assertEquals(2, index);
+    }
+    @Test
+    void findSymbolIndexTest5() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        int index = stringUtil.findSymbolIndex("", 'x');
+
+        Assertions.assertEquals(-1, index);
+    }
+    @Test
+    void findSymbolIndexTest6() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        int index = stringUtil.findSymbolIndex(null, 'x');
+
+        Assertions.assertEquals(-1, index);
+    }
+    @Test
+    void findSymbolIndexTest7() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        int index = stringUtil.findSymbolIndex("Hello world", ' ');
+
+        Assertions.assertEquals(5, index);
     }
 
     @Test
-    void FindSymbolIndexTest2() {
+    void checkEqualTest1() {
 
         StringUtil stringUtil = new StringUtil();
 
-        int result = stringUtil.FindSymbolIndex("Hello world", 'H');
+        boolean equality = stringUtil.checkEqual("Hello", "world");
 
-        Assertions.assertEquals(0, result);
+        Assertions.assertEquals(false, equality);
+    }
+    @Test
+    void checkEqualTest2() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        boolean equality = stringUtil.checkEqual("", "world");
+
+        Assertions.assertEquals(false, equality);
+    }
+    @Test
+    void checkEqualTest3() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        boolean equality = stringUtil.checkEqual(null, "world");
+
+        Assertions.assertEquals(false, equality);
+    }
+    @Test
+    void checkEqualTest4() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        boolean equality = stringUtil.checkEqual("world", null);
+
+        Assertions.assertEquals(false, equality);
+    }
+    @Test
+    void checkEqualTest5() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        boolean equality = stringUtil.checkEqual("world", "world");
+
+        Assertions.assertEquals(true, equality);
+    }
+    @Test
+    void checkEqualTest6() {
+
+        StringUtil stringUtil = new StringUtil();
+
+        boolean equality = stringUtil.checkEqual(null, null);
+
+        Assertions.assertEquals(false, equality);
     }
 
     @Test
-    void FindSymbolIndexTest3() {
+    void removeSpacesTest1() {
 
         StringUtil stringUtil = new StringUtil();
 
-        int result = stringUtil.FindSymbolIndex("Hello world", 'h');
+        String string = stringUtil.removeSpaces("   Hello World!  ");
 
-        Assertions.assertEquals(-1, result);
+        Assertions.assertEquals("Hello World!", string);
     }
-
     @Test
-    void FindSymbolIndexTest4() {
+    void removeSpacesTest2() {
 
         StringUtil stringUtil = new StringUtil();
 
-        int result = stringUtil.FindSymbolIndex("Hello world", 'l');
+        String string = stringUtil.removeSpaces("     ");
 
-        Assertions.assertEquals(2, result);
+        Assertions.assertEquals("", string);
     }
-
     @Test
-    void FindSymbolIndexTest5() {
+    void removeSpacesTest3() {
 
         StringUtil stringUtil = new StringUtil();
 
-        int result = stringUtil.FindSymbolIndex("", 'x');
+        String string = stringUtil.removeSpaces("");
 
-        Assertions.assertEquals(-1, result);
+        Assertions.assertEquals("", string);
     }
-
     @Test
-    void FindSymbolIndexTest6() {
+    void removeSpacesTest4() {
 
         StringUtil stringUtil = new StringUtil();
 
-        int result = stringUtil.FindSymbolIndex(null, 'x');
+        String string = stringUtil.removeSpaces(null);
 
-        Assertions.assertEquals(-1, result);
-    }
-
-    @Test
-    void FindSymbolIndexTest7() {
-
-        StringUtil stringUtil = new StringUtil();
-
-        int result = stringUtil.FindSymbolIndex("Hello world", ' ');
-
-        Assertions.assertEquals(5, result);
+        Assertions.assertEquals("String is empty", string);
     }
 }
