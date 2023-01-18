1aa594e9d10b192880023d8862d862d1925d8c56
akavity <basilakavity@gmail.com>
2023-01-19 00:36:07 +0300

    fix: solutions tests for Tasks 'oop_classes_and_objects'

--- src/main/java/com/rakovets/course/java/core/practice/strings/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/strings/StringUtil.java
@@ -22,122 +22,143 @@ public class StringUtil {
     }
 
     public String extractSubstringFromNtoM(String str, int n, int m) {
-        if (str == null || str.isEmpty()) {
-            return "";
-        }
-        return str.substring(n - 1, m);
+        String refusal = "";
+        return (str == null || str.isEmpty()) ? refusal : str.substring(n - 1, m);
     }
 
     public String dontSadSmile(String str) {
-        if (str == null || str.isEmpty()) {
-            return "";
-        }
-        return str.replace(":(", ":)");
+        String refusal = "";
+        return (str == null || str.isEmpty()) ? refusal : str.replace(":(", ":)");
     }
 
-    public boolean finedFistWordInString(String str, String start) {
-        if (str == null || str.isEmpty()) {
-            return false;
+    public boolean equalFistLastWords(String text, String word) {
+        boolean result;
+        if (text == null || text.isEmpty()) {
+            result = false;
+        } else {
+            String wordLowerCase = word.toLowerCase();
+            String textLowerCase = text.toLowerCase();
+            result = textLowerCase.startsWith(wordLowerCase) == textLowerCase.endsWith(wordLowerCase);
         }
-        return str.startsWith(start);
+        return result;
     }
 
     public int countVowels(String str) {
+        int result;
         if (str == null || str.isEmpty()) {
-            return 0;
-        }
-        char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u', 'y'};
-        char[] arrayFromString = str.toLowerCase().toCharArray();
-        int counter = 0;
-        for (int i : vowels) {
-            for (int j : arrayFromString) {
-                if (i == j) {
-                    counter++;
+            result = 0;
+        } else {
+            char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u', 'y'};
+            char[] arrayFromString = str.toLowerCase().toCharArray();
+            int counter = 0;
+            for (int i : vowels) {
+                for (int j : arrayFromString) {
+                    if (i == j) {
+                        counter++;
+                    }
                 }
             }
+            result = counter;
         }
-        return counter;
+        return result;
     }
 
     public int countPunctuationMarks(String str) {
+        int result;
         if (str == null || str.isEmpty()) {
-            return 0;
-        }
-        char[] vowels = new char[]{'.', ',', '?', '!'};
-        char[] arrayFromString = str.toCharArray();
-        int counter = 0;
-        for (int i : vowels) {
-            for (int j : arrayFromString) {
-                if (i == j) {
-                    counter++;
+            result = 0;
+        } else {
+            char[] vowels = new char[]{'.', ',', '?', '!'};
+            char[] arrayFromString = str.toCharArray();
+            int counter = 0;
+            for (int i : vowels) {
+                for (int j : arrayFromString) {
+                    if (i == j) {
+                        counter++;
+                    }
                 }
             }
+            result = counter;
         }
-        return counter;
+        return result;
     }
 
     public boolean isItPalindrome(String str1) {
+        boolean result;
         if (str1 == null || str1.isEmpty()) {
-            return false;
+            result = false;
+        } else {
+            str1 = str1.replace(" ", "");
+            StringBuffer buffer = new StringBuffer(str1);
+            String str2 = buffer.reverse().toString();
+            result = str1.equalsIgnoreCase(str2);
         }
-        str1 = str1.replace(" ", "");
-        StringBuffer buffer = new StringBuffer(str1);
-        String str2 = buffer.reverse().toString();
-        return str1.equalsIgnoreCase(str2);
+        return result;
     }
 
     public String[] getSubstrings(String str, int lengthSubstring) {
+        String[] result;
         if (str == null || str.isEmpty()) {
-            String[] empty = {""};
-            return empty;
+            result = new String[]{""};
+        } else {
+            int numberSubstrings = str.length() / lengthSubstring;
+            String[] substrings = new String[numberSubstrings];
+            int indexCut = 0;
+            for (int i = 0; i < numberSubstrings; i++) {
+                substrings[i] = str.substring(indexCut, indexCut + lengthSubstring);
+                indexCut += lengthSubstring;
+            }
+            result = substrings;
         }
-        int numberSubstrings = str.length() / lengthSubstring;
-        String[] substrings = new String[numberSubstrings];
-        int indexCut = 0;
-        for (int i = 0; i < numberSubstrings; i++) {
-            substrings[i] = str.substring(indexCut, indexCut + lengthSubstring);
-            indexCut += lengthSubstring;
-        }
-        return substrings;
+        return result;
     }
 
     public int countWords(String str) {
+        int result;
         if (str == null || str.isEmpty()) {
-            return 0;
+            result = 0;
+        } else {
+            int counter = 0;
+            for (String i : str.trim().split(" ")) {
+                counter++;
+            }
+            result = counter;
         }
-        int counter = 0;
-        for (String i : str.trim().split(" ")) {
-            counter++;
-        }
-        return counter;
+        return result;
     }
 
     public String getInitials(String str) {
+        String result;
         if (str == null || str.isEmpty()) {
-            return "";
+            result = "";
+        } else {
+            String[] firstSecondName = str.toUpperCase().trim().split(" ");
+            String initials = "";
+            for (String i : firstSecondName) {
+                initials += i.charAt(0);
+            }
+            result = initials;
         }
-        String[] firstSecondName = str.toUpperCase().trim().split(" ");
-        String initials = "";
-        for (String i : firstSecondName) {
-            initials += i.charAt(0);
-        }
-        return initials;
+        return result;
     }
 
     public String getAllNumbers(String str) {
+        String result;
         if (str == null || str.isEmpty()) {
-            return "";
-        }
-        char[] number = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
-        char[] arrayFromString = str.toCharArray();
-        StringBuffer stringBuffer = new StringBuffer();
-        for (char i : arrayFromString) {
-            for (char j : number) {
-                if (i == j) {
-                    stringBuffer.append(i);
+            result = "";
+        } else {
+            char[] number = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+            char[] arrayFromString = str.toCharArray();
+            StringBuffer stringBuffer = new StringBuffer();
+            for (char i : arrayFromString) {
+                for (char j : number) {
+                    if (i == j) {
+                        stringBuffer.append(i);
+                    }
                 }
             }
+            result = stringBuffer.toString();
         }
-        return stringBuffer.toString();
+        return result;
     }
 }
