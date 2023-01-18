cdb13e2b60340f217689540bad75f82ed6b27fb2
akavity <basilakavity@gmail.com>
2023-01-18 10:42:46 +0300

    feat: add solution for Task01 'oop_classes_and_objects'

--- src/main/java/com/rakovets/course/java/core/practice/strings/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/strings/Task01.java
@@ -33,9 +33,8 @@ class Task01 {
      * @return имя сотрудника
      */
     static String getName(String fullName) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String[] firstSecondName = fullName.split(" ");;
+        return firstSecondName[0];
     }
 
     /**
@@ -45,9 +44,8 @@ class Task01 {
      * @return фамилия сотрудника
      */
     static String getSurname(String fullName) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String[] firstSecondName = fullName.split(" ");;
+        return firstSecondName[1];
     }
 
     /**
@@ -57,8 +55,7 @@ class Task01 {
      * @return полное имя сотрудника в обратном формате, т.е. '${surname} ${name}'
      */
     static String reverseFullName(String fullName) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return null;
+        String[] firstSecondName = fullName.split(" ");;
+        return firstSecondName[1] + " " + firstSecondName[0];
     }
 }
