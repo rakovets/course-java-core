974967aa43977bfeec902edec782099875a1f232
akavity <basilakavity@gmail.com>
2023-01-20 13:22:57 +0300

    add blank line

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Hero.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Hero.java
@@ -27,6 +27,7 @@ public abstract class Hero implements Mortal {
     public int getHealth() {
         return health;
     }
+
     public void setHealth(int health) {
         this.health = health;
     }
