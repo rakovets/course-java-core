c6ff20743a3dda619f14b494c854986bcd6f91b9
akavity <basilakavity@gmail.com>
2023-01-20 13:16:13 +0300

    feat: add Zombie.java, Naga.java for Task Battle Ground 'oop principles'

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Naga.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Naga.java
@@ -1,13 +1,20 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
 
-public class Naga extends Enemy{
+public class Naga extends Enemy {
     public Naga(int health) {
         super(health);
     }
 
     @Override
     public void attackHero(Hero hero) {
-        int damage = 4;
+        int damage = 10;
         hero.takeDamage(damage);
+    }
+
+    public void drinkBlood(Hero hero) {
+        int damage = 20;
+        int regeneration = 10;
+        hero.takeDamage(damage);
+        setHealth(getHealth() + regeneration);
     }
 }
--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Zombie.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Zombie.java
@@ -1,20 +1,21 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle_ground;
 
-public class Zombie extends Enemy{
+public class Zombie extends Enemy {
     public Zombie(int health) {
         super(health);
     }
 
     @Override
     public void attackHero(Hero hero) {
-        int damage = 2;
+        int damage = 4;
         hero.takeDamage(damage);
     }
 
     public void resurrection() {
         int healthAfterResurrection = 30;
+        int totalResurrection = 3;
         int deathCounter = 0;
-        while (!isAlive() && deathCounter < 2) {
+        while (!isAlive() && deathCounter < totalResurrection) {
             setHealth(healthAfterResurrection);
             deathCounter++;
         }
