0d38246d330f174e8e77ff31361572c3a6904e6b
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-20 22:04:02 +0300

    add: Enemy.java

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle/ground/Enemy.java
@@ -0,0 +1,17 @@
+package com.rakovets.course.java.core.practice.oop_principles.battle.ground;
+
+public class Enemy {
+    private int health;
+
+    public Enemy(int health) {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
