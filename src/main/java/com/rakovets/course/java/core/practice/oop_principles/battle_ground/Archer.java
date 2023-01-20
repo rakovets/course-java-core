65117b4cb1c7f32344d3bf8cc669deeea110ad18
akavity <basilakavity@gmail.com>
2023-01-20 13:14:09 +0300

    feat: change Archer for Task Battle Ground 'oop principles'

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Archer.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle_ground/Archer.java
@@ -7,8 +7,14 @@ public class Archer extends Hero {
 
     @Override
     public void attackEnemy(Enemy enemy) {
-        int damage = 3;
+        int damage = 4;
         enemy.takeDamage(damage);
         System.out.println("Where is the enemy?");
+    }
+
+    public void aimedShot(Enemy enemy) {
+        int damage = 15;
+        enemy.takeDamage(damage);
+        System.out.println("He who shoots first laughs last.");
     }
 }
