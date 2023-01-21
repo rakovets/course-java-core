6dec64276ba5585d5c3b8ab5deb80e723e3d1023
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-21 11:13:24 +0300

    add: Mortal.java and refactor Enemy.java

--- src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle/ground/Enemy.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/battle/ground/Enemy.java
@@ -1,10 +1,15 @@
 package com.rakovets.course.java.core.practice.oop_principles.battle.ground;
 
-public class Enemy {
+public class Enemy implements Mortal{
     private int health;
 
     public Enemy(int health) {
         this.health = health;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return health > 0;
     }
 
     public void takeDamage(int damage) {
