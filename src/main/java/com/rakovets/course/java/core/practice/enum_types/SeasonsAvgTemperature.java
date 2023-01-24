7bb48ce6b35f75bec9b325560daa9d79e7815695
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-24 15:01:03 +0300

    refactor: enum SeasonsAvgTemperature

--- src/main/java/com/rakovets/course/java/core/practice/enum_types/SeasonsAvgTemperature.java
+++ src/main/java/com/rakovets/course/java/core/practice/enum_types/SeasonsAvgTemperature.java
@@ -1,8 +1,17 @@
 package com.rakovets.course.java.core.practice.enum_types;
 
 public enum SeasonsAvgTemperature {
-    WINTER_AVG_TEMPERATURE,
-    SPRING_AVG_TEMPERATURE,
-    SUMMER_AVG_TEMPERATURE,
-    AUTUMN_AVG_TEMPERATURE,
+    WINTER_AVG_TEMPERATURE {
+        public final double temperature = -2.5;
+    },
+    SPRING_AVG_TEMPERATURE {
+        public final double temperature = 12.3;
+    },
+    SUMMER_AVG_TEMPERATURE {
+        public final double temperature = 20.1;
+    },
+    AUTUMN_AVG_TEMPERATURE {
+        public final double temperature = 11.1;
+    },
+
 }
