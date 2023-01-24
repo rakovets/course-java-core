24c4b7652d73d25f77c9bc503e0a48405bebfa00
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-24 15:23:50 +0300

    refactor: del enum SeasonsAvgTemperature, refactor Seasons

--- src/main/java/com/rakovets/course/java/core/practice/enum_types/Seasons.java
+++ src/main/java/com/rakovets/course/java/core/practice/enum_types/Seasons.java
@@ -1,8 +1,11 @@
 package com.rakovets.course.java.core.practice.enum_types;
 
 public enum Seasons {
-    WINTER,
-    SPRING,
-    SUMMER,
-    AUTUMN,
+    WINTER(-2.5),
+    SPRING(11.3),
+    SUMMER(20.1),
+    AUTUMN(11.1);
+
+    Seasons(double averageTemperature) {
+    }
 }
