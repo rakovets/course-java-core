4027d638aea56f0b84bef593dec65af5d10587ee
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-24 19:16:24 +0300

    refactor: some code

--- src/test/java/com/rakovets/course/java/core/practice/enum_types/SeasonPrintDemo.java
+++
@@ -1,9 +0,0 @@
-package com.rakovets.course.java.core.practice.enum_types;
-
-public class SeasonPrintDemo {
-    public static void main(String[] args) {
-        SeasonPrint seasonPrint = new SeasonPrint();
-
-        seasonPrint.printAll();
-    }
-}
---
+++ src/test/java/com/rakovets/course/java/core/practice/enum_types/SeasonPrintTest.java
@@ -0,0 +1,12 @@
+package com.rakovets.course.java.core.practice.enum_types;
+
+import org.junit.jupiter.api.Test;
+
+public class SeasonPrintTest {
+        SeasonPrint seasonPrint = new SeasonPrint();
+
+        @Test
+        void printAllTest() {
+            seasonPrint.printAll();
+        }
+}
