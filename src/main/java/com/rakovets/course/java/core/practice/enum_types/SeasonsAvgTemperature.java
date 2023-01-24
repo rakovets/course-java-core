21afecb573955cc364522a233bd21a2cf30f0d25
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-24 14:52:49 +0300

    feat: enum SeasonsAvgTemperature

---
+++ src/main/java/com/rakovets/course/java/core/practice/enum_types/SeasonsAvgTemperature.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.enum_types;
+
+public enum SeasonsAvgTemperature {
+    WINTER_AVG_TEMPERATURE,
+    SPRING_AVG_TEMPERATURE,
+    SUMMER_AVG_TEMPERATURE,
+    AUTUMN_AVG_TEMPERATURE,
+}
