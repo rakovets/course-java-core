431a1c81a32838d0dd1d1be7d3110d9dfbf48f8f
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-24 14:46:34 +0300

    feat: add Interface FavoriteSeason, class MyFavoriteSeasonInfo, enum Seasons & Test MyFavoriteSeasonInfoTest

---
+++ src/main/java/com/rakovets/course/java/core/practice/enum_types/Seasons.java
@@ -0,0 +1,8 @@
+package com.rakovets.course.java.core.practice.enum_types;
+
+public enum Seasons {
+    WINTER,
+    SPRING,
+    SUMMER,
+    AUTUMN,
+}
---
+++ src/test/java/com/rakovets/course/java/core/practice/enum_types/MyFavoriteSeasonInfoTest.java
@@ -0,0 +1,38 @@
+package com.rakovets.course.java.core.practice.enum_types;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MyFavoriteSeasonInfoTest {
+    static private MyFavoriteSeasonInfo myFavoriteSeasonInfo;
+    private String result;
+    private String expected;
+
+    @BeforeAll
+    static void initAll() {
+        myFavoriteSeasonInfo = new MyFavoriteSeasonInfo(Seasons.SPRING);
+    }
+
+    @DisplayName("Test favoriteSeasonInfo(), returns String with info about my favorite season, Spring")
+    @Test
+    void printFavoriteSeasonInfoTest() {
+        result = myFavoriteSeasonInfo.favoriteSeasonInfo();
+        expected = "Spring is a transition season between the winter and summer seasons. The days get \n" +
+                "longer and nights get shorter, the temperatures are milder, and flowers bloom. There is a warm \n" +
+                "wind in the spring season in the air. Spring typically begins in March and usually lasts until \n" +
+                "May or June.";
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @DisplayName("Test printFavoriteSeason(), returns String 'I like Spring!'")
+    @Test
+    void printFavoriteSeasonTest() {
+        result = myFavoriteSeasonInfo.printFavoriteSeason(myFavoriteSeasonInfo.getFAVORITE_SEASON());
+        expected = "I like Spring!";
+
+        Assertions.assertEquals(expected, result);
+    }
+}
