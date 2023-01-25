c6e04f74efdfc7a80da3bef45bff3fec1cebf72d
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-26 01:34:18 +0300

    refactor: Square

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop_principles/geometry/Figure.java
@@ -0,0 +1,6 @@
+package com.rakovets.course.java.core.practice.oop_principles.geometry;
+
+public abstract class Figure implements MainUtils{
+    @Override
+    public abstract String toString();
+}
