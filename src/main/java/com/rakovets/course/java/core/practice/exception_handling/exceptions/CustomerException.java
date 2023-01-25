f52ca0c8a37c6c8ce5647d01e26c1b143069c99c
akavity <basilakavity@gmail.com>
2023-01-25 16:03:02 +0300

    some little change

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/exceptions/CustomerException.java
+++
@@ -1,7 +0,0 @@
-package com.rakovets.course.java.core.practice.exception_handling.exceptions;
-
-public class CustomerException extends RuntimeException {
-    public CustomerException(String massage) {
-        super(massage);
-    }
-}
