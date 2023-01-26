e78948b0df057743093ceaa9c099c7fe8d205634
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-26 23:10:30 +0300

    feat: Obscure

---
+++ src/main/java/com/rakovets/course/java/core/practice/generics/obscure/Obscure.java
@@ -0,0 +1,41 @@
+package com.rakovets.course.java.core.practice.generics.obscure;
+
+public class Obscure<T> {
+    private final T object;
+
+    public Obscure(T object) {
+        this.object = object;
+    }
+
+    public T get() {
+        return object;
+    }
+
+    public boolean isPresent() {
+        return object != null;
+    }
+
+    public boolean isEmpty() {
+        return object.toString().length() == 0;
+    }
+
+    public T orElse(T object) {
+        return isPresent() ? this.object : object;
+    }
+
+    public T orElseThrow(Exception exception) throws Exception {
+        T result;
+
+        if (isPresent()) {
+            result = object;
+        } else {
+            throw exception;
+        }
+
+        return result;
+    }
+
+    public static <T> Obscure of(T object) {
+        return null;
+    }
+}
