1677aed587426a7ea02acfef5ced6b6d663d9505
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-26 22:12:20 +0300

    refactor: TwoExceptions & TwoExceptionsDemo

--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/task7/TwoExceptionsDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/task7/TwoExceptionsDemo.java
@@ -1,4 +1,8 @@
 package com.rakovets.course.java.core.practice.exception_handling.task7;
 
 public class TwoExceptionsDemo {
+    public static void main(String[] args) {
+            TwoExceptions exception1 = new TwoExceptions(1);
+            exception1.trowException();
+    }
 }
