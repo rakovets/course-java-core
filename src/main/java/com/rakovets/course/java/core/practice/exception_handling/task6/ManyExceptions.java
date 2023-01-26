5f42dcab1050ada2b0f28c01b0a884bae1b55869
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-26 12:23:47 +0300

    feat: ManyExceptions

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/task6/ManyExceptions.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/task6/ManyExceptions.java
@@ -1,4 +1,31 @@
 package com.rakovets.course.java.core.practice.exception_handling.task6;
 
 public class ManyExceptions {
+    int[] outOfBounds = new int[1];
+    String outOfMemory = "Hello world!";
+    String nullPointer = null;
+
+    public void trowException(int i) {
+        if (i == 0) {
+            outOfBounds[1] = 1;
+        } else if (i == 1) {
+                nullPointer = nullPointer.concat(null);
+        } else {
+            while (true) {
+                outOfMemory = outOfMemory.concat(outOfMemory);
+            }
+        }
+    }
+
+    public void catchException() throws Exception {
+        try {
+            for (int i = 0; i < 3; i++) {
+                trowException(i);
+            }
+        } catch (IndexOutOfBoundsException | NullPointerException exception) {
+            System.out.println(exception.getMessage());
+        } catch (OutOfMemoryError error) {
+            throw new OutOfMemoryError(error.getMessage());
+        }
+    }
 }
---
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/task6/ManyExceptionsDemo.java
@@ -0,0 +1,9 @@
+package com.rakovets.course.java.core.practice.exception_handling.task6;
+
+public class ManyExceptionsDemo {
+    public static void main(String[] args) throws Exception {
+        ManyExceptions manyExceptions = new ManyExceptions();
+
+        manyExceptions.catchException();
+    }
+}
