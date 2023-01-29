4e4e153d7d6b219838b06ff028f9039b3211de3e
akavity <basilakavity@gmail.com>
2023-01-29 23:06:24 +0300

    add solution

--- src/main/java/com/rakovets/course/java/core/practice/generics/Math.java
+++ src/main/java/com/rakovets/course/java/core/practice/generics/Math.java
@@ -1,4 +1,24 @@
 package com.rakovets.course.java.core.practice.generics;
 
-public abstract class Math {
+import java.util.ArrayList;
+
+public abstract class Math<T extends Number> {
+    private T[] numbers;
+
+    public Math(T[] numbers) {
+        this.numbers = numbers;
+    }
+
+    public static <T extends Number> T getMaxFromThreeNumbers(T numberOne, T numberTwo, T numberThree) {
+        Number[] array = new Number[] {numberOne, numberTwo, numberThree};
+        Number maxNumber = numberOne;
+        for (Number i : array) {
+            //if (maxNumber < i) {
+                maxNumber = i;
+            }
+        return null;
+        }
+    }
+
+
 }
