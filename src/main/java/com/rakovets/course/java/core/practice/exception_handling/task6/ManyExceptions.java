9c7421eb5b3cc0d47827e9c4fc31375bc961ce68
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-31 00:10:19 +0300

    refactor: ManyExceptionsDemo & ManyExceptions

--- src/main/java/com/rakovets/course/java/core/practice/exception_handling/task6/ManyExceptions.java
+++ src/main/java/com/rakovets/course/java/core/practice/exception_handling/task6/ManyExceptions.java
@@ -8,21 +8,15 @@ public class ManyExceptions {
     }
 
     public void trowException() {
-        try {
-            switch (this.exceptionNumber) {
-                case 1:
-                    throw new NullPointerException();
-                case 2:
-                    throw new RuntimeException();
-                case 3:
-                    throw new IndexOutOfBoundsException();
-                default:
-                    System.out.println("To throw exception, please create class with 1 to 3 constructor parameter");
+        switch (this.exceptionNumber) {
+            case 1:
+                throw new NullPointerException();
+            case 2:
+                throw new RuntimeException();
+            case 3:
+                throw new IndexOutOfBoundsException();
+            default:
+                System.out.println("To throw exception, please create class with 1 to 3 constructor parameter");
             }
-        } catch (NullPointerException | IndexOutOfBoundsException exception) {
-            System.out.println("Hello! You got an Error in your code!");
-        } catch (RuntimeException exception) {
-            System.out.println(exception.getMessage());
-        }
     }
 }
--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/task6/ManyExceptionsDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/task6/ManyExceptionsDemo.java
@@ -2,9 +2,15 @@ package com.rakovets.course.java.core.practice.exception_handling.task6;
 
 public class ManyExceptionsDemo {
     public static void main(String[] args) {
-        for (int i = 0; i <= 4; i++) {
-            ManyExceptions exception = new ManyExceptions(i);
-            exception.trowException();
+        try {
+            for (int i = 0; i <= 4; i++) {
+                ManyExceptions exception = new ManyExceptions(i);
+                exception.trowException();
+            }
+        } catch (NullPointerException | IndexOutOfBoundsException exception) {
+            System.out.println("Hello! You got an Error in your code!");
+        } catch (RuntimeException exception) {
+            System.out.println(exception.getMessage());
         }
     }
 }
