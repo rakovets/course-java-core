819330e8c4c5003dc9519dfa929eb498d0a069f0
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-30 22:23:43 +0300

    refactor: Dmitry Rakovets Updates

--- src/main/java/com/rakovets/course/java/core/example/jcf/PartyDemo.java
+++ src/main/java/com/rakovets/course/java/core/example/jcf/PartyDemo.java
@@ -2,7 +2,13 @@ package com.rakovets.course.java.core.example.jcf;
 
 import com.rakovets.course.java.core.example.jcf.model.Student;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class PartyDemo {
     public static void main(String[] args) {
