f824b3bb6b8d8b45d96fcebe66540ea5751f0909
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-31 01:08:26 +0300

    refactor: TwoExceptionsTest

--- src/test/java/com/rakovets/course/java/core/practice/exception_handling/task7/TwoExceptionsTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/task7/TwoExceptionsTest.java
@@ -1,14 +1,28 @@
 package com.rakovets.course.java.core.practice.exception_handling.task7;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 public class TwoExceptionsTest {
-    @DisplayName("Test")
+    @DisplayName("Test trowException, throws NullPointerException")
     @Test
     void trowException1Test() {
         TwoExceptions exceptions = new TwoExceptions(1);
 
+        Executable executable = exceptions::trowException;
 
+        Assertions.assertThrows(NullPointerException.class, executable);
+    }
+
+    @DisplayName("Test trowException, throws RuntimeException")
+    @Test
+    void trowException2Test() {
+        TwoExceptions exceptions = new TwoExceptions(2);
+
+        Executable executable = exceptions::trowException;
+
+        Assertions.assertThrows(RuntimeException.class, executable);
     }
 }
