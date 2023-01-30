9ee4d21ae43c6faf11bf2cce320de91fb64cc4f3
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-30 22:22:43 +0300

    feat: PersonAgeValidationDemo refactor: PersonAgeValidation & PersonAgeValidationTest

--- src/main/java/com/rakovets/course/java/core/example/concurrent_utilities/Example9ExecutorServiceInvokeAll.java
+++ src/main/java/com/rakovets/course/java/core/example/concurrent_utilities/Example9ExecutorServiceInvokeAll.java
@@ -5,7 +5,11 @@ import com.rakovets.course.java.core.util.AnsiColorCode;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static com.rakovets.course.java.core.util.StandardOutputUtil.printlnWithTimeAndThread;
 
