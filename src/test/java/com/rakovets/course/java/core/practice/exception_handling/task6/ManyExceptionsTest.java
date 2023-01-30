192300fb705b25d1d861b6881c271b343294d038
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-31 01:02:22 +0300

    refactor: ManyExceptionsTest

---
+++ src/test/java/com/rakovets/course/java/core/practice/exception_handling/task6/ManyExceptionsTest.java
@@ -0,0 +1,33 @@
+package com.rakovets.course.java.core.practice.exception_handling.task6;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
+
+public class ManyExceptionsTest {
+    @DisplayName("Test trowExceptionTest, throws NullPointerException")
+    @Test
+    void trowExceptionTest1() {
+        ManyExceptions manyExceptions = new ManyExceptions(1);
+        Executable executable = manyExceptions::trowException;
+
+        Assertions.assertThrows(NullPointerException.class, executable);
+    }
+
+    @DisplayName("Test trowExceptionTest, throws RuntimeException")
+    @Test
+    void trowExceptionTest2() {
+        ManyExceptions manyExceptions = new ManyExceptions(2);
+        Executable executable = manyExceptions::trowException;
+
+        Assertions.assertThrows(RuntimeException.class, executable);
+    }
+
+    @DisplayName("Test trowExceptionTest, throws IndexOutOfBoundsException")
+    @Test
+    void trowExceptionTest3() {
+        ManyExceptions manyExceptions = new ManyExceptions(3);
+        Executable executable = manyExceptions::trowException;
+
+        Assertions.assertThrows(IndexOutOfBoundsException.class, executable);
+    }
+}
