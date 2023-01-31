e30a9f5ed1b696e57b6ca9b0db307ee2c429b139
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-31 20:47:12 +0300

    refactor: Math & MathDemo

--- src/test/java/com/rakovets/course/java/core/practice/generics/math/MathDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/generics/math/MathDemo.java
@@ -8,13 +8,13 @@ public class MathDemo {
 
         System.out.printf(Math.minOfFive(4.0, 2.0, 3.0, 4.0, 5.0) + "\n\n");
 
-        Number[] arrayAvg = {2, 2, 2, 2.2};
+        Number[] arrayAvg = {2, 2, 2, 3};
         System.out.printf(Math.avgInArray(arrayAvg) + "\n\n");
 
         Number[] arrayMax = {1, 2, 3, 4};
         System.out.printf(Math.maxInArray(arrayMax) + "\n\n");
 
-        Number[] arrayMin = {1.1, 2, 3.1, 4.1};
+        Number[] arrayMin = {1.1, 2.0, 3.1, 4.1};
         System.out.printf(Math.minInArray(arrayMin) + "\n\n");
 
         Number[] arraySort = {4.1, 3.2, 2, 1.1};
