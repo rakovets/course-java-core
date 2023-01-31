11c5cf2ea058d8abfc25c876f65ac08d0ca89565
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-31 04:18:03 +0300

    refactor: Math & MathDemo

--- src/main/java/com/rakovets/course/java/core/practice/generics/math/Math.java
+++ src/main/java/com/rakovets/course/java/core/practice/generics/math/Math.java
@@ -9,7 +9,7 @@ public abstract class Math {
         Math.arrayMath = arrayMath;
     }
 
-    public static  <T extends Number> Number min(T e1, T e2, T e3, T e4, T e5) {
+    public static  <T extends Number> Number minOfFive(T e1, T e2, T e3, T e4, T e5) {
         arrayMath = new Number[5];
         arrayMath[0] = e1;
         arrayMath[1] = e2;
@@ -22,7 +22,7 @@ public abstract class Math {
         return arrayMath[0];
     }
 
-    public static  <T extends Number> Number max(T e1, T e2, T e3) {
+    public static  <T extends Number> Number maxOfThree(T e1, T e2, T e3) {
         arrayMath = new Number[3];
         arrayMath[0] = e1;
         arrayMath[1] = e2;
@@ -35,6 +35,18 @@ public abstract class Math {
 
 
     public static <T extends Number> Number avgInArray(T[] array) {
+        Number result = 0;
+        arrayMath = array;
+
+        for (Number number : arrayMath) {
+            result = result.doubleValue() + number.doubleValue();
+        }
+        result = result.doubleValue() / arrayMath.length;
+
+        return result;
+    }
+
+    public static <T extends Number> Number maxInArray(T[] array) {
         Number result = 0;
         arrayMath = array;
 
--- src/test/java/com/rakovets/course/java/core/practice/generics/math/MathDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/generics/math/MathDemo.java
@@ -2,12 +2,17 @@ package com.rakovets.course.java.core.practice.generics.math;
 
 public class MathDemo {
     public static void main(String[] args) {
-        System.out.printf(Math.max(1, 3, 4) + "\n\n");
+        System.out.printf(Math.maxOfThree(1, 3, 4) + "\n\n");
 
-        System.out.printf(Math.min(4.0, 2.1, 3.0, 4.0, 5.0) + "\n\n");
+        System.out.printf(Math.minOfFive(4.0, 2.1, 3.0, 4.0, 5.0) + "\n\n");
 
         Number[] arrayAvg = {2, 2, 2, 2.2};
         System.out.println(Math.avgInArray(arrayAvg));
 
+        Number[] arrayMax = {1, 2, 3, 4};
+        System.out.println();
+
+        Number[] arrayMin = {1, 2, 3, 4};
+        System.out.println();
     }
 }
