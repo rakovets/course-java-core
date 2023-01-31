a338ad5f2382a2c27a5c098bdc547275bf8a035b
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-01-31 03:57:09 +0300

    refactor: Math & MathDemo

--- src/main/java/com/rakovets/course/java/core/practice/generics/math/Math.java
+++ src/main/java/com/rakovets/course/java/core/practice/generics/math/Math.java
@@ -1,51 +1,28 @@
 package com.rakovets.course.java.core.practice.generics.math;
 
-import com.rakovets.course.java.core.example.generics.model.restrict.D;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public abstract class Math {
-    private static Double[] arrayMath;
+    private static Number[] arrayMath;
 
-    public Math(Double[] arrayMath) {
+    public Math(Number[] arrayMath) {
         Math.arrayMath = arrayMath;
     }
 
-    public static <T extends Number> T max(T e1, T e2, T e3) {
-        T result;
-
-        result = e1.doubleValue() > e2.doubleValue() ? (e1.doubleValue() > e3.doubleValue() ?  e1 : e3) :
-                (e2.doubleValue() > e3.doubleValue() ? e2 : e3);
-
-        return result;
-    }
-
-    public static  <T extends Double> Double minOfFive(T e1, T e2, T e3, T e4, T e5) {
+    public static  <T extends Double> Number min(T e1, T e2, T e3, T e4, T e5) {
         arrayMath = new Double[5];
         arrayMath[0] = e1;
         arrayMath[1] = e2;
         arrayMath[2] = e3;
-        arrayMath[3] = e3;
-        arrayMath[4] = e3;
+        arrayMath[3] = e4;
+        arrayMath[4] = e5;
 
         Arrays.sort(arrayMath);
 
         return arrayMath[0];
     }
 
-    public static <T extends Number> T min(T e1, T e2, T e3, T e4, T e5) {
-        T result;
-
-        result = e1.doubleValue() < e2.doubleValue() ? e1 : e2;
-        result = result.doubleValue() < e3.doubleValue() ? result : e3;
-        result = result.doubleValue() < e4.doubleValue() ? result : e4;
-        result = result.doubleValue() < e5.doubleValue() ? result : e5;
-
-        return result;
-    }
-
-    public static  <T extends Double> Double maxOfThree(T e1, T e2, T e3) {
+    public static  <T extends Double> Number max(T e1, T e2, T e3) {
         arrayMath = new Double[3];
         arrayMath[0] = e1;
         arrayMath[1] = e2;
@@ -56,25 +33,14 @@ public abstract class Math {
         return arrayMath[arrayMath.length - 1];
     }
 
-    public static <T extends Double> T avgInArray() {
-        Double avg = 0.0;
 
-        for (Number number : arrayMath) {
-            avg += number.doubleValue();
-        }
-        avg = avg / arrayMath.length;
-
-        return (T) avg;
-    }
-
-    public static <T extends Double> Double avgInArrayS(T[] array) {
+    public static <T extends Double> Double avgInArray(T[] array) {
         Double avg = 0.0;
         arrayMath = array;
 
-        for (Double number : arrayMath) {
-            avg += number;
+        for (Number number : arrayMath) {
+            avg += (Double) number;
         }
-
         avg = avg / arrayMath.length;
 
         return avg;
--- src/test/java/com/rakovets/course/java/core/practice/generics/math/MathDemo.java
+++ src/test/java/com/rakovets/course/java/core/practice/generics/math/MathDemo.java
@@ -2,14 +2,12 @@ package com.rakovets.course.java.core.practice.generics.math;
 
 public class MathDemo {
     public static void main(String[] args) {
-        System.out.printf(Math.max(1.2, 3, 4.7) + "\n");
-        System.out.printf(Math.maxOfThree(1.2, 3.2, 4.7) + "\n\n");
+        System.out.printf(Math.max(1.2, 3.2, 4.7) + "\n\n");
 
-        System.out.printf(Math.min(4, 2, 3, 4, 5) + "\n");
-        System.out.printf(Math.minOfFive(4.0, 2.0, 3.0, 4.0, 5.0) + "\n\n");
+        System.out.printf(Math.min(4.0, 2.1, 3.0, 4.0, 5.0) + "\n\n");
 
         Double[] array = {2.0, 2.0, 2.0, 2.0};
-        System.out.println(Math.avgInArrayS(array));
+        System.out.println(Math.avgInArray(array));
 
     }
 }
