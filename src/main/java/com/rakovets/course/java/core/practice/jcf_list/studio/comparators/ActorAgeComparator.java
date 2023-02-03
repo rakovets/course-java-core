145741b5baee9b3bc402a275e48e1be33e2f4286
akavity <basilakavity@gmail.com>
2023-02-03 23:48:15 +0300

    feat: add solution for task 3 'JCF: List'

---
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_list/studio/comparators/ActorAgeComparator.java
@@ -0,0 +1,18 @@
+package com.rakovets.course.java.core.practice.jcf_list.studio.comparators;
+
+import com.rakovets.course.java.core.practice.jcf_list.studio.Actor;
+
+import java.util.Comparator;
+
+public class ActorAgeComparator implements Comparator<Actor> {
+    @Override
+    public int compare(Actor a, Actor b) {
+        int result = 0;
+        if (a.getAge() > b.getAge()) {
+            result = 1;
+        } else if (a.getAge() < b.getAge()) {
+            result = -1;
+        }
+        return result;
+    }
+}
