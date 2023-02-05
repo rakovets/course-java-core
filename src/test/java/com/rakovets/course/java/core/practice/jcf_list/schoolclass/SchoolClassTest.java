3ba3b4482deb7dae85785bf6df72debb163c89da
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-02-05 21:30:41 +0300

    feat: SchoolClassTest

---
+++ src/test/java/com/rakovets/course/java/core/practice/jcf_list/schoolclass/SchoolClassTest.java
@@ -0,0 +1,34 @@
+package com.rakovets.course.java.core.practice.jcf_list.schoolclass;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SchoolClassTest {
+    private final Person dzmitry = new Person("Dzmitry", "Krasiuk", 17, 8.3);
+    private final Person alexander = new Person("Alexander", "Braim", 16, 5.1);
+    private final Person elena = new Person("Elena", "Pankova", 16, 7.6);
+    private final Person julia = new Person("Julia", "Serbskaya",17, 4.9);
+    private final Person anna = new Person("Anna", "Krasiuk",16, 9.7);
+    private final Person iya = new Person("Iya", "Krasiuk",17, 8.7);
+    private ArrayList<Person> studentList;
+    private SchoolClass class11B;
+
+    @BeforeEach
+    void init() {
+        studentList = new ArrayList<>(Arrays.asList(dzmitry, alexander, elena, julia, anna, iya));
+        class11B = new SchoolClass(studentList);
+    }
+
+    @DisplayName("Test")
+    @Test
+    void getBestStudentTest() {
+        Person result = class11B.getBestStudent(studentList);
+
+        Assertions.assertEquals(anna, result);
+    }
+}
