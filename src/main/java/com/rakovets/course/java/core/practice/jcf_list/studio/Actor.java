f1d0df92f70590e51275a93ea248d7caef320a3a
akavity <basilakavity@gmail.com>
2023-02-05 17:48:28 +0300

    change solution for task 01 'List'

--- src/main/java/com/rakovets/course/java/core/practice/jcf_list/studio/Actor.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_list/studio/Actor.java
@@ -1,54 +1,43 @@
 package com.rakovets.course.java.core.practice.jcf_list.studio;
 
-public class Actor implements Comparable<Actor>{
-    private String firstName;
-    private String lastName;
-    private Integer age;
-    private Integer fee;
+public class Actor implements Comparable<Actor> {
+    private final String firstName;
+    private final String lastName;
+    private  final Integer age;
+    private final Integer fee;
 
-    public Actor(String firstName, String lastName, int age, int fee) {
+    public Actor(String firstName, String lastName, Integer age, Integer fee) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.fee = fee;
     }
 
-    @Override
-    public int compareTo(Actor actor) {
-        return fee.compareTo(actor.getFee());
+    public Actor() {
+        this.firstName = null;
+        this.lastName = null;
+        this.age = 0;
+        this.fee = 0;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
     public int getFee() {
         return fee;
     }
 
-    public void setFee(int fee) {
-        this.fee = fee;
+    @Override
+    public int compareTo(Actor actor) {
+        return fee.compareTo(actor.getFee());
     }
-
-
 }
