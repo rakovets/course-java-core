33240b9f29c524630cc9d7bf8e1216c8520d1abb
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-02-06 19:55:34 +0300

    refactor: Fine, Fines

--- src/main/java/com/rakovets/course/java/core/practice/jcf_set/tax_service/Fine.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_set/tax_service/Fine.java
@@ -1,7 +1,25 @@
 package com.rakovets.course.java.core.practice.jcf_set.tax_service;
 
 public class Fine {
-    private String name;
-    private String sum;
-    private String date;
+    private final String fineName;
+    private final String sum;
+    private final String date;
+
+    public Fine(String fineName, String sum, String date) {
+        this.fineName = fineName;
+        this.sum = sum;
+        this.date = date;
+    }
+
+    public String getFineName() {
+        return fineName;
+    }
+
+    public String getSum() {
+        return sum;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
--- src/main/java/com/rakovets/course/java/core/practice/jcf_set/tax_service/Fines.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_set/tax_service/Fines.java
@@ -3,5 +3,9 @@ package com.rakovets.course.java.core.practice.jcf_set.tax_service;
 import java.util.HashSet;
 
 public class Fines {
-    private HashSet<String> fines;
+    private HashSet<Fine> fines;
+
+    public void addFine(Fine fine) {
+        fines.add(fine);
+    }
 }
