ef9908016a1d99b8c61be021b04c1220c456d5a9
Dzmitriy Krasiuk <strixmymail@gmail.com>
2023-02-06 23:04:44 +0300

    refactor: Fine

--- src/main/java/com/rakovets/course/java/core/practice/jcf_set/tax_service/Fine.java
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_set/tax_service/Fine.java
@@ -1,16 +1,16 @@
 package com.rakovets.course.java.core.practice.jcf_set.tax_service;
 
 public class Fine {
-    private final String fineName;
+    private final String fineDetails;
     private final String sum;
 
     public Fine(String fineName, String sum) {
-        this.fineName = fineName;
+        this.fineDetails = fineName;
         this.sum = sum;
     }
 
     public String getFineName() {
-        return fineName;
+        return fineDetails;
     }
 
     public String getSum() {
