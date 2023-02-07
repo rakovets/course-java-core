94d184e453d2e49a86f259d7f6e3562469d87cac
Marina Danilenko <m.a_kurlovich@mail.ru>
2023-02-08 01:04:37 +0300

    feat: add solution to task JCF: List

---
+++ src/test/java/com/rakovets/course/java/core/practice/jcf_list/Actor.java
@@ -0,0 +1,31 @@
+package com.rakovets.course.java.core.practice.jcf_list;
+
+public class Actor {
+    private String firstName;
+    private String lastName;
+    private double fee;
+    private int age;
+
+    public Actor(String firstName, String lastName, double fee, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fee = fee;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
