357902e6b2fb13fa256ddf7410768d8ecc9edd52
akavity <basilakavity@gmail.com>
2023-02-09 20:21:53 +0300

    feat: add Taxpayer for Task Set.

---
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_set/Taxpayer.java
@@ -0,0 +1,41 @@
+package com.rakovets.course.java.core.practice.jcf_set;
+
+import java.util.HashSet;
+
+public class Taxpayer {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private Cities city;
+    private HashSet<Fine> fine;
+
+    public Taxpayer(String firstName, String lastName, Cities city, String id) {
+        this.id =id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+    }
+
+    public Taxpayer() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public HashSet<Fine> getFine() {
+        return fine;
+    }
+
+    public void setFine(HashSet<Fine> fine) {
+        this.fine = fine;
+    }
+}
