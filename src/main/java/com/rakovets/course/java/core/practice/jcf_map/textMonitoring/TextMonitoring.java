43fff88cbabbc203dcb1bde27483f8b4bb1845b9
akavity <basilakavity@gmail.com>
2023-02-11 17:55:32 +0300

    feat: add solution for 'TextMonitoring'

---
+++ src/main/java/com/rakovets/course/java/core/practice/jcf_map/textMonitoring/TextMonitoring.java
@@ -0,0 +1,38 @@
+package com.rakovets.course.java.core.practice.jcf_map.textMonitoring;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class TextMonitoring {
+    public Map<String, Integer> researchText(String text) {
+        Map<String, Integer> mapWords = new LinkedHashMap<>();
+        String[] words = text.trim().split("(\\W+)");
+        for (String word : words) {
+            if (mapWords.containsKey(word)) {
+                mapWords.put(word, mapWords.get(word) + 1);
+            } else {
+                mapWords.put(word, 1);
+            }
+        }
+        return mapWords;
+    }
+
+    public Integer getCountUniqueWords(String text) {
+        return researchText(text).size();
+    }
+
+    public Set<String> getUniqueWords(String text) {
+        return researchText(text).keySet();
+    }
+
+    public Integer getFrequencyWord(String word, String text) {
+        return researchText(text).containsKey(text) ? researchText(text).get(word) : 0;
+    }
+
+    public Map<String, Integer> getFrequencyWords(String text, boolean isAscendingFrequency) {
+        TreeMap<String, Integer> map = new TreeMap<>(researchText(text));
+        return isAscendingFrequency ? map : map.descendingMap();
+    }
+}
